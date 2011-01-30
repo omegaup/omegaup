b772c671615c4dc1b98831df3e5206c1418570a6
Luis Hector Chavez <lhchavez@lhchavez.com>
2011-01-30 17:02:24 -0600

    UVa

---
+++ grader/src/main/scala/omegaup/grader/drivers/UVa.scala
@@ -0,0 +1,201 @@
+package omegaup.grader.drivers
+
+import omegaup._
+import omegaup.grader._
+import java.io._
+import java.util.concurrent._
+import scala.util.matching.Regex
+import scala.actors.Actor
+import scala.actors.Actor._
+import Lenguaje._
+import Veredicto._
+import Estado._
+
+object UVa extends Actor with Log {
+	val home_url   = "http://uva.onlinejudge.org/index.php"
+	val login_url  = "http://uva.onlinejudge.org/index.php?option=com_comprofiler&task=login"
+	val submit_url = "http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=25&page=save_submission"
+	val status_url = "http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=9&limit=50&limitstart=0"
+	
+	val LoginFormRegex = "(?si).*<form action=\"http://uva.onlinejudge.org/index\\.php\\?option=com_comprofiler&amp;task=login\"(.*?)</form>.*".r
+	val InputFormRegex = "name=\"([^\">]*)\"[^>]*value=\"([^\">]*)\"".r
+	val ReceiveRegex = "(?si).*Submission.received.with.ID.([0-9]+)".r
+	
+	val TableRegex = "(?si).*?<!-- #col3: Main Content -->.*?<table(.*?)</table>.*".r
+	val RowRegex = "(?si)<tr.*?</tr>".r
+	val CellRegex = "(?si)<td.*?>(.*?)</td>".r
+	
+	private val locks  = for (i <- 1 until 50) yield new Semaphore(1)
+	private var lock_i = 0
+	private val rids   = Array.ofDim[Int](2,50)
+	
+	private val cookies = new scala.collection.mutable.HashMap[String,String]
+	private var logged_in = false
+	private val status_mapping = Map(
+		"Sent to judge" ->			Estado.Espera,
+		"Received" ->				Estado.Espera,
+		"In judge queue" ->			Estado.Espera,
+		"Queued" ->					Estado.Espera,
+		"Compiling" ->				Estado.Compilando,
+		"Linking" ->				Estado.Compilando,
+		"Running" ->				Estado.Ejecutando
+	)
+	private val veredict_mapping = Map(
+		"Compilation error"->		Veredicto.CompileError,
+		"Runtime error"->			Veredicto.RuntimeError,
+		"Wrong answer"->			Veredicto.WrongAnswer,
+		"Time limit exceeded"->		Veredicto.TimeLimitExceeded,
+		"Memory limit exceeded"->	Veredicto.MemoryLimitExceeded,
+		"Output limit exceeded"->	Veredicto.OutputLimitExceeded,
+		"Restricted function"->		Veredicto.RestrictedFunctionError,
+		"Presentation error"->		Veredicto.PresentationError,
+		"Accepted"->				Veredicto.Accepted
+	)
+	
+	def act() = {
+		while(true) {
+			receive {
+				case Login => {
+					try {
+						val LoginFormRegex(form: String) = Http.send_wait(home_url, cookies = cookies)
+					
+						val post_data = Map(
+							"username" -> Config.get("driver.uva.user", "omegaup"),
+							"passwd"   -> Config.get("driver.uva.password", "omegaup")
+						) ++ InputFormRegex.findAllIn(form).matchData.map { (x) => x.group(1) -> x.group(2) }
+					
+						if(Http.send_wait(login_url, post_data, headers = Map("Referer"->home_url), cookies=cookies).startsWith("http://")) {
+							logged_in = true
+						}
+					} catch {
+						case e: Exception => {
+							error("UVa communication failure: {}", e.getMessage)
+						}
+					}
+					
+					if(logged_in) {
+						info("UVa logged in")
+					} else {
+						error("UVa login failure")
+					}
+				}
+				case Submission(id: Int, lang: Lenguaje, pid: Int, code: String) => {
+					locks(lock_i).acquire()
+					
+					info("UVa Submission {} for problem {}", id, pid)
+					
+					val response = if(!logged_in) {
+						error("UVa not logged in")
+						(Estado.Listo, Some(Veredicto.JudgeError), 0, 0)
+					} else {
+						val post_data = Map(
+							"problemid" ->	"",
+							"category" ->	"",
+							"localid" ->	pid,
+							lang match {
+								case Lenguaje.C => "language" -> "1"
+								case Lenguaje.Cpp => "language" -> "3"
+								case Lenguaje.Java => "language" -> "2"
+							},
+							"code" ->		code
+						)
+					
+						debug("UVa Sending data: {}", post_data)
+
+						try {
+							val ReceiveRegex(rid) = Http.send_wait(submit_url, data = post_data, cookies = cookies)
+							
+							debug("UVa received with id {}", rid)
+							
+							rids(0)(lock_i) = rid.toInt
+							rids(1)(lock_i) = id
+							
+							veredictReader ! lock_i
+						} catch {
+							case e: Exception => {
+								error("UVa Submission {} failed for problem {}", id, pid)
+								error(e.getMessage)
+								Grader.updateVeredict(id, Estado.Listo, Some(Veredicto.JudgeError), 0, 0, 0)
+							}
+						}
+					}
+					
+					lock_i = if(lock_i == 49) {
+						0
+					} else {
+						lock_i + 1
+					}
+				}
+			}
+		}
+	}
+	
+	private val veredictReader: Actor = actor {
+		while(true) {
+			self.receive {
+				case x: Int => {
+					if (rids(0)(x) != 0) readVeredict()
+				}
+			}
+		}
+	}
+	
+	private def readVeredict(triesLeft: Int = 5): Unit = {
+		if (triesLeft == 0)
+			throw new Exception("Retry limit exceeded")
+		
+		try { Thread.sleep(3000) }
+		
+		info("UVa Reading response, {} tries left", triesLeft)
+		
+		try {
+			val TableRegex(data) = Http.send_wait(status_url, cookies = cookies)
+			
+			RowRegex.findAllIn(data).
+				map { (row) => { CellRegex.findAllIn(row).matchData.map { _.group(1) } .toList } }.
+				map { (row) => (row, rids(0).findIndexOf { row(0) == _.toString } ) }.
+				filter { (x) => x._2 != -1 }.
+				foreach { case (row, id) => {
+					var estado: Estado = Estado.Listo
+					var veredicto: Option[Veredicto] = None
+					
+					if (row(3) == "") {
+						 estado = Estado.Espera
+					} else {
+						status_mapping find { (k) => row(3).contains(k._1) } match {
+							case Some((_, x: Estado)) => {
+								estado = x
+							}
+							case None => veredict_mapping find { (k) => row(3).contains(k._1) } match {	
+								case Some((_, x: Veredicto)) => {
+									veredicto = Some(x)
+								}
+								case None => {
+									error("UVa {} no contiene un veredicto válido", data(2))
+									veredicto = Some(Veredicto.JudgeError)
+								}
+							}
+						}
+					}
+					
+					Grader.updateVeredict(rids(1)(id), estado, veredicto, 1, row(5).toDouble, 0)
+					
+					if(estado == Estado.Listo) {
+						rids(0)(id) = 0
+						rids(1)(id) = 0
+						
+						locks(id).release
+					}
+				}}
+			
+			if( rids(0) exists { _ != 0 } ) {
+				readVeredict()
+			}
+		} catch {
+			case e: IOException => {
+				error("UVa communication error: {}", e.getMessage)
+				readVeredict(triesLeft-1)
+			}
+		}
+	}
+}
