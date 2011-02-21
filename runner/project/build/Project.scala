5352b9e1791ea6bea6487c3202df903c8e163b3f
Luis Hector Chavez <lhchavez@lhchavez.com>
2011-02-20 23:03:03 -0600

    Proguard plugin

--- runner/project/build/Project.scala
+++ runner/project/build/Project.scala
@@ -5,10 +5,19 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProje
 	override def mainClass = Some("omegaup.runner.Runner")
 
 	override def proguardOptions = List(
-		"-keepclasseswithmembers public class * { public static void main(java.lang.String[]); }",
+		"-dontskipnonpubliclibraryclasses",
+		"-dontskipnonpubliclibraryclassmembers",
 		"-dontobfuscate",
-		proguardKeepLimitedSerializability,
-		"-keep interface scala.ScalaObject"
+		"-dontpreverify",
+		"-dontnote",
+		"-dontwarn",
+		"-keep interface scala.ScalaObject",
+		"-keep class omegaup.*",
+		"-keep class omegaup.runner.*",
+		"-keepclassmembers class omegaup.runner.* { *; }",
+		"-keep class scala.collection.JavaConversions",
+		"-keep class org.mortbay.log.Slf4jLog",
+		proguardKeepLimitedSerializability
 	)
 
 	override def libraryDependencies = Set(
@@ -16,8 +25,8 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProje
 		"org.mortbay.jetty" % "jetty-sslengine" % "6.1.26",
 		"org.mortbay.jetty" % "jetty-client" % "6.1.26",
 		"net.liftweb" % "lift-json_2.8.1" % "2.2-RC5",
-		"org.slf4j" % "slf4j-simple" % "1.6.1" % "compile"
+		"org.slf4j" % "slf4j-jdk14" % "1.6.1" % "compile"
 	) ++ super.libraryDependencies
 	
-	val scalatest = "org.scalatest" % "scalatest" % "1.2"
+	val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
 }
