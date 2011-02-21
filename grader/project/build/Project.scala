d464b383d17ee655f6503afd6d1549ea42e22e97
Luis Hector Chavez <lhchavez@lhchavez.com>
2011-02-21 00:55:33 -0600

    Proguard ahora en Grader

--- grader/project/build/Project.scala
+++ grader/project/build/Project.scala
@@ -2,23 +2,32 @@ import sbt._
 class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProject {
 	override def proguardInJars = super.proguardInJars +++ scalaLibraryPath
 	
-	override def mainClass = Some("omegaup.grader.Grader")
+	override def mainClass = Some("omegaup.grader.Manager")
 
 	override def proguardOptions = List(
-		"-keepclasseswithmembers public class * { public static void main(java.lang.String[]); }",
+		"-dontskipnonpubliclibraryclasses",
+		"-dontskipnonpubliclibraryclassmembers",
 		"-dontobfuscate",
-		proguardKeepLimitedSerializability,
-		"-keep interface scala.ScalaObject"
+		"-dontpreverify",
+		"-dontnote",
+		"-dontwarn",
+		"-keep interface scala.ScalaObject",
+		"-keep class omegaup.*",
+		"-keep class omegaup.grader.*",
+		"-keepclassmembers class omegaup.grader.* { *; }",
+		"-keep class scala.collection.JavaConversions",
+		"-keep class org.mortbay.log.Slf4jLog",
+		proguardKeepLimitedSerializability
 	)
 
 	override def libraryDependencies = Set(
-		"mysql" % "mysql-connector-java" % "5.1.12",
 		"org.mortbay.jetty" % "jetty" % "6.1.26",
 		"org.mortbay.jetty" % "jetty-sslengine" % "6.1.26",
 		"org.mortbay.jetty" % "jetty-client" % "6.1.26",
 		"net.liftweb" % "lift-json_2.8.1" % "2.2-RC5",
-		"org.slf4j" % "slf4j-jdk14" % "1.6.1" % "compile"
+		"org.slf4j" % "slf4j-jdk14" % "1.6.1"
 	) ++ super.libraryDependencies
 	
+	val mysql = "mysql" % "mysql-connector-java" % "5.1.12" % "runtime"
 	val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
 }
