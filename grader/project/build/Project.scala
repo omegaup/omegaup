35e8cfa78a439a570027f03f8103110296ec9021
Luis Hector Chavez <lhchavez@lhchavez-nb.(none)>
2011-03-05 20:52:47 -0600

    configuración de sbt

--- grader/project/build/Project.scala
+++ grader/project/build/Project.scala
@@ -28,13 +28,13 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProje
 		"org.mortbay.jetty" % "jetty" % "6.1.26",
 		"org.mortbay.jetty" % "jetty-sslengine" % "6.1.26",
 		"org.mortbay.jetty" % "jetty-client" % "6.1.26",
-		"net.liftweb" % "lift-json_2.8.1" % "2.2-RC5",
+		"net.liftweb" % "lift-json_2.8.1" % "2.2-RC6",
 		"org.slf4j" % "slf4j-jdk14" % "1.6.1"
 	) ++ super.libraryDependencies
 	
 	val mysql = "mysql" % "mysql-connector-java" % "5.1.12" % "runtime"
 	val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
 	
-	val common = "omegaup" % "omegaup-common" % "1.0" from "file://"+(new java.io.File("../common/target/scala_2.8.1/common_2.8.1-1.0.jar").getCanonicalPath)
+	val common = "omegaup" % "omegaup-common" % "1.0" from "file://"+(new java.io.File("../common/target/scala_2.8.1/omegaup-common_2.8.1-1.0.jar").getCanonicalPath)
 	val runner = "omegaup" % "omegaup-runner" % "1.0" from "file://"+(new java.io.File("../runner/target/scala_2.8.1/omegaup-runner_2.8.1-1.0.jar").getCanonicalPath)
 }
