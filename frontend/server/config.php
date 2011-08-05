a9de8cd3ee94445d032f54e48381b0b75ade66b0
unknown <Ruben@.(none)>
2011-08-05 05:54:21 -0500

    test

--- frontend/server/config.php
+++ frontend/server/config.php
@@ -6,7 +6,7 @@
 	# ####################################
 	# DATABASE CONFIG
 	# ####################################	
-	define("OMEGAUP_DB_USER", 	"omegaup");
+	define("OMEGAUP_DB_USER", 	"root");
 	define("OMEGAUP_DB_PASS", 	"");
 	define("OMEGAUP_DB_HOST", 	"localhost");
 	define("OMEGAUP_DB_NAME", 	"omegaup");	
@@ -15,4 +15,4 @@
 	
 	
 	
-	ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . "/Applications/XAMPP/xamppfiles/htdocs/omegaupgit/omegaup/frontend/server");
+	ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . "C:/XAMPP/htdocs/omegaup/frontend/server");
