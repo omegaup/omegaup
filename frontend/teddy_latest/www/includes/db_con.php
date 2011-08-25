b28511f0d1c863f35b98131ad314d2a0d8ae1a68
Alan Gonzalez <alan.gohe@gmail.com>
2011-08-25 06:13:52 -0500

    Removing Teddy Source

--- frontend/teddy_latest/www/includes/db_con.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	$enlace = mysql_connect($TEDDY_DB_SERVER, $TEDDY_DB_USER, $TEDDY_DB_PASS) or die( "Error al conectar con la Base de Datos" );
-	mysql_select_db($TEDDY_DB_NAME) or die('No pudo seleccionarse la base de datos.');
-
