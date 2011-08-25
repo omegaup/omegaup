d4c4a64f0fc0b304a19093dcea4d1cf30b4fe23a
Alan Gonzalez <alan.gohe@gmail.com>
2011-08-25 07:37:14 -0500

    /arena/login finished

--- frontend/server/config.php
+++ frontend/server/config.php
@@ -12,8 +12,7 @@
 	define("OMEGAUP_DB_NAME", 	"omegaup2");	
 	define("OMEGAUP_DB_DRIVER", "mysqlt");
 	define("OMEGAUP_DB_DEBUG", 	false);	
-	
-	
+	define("OMEGAUP_MD5_SALT", 	"om3gaup");	
 	
 	ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . "C:/XAMPP/htdocs/omegaup/frontend/server");
 	ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . "/Applications/XAMPP/xamppfiles/htdocs/omegaup/frontend/server");	
---
+++ frontend/www/arena/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 
+ * Please read full (and updated) documentation at: 
+ * https://github.com/omegaup/omegaup/wiki/Arena
+ * 
+ *
+ * GET /arena/
+ * Regresa el HTML de la arena. Si el usuario no esta loggeado, 
+ * muestra el login. Si está loggeado, por el momento muestra 
+ * la lista de concursos obtenida por /contests/
+ *
+ *
+ * */
+
+
+
+
+
