6221a5c6b3ec423df61cb1cd262106e404b4d9d2
Alan Gonzalez <alan.gohe@gmail.com>
2011-09-16 13:17:22 -0500

    Server path is now being automagically found by bootstrap thanks to Ruben, now there is no need for the server path in config.php

--- frontend/server/inc/bootstrap.php
+++ frontend/server/inc/bootstrap.php
@@ -36,6 +36,9 @@
 
    // Loads config
    define('SERVER_PATH', dirname(__DIR__));      
+
+   ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . SERVER_PATH);
+
    require_once(SERVER_PATH."/config.php");
 
 	/**
