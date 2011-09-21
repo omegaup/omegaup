b75dc98f3c5fb26d1b205f73f6b53e0535292e77
Joe Ponce <Joemmanuel@gmail.com>
2011-09-21 08:15:27 -0700

    /clarifications/show/:id Issue #62

---
+++ frontend/server/api/ShowClarification.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * 
+ * Please read full (and updated) documentation at: 
+ * https://github.com/omegaup/omegaup/wiki/Arena 
+ *
+ * 
+ * POST /contests/:id
+ * Si el usuario puede verlos, muestra los detalles del concurso :id.
+ *
+ * */
+
+require_once("ApiHandler.php");
+
+class ShowClarification extends ApiHandler
+{
+    
+    protected function ProcessRequest()
+    {
+        $this->request = array(
+            "clarification_id" => new ApiExposedProperty("clarification_id", true, GET, array(
+                new NumericValidator(),
+                new CustomValidator( 
+                    function ($value)
+                    {
+                        // Check if the contest exists
+                        return ClarificationsDAO::getByPK($value);
+                    }) 
+            ))                                 
+        );
+                
+    }   
+    
+    protected function ValidateRequest() 
+    {
+        // Call generic validation
+        parent::ValidateRequest();
+        
+        
+        // If the clarification is private, verify that our user is invited                
+        $clarification = ClarificationsDAO::getByPK($this->request["clarification_id"]->getValue());                
+                
+        if ($clarification->getPublic() === '0')
+        {        
+            if ($clarification->getAuthorId() != $this->user_id )
+            {
+                die(json_encode($this->error_dispatcher->forbiddenSite()));
+            }        
+        }                
+ 
+    }
+
+
+    protected function GenerateResponse() 
+    {
+       // Create array of relevant columns
+        $relevant_columns = array("message", "answer", "time", "problem_id", "contest_id");
+        
+        // Get our clarificatoin given the id
+        try
+        {
+            
+            $clarification = ClarificationsDAO::getByPK($this->request["clarification_id"]->getValue());
+        }
+        catch(Exception $e)
+        {
+            // Operation failed in the data layer
+            die(json_encode( $this->error_dispatcher->invalidDatabaseOperation() ));        
+        
+        }
+        
+        // Add the clarificatoin the response
+        $this->response = $clarification->asFilteredArray($relevant_columns);               
+             
+        
+    }
+    
+    protected function SendResponse() 
+    {
+        // There should not be any failing path that gets into here
+        
+        // Happy ending.
+        die(json_encode(array(
+                    "status"  => "ok",
+                    "clarification" => $this->response
+        )));
+               
+    }
+}
+
+?>
--- frontend/www/arena/clarifications/index.php
+++ frontend/www/arena/clarifications/index.php
@@ -1,4 +1,16 @@
 <?php
 
+/*
+* Si el usuario tiene permiso, la API regresa la pregunta y la respuesta asociada con la clarifiación :id.
+*/
+
+
+define("WHOAMI", "API");
+require_once("../../../server/inc/bootstrap.php");
+require_once("../../../server/api/ShowClarification.php");
+
+
+$apiHandler = new ShowClarification();
+$apiHandler->ExecuteApi();
 
 
