804d1f4dda34a2b210d9a764ec1e92e74f7ce112
Joe Ponce <Joemmanuel@gmail.com>
2011-09-21 08:38:50 -0700

    Clarifications/problem/:id Issue #59

---
+++ frontend/server/api/ShowClarificationsInProblem.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * 
+ * Please read full (and updated) documentation at: 
+ * https://github.com/omegaup/omegaup/wiki/Arena 
+ *
+ * 
+GET /clarifications/problem/:id
+Regresa TODAS las clarificaciones de un problema en particular, a las cuales el usuario puede ver (equivale a las que el personalmente mandó más todas las clarificaciones del problema marcadas como globales)
+ *
+ * */
+
+require_once("ApiHandler.php");
+
+class ShowClarificationsInProblem extends ApiHandler
+{
+    
+    protected function ProcessRequest()
+    {
+        $this->request = array(
+            "problem_id" => new ApiExposedProperty("problem_id", true, GET, array(
+                new NumericValidator(),
+                new CustomValidator( 
+                    function ($value)
+                    {
+                        // Check if the problem exists
+                        return ProblemsDAO::getByPK($value);
+                    }) 
+            ))                                 
+        );
+                
+    }   
+   
+
+    protected function GenerateResponse() 
+    {
+       // Create array of relevant columns
+        $relevant_columns = array("message", "answer", "time");
+        
+        //Get all public clarifications
+        $public_clarification_mask = new Clarifications ( array (
+           "public" => '1',
+           "problem_id" => $this->request["problem_id"]->getValue()
+        ));
+        
+        // Get all private clarifications of the user 
+        $private_clarification_mask = new Clarifications ( array (
+           "public" => '0',
+           "problem_id" => $this->request["problem_id"]->getValue(),
+           "author_id" => $this->user_id
+            
+        ));
+        
+        //@todo This query should be merged and optimized ....
+        // Get our clarification given the masks
+        try
+        {
+            
+            $clarifications_public = ClarificationsDAO::search($public_clarification_mask);
+            $clarifications_private = ClarificationsDAO::search($private_clarification_mask);
+        }
+        catch(Exception $e)
+        {
+            // Operation failed in the data layer
+            die(json_encode( $this->error_dispatcher->invalidDatabaseOperation() ));        
+        
+        }
+                        
+        // Filter each clarification and add it to the response
+        foreach($clarifications_public as $clarification)
+        {
+            array_push($this->response, $clarification->asFilteredArray($relevant_columns));               
+        }
+         
+        // Filter each clarification and add it to the response
+        foreach($clarifications_private as $clarification)
+        {
+            array_push($this->response, $clarification->asFilteredArray($relevant_columns));               
+        }
+        
+    }
+    
+    protected function SendResponse() 
+    {
+        // There should not be any failing path that gets into here
+        
+        // Happy ending.
+        die(json_encode(array(
+                    "status"  => "ok",
+                    "clarifications" => $this->response
+        )));
+               
+    }
+}
+
+?>
--- frontend/www/arena/clarifications/problem/_id_/index.php
+++ frontend/www/arena/clarifications/problem/_id_/index.php
@@ -1,4 +1,17 @@
 <?php
 
+/*
+* GET /clarifications/problem/:id
+Regresa TODAS las clarificaciones de un problema en particular, a las cuales el usuario puede ver (equivale a las que el personalmente mandó más todas las clarificaciones del problema marcadas como globales)
+ */
+
+
+define("WHOAMI", "API");
+require_once("../../../../../server/inc/bootstrap.php");
+require_once("../../../../../server/api/ShowClarificationsInProblem.php");
+
+
+$apiHandler = new ShowClarificationsInProblem();
+$apiHandler->ExecuteApi();
 
 
