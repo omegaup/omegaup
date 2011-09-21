5fadc90dd864e6eb8d88cca1dcc2b21e66a21162
Joe Ponce <Joemmanuel@gmail.com>
2011-09-21 07:49:06 -0700

    Clarifications/new Issue #56

--- frontend/www/arena/clarifications/new/index.php
+++ frontend/www/arena/clarifications/new/index.php
@@ -1,4 +1,17 @@
 <?php
 
+/*
+* GET /clarification/new
+Si el usuario tiene permiso, envía una clarificación sobre un problema en particular. En los parámetros se envía el ID del problema. La API regresa un ID para trackearla de alguna forma.
+*/
+
+
+define("WHOAMI", "API");
+require_once("../../../../server/inc/bootstrap.php");
+require_once("../../../../server/api/NewClarification.php");
+
+
+$apiHandler = new NewClarification();
+$apiHandler->ExecuteApi();
 
 
