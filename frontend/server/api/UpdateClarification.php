bd0792e3417c523ec6945b98bbdfc5890dac2706
Joe Ponce <Joemmanuel@gmail.com>
2011-09-22 07:56:46 -0700

    Answer clarification, Issue #97

---
+++ frontend/server/api/UpdateClarification.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * 
+ * Please read full (and updated) documentation at: 
+ * https://github.com/omegaup/omegaup/wiki/Arena 
+ *
+ * 
+ * POST /contests/update/:id
+ * Si el usuario puede verlo, actualiza la clarificación ID.
+ *
+ * */
+
+require_once("ApiHandler.php");
+
+class UpdateClarification extends ApiHandler
+{
+    
+    protected function ProcessRequest()
+    {
+        $this->request = array(
+            "clarification_id" => new ApiExposedProperty("clarification_id", true, GET, array(
+                new NumericValidator(),
+                new CustomValidator( 
+                    function ($value)
+                    {
+                        // Check if the contest exists
+                        return ClarificationsDAO::getByPK($value);
+                    }) 
+            )),
+            
+            "answer" => new ApiExposedProperty("answer", true, POST, array(
+                new StringValidator()
+            )),
+                            
+            "message" => new ApiExposedProperty("message", false, POST, array(
+                new StringValidator()
+            )),                
+                            
+            "public" => new ApiExposedProperty("public", true, POST, array(
+                new NumericValidator()
+            ))
+                            
+        );
+                
+    }   
+       
+
+
+    protected function GenerateResponse() 
+    {
+                
+        // Get our clarificatoin given the id
+        try
+        {
+            
+            $clarification = ClarificationsDAO::getByPK($this->request["clarification_id"]->getValue());
+        }
+        catch(Exception $e)
+        {
+            // Operation failed in the data layer
+            die(json_encode( $this->error_dispatcher->invalidDatabaseOperation() ));        
+        
+        }
+        
+        // Update clarification
+        if(!is_null($this->request["message"]->getValue()) )
+        {
+            // The clarificator may opt to modify the message (typos)
+            $clarification->setMessage($this->request["message"]->getValue());
+        }        
+        $clarification->setAnswer($this->request["answer"]->getValue());
+        $clarification->setPublic($this->request["public"]->getValue());
+        
+        
+        // Save the clarification
+        try
+        {
+            
+            ClarificationsDAO::save($clarification);
+        }
+        catch( Exception $e)
+        {
+            // Operation failed in the data layer
+            die(json_encode( $this->error_dispatcher->invalidDatabaseOperation() ));         
+        }
+        
+    }
+    
+    protected function SendResponse() 
+    {
+        // There should not be any failing path that gets into here
+        
+        // Happy ending.
+        die(json_encode(array(
+            "status"  => "ok"                    
+        )));
+               
+    }
+}
+
+?>
