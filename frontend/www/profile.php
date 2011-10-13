b267e30a40a00a57f4ec99dae1a3789cd1947d35
Alan Gonzalez <alan.gohe@gmail.com>
2011-10-13 01:07:18 -0500

    merge to www

--- frontend/www/profile.php
+++ frontend/www/profile.php
@@ -9,12 +9,24 @@
 		* leaving open holes for new pages.
 		* 
 		* */
-	define( "LEVEL_NEEDED", false );
+	define( "LEVEL_NEEDED", true );
 
 	require_once( "../server/inc/bootstrap.php" );
 
 
 	$page = new OmegaupComponentPage();
 
+	if(!isset($_GET["id"])){
+
+		$page->addComponent( new TitleComponent("asdf") );
+		$page->render();
+
+	}
+
+	$this_user = UsersDAO::getByPK( $_GET["id"] );
+	
+	$html = '<img src="http://www.gravatar.com/avatar/'. md5($this_user->getUsername())  .'?s=128&amp;d=identicon&amp;r=PG"  >';
+	
+	$page->addComponent( new FreeHtmlComponent($html) );
 
 	$page->render();
