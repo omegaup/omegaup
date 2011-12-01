a24827684136f74647b41ce24249460a08563d5f
Alan Gonzalez <alan.gohe@gmail.com>
2011-12-01 14:27:33 -0600

    Last changes, added problems frontend view

---
+++ frontend/www/probs.php
@@ -0,0 +1,44 @@
+<?php
+
+	   /*
+		* LEVEL_NEEDED defines the users who can see this page.
+		* Anyone without permission to see this page, will	
+		* be redirected to a page saying so.
+		* This variable *must* be set in order to bootstrap
+		* to continue. This is by design, in order to prevent
+		* leaving open holes for new pages.
+		* 
+		* */
+	define( "LEVEL_NEEDED", false );
+
+	require_once( "../server/inc/bootstrap.php" );
+
+
+	$page = new OmegaupComponentPage();
+
+
+	
+	$lista = ProblemsController::getProblemList();
+
+	$page->addComponent( 
+		new TableComponent (
+			array(
+				"problem_id" 	=> "problem_id",
+				"title" 		=> "title",
+				"alias" 		=> "alias",
+				"validator" 	=> "validator",
+				"server" 		=> "server",
+				"visits" 		=> "visits",
+				"submissions" 	=> "submissions",
+				"accepted" 		=> "accepted",
+				"difficulty" 	=> "difficulty"
+			),
+			$lista
+		)
+	);
+
+	$page->render();
+
+
+
+
