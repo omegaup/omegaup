4b4f143d4af1e314b2287acc4a82042e42004c6c
lhchavez <lhchavez@lhchavez.com>
2011-12-05 01:28:48 -0800

    The beginnings of a sandbox test suite

---
+++ sandbox/test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+gcc -x c -o .sample - <<EOF
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int main() {
+        socket(AF_INET, SOCK_STREAM, 0);
+        perror("socket");
+
+        return 0;
+}
+EOF
+
+echo -n "Opening a socket                            "
+if [ "`./.sample 2>&1`" == "socket: Success" ]; then
+	echo "[OK]"
+else
+	echo "[FAIL]"
+fi
+
+echo -n "Opening a socket (sandboxed)                "
+if [ "`./box -q -S profiles/java -- ./.sample 2>&1`" == "socket: Permission denied" ]; then
+	echo "[OK]"
+else
+	echo "[FAIL]"
+fi
+
+gcc -x c -o .sample - <<EOF
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+	sleep(1);
+
+        return 0;
+}
+EOF
+
+echo -n "Time Limit Exceeded                         "
+
+if [ "`./box -S profiles/c -t 1 -- ./.sample 2>&1` | grep Forbidden" != "" ]; then
+	echo "[OK]"
+else
+	echo "[FAIL]"
+fi
+
+rm .sample
