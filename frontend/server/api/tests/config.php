bd39c666d200642af9e5582fda9941a975b33f12
Joe Ponce <Joemmanuel@gmail.com>
2012-01-03 21:44:17 -0800

    Fixing config.php for test environment and ignoring config.ph

--- frontend/server/api/tests/config.php
+++ frontend/server/api/tests/config.php
@@ -12,18 +12,18 @@ define('OMEGAUP_DB_HOST',         'localhost');
 define('OMEGAUP_DB_NAME',         'omegaup_beta');	
 define('OMEGAUP_DB_DRIVER',       'mysqlt');
 define('OMEGAUP_DB_DEBUG',        false);	
-define('OMEGAUP_ROOT',            OMEGAUP_BASEPATH . '/frontend');
+define('OMEGAUP_ROOT',            '/opt/omegaup/frontend');
 define("OMEGAUP_MD5_SALT", 	"om3gaup");	
 
 define('OMEGAUP_LOG_TO_FILE',     true);
-define('OMEGAUP_LOG_ACCESS_FILE', OMEGAUP_ROOT . '/log/omegaup.log');
-define('OMEGAUP_LOG_ERROR_FILE',  OMEGAUP_ROOT . '/log/omegaup.log');
+define('OMEGAUP_LOG_ACCESS_FILE', '/opt/omegaup/frontend/log/omegaup.log');	  	
+define('OMEGAUP_LOG_ERROR_FILE',  '/opt/omegaup/frontend/log/omegaup.log');
 define('OMEGAUP_LOG_TRACKBACK',   false);
 define('OMEGAUP_LOG_DB_QUERYS',   true);
 
-define("OMEGAUP_SSLCERT_URL",     OMEGAUP_ROOT . "/omegaup.pem");
-define("OMEGAUP_CACERT_URL",      OMEGAUP_BASEPATH . "/ssl/omegaup-ca.crt");
-define("OMEGAUP_GRADER_URL",      "https://localhost:21680/grade/");
+define("OMEGAUP_SSLCERT_URL", "/opt/omegaup/frontend/omegaup.pem");
+define("OMEGAUP_CACERT_URL", "/opt/omegaup/ssl/omegaup-ca.crt");
+define("OMEGAUP_GRADER_URL", "https://localhost:21680/grade/");
 
-define('RUNS_PATH',               OMEGAUP_BASEPATH . '/runs');
-define('PROBLEMS_PATH',           OMEGAUP_BASEPATH . '/problems');
+define('RUNS_PATH',               '/opt/omegaup/omegaup/submissions');	  	
+define('PROBLEMS_PATH',           '/opt/omegaup/omegaup/problems');
