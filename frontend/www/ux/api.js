cc9d278f703f399e9e1bec1737245169b7db2d68
lhchavez <lhchavez@lhchavez.com>
2012-01-08 11:47:31 +0400

    First iteration of UX

---
+++ frontend/www/ux/api.js
@@ -0,0 +1,106 @@
+function OmegaUp() {
+	var self = this;
+
+	this.deltaTime = 0;
+	this.authenticated(function(data) {
+		if (data.status == 'ok') {
+			self.syncTime();
+		} else {
+			window.location = data.login_url;
+		}
+	});
+}
+
+OmegaUp.prototype.authenticated = function(callback) {
+	$.get(
+		'/arena/authenticated/',
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.syncTime = function() {
+	var self = this;
+
+	var t0 = new Date().getTime();
+	$.get(
+		'/arena/time/',
+		function (data) {
+			self.deltaTime = data.time * 1000 - t0;
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.time = function(date) {
+	var newDate = this.deltaTime;
+	if (date) {
+		newDate += new Date(date).getTime();
+	} else {
+		newDate += new Date().getTime();
+	}
+	return new Date(newDate);
+};
+
+OmegaUp.prototype.login = function(username, password, callback) {
+	$.post(
+		'/arena/login/',
+		{ username: username, password: password },
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.getContests = function(callback) {
+	var self = this;
+
+	$.get(
+		'/arena/contests/',
+		function (data) {
+			for (var idx in data.contests) {
+				var contest = data.contests[idx];
+				contest.start_time = self.time(contest.start_time);
+				contest.finish_time = self.time(contest.finish_time);
+			}
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.getContest = function(alias, callback) {
+	var self = this;
+
+	$.get(
+		'/arena/contests/' + alias + '/',
+		function (contest) {
+			if (contest.status == 'ok') {
+				contest.start_time = self.time(contest.start_time);
+				contest.finish_time = self.time(contest.finish_time);
+			}
+			callback(contest);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.getProblem = function(contestAlias, problemAlias, callback) {
+	var self = this;
+
+	$.post(
+		'/arena/contests/' + contestAlias + '/problem/' + problemAlias + '/',
+		{lang:"es"},
+		function (contest) {
+			if (contest.status == 'ok') {
+				contest.start_time = self.time(contest.start_time);
+				contest.finish_time = self.time(contest.finish_time);
+			}
+			callback(contest);
+		},
+		'json'
+	);
+};
