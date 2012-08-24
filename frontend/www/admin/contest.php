e7163ab2558d3de75481a177b5b7a58765ec9872
alanboy <alan.gohe@gmail.com>
2012-08-24 03:26:09 -0700

    contest details rendering started

--- frontend/www/admin/contest.php
+++ frontend/www/admin/contest.php
@@ -40,7 +40,154 @@
 	}
 
 	$page->nextTab("Detalles");
-	$page->addComponent( "<a href='../arena/{$_GET['alias']}/'>Ir al concurso</a>"  );
+
+	//$page->addComponent( "<a href='../arena/{$_GET['alias']}/'>Ir al concurso</a>"  );
+
+
+
+
+
+
+	$json = file_get_contents("file:///home/alanboy/Downloads/cacheado.json");
+
+
+
+
+
+
+
+
+
+
+	$djson = json_decode( $json );
+
+	$nreports = sizeof($djson->report); 
+
+	$page->addComponent( "<table style='width:100%' border=0>"  );	
+
+	//reports
+	for($r = 0 ; $r < $nreports ; $r++)	
+	{
+		$page->addComponent( "<tr><td><hr>"  );	
+		
+		$page->addComponent( "<strong>" . $djson->report[$r]->username . "</strong> ".$djson->report[$r]->name."" );
+
+		/* 	$djson->report[$r]->total->points;  $djson->report[$r]->total->penalty;*/
+
+		$nprobs = sizeof($djson->report[$r]->problems);
+
+		$pkeys = get_object_vars( $djson->report[$r]->problems  );
+
+		$page->addComponent( "</td></tr>"  );	
+
+		//team, i think
+		foreach($pkeys as $pp ){
+			$page->addComponent( "<tr><td>"  );	
+			
+			$page->addComponent( "<div style='background-color: #0072C6; color: white; padding: 5px; overflow: hidden'> Puntos : " .$pp->points. "&nbsp;&nbsp;&nbsp;Penalty : ".$pp->penalty . "</div>" );
+
+
+			if(!property_exists($pp, "run_details" )) continue;
+
+			if(array_key_exists("source", $pp->run_details )){
+				$page->addComponent( "<div style='display:block; overflow: hidden'><code>".$pp->run_details->source. "</code></div>" );
+			}			
+
+
+			if(!array_key_exists("cases", $pp->run_details )) continue;
+	
+			$page->addComponent( "</td></tr>"  );				
+
+
+			$ccolor = 0;
+			foreach($pp->run_details->cases as $c)
+			{
+
+				$ccolor ++;
+
+
+
+				if($ccolor % 2 == 0)				
+					$page->addComponent( "<tr >"  );
+				else
+					$page->addComponent( "<tr style='background-color: #F0F0F0'>"  ); 
+
+				$page->addComponent( "<td>"  );
+
+				$page->addComponent( "<table style='width:100%'>"  );
+				$page->addComponent( "<tr>"  );
+				$page->addComponent( "<td>name</td>"  );
+				$page->addComponent( "<td>out_diff </td>"  );
+				$page->addComponent( "<td>time</td>"  );
+				$page->addComponent( "<td>time-wall</td>"  );
+				$page->addComponent( "<td>syscall-count</td>"  );
+				$page->addComponent( "<td>status</td>"  );
+				$page->addComponent( "</tr>"  );
+
+
+
+
+
+				$page->addComponent( "<tr>"  );
+				$page->addComponent( "<td>" . $c->name. "</td>"  );
+
+				if(property_exists($c->meta, "out_diff"))
+					$page->addComponent( "<td>" .$c->meta->out_diff. " </td>"  );
+				else
+					$page->addComponent( "<td> - </td>"  );
+
+
+
+				if(property_exists($c->meta, "time"))
+					$page->addComponent( "<td>" .$c->meta->time. " </td>"  );
+				else
+					$page->addComponent( "<td> - </td>"  );
+
+
+
+				if(property_exists($c->meta, "time-wall"))
+					$page->addComponent( "<td>" .$c->meta->{'time-wall'} . " </td>"  );
+				else
+					$page->addComponent( "<td> - </td>"  );
+
+
+				if(property_exists($c->meta, "syscall-count"))
+					$page->addComponent( "<td>" .$c->meta->{'syscall-count'}. " </td>"  );
+				else
+					$page->addComponent( "<td> - </td>"  );
+
+
+
+
+				$page->addComponent( "<td><strong>" .$c->meta->status. "</strong></td>"  );
+				$page->addComponent( "</tr>"  );
+
+				$page->addComponent( "</table>"  );
+
+				$page->addComponent( "</td></tr>"  );	
+			}
+
+		}
+		$page->addComponent( "</td>"  );	
+		$page->addComponent( "</tr>"  );	
+		
+
+	}
+	
+	$page->addComponent( "</table>"  );	
+
+	
+
+
+
+
+
+
+
+
+
+
+
 
 
 
