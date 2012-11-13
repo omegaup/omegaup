3581a8c9c88225a823c37916e0e504e46535b50a
Joemmanuel <joemmanuel@gmail.com>
2012-11-12 18:59:47 -0800

    Fix para no cachear el tiempo restante de cada concursante de forma general

--- frontend/server/api/ShowContest.php
+++ frontend/server/api/ShowContest.php
@@ -16,7 +16,7 @@ require_once(SERVER_PATH . '/libs/Cache.php');
 
 class ShowContest extends ApiHandler
 {       
-    
+    private $contest;
     protected function RegisterValidatorsToRequest()
     {
         ValidatorFactory::stringNotEmptyValidator()->addValidator(new CustomValidator(
@@ -28,12 +28,12 @@ class ShowContest extends ApiHandler
             ->validate(RequestContext::get("contest_alias"), "contest_alias");
         
         // If the contest is private, verify that our user is invited                
-        $contest = ContestsDAO::getByAlias(RequestContext::get("contest_alias"));                                
-        if ($contest->getPublic() === '0')            
+        $this->contest = ContestsDAO::getByAlias(RequestContext::get("contest_alias"));                                
+        if ($this->contest->getPublic() === '0')            
         {      
             try
             {
-                if (is_null(ContestsUsersDAO::getByPK($this->_user_id, $contest->getContestId())) && !Authorization::IsContestAdmin($this->_user_id, $contest))
+                if (is_null(ContestsUsersDAO::getByPK($this->_user_id, $this->contest->getContestId())) && !Authorization::IsContestAdmin($this->_user_id, $this->contest))
                 {
                     throw new ApiException(ApiHttpErrors::forbiddenSite());
                 }
@@ -51,9 +51,9 @@ class ShowContest extends ApiHandler
         }  
         
         // If the contest has not started, user should not see it, unless it is admin
-        if (!$contest->hasStarted($this->_user_id) && !Authorization::IsContestAdmin($this->_user_id, $contest))
+        if (!$this->contest->hasStarted($this->_user_id) && !Authorization::IsContestAdmin($this->_user_id, $this->contest))
         {
-            throw new ApiException(ApiHttpErrors::preconditionFailed("Contest has not started yet.", array("start_time" => strtotime($contest->getStartTime()))) );
+            throw new ApiException(ApiHttpErrors::preconditionFailed("Contest has not started yet.", array("start_time" => strtotime($this->contest->getStartTime()))) );
         }
     }      
 
@@ -67,28 +67,17 @@ class ShowContest extends ApiHandler
     	if(is_null($result))
     	{
             // Create array of relevant columns
-             $relevant_columns = array("title", "description", "start_time", "finish_time", "window_length", "alias", "scoreboard", "points_decay_factor", "partial_score", "submissions_gap", "feedback", "penalty", "time_start", "penalty_time_start", "penalty_calc_policy");
-
-             // Get our contest given the alias
-             try
-             {            
-                 $contest = ContestsDAO::getByAlias(RequestContext::get("contest_alias"));
-             }
-             catch(Exception $e)
-             {
-                 // Operation failed in the data layer
-                throw new ApiException( ApiHttpErrors::invalidDatabaseOperation(), $e );                
-             }
+             $relevant_columns = array("title", "description", "start_time", "finish_time", "window_length", "alias", "scoreboard", "points_decay_factor", "partial_score", "submissions_gap", "feedback", "penalty", "time_start", "penalty_time_start", "penalty_calc_policy");            
 
              // Initialize response to be the contest information
-             $result = $contest->asFilteredArray($relevant_columns);                    
+             $result = $this->contest->asFilteredArray($relevant_columns);                    
 
              $result['start_time'] = strtotime($result['start_time']);
              $result['finish_time'] = strtotime($result['finish_time']);
 
              // Get problems of the contest
              $key_problemsInContest = new ContestProblems(
-                 array("contest_id" => $contest->getContestId()));        
+                 array("contest_id" => $this->contest->getContestId()));        
              try
              {
                  $problemsInContest = ContestProblemsDAO::search($key_problemsInContest, "order");
@@ -131,33 +120,34 @@ class ShowContest extends ApiHandler
              try
              {
                  $contest_user = ContestsUsersDAO::CheckAndSaveFirstTimeAccess(
-                         $this->_user_id, $contest->getContestId());
+                         $this->_user_id, $this->contest->getContestId());
              }
              catch(Exception $e)
              {
                   // Operation failed in the data layer
                   throw new ApiException( ApiHttpErrors::invalidDatabaseOperation(), $e );        
              }
-
+             
              // Add problems to response
              $result['problems'] = $problemsResponseArray;
-
-             // Add time left to response
-             if ($contest->getWindowLength() === null)
-             {
-                 $result['submission_deadline'] = strtotime($contest->getFinishTime());
-             }
-             else
-             {
-                 $result['submission_deadline'] = min(strtotime($contest->getFinishTime()),
-                 strtotime($contest_user->getAccessTime()) + $contest->getWindowLength() * 60);
-             }
-
-             // @TODO Add mini ranking here
-
+             
              $cache->set($result, APC_USER_CACHE_CONTEST_INFO_TIMEOUT);
 
-    	}	// closes if( $result == null )
+        }// closes if( $result == null )
+        
+        // Adding timer info separately as it depends on the current user and we don't
+        // want this to get generally cached
+                     
+        // Add time left to response
+        if ($this->contest->getWindowLength() === null)
+        {
+            $result['submission_deadline'] = strtotime($this->contest->getFinishTime());
+        }
+        else
+        {
+            $result['submission_deadline'] = min(strtotime($this->contest->getFinishTime()),
+            strtotime($contest_user->getAccessTime()) + $this->contest->getWindowLength() * 60);
+        }
     	
     	$this->addResponseArray($result);
     }    
