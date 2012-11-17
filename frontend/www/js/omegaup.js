7523db5882d94d630a36cc0315c6eee84a69605a
alanboy <alanboy@alanboy.net>
2012-11-17 07:50:36 -0800

    Missing files, reeallly removin gold api.js and moving most of it int omegaup.js

--- frontend/www/js/omegaup.js
+++ frontend/www/js/omegaup.js
@@ -7,20 +7,23 @@ function OmegaUp( ) {
 	this.deltaTime = 0;
 }
 
+/*function OmegaUp() {
+	var self = this;
+	this.username = null;
 
-
-/*
-OmegaUp.prototype.CreateUser = function(s_Email, s_Username, s_PlainPassword, callback) {
-	$.post(
-		'/api/user/create',
-		{ s_Email: s_Email, s_Username: s_Username, s_PlainPassword : s_PlainPassword },
-		function (data) {
-			callback(data);
-		},
-		'json'
-	);
-};
+	this.deltaTime = 0;
+	this.authenticated(function(data) {
+		if (data.status == 'ok') {
+			self.syncTime();
+			self.username = data.username;
+		} else {
+			//window.location = data.login_url;
+		}
+	});
+}
 */
+
+
 OmegaUp.UI = {
 	Error : function ( reason ){
 		$.msgBox({
@@ -53,20 +56,343 @@ OmegaUp.prototype.CreateUser = function(s_Email, s_Username, s_PlainPassword, ca
 		'/api/user/create/email/' + s_Email + "/username" + s_Username + "/password/" + s_PlainPassword ,
 		{ email: s_Email, username: s_Username, password : s_PlainPassword },
 		function (data) {
-			
-			console.log("returned", data);
-
-			if( data.status !== undefined && data.status == "error")
-			{
+			if( data.status !== undefined && data.status == "error") {
 				OmegaUp.UI.Error( data.error );
 			}else{
 				if(callback !== undefined){ callback( data ) }
 			}
-			
-			
 		},
 		'json'
 	);
+};
+/*
+OmegaUp.prototype.CreateUser = function(s_Email, s_Username, s_PlainPassword, callback) {
+	$.post(
+		'/api/user/create',
+		{ s_Email: s_Email, s_Username: s_Username, s_PlainPassword : s_PlainPassword },
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+*/
+--------------------------------------------------------------------------------
+
+OmegaUp.prototype.authenticated = function(callback) {
+	$.get(
+		'/api/Sesion/CurrentSesion',
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.syncTime = function() {
+	var self = this;
+
+	var t0 = new Date().getTime();
+	$.get(
+		'/api/controllername/time/',
+		function (data) {
+			self.deltaTime = data.time * 1000 - t0;
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.time = function(date) {
+	var newDate = this.deltaTime;
+	if (date) {
+		newDate += new Date(date).getTime();
+	} else {
+		newDate += new Date().getTime();
+	}
+	return new Date(newDate);
+};
+
+OmegaUp.prototype.login = function(username, password, callback) {
+	$.post(
+		'/api/sesion/login/',
+		{ username: username, password: password },
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.getContests = function(callback) {
+	var self = this;
+
+	$.get(
+		'/api/api/controllername/contests/',
+		function (data) {
+			for (var idx in data.contests) {
+				var contest = data.contests[idx];
+				contest.start_time = self.time(contest.start_time * 1000);
+				contest.finish_time = self.time(contest.finish_time * 1000);
+			}
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.getContest = function(alias, callback) {
+	var self = this;
+
+	$.get(
+		'/api/api/controllername/contests/' + alias + '/',
+		function (contest) {
+			if (contest.status == 'ok') {
+				contest.start_time = self.time(contest.start_time * 1000);
+				contest.finish_time = self.time(contest.finish_time * 1000);
+				contest.submission_deadline = self.time(contest.submission_deadline * 1000);
+			}
+			callback(contest);
+		},
+		'json'
+	).error(function(j, status, errorThrown) {
+		try {
+			callback(JSON.parse(j.responseText));
+		} catch (err) {
+			callback({status:'error', 'error':undefined});
+		}
+	});
+
+};
+
+OmegaUp.prototype.getProblem = function(contestAlias, problemAlias, callback) {
+	var self = this;
+
+	$.post(
+		'/api/controllername/contests/' + contestAlias + '/problem/' + problemAlias + '/',
+		{lang:"es"},
+		function (problem) {
+			if (problem.runs) {
+				for (var i = 0; i < problem.runs.length; i++) {
+					problem.runs[i].time = self.time(problem.runs[i].time * 1000);
+				}
+			}
+			callback(problem);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.getContestRuns = function(contestAlias, offset, rowcount, callback) {
+	var self = this;
+
+	$.post(
+		'/api/controllername/contests/' + contestAlias + '/runs/',
+		{offset: offset, rowcount: rowcount},
+		function (data) {
+			for (var i = 0; i < data.runs.length; i++) {
+				data.runs[i].time = self.time(data.runs[i].time * 1000);
+			}
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.submit = function(contestAlias, problemAlias, language, code, callback) {
+	var self = this;
+
+	$.post(
+		'/api/controllername/runs/new',
+		{
+			contest_alias: contestAlias,
+			problem_alias: problemAlias,
+			language: language,
+			source: code
+		},
+		function (data) {
+			callback(data);
+		},
+		'json'
+	).error(function(j, status, errorThrown) {
+		try {
+			callback(JSON.parse(j.responseText));
+		} catch (err) {
+			callback({status:'error', 'error':undefined});
+		}
+	});
+};
+
+OmegaUp.prototype.runStatus = function(guid, callback) {
+	var self = this;
+
+	$.get(
+		'/api/controllername/runs/' + guid + '/',
+		function (data) {
+			data.time = self.time(data.time * 1000);
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.runDetails = function(guid, callback) {
+	var self = this;
+
+	$.get(
+		'/api/controllername/runs/' + guid + '/details/',
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.runSource = function(guid, callback) {
+	var self = this;
+
+	$.get(
+		'/api/controllername/runs/' + guid + '/source/',
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.runRejudge = function(guid, callback) {
+	var self = this;
+
+	$.get(
+		'/api/controllername/runs/' + guid + '/rejudge/',
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.rejudgeProblem = function(problemAlias, callback) {
+	var self = this;
+
+	$.get(
+		'/api/controllername/problems/' + problemAlias + '/rejudge/',
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.getRanking = function(contestAlias, callback) {
+	var self = this;
+
+	$.get(
+		'/api/controllername/contests/' + contestAlias + '/ranking/',
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.getRankingEvents = function(contestAlias, callback) {
+	var self = this;
+
+	$.get(
+		'/api/controllername/contests/' + contestAlias + '/ranking/events/',
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.getClarifications = function(contestAlias, callback) {
+	var self = this;
+
+	$.get(
+		'/api/controllername/contests/' + contestAlias + '/clarifications/',
+		function (data) {
+			callback(data);
+		},
+		'json'
+	);
+};
+
+OmegaUp.prototype.newClarification = function(contestAlias, problemAlias, message, callback) {
+	var self = this;
+
+	$.post(
+		'/api/controllername/clarifications/new',
+		{
+			contest_alias: contestAlias,
+			problem_alias: problemAlias,
+			message: message
+		},
+		function (data) {
+			callback(data);
+		},
+		'json'
+	).error(function(j, status, errorThrown) {
+		try {
+			callback(JSON.parse(j.responseText));
+		} catch (err) {
+			callback({status:'ok', 'error':undefined});
+		}
+	});
+};
+
+OmegaUp.prototype.updateClarification = function(clarificationId, answer, public, callback) {
+	var self = this;
+
+	$.post(
+		'/api/controllername/clarifications/update/' + clarificationId,
+		{
+			answer: answer,
+			public: public ? 1 : 0
+		},
+		function (data) {
+			callback(data);
+		},
+		'json'
+	).error(function(j, status, errorThrown) {
+		try {
+			callback(JSON.parse(j.responseText));
+		} catch (err) {
+			callback({status:'error', 'error':undefined});
+		}
+	});
+};
+
+
+
+OmegaUp.prototype.UserEdit = function( username, name, email, birthDate, school, password, oldPassword, callback ){
+	var self = this,
+		toSend = {};
+
+	if(username !== null) toSend.username = username;
+	if(name !== null) toSend.name = name;
+	if(email !== null) toSend.email = email;
+	if(birthDate !== null) toSend.birthDate = birthDate;		
+	if(school !== null) toSend.school = school;
+	if(password !== null) toSend.password = password;
+	if(oldPassword !== null) toSend.oldPassword = oldPassword;
+
+
+	$.post(
+		'/api/controllername/user/edit/',
+		toSend,
+		function (data) {
+			callback(data);
+		},
+		'json'
+	).error(function(j, status, errorThrown) {
+		try {
+			callback(JSON.parse(j.responseText));
+		} catch (err) {
+			callback({status:'error', 'error':undefined});
+		}
+	});
 };
 
 var omega = new OmegaUp( );
