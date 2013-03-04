98d041d7b61132f081d4d3238a9d9864ade5d656
Joemmanuel <joemmanuel@gmail.com>
2013-03-04 07:35:58 -0800

    Better UI test and adding web entry point to test config

--- frontend/tests/controllers/ArenaTest.php
+++ frontend/tests/controllers/ArenaTest.php
@@ -6,17 +6,29 @@
  * @author joemmanuel
  */
 require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+require_once 'ContestsFactory.php';
 
 class ArenaTest extends PHPUnit_Extensions_SeleniumTestCase {
 
 	protected function setUp() {
 		$this->setBrowser('*firefox');
-		$this->setBrowserUrl('http://localhost/');
+		$this->setBrowserUrl(OMEGAUP_BASE_URL);
 	}
 
 	public function testArenaRoot() {
-		$this->open('http://localhost/arena');
+		
+		// Create a contest
+		$contestData = ContestsFactory::createContest();
+		
+		// Open URL
+		$this->open(OMEGAUP_BASE_URL . '/arena');
+		
+		// Sanity check: Arena at leasts says 'Arena', it is not badly broken
 		$this->assertElementContainsText('//*[@id="root"]/h1', 'Arena');
+		
+		// Check that our latest contest is there
+		$this->waitForElementPresent('//*[@id="past-contests"]/tr[1]/td[1]/a');
+		$this->assertElementContainsText('//*[@id="past-contests"]/tr[1]/td[1]/a', $contestData["request"]["title"]);		
 	}
 
 }
