e2a22c33465048b27e3fa18deffb8c90bd423ab4
Joemmanuel <joemmanuel@gmail.com>
2013-04-20 11:42:38 -0700

    Tests for scoreboard show %

--- frontend/tests/controllers/ContestScoreboardTest.php
+++ frontend/tests/controllers/ContestScoreboardTest.php
@@ -55,5 +55,109 @@ class ContestScoreboardTest extends OmegaupTestCase {
 		$this->assertEquals(60, $response["ranking"][0]["problems"][$problemData["request"]["alias"]]["penalty"]);
 		$this->assertEquals(0, $response["ranking"][0]["problems"][$problemData["request"]["alias"]]["wrong_runs_count"]);
 	}
+	
+	/**
+	 * Set 0% of scoreboard for contestants, should show all 0s
+	 */
+	public function testScoreboardPercentajeForContestant() {
+		
+		DAO::$useDAOCache = false;
+		// Get a problem
+		$problemData = ProblemsFactory::createProblem();
+
+		// Get a contest 
+		$contestData = ContestsFactory::createContest();
+		
+		// Set 0% of scoreboard show
+		$contest = ContestsDAO::getByAlias($contestData["request"]["alias"]);
+		$contest->setScoreboard(0);
+		ContestsDAO::save($contest);		
+
+		// Add the problem to the contest
+		ContestsFactory::addProblemToContest($problemData, $contestData);
+
+		// Create our contestant
+		$contestant = UserFactory::createUser();
+		
+		// Create a run
+		$runData = RunsFactory::createRun($problemData, $contestData, $contestant);
+		
+		// Grade the run
+		RunsFactory::gradeRun($runData);
+		
+		// Create request
+		$r = new Request();
+		$r["contest_alias"] = $contestData["request"]["alias"];
+		$r["auth_token"] = $this->login($contestant);
+		
+		// Create API
+		$response = ContestController::apiScoreboard($r);								
+		
+		// Validate that we have ranking
+		$this->assertEquals(1, count($response["ranking"]));
+		
+		$this->assertEquals($contestant->getUsername(), $response["ranking"][0]["username"]);
+		
+		//Check totals
+		$this->assertEquals(0, $response["ranking"][0]["total"]["points"]);
+		$this->assertEquals(0, $response["ranking"][0]["total"]["penalty"]); /* 60 because contest started 60 mins ago in the default factory */
+		
+		// Check data per problem
+		$this->assertEquals(0, $response["ranking"][0]["problems"][$problemData["request"]["alias"]]["points"]);
+		$this->assertEquals(0, $response["ranking"][0]["problems"][$problemData["request"]["alias"]]["penalty"]);
+		$this->assertEquals(0, $response["ranking"][0]["problems"][$problemData["request"]["alias"]]["wrong_runs_count"]);
+	}
+	
+	/**
+	 * Set 0% of scoreboard for admins
+	 */
+	public function testScoreboardPercentajeForContestAdmin() {
+		
+		DAO::$useDAOCache = false;
+		// Get a problem
+		$problemData = ProblemsFactory::createProblem();
+
+		// Get a contest 
+		$contestData = ContestsFactory::createContest();
+		
+		// Set 0% of scoreboard show
+		$contest = ContestsDAO::getByAlias($contestData["request"]["alias"]);
+		$contest->setScoreboard(0);
+		ContestsDAO::save($contest);		
+
+		// Add the problem to the contest
+		ContestsFactory::addProblemToContest($problemData, $contestData);
+
+		// Create our contestant
+		$contestant = UserFactory::createUser();
+		
+		// Create a run
+		$runData = RunsFactory::createRun($problemData, $contestData, $contestant);
+		
+		// Grade the run
+		RunsFactory::gradeRun($runData);
+		
+		// Create request
+		$r = new Request();
+		$r["contest_alias"] = $contestData["request"]["alias"];
+		$r["auth_token"] = $this->login($contestData["director"]);
+		
+		// Create API
+		$response = ContestController::apiScoreboard($r);								
+		
+		// Validate that we have ranking
+		$this->assertEquals(1, count($response["ranking"]));
+		
+		$this->assertEquals($contestant->getUsername(), $response["ranking"][0]["username"]);
+		
+		//Check totals
+		$this->assertEquals(100, $response["ranking"][0]["total"]["points"]);
+		$this->assertEquals(60, $response["ranking"][0]["total"]["penalty"]); /* 60 because contest started 60 mins ago in the default factory */
+		
+		// Check data per problem
+		$this->assertEquals(100, $response["ranking"][0]["problems"][$problemData["request"]["alias"]]["points"]);
+		$this->assertEquals(60, $response["ranking"][0]["problems"][$problemData["request"]["alias"]]["penalty"]);
+		$this->assertEquals(0, $response["ranking"][0]["problems"][$problemData["request"]["alias"]]["wrong_runs_count"]);
+	}		
 }
 
