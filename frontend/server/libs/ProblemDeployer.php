fd4818d265ab2e000f53688b3be523a403fa16bb
Joemmanuel <joemmanuel@gmail.com>
2013-07-15 08:28:10 -0700

    Soporte para PNG

--- frontend/server/libs/ProblemDeployer.php
+++ frontend/server/libs/ProblemDeployer.php
@@ -43,7 +43,7 @@ class ProblemDeployer {
 		}
 
 		// Also extract any images in the statements directory.
-		$images = preg_grep('/^statements\/.*\.(gif|jpg|jpeg)$/', $zipFilesArray);
+		$images = preg_grep('/^statements\/.*\.(gif|jpg|jpeg|png)$/', $zipFilesArray);
 
 		// Add images to the files to be unzipped.
 		foreach ($images as $file) {
