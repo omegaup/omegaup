ddac8d58e24e9690f1e80af82cb010e2ac368fed
lhchavez <lhchavez@lhchavez.com>
2013-12-25 15:38:09 -0800

    Un paquete que se me olvidó agregar

--- stuff/install.sh
+++ stuff/install.sh
@@ -75,7 +75,7 @@ EOF
 	
 	sudo apt-get update -qq -y
 	
-	sudo apt-get install -qq -y nginx mysql-client php5-fpm php5-cli php5-mysql php-pear php5-mcrypt php5-curl git phpunit g++ fp-compiler unzip openssh-client make zip libcap-dev ghc
+	sudo apt-get install -qq -y nginx mysql-client php5-fpm php5-cli php5-mysql php-pear php5-mcrypt php5-curl git phpunit g++ fp-compiler unzip openssh-client make zip libcap-dev libgfortran3 ghc
 	sudo apt-get install -qq -y openjdk-7-jdk || sudo apt-get install -qq -y openjdk-6-jdk
 	
 	if [ ! -f /usr/sbin/mysqld ]; then
