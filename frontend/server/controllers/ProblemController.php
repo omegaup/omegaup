ee19dd2b6458b4870d8a05fbf087c6964c70a5bd
Joemmanuel <joemmanuel@gmail.com>
2014-02-04 07:17:43 -0800

    #486, Problem/list soporta query para buscar por title

--- frontend/server/controllers/ProblemController.php
+++ frontend/server/controllers/ProblemController.php
@@ -884,6 +884,8 @@ class ProblemController extends Controller {
 		if (!isset($r["rowcount"])) {
 			$r["rowcount"] = 400;
 		}
+		
+		Validators::isStringNonEmpty($r["query"], "query", false);
 	}
 
 	/**
@@ -929,7 +931,18 @@ class ProblemController extends Controller {
 				}
 
 				if (!is_null($problem_mask)) {					
-					$problems = ProblemsDAO::search($problem_mask, "problem_id", 'DESC', $r["offset"], $r["rowcount"]);
+					$problems = ProblemsDAO::search(
+							$problem_mask, 
+							"problem_id", 
+							'DESC', 
+							$r["offset"], 
+							$r["rowcount"],
+							is_null($r["query"]) ? 
+								null : 
+								array(
+									"title" => $r["query"]
+								)
+						);
 					
 					foreach ($problems as $problem) {
 						array_push($response["results"], $problem->asArray());
