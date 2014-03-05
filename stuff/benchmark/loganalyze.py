139a906febe4c6e4bf290d5946de5ea642371374
lhchavez <lhchavez@lhchavez.com>
2014-03-05 01:39:16 -0800

    Script para analizar el log de benchmark

---
+++ stuff/benchmark/loganalyze.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import sys
+
+with open(sys.argv[1], 'r') as f:
+	data = []
+	for line in f:
+		line = line.split()
+		data.append([int(line[1]) / 1e6, int(line[3]) / 1e6, int(line[0]), line[2]])
+
+data.sort()
+t0 = data[0][0]
+
+for row in data:
+	row[0] -= t0
+
+t1 = int(data[-1][0])
+
+window = 10000
+
+i0 = 0
+i1 = 0
+for t in xrange(0, t1, 1000):
+	while i0 < len(data) and data[i0][0] < t:
+		i0 += 1
+	i1 = i0
+	s = 0
+	threads = {}
+	while i1 < len(data) and data[i1][0] < t + window:
+		s += data[i1][1]
+		threads[data[i1][2]] = True
+		i1 += 1
+	print t / 1000, (i1 - i0) / (window / 1000.0), s / max(1, i1 - i0), len(threads)
