6b23ab53ebfd44df0315e16ce5d12308492fd285
lhchavez <lhchavez@lhchavez.com>
2014-05-13 06:26:53 +0400

    Arreglando el bug del login: expirando cookies viejos

--- frontend/server/libs/SessionManager.php
+++ frontend/server/libs/SessionManager.php
@@ -1,6 +1,18 @@
 <?php
 class SessionManager {
 	public function setCookie($name, $value = null, $expire = null, $path = null, $domain = null, $secure = null, $httponly = null) {
+		// Expire all old cookies
+		if (isset($_SERVER['HTTP_COOKIE'])) {
+			$cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+			foreach ($cookies as $cookie) {
+				$parts = explode('=', $cookie);
+				$old_name = trim($parts[0]);
+				setcookie($old_name, '', time()-1000);
+				setcookie($old_name, '', time()-1000, '/');
+			}
+		}
+		
+		// Set the new one
 		$httponly = true;
 		$domain = OMEGAUP_COOKIE_DOMAIN;
 		if (!empty($_SERVER['HTTPS'])) {
