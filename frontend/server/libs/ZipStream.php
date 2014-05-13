d751a197d718f6c81e87e5ef060debb60801fda6
lhchavez <lhchavez@lhchavez.com>
2014-05-13 10:56:38 +0400

    Arreglando un montón de warnings

--- frontend/server/libs/ZipStream.php
+++ frontend/server/libs/ZipStream.php
@@ -156,13 +156,13 @@ class ZipStream {
     $this->opt = $opt;
 
     # set large file defaults: size = 20 megabytes, method = store
-    if (!$this->opt['large_file_size'])
+    if (!isset($this->opt['large_file_size']))
       $this->opt['large_file_size'] = 20 * 1024 * 1024;
-    if (!$this->opt['large_file_method'])
+    if (!isset($this->opt['large_file_method']))
       $this->opt['large_file_method'] = 'store';
 
     $this->output_name = $name;
-    if ($name || $opt['send_http_headers'])
+    if ($name || isset($opt['send_http_headers']) || !$opt['send_http_headers'])
       $this->need_headers = true; 
   }
 
@@ -293,7 +293,7 @@ class ZipStream {
     $nlen = strlen($name);
 
     # create dos timestamp
-    $opt['time'] = $opt['time'] ? $opt['time'] : time();
+    $opt['time'] = isset($opt['time']) ? $opt['time'] : time();
     $dts = $this->dostime($opt['time']);
 
     # build file header
@@ -405,7 +405,7 @@ class ZipStream {
     list ($name, $opt, $meth, $crc, $zlen, $len, $ofs) = $args;
 
     # get attributes
-    $comment = $opt['comment'] ? $opt['comment'] : '';
+    $comment = isset($opt['comment']) ? $opt['comment'] : '';
 
     # get dos timestamp
     $dts = $this->dostime($opt['time']);
@@ -448,7 +448,7 @@ class ZipStream {
 
     # grab comment (if specified)
     $comment = '';
-    if ($opt && $opt['comment'])
+    if ($opt && isset($opt['comment']))
       $comment = $opt['comment'];
 
     $fields = array(                # (from V,F of APPNOTE.TXT)
@@ -499,12 +499,12 @@ class ZipStream {
     
     # grab content type from options
     $content_type = 'application/x-zip';
-    if ($opt['content_type'])
+    if (isset($opt['content_type']))
       $content_type = $this->opt['content_type'];
 
     # grab content disposition 
     $disposition = 'attachment';
-    if ($opt['content_disposition'])
+    if (isset($opt['content_disposition']))
       $disposition = $opt['content_disposition'];
 
     if ($this->output_name) 
