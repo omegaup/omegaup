c0af98da1c4997629d1cf3a93cbad5804e6882da
lhchavez <lhchavez@lhchavez.com>
2014-05-26 23:10:06 +0000

    CSP ya funciona para el lockdown mode.

---
+++ frontend/www/js/login.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	function registerAndLogin(){
+		if ($('#reg_pass').val() != $('#reg_pass2').val()) {
+			OmegaUp.ui.error("{#loginPasswordNotEqual#}");
+			return false;
+		}
+
+		if ($('#reg_pass').val().length < 8) {
+			OmegaUp.ui.error("{#loginPasswordTooShort#}");
+			return false;
+		}
+
+		omegaup.createUser(
+			$('#reg_email').val(),
+			$('#reg_username').val(),
+			$('#reg_pass').val(),
+			function (data) { 
+				//registration callback
+				if (data.status != 'ok') {
+					OmegaUp.ui.error(data.error);	
+				} else {
+					$("#user").val($('#reg_email').val());
+					$("#pass").val($('#reg_pass').val());
+					$("#login_form").submit();
+				}
+			}
+		);
+		return false; // Prevent form submission
+	}
+	
+	$("#register-form").submit(registerAndLogin);
+});
