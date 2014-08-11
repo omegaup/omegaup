0a9c177b90ee802d3099aab0a90621a67af6e36f
Rafael Rendón Pablo <rafaelrendonpablo@gmail.com>
2014-08-11 14:32:56 -0500

    Renombra banderas de alerta (2).

--- frontend/www/contests.php
+++ frontend/www/contests.php
@@ -6,8 +6,8 @@
 	// suggesting to contribute to the community by releasing the material to
 	// the public. This flag ensures that this alert is shown only once per
 	// session, the first time the user visits the "My contests" page.
-	if ($session['valid'] && $session['private_contests_count'] > 0 && !isset($_SESSION['free_contests_alert'])) {
-		$_SESSION['free_contests_alert'] = 1;
+	if ($session['valid'] && $session['private_contests_count'] > 0 && !isset($_SESSION['private_contests_alert'])) {
+		$_SESSION['private_contests_alert'] = 1;
 		$smarty->assign("PRIVATE_CONTESTS_ALERT", 1);
 	} else {
 		$smarty->assign("PRIVATE_CONTESTS_ALERT", 0);
