0161f9bf342745fd827a8748e2518cb703ad2661
lhchavez <lhchavez@lhchavez.com>
2015-09-12 20:26:05 +0000

    Más código muerto

--- frontend/server/controllers/ProblemController.php
+++ frontend/server/controllers/ProblemController.php
@@ -21,55 +21,6 @@ class ProblemController extends Controller {
 	}
 
 	/**
-	 * Adds a problem from a remote server to the list of known
-	 * problems.
-	 *
-	 * @return bool|string True if problem was added successfully.
-	 *                     Error message, otherwise.
-	 * @todo Add JSON responses
-	 */
-	public static function addRemoteProblem(
-	$judge
-	, $remote_id
-	, $public = true
-	) {
-
-		try {
-			$prob = new Problems();
-			// Validating that $judge is in fact a valid judge happens
-			// in setServidor
-			$prob->setServidor($judge);
-			$prob->setIdRemoto($remote_id);
-			$prob->setPublico($public);
-
-			ProblemsDAO::save($prob);
-		} catch (Exception $e) {
-			return $e->getMessage();
-		}
-
-		// If we make it this far, the problem was added successfully
-		return true;
-	}
-
-	/**
-	 * Adds one or more tags to a problem.
-	 * This function should allow tagging multiple problems with multiple tags
-	 * in a single function call. Arguments may be single values or arrays.
-	 *
-	 * @param mixed $problem_id The id may be a numeric problem_id or
-	 *              a problem alias.
-	 * @param int|array $tag_id Id of the tag (or tags) to be added.
-	 *
-	 * @return bool True if successful.
-	 */
-	public static function addTags(
-	$problem_id
-	, $tag_id
-	) {
-		
-	}
-
-	/**
 	 * Validates a Create or Update Problem API request
 	 * 
 	 * @param Request $r
