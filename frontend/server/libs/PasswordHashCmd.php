5cf76b4c083f074302ccc2b1de25932ad4eb9977
lhchavez <lhchavez@lhchavez.com>
2015-12-16 05:09:21 +0000

    Agregada una utilidad para generar contraseñas desde consola

---
+++ frontend/server/libs/PasswordHashCmd.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("SecurityTools.php");
+
+function randomString($length) {
+	$chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+	$str = "";
+	$size = strlen($chars);
+	for ($i = 0; $i < $length; $i++) {
+		$str .= $chars[rand(0, $size - 1)];
+	}
+
+	return $str;
+}
+
+
+$password = randomString(8);
+echo "$password\n";
+echo SecurityTools::hashString($password) . "\n";
