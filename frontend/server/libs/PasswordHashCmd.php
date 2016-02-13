b5ae28b247186e314b319e516f50667fbbf9ad79
Joemmanuel <joemmanuel@gmail.com>
2016-02-13 11:49:54 -0800

    Quitando un espacio extra (style)...

--- frontend/server/libs/PasswordHashCmd.php
+++ frontend/server/libs/PasswordHashCmd.php
@@ -3,7 +3,6 @@
 require_once('SecurityTools.php');
 require_once('Controller.php');
 
-
 $password = Controller::randomString(8);
 echo "$password\n";
 echo SecurityTools::hashString($password) . "\n";
