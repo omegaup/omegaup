495d53b7038f0a5e674fa314b2bea25b04e5b725
Alan Gonzalez <alanboy@alanboy.net>
2016-03-07 05:39:34 +0000

    arena/[contest]/print abre el concurso para que los admins puedan imprimir problemas antes de que el concurso inicie

--- frontend/www/arena/print.php
+++ frontend/www/arena/print.php
@@ -1,12 +1,21 @@
 <?php
 require_once('../../server/bootstrap.php');
-require_once('../api/ApiCaller.php');
-$r = new Request(array(
-        'contest_alias' => $_REQUEST['alias'],
-        'auth_token' => $smarty->getTemplateVars('CURRENT_USER_AUTH_TOKEN'),
-    ));
-$r->method = 'ContestController::apiDetails';
-$contest = ApiCaller::call($r);
+
+try {
+    $r = new Request(array(
+            'contest_alias' => $_REQUEST['alias'],
+            'auth_token' => array_key_exists('ouat', $_REQUEST) ? $_REQUEST['ouat'] : null,
+        ));
+
+    // Open the contest for the current user
+    $contest = ContestController::apiOpen($r);
+
+    // with the contest opened, request the contest details
+    $contest = ContestController::apiDetails($r);
+} catch (Exception $e) {
+    header('HTTP/1.1 404 Not Found');
+    die(file_get_contents('../404.html'));
+}
 
 $problems = $contest['problems'];
 foreach ($problems as &$problem) {
