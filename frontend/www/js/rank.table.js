ad16cd76f4ea1767c431df44b89f2056360f1f31
Sergio Marquez <samdsmx@gmail.com>
2016-05-20 17:17:02 +0000

    Rediseño de tabla del ranking

--- frontend/www/js/rank.table.js
+++ frontend/www/js/rank.table.js
@@ -9,11 +9,14 @@
 			function(result) {
 				var html = "";
 				for (a = 0; a < result.rank.length; a++) {
-					html += "<tr><td>" + result.rank[a].rank + "</td><td class=\"forcebreaks forcebreaks-top-5\"><b><a href=/profile/"+ result.rank[a].username + ">"
-					+ ""+result.rank[a].username + "</a></b>" + getFlag(result.rank[a]['country_id'])
+					html += "<tr>"
+                                        + "<td>" + result.rank[a].rank + "</td>" 
+                                        + "<td style='width:5px'>" + getFlag(result.rank[a]['country_id']) + "</td>"
+     					+ "<td class=\"forcebreaks forcebreaks-top-5\">"  
+					+ "<b><a href=/profile/" + result.rank[a].username + ">" + result.rank[a].username + "</a></b>"
 					+ "<br/>" + (result.rank[a].name == null ? "&nbsp;" : result.rank[a].name) + "</td>"
-					+ "<td>"+result.rank[a].score + "</td>"
-					+ (is_index === true ? "" : ("<td>"+result.rank[a].problems_solved + "</td>"))
+					+ "<td class='numericColumn'>" + result.rank[a].score + "</td>"
+					+ (is_index === true ? "" : ("<td class='numericColumn'>"+result.rank[a].problems_solved + "</td>"))
 					+ "</tr>";
 				}
 				$("#rank-by-problems-solved>tbody").append(html);
