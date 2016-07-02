c2ed3c885ef38875e195f4cb8c159b0391c74559
Alan Gonzalez <alanboy@alanboy.net>
2016-07-02 17:08:03 +0000

    quotes

--- frontend/server/bootstrap.php
+++ frontend/server/bootstrap.php
@@ -21,7 +21,7 @@ if (!(defined('IS_TEST') && IS_TEST === true)) {
     <body style="padding:5px">
         <h1>No config file.</h1>
         <p>You are missing the config file. These are the default values:</p>
-        <pre class='code' style='margin: 3em; border: 1px solid #000; background: #ccc;'>
+        <pre class="code" style="margin: 3em; border: 1px solid #000; background: #ccc;">
         <?php echo htmlspecialchars(file_get_contents(__DIR__ . '/config.default.php')); ?>
         </pre>
         <p>Create a file called <code>config.php</code> &emdash; the settings there will
