63fdfa20cb145fb0f3fb8a43cf17dcfbe115c02e
Joemmanuel <joemmanuel@gmail.com>
2016-09-05 18:30:50 +0000

    Replace filename/url

--- frontend/server/libs/UrlHelper.php
+++ frontend/server/libs/UrlHelper.php
@@ -4,11 +4,10 @@ class UrlHelper {
     /**
      * Wrapper of file_get_contents
      *
-     * @param string $filename
-     * @param bool $use_include_path
+     * @param string $url
      * @param resource $context
      */
-    public function fetchUrl($filename, $context = null) {
-        return file_get_contents($filename, false /*use_include_path*/, $context);
+    public function fetchUrl($url, $context = null) {
+        return file_get_contents($url, false /*use_include_path*/, $context);
     }
 }
