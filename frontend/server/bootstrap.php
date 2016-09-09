78f062fa1ee736a90688e0a98dee9ca2e33efa59
lhchavez <lhchavez@lhchavez.com>
2016-09-09 04:25:25 +0000

    Hotfix de la migración de las sesiones
    Se me fue una variable y eso estaba rompiendo el perfil.

--- frontend/server/bootstrap.php
+++ frontend/server/bootstrap.php
@@ -190,7 +190,7 @@ if (/* do we need smarty to load? */true && !(defined('IS_TEST') && IS_TEST === 
         $smarty->assign('LOGGED_IN', '1');
         UITools::$IsLoggedIn = true;
 
-        $smarty->assign('CURRENT_USER_USERNAME', $session['username']);
+        $smarty->assign('CURRENT_USER_USERNAME', $session['user']->username);
         $smarty->assign('CURRENT_USER_EMAIL', $session['email']);
         $smarty->assign('CURRENT_USER_IS_EMAIL_VERIFIED', $session['user']->verified);
         $smarty->assign('CURRENT_USER_IS_ADMIN', $session['is_admin']);
