107c7ff27cbbba5a24a3d89f8b9a20afbe731b74
lhchavez <lhchavez@lhchavez.com>
2016-09-19 21:55:21 -0700

    Documentando mejor los git hooks (#885)
    En un comentario de la revisión pasada, se sugirió documentar mejor los
    git hooks. Este cambio hace eso, los convierte a Python3 y de paso
    arregla un montón de errores que tenían para hacerlos más robustos.
    
    También se arregló un error de lint que se le pasaba al script anterior.

--- stuff/php-format.py
+++ stuff/php-format.py
@@ -1,72 +1,96 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
+'''
+Runs the PHP Code Beautifier against files that will be uploaded through git.
+'''
 
 import argparse
 import git_tools
+import io
 import os.path
 import subprocess
 import sys
 
-IGNORE_LIST = ['frontend/server/libs/dao/base/',
-	'frontend/server/libs/dao/Estructura.php', 'frontend/server/libs/third_party/',
-	'frontend/server/config.php', 'frontend/server/test/test_config.php',
-	'frontend/tests/templates_c']
-
-class colors:
-	HEADER = '\033[95m'
-	OKGREEN = '\033[92m'
-	FAIL = '\033[91m'
-	NORMAL = '\033[0m'
+from git_tools import COLORS
 
 def which(program):
-	for path in os.environ["PATH"].split(os.pathsep):
-		exe_file = os.path.join(path.strip('"'), program)
-		if os.path.isfile(exe_file) and os.access(exe_file, os.X_OK):
-			return exe_file
-	raise Exception('`%s` not found' % program)
+  '''Looks for |program| in $PATH. Similar to UNIX's `which` command.'''
+  for path in os.environ["PATH"].split(os.pathsep):
+    exe_file = os.path.join(path.strip('"'), program)
+    if os.path.isfile(exe_file) and os.access(exe_file, os.X_OK):
+      return exe_file
+  raise Exception('`%s` not found' % program)
 
 def main():
-	parser = argparse.ArgumentParser(description='PHP linter')
-	parser.add_argument('--from-commit', dest='from_commit', type=str,
-			help='Only include files changed from a certain commit')
-	parser.add_argument('--validate', dest='validate', action='store_true',
-			default=False, help='Only validates, does not make changes')
+  parser = argparse.ArgumentParser(description='PHP linter')
+  subparsers = parser.add_subparsers(dest='tool')
 
-	args = parser.parse_args()
+  validate_parser = subparsers.add_parser('validate',
+      help='Only validates, does not make changes')
+  validate_parser.add_argument('commits', metavar='commit', nargs='*',
+      type=str, help='Only include files changed between commits')
 
-	root_dir = git_tools.root_dir()
-	changed_files = git_tools.changed_files(args.from_commit)
+  fix_parser = subparsers.add_parser('fix',
+      help='Fixes all violations and leaves the results in the working tree.')
+  fix_parser.add_argument('commits', metavar='commit', nargs='*',
+      type=str, help='Only include files changed between commits')
 
-	if not changed_files:
-		return 0
+  args = parser.parse_args()
+  if not git_tools.validate_args(args):
+    return 1
 
-	if args.validate:
-		# TODO(lhchavez): Remove the -n to also enforce being warning-free.
-		phpcs_args = [which('phpcs'), '-n', '-s']
-	else:
-		phpcs_args = [which('phpcbf')]
+  changed_files = git_tools.changed_files(args.commits,
+      whitelist=[br'^frontend.*\.php$'],
+      blacklist=[br'.*third_party.*', br'.*dao/base.*',
+                 br'frontend/server/libs/dao/Estructura.php'])
+  if not changed_files:
+    return 0
 
-	phpcs_args += ['--extensions=php', '--encoding=utf-8', '--standard=%s' %
-			os.path.join(root_dir, 'stuff/omegaup-standard.xml'), '--ignore=%s' %
-			','.join([os.path.join(root_dir, f) for f in IGNORE_LIST])] + changed_files
+  root = git_tools.root_dir()
+  phpcs_args = [which('phpcbf'), '--encoding=utf-8',
+      '--standard=%s' % os.path.join(root, 'stuff/omegaup-standard.xml')]
 
-	errors = False
+  validate_only = args.tool == 'validate'
+  validation_passed = True
 
-	try:
-		subprocess.check_call(phpcs_args)
-	except subprocess.CalledProcessError:
-		errors = True
+  for filename in changed_files:
+    filename = str(filename, encoding='utf-8')
+    contents = git_tools.file_at_commit(args.commits[1], filename)
+    cmd = phpcs_args + ['--stdin-path=%s' % filename]
+    with subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+        cwd=root) as p:
+      replaced = p.communicate(contents)[0]
+      if p.returncode != 0 and not replaced:
+        # phpcbf returns 1 if there was no change to the file. If there was an
+        # actual error, there won't be anything in stdout.
+        validation_passed = False
+        print('Execution of "%s" %sfailed with return code %d%s.' % (
+              ' '.join(cmd), COLORS.FAIL, COLORS.NORMAL), file=sys.stderr)
+    if contents != replaced:
+      validation_passed = False
+      if validate_only:
+        print('File %s%s%s has %slint errors%s.' % (COLORS.HEADER, filename,
+          COLORS.NORMAL, COLORS.FAIL, COLORS.NORMAL), file=sys.stderr)
+      else:
+        print('Fixing %s%s%s for %slint errors%s.' % (COLORS.HEADER, filename,
+          COLORS.NORMAL, COLORS.FAIL, COLORS.NORMAL), file=sys.stderr)
+        with open(os.path.join(root, filename), 'wb') as f:
+          f.write(replaced)
 
-	if errors:
-		if args.validate:
-			if args.from_commit:
-				extra_args = ' --from-commit=%s' % args.from_commit
-			else:
-				extra_args = ''
-			print >> sys.stderr, '%sPHP validation errors.%s Please run `%s%s` to fix them.' % (colors.FAIL, colors.NORMAL, sys.argv[0], extra_args)
-		return 1
-	return 0
+  if not validation_passed:
+    if validate_only:
+      print('%sPHP validation errors.%s '
+            'Please run `%s fix %s` to fix them.' % (git_tools.COLORS.FAIL,
+              git_tools.COLORS.NORMAL, sys.argv[0], ' '.join(args.commits)),
+              file=sys.stderr)
+    else:
+      print('Files written to working directory. '
+          '%sPlease commit them before pushing.%s' % (COLORS.HEADER,
+          COLORS.NORMAL), file=sys.stderr)
+    return 1
+  return 0
 
 if __name__ == '__main__':
-	sys.exit(main())
+  sys.exit(main())
 
-# vim: noexpandtab shiftwidth=2 tabstop=2
+# vim: expandtab shiftwidth=2 tabstop=2
