0c6ab4cc142fa29cbf46b92cda767507f10cc81d
Joemmanuel <joemmanuel@gmail.com>
2016-09-30 18:51:58 +0000

    Whitespace fix

--- frontend/server/controllers/CourseController.php
+++ frontend/server/controllers/CourseController.php
@@ -1,7 +1,6 @@
 <?php
 
 class CourseController extends Controller {
-
     /**
      * Validates request for Assignment on Course operations
      *
--- frontend/tests/controllers/CourseCreateTest.php
+++ frontend/tests/controllers/CourseCreateTest.php
@@ -1,7 +1,6 @@
 <?php
 
 class CourseCreateTest extends OmegaupTestCase {
-
     /**
      * Create course helper
      */
