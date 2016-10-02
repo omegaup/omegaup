4cefd1e754efb7729d1a27eea13d7aec84c9b793
lhchavez <lhchavez@lhchavez.com>
2016-10-02 08:08:13 -0700

    Introducción de Problemsets y ACLs (#889)
    Este es el primer cambio para la migración de problemsets. Es
    pre-requisito de omegaUp for Schools.

--- frontend/database/schema.sql
+++ frontend/database/schema.sql
@@ -97,6 +97,47 @@ CREATE TABLE IF NOT EXISTS `Coder_Of_The_Month` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `Problemsets`
+--
+
+CREATE TABLE IF NOT EXISTS `Problemsets` (
+  `problemset_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'El identificador único para cada conjunto de problemas',
+  `access_mode` enum('private', 'public', 'registration') NOT NULL DEFAULT 'public' COMMENT 'La modalidad de acceso a este conjunto de problemas',
+  `languages` set('c','cpp','java','py','rb','pl','cs','pas','kp','kj','cat','hs','cpp11') DEFAULT NULL COMMENT 'Un filtro (opcional) de qué lenguajes se pueden usar para resolver los problemas',
+  PRIMARY KEY (`problemset_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Conjunto de problemas.';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Problemset_Problems`
+--
+
+CREATE TABLE IF NOT EXISTS `Problemset_Problems` (
+  `problemset_id` int(11) NOT NULL,
+  `problem_id` int(11) NOT NULL,
+  `points` double NOT NULL DEFAULT '1',
+  `order` INT NOT NULL DEFAULT  '1' COMMENT 'Define el orden de aparición de los problemas en un concurso',
+  PRIMARY KEY (`problemset_id`,`problem_id`),
+  KEY `problemset_id` (`problemset_id`),
+  KEY `problem_id` (`problem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Los problemas de cada conjunto';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ACLs`
+--
+
+CREATE TABLE IF NOT EXISTS `ACLs` (
+  `acl_id` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL COMMENT 'El usuario que creó el objeto y que tiene un rol de administrador implícito',
+  PRIMARY KEY (`acl_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Lista de control de acceso.';
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `Contests`
 --
 
@@ -753,6 +794,19 @@ ALTER TABLE `Clarifications`
 --
 ALTER TABLE `Coder_Of_The_Month`
   ADD CONSTRAINT `fk_cotmu_user_id` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `Problemset_Problems`
+--
+ALTER TABLE `Problemset_Problems`
+  ADD CONSTRAINT `fk_ppp_problemset_id` FOREIGN KEY (`problemset_id`) REFERENCES `Problemsets` (`problemset_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ppp_problem_id` FOREIGN KEY (`problem_id`) REFERENCES `Problems` (`problem_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `ACLs`
+--
+ALTER TABLE `ACLs`
+  ADD CONSTRAINT `fk_au_owner_id` FOREIGN KEY (`owner_id`) REFERENCES `Users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `Contests`
---
+++ frontend/server/libs/dao/Problemset_Problems.dao.php
@@ -0,0 +1,14 @@
+<?php
+
+include('base/Problemset_Problems.dao.base.php');
+include('base/Problemset_Problems.vo.base.php');
+/** ProblemsetProblems Data Access Object (DAO).
+  *
+  * Esta clase contiene toda la manipulacion de bases de datos que se necesita para
+  * almacenar de forma permanente y recuperar instancias de objetos {@link ProblemsetProblems }.
+  * @access public
+  *
+  */
+class ProblemsetProblemsDAO extends ProblemsetProblemsDAOBase
+{
+}
