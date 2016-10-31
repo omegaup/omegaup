1378e9c9d97c062d3aec60b21e752cc45797d38c
Edgar Hernandez <edhzsz@gmail.com>
2016-10-31 22:20:41 +0100

    Agrega el jquery_helper

---
+++ frontend/www/js/test_helpers/jquery_helper.js
@@ -0,0 +1,12 @@
+(function jquery_helper() {
+  'use strict';
+
+  if(typeof window === 'undefined') {
+    var jsdom = require('jsdom');
+    var window = jsdom.jsdom().defaultView;
+    global.document = window.document;
+
+    var jQuery = require('jquery')(window);
+    global.jQuery = global.$ = jQuery;
+  }
+})();
