2bb446a636276fc195ce8d0b88e64e82ced82b25
Edgar Hernandez <edhzsz@gmail.com>
2016-11-02 11:07:10 +0100

    Corrige errores de whitespace

--- frontend/www/tests/require_helper.js
+++ frontend/www/tests/require_helper.js
@@ -1,8 +1,7 @@
-if(typeof require === 'undefined') {
-  window.require = function require() {
+if (typeof require === 'undefined') {
+  window.require = function () {
     // do nothing
     // allows jasmine specs to require a node module on node
     // without failing on the browser
   } 
 }
-
