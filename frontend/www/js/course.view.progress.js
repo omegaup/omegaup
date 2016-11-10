7ee602f67f4d8d4d0de7282e18f65672fd8942f2
Joemmanuel <joemmanuel@gmail.com>
2016-11-10 14:32:09 +0000

    Fix js

--- frontend/www/js/course.view.progress.js
+++ frontend/www/js/course.view.progress.js
@@ -1,30 +1,34 @@
-$('document').ready(function() {
-    var courseAlias = /\/course\/([^\/]+)\/edit\/?.*/.exec(window.location.pathname)[1];
+$('document')
+    .ready(function() {
+      var courseAlias =
+          /\/course\/([^\/]+)\/edit\/?.*/.exec(window.location.pathname)[1];
 
-    ko.bindingProvider.instance =
-        new ko.secureBindingsProvider({attribute: 'data-bind'});
+      ko.bindingProvider.instance =
+          new ko.secureBindingsProvider({attribute: 'data-bind'});
 
-    omegaup.API.getCourseStudentList(courseAlias, function(data){
+      omegaup.API.getCourseStudentList(courseAlias, function(data) {
         if (data.status != 'ok') {
-            omegaup.UI.error(data.error);
-            return;
+          omegaup.UI.error(data.error);
+          return;
         }
 
-        var koStudentsList = {
-            getStudentsList: ko.observableArray()
-        };
+        var koStudentsList = {getStudentsList: ko.observableArray()};
         for (var i = 0; i < data['students'].length; ++i) {
-            var student = data['students'][i];
-            student.profileURL = '/profile/' + student.username,
+          var student = data['students'][i];
+          student.profileURL = '/profile/' + student.username,
 
-            totalHomeworks = (data['counts']['homework'] != null) ? data['counts']['homework'] : 0;
-            totalTests = (data['counts']['test'] != null) ? data['counts']['test'] : 0;
-            student.totalHomeworks = student.count_homeworks_done + '/' + totalHomeworks;
-            student.totalTests = student.count_tests_done + '/' + totalTests;
+          totalHomeworks = (data['counts']['homework'] != null) ?
+                               data['counts']['homework'] :
+                               0;
+          totalTests =
+              (data['counts']['test'] != null) ? data['counts']['test'] : 0;
+          student.totalHomeworks =
+              student.count_homeworks_done + '/' + totalHomeworks;
+          student.totalTests = student.count_tests_done + '/' + totalTests;
 
-            koStudentsList['getStudentsList'].push(student);
+          koStudentsList['getStudentsList'].push(student);
         }
 
         ko.applyBindings(koStudentsList);
+      });
     });
-});
