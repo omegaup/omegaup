18dafe91fea1c352cff08108d2e05390ea14d4b8
Joemmanuel <joemmanuel@gmail.com>
2016-11-24 17:53:28 +0000

    Fix EvalError

--- frontend/www/ux/arena.js
+++ frontend/www/ux/arena.js
@@ -1,48 +1,42 @@
-$(document)
-    .ready(function() {
-      Date.setLocale(omegaup.T.locale);
+omegaup.OmegaUp.on('ready', function() {
+  Date.setLocale(omegaup.T.locale);
 
-      var contestLists = [
-        // List Id, Active, Recommended, List header
-        [
-          '#current-contests',
-          'ACTIVE',
-          'NOT_RECOMMENDED',
-          omegaup.T.arenaCurrentContests
-        ],
-        [
-          '#recommended-current-contests',
-          'ACTIVE',
-          'RECOMMENDED',
-          omegaup.T.arenaRecommendedCurrentContests
-        ],
-        [
-          '#past-contests',
-          'PAST',
-          'NOT_RECOMMENDED',
-          omegaup.T.arenaOldContests
-        ],
-        [
-          '#recommended-past-contests',
-          'PAST',
-          'RECOMMENDED',
-          omegaup.T.arenaRecommendedOldContests
-        ],
-      ];
+  var contestLists = [
+    // List Id, Active, Recommended, List header
+    [
+      '#current-contests',
+      'ACTIVE',
+      'NOT_RECOMMENDED',
+      omegaup.T.arenaCurrentContests
+    ],
+    [
+      '#recommended-current-contests',
+      'ACTIVE',
+      'RECOMMENDED',
+      omegaup.T.arenaRecommendedCurrentContests
+    ],
+    ['#past-contests', 'PAST', 'NOT_RECOMMENDED', omegaup.T.arenaOldContests],
+    [
+      '#recommended-past-contests',
+      'PAST',
+      'RECOMMENDED',
+      omegaup.T.arenaRecommendedOldContests
+    ],
+  ];
 
-      var requests = [];
-      for (var i = 0, len = contestLists.length; i < len; i++) {
-        var contestList = new omegaup.arena.ContestList(
-            contestLists[i][0],
-            {active: contestLists[i][1], recommended: contestLists[i][2]},
-            {header: contestLists[i][3]});
-        requests.push(contestList.deferred);
-      }
+  var requests = [];
+  for (var i = 0, len = contestLists.length; i < len; i++) {
+    var contestList = new omegaup.arena.ContestList(
+        contestLists[i][0],
+        {active: contestLists[i][1], recommended: contestLists[i][2]},
+        {header: contestLists[i][3]});
+    requests.push(contestList.deferred);
+  }
 
-      // Wait until all of the calls above finish before showing the contents.
-      $.when.apply($, requests)
-          .done(function() {
-            $('#root').show();
-            $('#loading').fadeOut('slow');
-          });
-    });
+  // Wait until all of the calls above finish before showing the contents.
+  $.when.apply($, requests)
+      .done(function() {
+        $('#root').show();
+        $('#loading').fadeOut('slow');
+      });
+});
