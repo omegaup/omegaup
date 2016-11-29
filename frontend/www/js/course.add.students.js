be95b46258a28a605bac34bae6dd4e96927895bf
Joemmanuel <joemmanuel@gmail.com>
2016-11-29 05:59:34 +0000

    OmegaUp.on ready y bugfix en refresh

--- frontend/www/js/course.add.students.js
+++ frontend/www/js/course.add.students.js
@@ -1,22 +1,21 @@
-$('document')
-    .ready(function() {
-      $('#add-member-form')
-          .submit(function() {
-            var courseAlias = /\/course\/([^\/]+)\/edit\/?.*/.exec(
-                window.location.pathname)[1];
+omegaup.OmegaUp.on('ready', function() {
+  $('#add-member-form')
+      .submit(function() {
+        var courseAlias =
+            /\/course\/([^\/]+)\/edit\/?.*/.exec(window.location.pathname)[1];
 
-            omegaup.API.addStudentToCourse({
-                         course_alias: courseAlias,
-                         username: $('#member-username').val()
-                       })
-                .then(function(data) {
-                  if (data.status != 'ok') {
-                    omegaup.UI.error(data.error || 'error');
-                    return;
-                  }
-                  omegaup.UI.success(omegaup.T.courseStudentAdded);
-                });
-            refreshStudentList();
-            return false;
-          });
-    });
+        omegaup.API.addStudentToCourse({
+                     course_alias: courseAlias,
+                     username: $('#member-username').val()
+                   })
+            .then(function(data) {
+              if (data.status != 'ok') {
+                omegaup.UI.error(data.error || 'error');
+                return;
+              }
+              refreshStudentList();
+              omegaup.UI.success(omegaup.T.courseStudentAdded);
+            });
+        return false;
+      });
+});
