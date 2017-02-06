011fb3f08385a8a4bd539e3dd74b6eeb45ef8f47
lhchavez <lhchavez@lhchavez.com>
2017-02-06 03:20:42 +0000

    Resulta que jQuery 1.10 no tiene `.catch`
    Usemos `.then(..., ...)` por lo mientras.

--- frontend/www/js/course.add.students.js
+++ frontend/www/js/course.add.students.js
@@ -1,17 +1,19 @@
 omegaup.OmegaUp.on('ready', function() {
   $('#add-member-form')
-      .submit(function() {
+      .submit(function(ev) {
+        ev.preventDefault();
         var courseAlias =
             /\/course\/([^\/]+)\/edit\/?.*/.exec(window.location.pathname)[1];
 
         omegaup.API.addStudentToCourse({
-                     course_alias: courseAlias,
-                     username: $('#member-username').val()
-                   })
-            .then(function(data) {
-              refreshStudentList();
-              omegaup.UI.success(omegaup.T.courseStudentAdded);
-            });
-        return false;
+          course_alias: courseAlias,
+          username: $('#member-username').val()
+        })
+        .then(function(data) {
+          refreshStudentList();
+          omegaup.UI.success(omegaup.T.courseStudentAdded);
+        }, function(data) {
+          omegaup.UI.error(data.error);
+        });
       });
 });
