26d372b8f7e349d5d17c15933b412ed2e8e0cd63
lhchavez <lhchavez@lhchavez.com>
2017-02-24 20:32:02 +0000

    Vue-ificando más edición de cursos
    Este cambio agrega eventos y acciones en `mounted`.

---
+++ frontend/www/js/omegaup/components/course/AddStudents.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="omegaup-course-addstudent panel">
+    <div class="panel-body">
+      <form class="form" v-on:submit.prevent="onAddStudent">
+        <div class="form-group">
+          <label for="member-username">{{ T.wordsStudent }}</label>
+          <span data-toggle="tooltip" data-placement="top" title="Add students using they username in omegaup"  class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>
+          <input id="student-username" v-model="studentUsername" type="text" size="20" class="form-control" autocomplete="off" />
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary" type="submit">{{ T.wordsAddStudent }}</button>
+        </div>
+      </form>
+      <div>
+          <table class="table table-striped table-over">
+            <thead>
+              <th>{{ T.wordsUser }}</th>
+              <th>{{ T.contestEditRegisteredAdminDelete }}</th>
+            </thead>
+            <tbody v-for="student in students">
+              <tr>
+                <td><a v-bind:href="'/profile/' + student.username + '/'">{{ student.name || student.username }}</a></td>
+                <td><button type="button" class="close" v-on:click="onRemove(student)">&times;</button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UI from '../../ui.js';
+
+export default {
+  props: {
+    T: Object,
+    students: Array,
+  },
+  data: function() {
+    return {
+      studentUsername: '',
+    };
+  },
+  mounted: function() {
+    UI.userTypeahead($('#student-username'));
+  },
+  methods: {
+    onAddStudent: function() {
+      this.$emit('add-student', this.studentUsername);
+    },
+    onRemove: function(student) {
+      this.$emit('remove', student);
+    },
+  },
+};
+</script>
