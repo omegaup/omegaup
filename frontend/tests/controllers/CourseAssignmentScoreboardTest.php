c12404a1817b4af641bcd98d0eea658779e80253
Joemmanuel Ponce <joemmanuel@gmail.com>
2017-03-31 23:04:09 +0000

    Fixed /opt/omegaup/stuff/php-format.py lints

--- frontend/tests/controllers/CourseAssignmentScoreboardTest.php
+++ frontend/tests/controllers/CourseAssignmentScoreboardTest.php
@@ -6,19 +6,19 @@
  */
 
 class CourseAssignmentScoreboardTest extends OmegaupTestCase {
-	/**
-	 * Get score of a given assignment happy path
-	 */
-	public function testGetAssignmentScoreboard() {
-		$courseData = CoursesFactory::createCourseWithOneAssignment();
-		$problemsInAssignment = 3;
-		$studentsInCourse = 5;
+    /**
+     * Get score of a given assignment happy path
+     */
+    public function testGetAssignmentScoreboard() {
+        $courseData = CoursesFactory::createCourseWithOneAssignment();
+        $problemsInAssignment = 3;
+        $studentsInCourse = 5;
 
-		// Prepare assignment. Create problems
-		$adminLogin = self::login($courseData['admin']);
-		$problemAssignmentsMap = [];
-		for ($i = 0; $i < $problemsInAssignment; $i++) {
-			$problemData = ProblemsFactory::createProblem();
+        // Prepare assignment. Create problems
+        $adminLogin = self::login($courseData['admin']);
+        $problemAssignmentsMap = [];
+        for ($i = 0; $i < $problemsInAssignment; $i++) {
+            $problemData = ProblemsFactory::createProblem();
 
             CourseController::apiAddProblem(new Request([
                 'auth_token' => $adminLogin->auth_token,
@@ -28,9 +28,9 @@ class CourseAssignmentScoreboardTest extends OmegaupTestCase {
             ]));
 
             $problemAssignmentsMap[$courseData['assignment_alias']][] = $problemData;
-		}
+        }
 
-		// Create & add students to course
+        // Create & add students to course
         $students = [];
         for ($i = 0; $i < $studentsInCourse; $i++) {
             $students[] = CoursesFactory::addStudentToCourse($courseData);
@@ -39,28 +39,28 @@ class CourseAssignmentScoreboardTest extends OmegaupTestCase {
         // Generate runs
         $expectedScores = CoursesFactory::submitRunsToAssignmentsInCourse($courseData, $students, [$courseData['assignment_alias']], $problemAssignmentsMap);
 
-		// Call API
-		$adminLogin = self::login($courseData['admin']);
-		$response = CourseController::apiScoreboard(new Request([
+        // Call API
+        $adminLogin = self::login($courseData['admin']);
+        $response = CourseController::apiScoreboard(new Request([
             'auth_token' => $adminLogin->auth_token,
             'course_alias' => $courseData['course_alias'],
             'assignment_alias' => $courseData['assignment_alias']
         ]));
 
-		// Validation
-		array_multisort(array_values($expectedScores), SORT_DESC, array_keys($expectedScores), SORT_ASC, $expectedScores);
-		$expectedPlace = 0;
-		$lastScore = 0;
-		$i = 0;
-		foreach ($expectedScores as $username => $score) {
-			if ($lastScore != $score) {
-				$expectedPlace = $i + 1;
-				$lastScore = $score;
-			}
+        // Validation
+        array_multisort(array_values($expectedScores), SORT_DESC, array_keys($expectedScores), SORT_ASC, $expectedScores);
+        $expectedPlace = 0;
+        $lastScore = 0;
+        $i = 0;
+        foreach ($expectedScores as $username => $score) {
+            if ($lastScore != $score) {
+                $expectedPlace = $i + 1;
+                $lastScore = $score;
+            }
 
-			$this->assertEquals($username, $response["ranking"][$i]["username"], "Scoreboard is not properly sorted by contest score.");
-			$this->assertEquals($expectedPlace, $response["ranking"][$i]["place"]);
-			$i++;
-		}
-	}
+            $this->assertEquals($username, $response['ranking'][$i]['username'], 'Scoreboard is not properly sorted by contest score.');
+            $this->assertEquals($expectedPlace, $response['ranking'][$i]['place']);
+            $i++;
+        }
+    }
 }
