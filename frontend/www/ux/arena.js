d474ad152606c9528154c6dcd38da16a521a33a6
Pablo Aguilar <pablo.aguilar@gmail.com>
2017-05-25 18:46:35 -0700

    Seleccionar el primer tab de concursos no-vacio (#1218)
    Fixes: #1153

--- frontend/www/ux/arena.js
+++ frontend/www/ux/arena.js
@@ -1,8 +1,14 @@
 omegaup.OmegaUp.on('ready', function() {
   Date.setLocale(omegaup.T.locale);
 
-  var contestLists = [
+  var contestListConfigs = [
     // List Id, Active, Recommended, List header
+    [
+      '#recommended-current-contests',
+      'ACTIVE',
+      'RECOMMENDED',
+      omegaup.T.arenaRecommendedCurrentContests
+    ],
     [
       '#current-contests',
       'ACTIVE',
@@ -10,32 +16,35 @@ omegaup.OmegaUp.on('ready', function() {
       omegaup.T.arenaCurrentContests
     ],
     [
-      '#recommended-current-contests',
-      'ACTIVE',
-      'RECOMMENDED',
-      omegaup.T.arenaRecommendedCurrentContests
-    ],
-    ['#past-contests', 'PAST', 'NOT_RECOMMENDED', omegaup.T.arenaOldContests],
-    [
       '#recommended-past-contests',
       'PAST',
       'RECOMMENDED',
       omegaup.T.arenaRecommendedOldContests
     ],
+    ['#past-contests', 'PAST', 'NOT_RECOMMENDED', omegaup.T.arenaOldContests],
   ];
 
   var requests = [];
-  for (var i = 0, len = contestLists.length; i < len; i++) {
+  var contestLists = [];
+  for (var i = 0, len = contestListConfigs.length; i < len; i++) {
+    var config = contestListConfigs[i];
     var contestList = new omegaup.arena.ContestList(
-        contestLists[i][0],
-        {active: contestLists[i][1], recommended: contestLists[i][2]},
-        {header: contestLists[i][3]});
+        config[0],
+        {active: config[1], recommended: config[2]},
+        {header: config[3]});
+    contestLists.push(contestList);
     requests.push(contestList.deferred);
   }
 
   // Wait until all of the calls above finish before showing the contents.
   $.when.apply($, requests)
       .done(function() {
+        for (var i = 0, len = contestLists.length; i < len; i++) {
+          if (contestLists[i].totalPages()) {
+            $('.nav-link', $('.nav-item')[i]).tab('show');
+            break;
+          }
+        }
         $('#root').show();
         $('#loading').fadeOut('slow');
       });
