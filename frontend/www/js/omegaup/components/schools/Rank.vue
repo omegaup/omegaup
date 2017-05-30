37ef13b55d2c9f5b4ac897e18c6d747eab485c87
Joemmanuel Ponce <joemmanuel@gmail.com>
2017-05-30 04:26:02 +0000

    linter fix

--- frontend/www/js/omegaup/components/schools/Rank.vue
+++ frontend/www/js/omegaup/components/schools/Rank.vue
@@ -1,33 +1,39 @@
 <template>
-<div class=" panel panel-default" id="school-rank" >
-	<div class="panel-heading">
-		<h3 class="panel-title">{{T.rankHeaderPreCount}} {{rank.length}} {{T.schoolRankHeaderPostCount}}</h3>
-	</div>
-	<div class="panel-body no-padding">
-		<div class="table-responsive">
-			<table class="table table-striped table-hover no-margin" id="school-rank-table">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th colspan="2">{{T.profileSchool}}</th>
-						<th class="numericColumn data-rank">{{T.activeUsers}}</th>
-						<th class="numericColumn data-rank">{{T.profileSolvedProblems}}</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="(school, index) in rank">
-						<td>{{index + 1}}</td>
-						<td colspan="2" class="cell-school-name">
-							<div class="school-name-rank">{{school.name}}</div>
-						</td>
-						<td class="numericColumn data-rank">{{school.distinct_users}}</td>
-						<td class="numericColumn data-rank">{{school.distinct_problems}}</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
-</div>
+  <div class="panel panel-default"
+       id="school-rank">
+    <div class="panel-heading">
+      <h3 class="panel-title">{{T.rankHeaderPreCount}} {{rank.length}}
+      {{T.schoolRankHeaderPostCount}}</h3>
+    </div>
+    <div class="panel-body no-padding">
+      <div class="table-responsive">
+        <table class="table table-striped table-hover no-margin"
+               id="school-rank-table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th colspan="2">{{T.profileSchool}}</th>
+              <th class="numericColumn data-rank">{{T.activeUsers}}</th>
+              <th class="numericColumn data-rank">{{T.profileSolvedProblems}}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(school, index) in rank">
+              <td>{{index + 1}}</td>
+              <td class="cell-school-name"
+                  colspan="2">
+                <div class="school-name-rank">
+                  {{school.name}}
+                </div>
+              </td>
+              <td class="numericColumn data-rank">{{school.distinct_users}}</td>
+              <td class="numericColumn data-rank">{{school.distinct_problems}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -37,11 +43,7 @@ export default {
   props: {
     rank: Array,
   },
-  data: function() {
-    return {
-      T: T
-    };
-  },
+  data: function() { return {T: T};},
 };
 </script>
 
--- frontend/www/js/omegaup/schools/rank.js
+++ frontend/www/js/omegaup/schools/rank.js
@@ -3,30 +3,29 @@ import {API, UI, OmegaUp, T} from '../omegaup.js';
 import Vue from 'vue';
 
 OmegaUp.on('ready', function() {
-	let payload = JSON.parse(document.getElementById('schools-rank-payload').innerText);
-	let schoolsRank = new Vue({
-		el: '#schools-rank',
-		render: function(createElement) {
-			return createElement('omegaup-schools-rank', {
-				props: {rank: this.rank},
-			});
-		},
-		data: {
-			rank: payload,
-		},
-		components: {
- 	     'omegaup-schools-rank': schools_Rank,
-	    },
-	});
+  let payload =
+      JSON.parse(document.getElementById('schools-rank-payload').innerText);
+  let schoolsRank = new Vue({
+    el: '#schools-rank',
+    render: function(createElement) {
+      return createElement('omegaup-schools-rank', {
+        props: {rank: this.rank},
+      });
+    },
+    data: {
+      rank: payload,
+    },
+    components: {
+      'omegaup-schools-rank': schools_Rank,
+    },
+  });
 });
 
-$(document).on('mouseenter', ".school-name-rank", function () {
- var $this = $(this);
- if (this.offsetWidth < this.scrollWidth && !$this.attr('title')) {
-     $this.tooltip({
-         title: $this.text(),
-         placement: "bottom"
-     });
-     $this.tooltip('show');
- }
-});
+$(document)
+    .on('mouseenter', '.school-name-rank', function() {
+      var $this = $(this);
+      if (this.offsetWidth < this.scrollWidth && !$this.attr('title')) {
+        $this.tooltip({title: $this.text(), placement: 'bottom'});
+        $this.tooltip('show');
+      }
+    });
