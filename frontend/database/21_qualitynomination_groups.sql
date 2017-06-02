9f3c2dac13a2030b8aae092decdd2b9cf422fdc2
lhchavez <lhchavez@lhchavez.com>
2017-06-02 20:33:22 +0000

    Agregando los ACLs para los jueces de problemas de calidad
    Este cambio agrega el grupo de usuarios que son jueces
    (`omegaup:quality-reviewer`) y el grupo de usuarios que pueden
    administrarlos (`omegaup:quality-reviewer-admin`). Se eligió el prefijo
    `omegaup:` porque son grupos de sistema y nadie puede crear grupos con
    ese nombre porque `:` no es un caracter válido en un alias.

---
+++ frontend/database/21_qualitynomination_groups.sql
@@ -0,0 +1,23 @@
+-- omegaup:quality-reviewer-admin
+INSERT INTO `ACLs` (`acl_id`, `owner_id`) VALUES (2, 1);
+
+INSERT INTO `Groups` (`acl_id`, `alias`, `name`, `description`) VALUES (
+	2,
+	'omegaup:quality-reviewer-admin',
+	'omegaup:quality-reviewer-admin',
+	'Administradores de omegaup:quality-reviewer'
+);
+
+SET @quality_reviewer_admin_id = LAST_INSERT_ID();
+
+-- omegaup:quality-reviewer
+INSERT INTO `ACLs` (`acl_id`, `owner_id`) VALUES (3, 1);
+
+INSERT INTO `Groups` (`acl_id`, `alias`, `name`, `description`) VALUES (
+	3,
+	'omegaup:quality-reviewer',
+	'omegaup:quality-reviewer',
+	'Administradores de omegaup:quality-reviewer'
+);
+
+INSERT INTO `Group_Roles` VALUES(@quality_reviewer_admin_id, 1, 3);
---
+++ frontend/database/ACLs.md
@@ -0,0 +1,7 @@
+Este archivo tiene la lista de todos los well-known ACLs.
+
+ID    | Descripción
+-----:| ----------------------------------------------
+1     | ACL reservado para el sistema. Los miembros de este ACL tienen capacidad de administradores del sistema.
+2     | `omegaup:quality-reviewer-admin`. Puede administrar `omegaup:quality-reviewer`.
+3     | `omegaup:quality-reviewer`. Los miembros de este grupo son jueces de calidad de problemas.
