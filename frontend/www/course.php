7c649a466282984295907237f09508544cff8d8d
Pablo Aguilar <pablo.aguilar@gmail.com>
2017-06-15 05:58:54 +0000

    Agregar vista del scoreboard de cursos

--- frontend/www/course.php
+++ frontend/www/course.php
@@ -26,7 +26,7 @@ if ($show_intro) {
     $smarty->assign('jsfile', '/ux/assignment.js');
     $smarty->assign('admin', false);
     $smarty->assign('practice', false);
-    $smarty->assign('showRanking', false);
+    $smarty->assign('showRanking', true);
     $smarty->display('../templates/arena.contest.tpl');
 } else {
     $smarty->display('../templates/course.details.tpl');
