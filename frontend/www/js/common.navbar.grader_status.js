5634b1b30cbc91b1d7a14da2941d5ce204cf76a1
Pablo Aguilar <pablo.aguilar@gmail.com>
2017-07-04 08:11:34 +0000

    Mas limpieza de JS
    * Otros cambios de jQuery.html => jQuery.text
    * i18n de algunos mensajes de la prehistoria
    * Opinionated JS changes, pero solo poquitos

--- frontend/www/js/common.navbar.grader_status.js
+++ frontend/www/js/common.navbar.grader_status.js
@@ -1,50 +1,52 @@
 (function() {
+  var graderStatus = $('#grader-status > a');
+  var graderCount = $('#grader-count');
+  var graderDropDown = $('#grader-status .dropdown-menu');
+
   function updateGraderStatus() {
-    $('#grader-status > a')
-        .removeClass('grader-error grader-ok grader-warning grader-unknown');
-    $('#grader-count').html("<img src='/media/waitcircle.gif' />");
-    var html = "<li><a href='/arena/admin/'>Últimos envíos</a></li>";
+    graderStatus.removeClass(
+        'grader-error grader-ok grader-warning grader-unknown');
+    graderCount.html("<img src='/media/waitcircle.gif' />");
+    var html = '<li><a href="/arena/admin/">' +
+               omegaup.T.wordsLatestSubmissions + '</a></li>';
     omegaup.API.Grader.status()
         .then(function(stats) {
           var graderInfo = stats.grader;
           var queueLength = -1;
-
           if (graderInfo.status == 'ok') {
             if (graderInfo.queue) {
               queueLength = graderInfo.queue.run_queue_length +
                             graderInfo.queue.running.length;
             }
             if (queueLength < 5) {
-              $('#grader-status > a').addClass('grader-ok');
+              graderStatus.addClass('grader-ok');
             } else {
-              $('#grader-status > a').addClass('grader-warning');
+              graderStatus.addClass('grader-warning');
             }
-            html += '<li><a href=\"#\">Grader OK</a></li>';
-            html += '<li><a href=\"#\">Broadcaster sockets: ' +
+            html += '<li><a>Grader OK</a></li>';
+            html += '<li><a>Broadcaster sockets: ' +
                     graderInfo.broadcaster_sockets + '</a></li>';
-            html += '<li><a href=\"#\">Embedded runner: ' +
-                    graderInfo.embedded_runner + '</a></li>';
-            html += '<li><a href=\"#\">Queues: <pre style=\"width: 50em;\">' +
+            html += '<li><a>Embedded runner: ' + graderInfo.embedded_runner +
+                    '</a></li>';
+            html += '<li><a>Queues: <pre style="width: 50em;">' +
                     omegaup.UI.prettyPrintJSON(graderInfo.queue) +
                     '</pre></a></li>';
           } else {
-            $('#grader-status > a').addClass('grader-error');
-            html += '<li><a href=\"#\">Grader DOWN</a></li>';
+            graderStatus.addClass('grader-error');
+            html += '<li><a>Grader DOWN</a></li>';
           }
 
-          $('#grader-count').html(queueLength);
-          $('#grader-status .dropdown-menu').html(html);
+          graderCount.text(queueLength);
+          graderDropDown.html(html);
         })
         .fail(function(stats) {
-          $('#grader-status > a').addClass('grader-unknown');
-          html += '<li><a href=\"#\">Grader DOWN</a></li>';
-          html += '<li><a href=\"#\">API api/grader/status call failed:';
-          html += stats.error.replace(/&/g, '&amp;')
-                      .replace(/</g, '&lt;')
-                      .replace(/>/g, '&gt;');
+          graderStatus.addClass('grader-unknown');
+          html += '<li><a>Grader DOWN</a></li>';
+          html += '<li><a>API api/grader/status call failed:';
+          html += omegaup.UI.escape(stats.error);
           html += '</a></li>';
-          $('#grader-count').html('?');
-          $('#grader-status .dropdown-menu').html(html);
+          graderCount.text('?');
+          graderDropDown.html(html);
         });
   }
 
--- frontend/www/js/user.email.edit.js
+++ frontend/www/js/user.email.edit.js
@@ -7,12 +7,9 @@ $('document')
             omegaup.API.User.updateMainEmail({email: $('#email').val()})
                 .then(function(response) {
                   $('#status')
-                      .html(
-                          'Email actualizado correctamente! En unos minutos ' +
-                          'recibirás más instrucciones en tu email. No ' +
-                          'olvides revisar tu carpeta de Spam.');
-                  $('#status').addClass('alert-success');
-                  $('#status').slideDown();
+                      .text(omegaup.T.userEditSuccessfulEmailUpdate)
+                      .addClass('alert-success')
+                      .slideDown();
                 })
                 .fail(omegaup.UI.apiError)
                 .always(function() { $('#wait')
