0c52d80ba7da745b64b46e71787342a740b463f9
alanboy <alanboy@alanboy.net>
2017-08-13 19:41:15 +0000

    Usar solo clases de bootstrap

--- frontend/www/js/omegaup/components/qualitynomination/Details.vue
+++ frontend/www/js/omegaup/components/qualitynomination/Details.vue
@@ -1,51 +1,41 @@
 <template>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h2 class="panel-title">{{T.wordsReviewingProblem}}</h2>
-    </div>
-    <div class="panel-body">
-      <div id="SettingsPage_Content">
-        <ul class="uiList fbSettingsList _4kg _6-h _4ks">
-          <li class="fbSettingsListItem clearfix uiListItem"></li>
-          <li class="fbSettingsListItem clearfix uiListItem">
-            <a class="pvm phs fbSettingsListLink clearfix"><span class=
-            "pls fbSettingsListItemLabel"><strong>{{T.qualityNominationType}}</strong></span>
-            <span class="fbSettingsListItemContent fcg">{{this.nomination}}</span></a>
-          </li>
-          <li class="fbSettingsListItem clearfix uiListItem"></li>
-          <li class="fbSettingsListItem clearfix uiListItem">
-            <a class="pvm phs fbSettingsListLink clearfix"><span class=
-            "pls fbSettingsListItemLabel"><strong>{{T.qualityUserThatNominated}}</strong></span>
-            <span class="fbSettingsListItemContent fcg">{{this.nominator.name}}
-            ({{this.nominator.username}})</span></a>
-          </li>
-          <li class="fbSettingsListItem clearfix uiListItem"></li>
-          <li class="fbSettingsListItem clearfix uiListItem">
-            <a class="pvm phs fbSettingsListLink clearfix"><span class=
-            "pls fbSettingsListItemLabel"><strong>{{T.wordsProblem}}</strong></span> <span class=
-            "fbSettingsListItemContent fcg">{{this.problem.title}}
-            ({{this.problem.alias}})</span></a> <a v-bind:href=
-            "userUrl(this.problem.alias)">{{T.problemEditGoToProblem}}</a>
-          </li>
-          <li class="fbSettingsListItem clearfix uiListItem"></li>
-          <li class="fbSettingsListItem clearfix uiListItem">
-            <a class="pvm phs fbSettingsListLink clearfix"><span class=
-            "pls fbSettingsListItemLabel"><strong>{{T.wordsDetails}}</strong></span> <span class=
-            "fbSettingsListItemContent fcg">{{this.contents}}</span></a>
-          </li>
-          <li class="fbSettingsListItem clearfix uiListItem"></li>
-          <li class="fbSettingsListItem clearfix uiListItem">
-            <a class="pvm phs fbSettingsListLink clearfix"><span class=
-            "pls fbSettingsListItemLabel"><strong>{{T.wordsVerdict}}</strong></span> <span class=
-            "fbSettingsListItemContent fcg">
-            <div class="">
-              <button class="btn btn-danger">{{T.wordsBanProblem}}</button> <button class=
-              "btn btn-success">{{T.wordsKeepProblem}}</button>
-            </div></span></a>
-          </li>
-        </ul>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="panel-title">{{T.wordsReviewingProblem}}</h2>
+        </div>
+        <div class="panel-body">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-3"><strong>{{T.qualityNominationType}}</strong></div>
+                    <div class="col-sm-4">{{this.nomination}}</div>
+                </div>
+
+            <div class="row">
+                <div class="col-sm-3"><strong>{{T.qualityUserThatNominated}}</strong></div>
+                <div class="col-sm-4">{{ this.nominator.name}} ({{this.nominator.username}})</div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-3"><strong>{{T.wordsProblem}}</strong> </div>
+                <div class="col-sm-4">{{this.problem.title}} ({{this.problem.alias}})
+                    - <a v-bind:href="userUrl(this.problem.alias)">{{T.problemEditGoToProblem}}</a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-3"><strong>{{T.wordsDetails}}</strong> </div>
+                <div class="col-sm-4">{{this.contents}}</div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-3"><strong>{{T.wordsVerdict}}</strong></div>
+                <div class="col-sm-4">
+                  <button class="btn btn-danger">{{T.wordsBanProblem}}</button> 
+                  <button class="btn btn-success">{{T.wordsKeepProblem}}</button>
+                </div>
+            </div>
+        </div>
       </div>
-    </div>
   </div>
 </template>
 
