8b8a89e5f3d40096125b2fc1a7e233cc16193733
alanboy <alanboy@alanboy.net>
2017-08-13 05:02:41 +0000

    Fixed /opt/omegaup/stuff/hook_tools/lint.py lints

--- frontend/www/js/omegaup/qualitynomination/details.js
+++ frontend/www/js/omegaup/qualitynomination/details.js
@@ -4,30 +4,26 @@ import {API, UI, OmegaUp, T} from '../omegaup.js';
 
 OmegaUp.on('ready', function() {
   let payload = JSON.parse(document.getElementById('payload').innerText);
-console.log(payload);
+  console.log(payload);
   var viewDetails = new Vue({
     el: '#qualitynomination-details',
     render: function(createElement) {
       return createElement('omegaup-qualitynomination-details', {
-            props: {
-                contents : payload.contents,
-                nomination : payload.nomination,
-                nominator : {
-                    username: payload.nominator.username,
-                    name: payload.nominator.name,
-                },
-                problem : {
-                    alias: payload.problem.alias,
-                    title: payload.problem.title
-                },
-                qualitynomination_id : parseInt(payload.qualitynomination_id),
-                votes : payload.votes 
-            },
+        props: {
+          contents: payload.contents,
+          nomination: payload.nomination,
+          nominator: {
+            username: payload.nominator.username,
+            name: payload.nominator.name,
+          },
+          problem: {alias: payload.problem.alias, title: payload.problem.title},
+          qualitynomination_id: parseInt(payload.qualitynomination_id),
+          votes: payload.votes
+        },
       });
     },
     components: {
-       'omegaup-qualitynomination-details': qualitynomination_Details,
+      'omegaup-qualitynomination-details': qualitynomination_Details,
     },
   });
 });
-
