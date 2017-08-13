aa60482eda50168128ac466f2e54984c3147a8b7
galloska <pockeetgym@gmail.com>
2017-08-13 05:55:44 +0000

    Fixed /opt/omegaup/stuff/hook_tools/lint.py lints

--- frontend/www/js/omegaup/components/reportinappropriateproblem/Popup.vue
+++ frontend/www/js/omegaup/components/reportinappropriateproblem/Popup.vue
@@ -1,43 +1,58 @@
 <template>
-	<div class="reportinappropriateproblem-popup">
-		<a href="#" v-on:click="onReportInappropriateProblem">
-			{{ T.wordsReportProblem }}
-		</a>
-		<form v-show="showReportDialog" class="panel panel-default popup">
-			<template v-if="currentView == 'question'">
-				<button class="close" type="button" v-on:click="onHide">×</button>
-				<div class="title-text">
-					{{ T.reportProblemFormTitle }}
-				</div>
-				<div class="form-group">
-					<div class="question-text">
-						{{ T.reportProblemFormQuestion }}
-					</div>
-					<select name="selectedReason" v-model="selectedReason" class="control-label"> 
-						<option value="not a statement">{{ T.reportProblemFormNotAProblemStatement }}</option>
-						<option value="offensive">{{ T.reportProblemFormOffensive }}</option>
-						<option value="spam">{{ T.reportProblemFormSpam }}</option>
-						<option value="other">{{ T.reportProblemFormOtherReason }}</option>
-					</select>
-				</div>
-				<div class="form-group">
-					<label class="control-label">{{ T.reportProblemFormAdditionalComments }}</label>
-					<textarea name="rationale" class="input-text" type="text" v-model="rationale"></textarea>
-				</div>
-				<div class="button-row">
-					<button class="col-md-4 btn btn-primary"
-						type="submit"
-						v-bind:disabled="selectedReason == undefined ? true : (rationale.length &lt;= 0 ? selectedReason == 'other' : false)"
-						v-on:click.prevent="onSubmit">{{ T.wordsSend }}</button>
-				</div>
-			</template>
-			<template v-if="currentView == 'thanks'">
-				<div class="centered">
-					<h1>{{ T.reportProblemFormThanksForReview }}</h1>
-				</div>
-			</template>
-		</form>
-	</div>
+  <div class="reportinappropriateproblem-popup">
+    <a href="#"
+         v-on:click="onReportInappropriateProblem">{{ T.wordsReportProblem }}</a>
+    <form class="panel panel-default popup"
+          v-show="showReportDialog">
+      <template v-if="currentView == 'question'">
+        <button class="close"
+                  type="button"
+                  v-on:click="onHide">×</button>
+        <div class="title-text">
+          {{ T.reportProblemFormTitle }}
+        </div>
+        <div class="form-group">
+          <div class="question-text">
+            {{ T.reportProblemFormQuestion }}
+          </div><select class="control-label"
+               name="selectedReason"
+               v-model="selectedReason">
+            <option value="not a statement">
+              {{ T.reportProblemFormNotAProblemStatement }}
+            </option>
+            <option value="offensive">
+              {{ T.reportProblemFormOffensive }}
+            </option>
+            <option value="spam">
+              {{ T.reportProblemFormSpam }}
+            </option>
+            <option value="other">
+              {{ T.reportProblemFormOtherReason }}
+            </option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label class="control-label">{{ T.reportProblemFormAdditionalComments }}</label>
+          <textarea class="input-text"
+               name="rationale"
+               type="text"
+               v-model="rationale"></textarea>
+        </div>
+        <div class="button-row">
+          <button class="col-md-4 btn btn-primary"
+               type="submit"
+               v-bind:disabled=
+               "selectedReason == undefined ? true : (rationale.length &lt;= 0 ? selectedReason == 'other' : false)"
+               v-on:click.prevent="onSubmit">{{ T.wordsSend }}</button>
+        </div>
+      </template>
+      <template v-if="currentView == 'thanks'">
+        <div class="centered">
+          <h1>{{ T.reportProblemFormThanksForReview }}</h1>
+        </div>
+      </template>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -45,39 +60,37 @@ import {T} from '../../omegaup.js';
 import UI from '../../ui.js';
 
 export default {
-	props: {},
+  props: {},
 
-	data: function() {
-		return {
-			T: T,
-			UI: UI,
-			rationale: '',
-			currentView: 'question',
-			showReportDialog: false,
-			selectedReason: undefined
-		};
-	},
+  data: function() {
+    return {
+      T: T,
+      UI: UI,
+      rationale: '',
+      currentView: 'question',
+      showReportDialog: false,
+      selectedReason: undefined
+    };
+  },
 
-	methods: {
+  methods: {
 
-		onHide() {
-			this.showReportDialog = false;
-		},
+    onHide() { this.showReportDialog = false;},
 
-		onReportInappropriateProblem() {
-			this.showReportDialog = true;
-			this.currentView = 'question';
-			this.rationale = '';
-			this.selectedReason = undefined;
-		},
+    onReportInappropriateProblem() {
+      this.showReportDialog = true;
+      this.currentView = 'question';
+      this.rationale = '';
+      this.selectedReason = undefined;
+    },
 
-		onSubmit() {
-			this.$emit('submit', this);
-			this.currentView = 'thanks';
-			var self = this;
-			setTimeout(function() { self.onHide() }, 1000);
-		}
-	}
+    onSubmit() {
+      this.$emit('submit', this);
+      this.currentView = 'thanks';
+      var self = this;
+      setTimeout(function() { self.onHide() }, 1000);
+    }
+  }
 };
 
 </script>
