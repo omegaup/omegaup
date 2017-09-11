b4f6ba4a3c7b5f36a4cebcdcca05356167438e94
Pablo Aguilar <pablo.aguilar@gmail.com>
2017-09-11 08:52:32 +0000

    Vue components for Arena code submit
    The code view will eventually use a nice editor (e.g. Ace) and can
    be used for non-templated code.
    CodeSubmit is just an array of Code components.

---
+++ frontend/www/js/omegaup/components/arena/Code.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- TODO: Use a code editor for both -->
+  <pre v-if="fragment.readOnly">{{ fragment.contents }}</pre>
+  <textarea v-else=""
+        v-model="fragment.contents"></textarea>
+</template>
+
+<script>
+import {T, API} from '../../omegaup.js';
+import UI from '../../ui.js';
+
+export default {
+  props: {
+    // Object for mutable access
+    fragment: Object,
+    language: String,
+  },
+};
+</script>
---
+++ frontend/www/js/omegaup/components/arena/CodeSubmit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="template-code-submit">
+    <code-view v-bind:fragment="fragment"
+         v-bind:key="index"
+         v-bind:language="language"
+         v-for="(fragment, index) in codeFragments"></code-view>
+  </div>
+</template>
+
+<script>
+import {T, API} from '../../omegaup.js';
+import UI from '../../ui.js';
+import arena_Code from './Code.vue';
+
+export default {
+  props: {
+    // An array of code fragment objects. Each object
+    // will be passed on to its own code Vue and is
+    // mutable. This is to save on rebuilding the final
+    // code on every edit event.
+    codeFragments: Array,
+    language: String,
+  },
+  components: {
+    'code-view': arena_Code,
+  }
+};
+
+</script>
