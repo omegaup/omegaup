9fed548159a81c898733325201376ba1bc8c133a
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2017-09-11 18:48:09 -0500

    Agregar que un coder del mes pueda volver a serlo después de un año (#1425)
    Se agrega una condición en la consulta, para que permita a un coder del mes, poder volver a serlo después de un año. También se agrega un nuevo test para este caso, y se tuvieron que corregir algunos métodos donde se hard codeaban las fechas y esto no permitía realizar la prueba correctamente.
    
    Fixes: #1404

--- frontend/tests/controllers/CoderOfTheMonthTest.php
+++ frontend/tests/controllers/CoderOfTheMonthTest.php
@@ -9,32 +9,12 @@ class CoderOfTheMonthTest extends OmegaupTestCase {
     public function testCoderOfTheMonthCalc() {
         $user = UserFactory::createUser();
 
-        $contest = ContestsFactory::createContest();
-        $problem = ProblemsFactory::createProblem();
-
-        ContestsFactory::addProblemToContest($problem, $contest);
-        ContestsFactory::addUser($contest, $user);
-
         // Creating 10 AC runs for our user in the last month
-        $n = 10;
+        $runCreationDate = date_create(date('Y-m-d'));
+        date_add($runCreationDate, date_interval_create_from_date_string('-1 month'));
+        $runCreationDate = date_format($runCreationDate, 'Y-m-01');
 
-        $lastMonth = intval(date('m')) - 1;
-        $runCreationDate = null;
-        if ($lastMonth == 0) {
-            $runCreationDate = date(intval((date('Y')) - 1) . '-12-01');
-        } else {
-            $runCreationDate = date('Y-' . $lastMonth . '-01');
-        }
-
-        for ($i = 0; $i < $n; $i++) {
-            $runData = RunsFactory::createRun($problem, $contest, $user);
-            RunsFactory::gradeRun($runData);
-
-            // Force the run to be in last month
-            $run = RunsDAO::getByAlias($runData['response']['guid']);
-            $run->time = $runCreationDate;
-            RunsDAO::save($run);
-        }
+        $this->createRuns($user, $runCreationDate, 10 /*numRuns*/);
 
         $response = UserController::apiCoderOfTheMonth(new Request());
 
@@ -53,5 +33,62 @@ class CoderOfTheMonthTest extends OmegaupTestCase {
         $response = UserController::apiCoderOfTheMonthList($r);
 
         $this->assertEquals(1, count($response['coders']));
+    }
+
+    public function testCoderOfTheMonthAfterYear() {
+        $userLastYear = UserFactory::createUser();
+        $userThisYear = UserFactory::createUser();
+
+        $today = date('Y-m-d');
+
+        $runCreationDate = date_create($today);
+        date_add($runCreationDate, date_interval_create_from_date_string('-13 month'));
+        $runCreationDate = date_format($runCreationDate, 'Y-m-d');
+        $this->createRuns($userLastYear, $runCreationDate, 2 /*numRuns*/);
+
+        $runCreationDate = date_create($runCreationDate);
+        date_add($runCreationDate, date_interval_create_from_date_string('1 month'));
+        $runCreationDate = date_format($runCreationDate, 'Y-m-d');
+        $this->createRuns($userLastYear, $runCreationDate, 2 /*numRuns*/);
+        $this->createRuns($userThisYear, $runCreationDate, 1 /*numRuns*/);
+
+        $this->createRuns($userLastYear, $today, 2 /*numRuns*/);
+
+        // Getting Coder Of The Month
+        $responseCoder = $this->getCoderOfTheMonth($today, '-1 year');
+        $this->assertEquals($userLastYear->username, $responseCoder['userinfo']['username']);
+
+        $responseCoder = $this->getCoderOfTheMonth($today, '-11 month');
+        $this->assertNotEquals($userLastYear->username, $responseCoder['userinfo']['username']);
+
+        $responseCoder = $this->getCoderOfTheMonth($today, '1 month');
+        $this->assertEquals($userLastYear->username, $responseCoder['userinfo']['username']);
+    }
+
+    private function createRuns($user, $runCreationDate, $n) {
+        $contest = ContestsFactory::createContest();
+        $problem = ProblemsFactory::createProblem();
+        ContestsFactory::addProblemToContest($problem, $contest);
+        ContestsFactory::addUser($contest, $user);
+
+        for ($i = 0; $i < $n; $i++) {
+            $runData = RunsFactory::createRun($problem, $contest, $user);
+            RunsFactory::gradeRun($runData);
+
+            // Force the run to be in any date
+            $run = RunsDAO::getByAlias($runData['response']['guid']);
+            $run->time = $runCreationDate;
+            RunsDAO::save($run);
+        }
+    }
+
+    private function getCoderOfTheMonth($revDate, $interval) {
+        $reviewDate = date_create($revDate);
+        date_add($reviewDate, date_interval_create_from_date_string($interval));
+        $reviewDate = date_format($reviewDate, 'Y-m-01');
+        $response = UserController::apiCoderOfTheMonth(
+            new Request(['date' => $reviewDate])
+        );
+        return $response;
     }
 }
