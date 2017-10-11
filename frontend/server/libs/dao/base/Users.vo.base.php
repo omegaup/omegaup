d56970da7e98ebbb56a897f66ccbfffa1d969c2e
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2017-10-10 22:20:28 -0500

    Corrigiendo error en schema de base de datos (#1464)
    Se corrige un campo que se guardó incorrectamente en el esquema de la base de datos

--- frontend/server/libs/dao/base/Users.vo.base.php
+++ frontend/server/libs/dao/base/Users.vo.base.php
@@ -218,7 +218,7 @@ class Users extends VO {
     /**
       *  [Campo no documentado]
       * @access public
-      * @var enum('female','other','other','decline')
+      * @var enum('female','male','other','decline')
       */
     public $gender;
 
