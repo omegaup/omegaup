9543ff6cb2bf8b1c93f5b2c3fe61b46e73d7787b
lhchavez <lhchavez@lhchavez.com>
2017-10-18 01:55:19 +0000

    La última y nos vamos!

--- stuff/database_schema.py
+++ stuff/database_schema.py
@@ -5,8 +5,6 @@
 
 from __future__ import print_function
 
-import base64
-import gzip
 import logging
 import os.path
 import re
@@ -21,6 +19,12 @@ def _expected_database_schema(*, dbname='omegaup', auth=None):
     '''Runs mysqldump and removes the AUTO_INCREMENT annotation.'''
     schema = database_utils.mysqldump(dbname=dbname, auth=auth)
     return re.sub(r'AUTO_INCREMENT=\d+\s+', '', schema)
+
+
+def strip_mysql_extensions(sql):
+    '''Strips MySQL extension comments.'''
+    return re.sub(r'/\*!([^*]|\*[^/])*\*/', '', sql,
+                  flags=re.MULTILINE|re.DOTALL)
 
 
 def main():
@@ -61,9 +65,8 @@ def main():
     actual = git_tools.file_contents(
             args, root, 'frontend/database/schema.sql').decode('utf-8')
 
-    print(base64.b64encode(gzip.compress(expected.encode('utf-8'))))
-
-    if expected.strip() != actual.strip():
+    if (strip_mysql_extensions(expected.strip()) !=
+        strip_mysql_extensions(actual.strip())):
         if validate_only:
             if git_tools.attempt_automatic_fixes(sys.argv[0], args,
                                                  filtered_files):
