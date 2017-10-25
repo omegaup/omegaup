d862c6a915a5d4598333a90cae65afcd62a79125
lhchavez <lhchavez@lhchavez.com>
2017-10-25 07:21:23 -0700

    Descargando resultados de S3 si no se encuentran localmente (#1488)
    Este cambio hace que si los resultados de un envío no se encuentran en
    el sistema de archivos local, se obtienen de S3, regresando 404 si no se
    encuentran ahí tampoco.

--- frontend/server/config.default.php
+++ frontend/server/config.default.php
@@ -153,3 +153,9 @@ try_define('USER_ANONYMOUS', 'user_anonymous');
 # ########################
 try_define('PASSWORD_RESET_TIMEOUT', 2 * 3600);
 try_define('PASSWORD_RESET_MIN_WAIT', 5 * 60);
+
+# ########################
+# S3 CONFIG
+# ########################
+try_define('AWS_CLI_ACCESS_KEY_ID', null);
+try_define('AWS_CLI_SECRET_ACCESS_KEY', null);
