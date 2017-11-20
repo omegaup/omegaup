c349878ca838cd153cf4dbf66395a51a57d904f6
lhchavez <lhchavez@lhchavez.com>
2017-11-20 15:10:28 +0000

    nit simplificatorio

--- frontend/server/controllers/ProblemController.php
+++ frontend/server/controllers/ProblemController.php
@@ -1484,7 +1484,7 @@ class ProblemController extends Controller {
                 if (file_exists("$grade_dir/details.json")) {
                     $details = json_decode(file_get_contents("$grade_dir/details.json"));
                     foreach ($details as $group) {
-                        if (!is_object($group) || !isset($group->cases) || !is_array($group->cases)) {
+                        if (!isset($group->cases) || !is_array($group->cases)) {
                             continue;
                         }
                         foreach ($group->cases as $case) {
