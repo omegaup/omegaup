0ccd8abf56101dd0da6a461c0976c1f1d83dead7
Nancy Pacheco <nancydanielap@gmail.com>
2017-11-23 06:56:09 +0000

    Cosas que hace el linter

--- frontend/www/js/omegaup/components/Carousel.vue
+++ frontend/www/js/omegaup/components/Carousel.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="col-md-8">
     <div class="panel panel-default">
-      <div id="carousel-display" class="carousel slide" data-ride="carousel" data-interval="3000">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li v-for="(slide, index) of slides"
-                    data-target="#carousel-display"
-                    v-bind:class="{active: !index}"
-                    v-bind:data-slide-to="index"></li>
-      </ol>
-
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item" v-for="(slide, index) of slides" v-bind:class="{active: !index}">
-          <img v-bind:src="slide.image">
-          <div class="carousel-caption">
-            <h3>{{ localized(slide.title) }}</h3>
-            <p>{{ localized(slide.description) }}</p>
+      <div class="carousel slide"
+           data-interval="3000"
+           data-ride="carousel"
+           id="carousel-display">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-display"
+              v-bind:class="{active: !index}"
+              v-bind:data-slide-to="index"
+              v-for="(slide, index) of slides"></li>
+        </ol><!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item"
+               v-bind:class="{active: !index}"
+               v-for="(slide, index) of slides">
+            <img v-bind:src="slide.image">
+            <div class="carousel-caption">
+              <h3>{{ localized(slide.title) }}</h3>
+              <p>{{ localized(slide.description) }}</p>
+            </div>
           </div>
-        </div>
-      </div>
-      <!-- Controls -->
-      <a class="left carousel-control" href="#carousel-display" data-slide="prev">
-        <span class="icon-prev"></span>
-      </a>
-      <a class="right carousel-control" href="#carousel-display" data-slide="next">
-        <span class="icon-next"></span>
-      </a>
+        </div><!-- Controls -->
+         <a class="left carousel-control"
+             data-slide="prev"
+             href="#carousel-display"><span class="icon-prev"></span></a> <a class=
+             "right carousel-control"
+             data-slide="next"
+             href="#carousel-display"><span class="icon-next"></span></a>
       </div>
     </div>
   </div>
@@ -50,14 +52,9 @@ export default {
       return filteredSlides;
     },
   },
-  data: function() {
-    return {
-    };
-  },
+  data: function() { return {};},
   methods: {
-    localized: function(text) {
-      return text[T.locale];
-    },
+    localized: function(text) { return text[T.locale];},
   },
 }
 </script>
