eaed731b3f84bf4b1f4aac4daabc3250e2bbea6d
Hugo Eduardo Dueñas <heduenas@users.noreply.github.com>
2017-11-29 22:15:40 -0800

    Please linter

--- frontend/www/js/omegaup/components/qualitynomination/Popup.vue
+++ frontend/www/js/omegaup/components/qualitynomination/Popup.vue
@@ -36,7 +36,8 @@
                 <li class="tag-select"
                     v-for="problemTopic in sortedProblemTopics"><label class=
                     "tag-select"><input type="checkbox"
-                       v-model="tags" v-bind:value = "problemTopic.text"> {{ problemTopic.text }}</label></li>
+                       v-bind:value="problemTopic.text"
+                       v-model="tags"> {{ problemTopic.text }}</label></li>
               </ul></label>
             </div>
             <div class="formGroup">
