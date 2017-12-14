1fbd7c393cd49bba9f1e4e8ca704d87839a34443
pabo99 <gomez_aguilar_juan_pablo@hotmail.com>
2017-12-14 04:08:39 +0000

    Agregando promesas al guardar multiples usuarios en concurso

--- frontend/www/js/omegaup/ui.js
+++ frontend/www/js/omegaup/ui.js
@@ -69,13 +69,6 @@ let UI = {
             operation(this.id, resolve, reject);
           }
         });
-    if (typeof options != 'undefined') {
-      if (options.hasOwnProperty('usernames')) {
-        options.usernames.each(function(event, index) {
-          operation(event, resolve, reject);
-        });
-      }
-    }
 
     // Wait for all
     $(document)
@@ -94,9 +87,7 @@ let UI = {
                                            omegaup.T.bulkOperationError,
                                        error));
             } else {
-              UI.success((typeof usernames != 'undefined') ?
-                             omegaup.T.bulkUserAddSuccess :
-                             omegaup.T.updateItemsSuccess);
+              UI.success(omegaup.T.updateItemsSuccess);
             }
           }
         });
