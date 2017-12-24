f9c463b6822fae847abeb3b1c5e6521f38e2e175
lhchavez <lhchavez@lhchavez.com>
2017-12-24 04:11:19 +0000

    Travis no saca los logs, así que ando a ciegas

--- stuff/travis/lint.sh
+++ stuff/travis/lint.sh
@@ -8,6 +8,8 @@ stage_before_install() {
 
 	pip install --user six
 	pip install --user https://github.com/google/closure-linter/zipball/master
+	pip3 install --user pylint
+	pip3 install --user pep8
 
 	install_yarn
 }
