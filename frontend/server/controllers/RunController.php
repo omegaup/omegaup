efc9b14bf2f8082f693faf0cd2e0f0509c360ead
lhchavez <lhchavez@lhchavez.com>
2018-01-01 23:27:13 +0000

    Permitiendo usar otras rutas para awscli
    Este cambio pretende reemplazar awscli (de Ubuntu) por awscli (de pip3),
    ya que Ubuntu no ha actualizado su versión y ahora es incompatible con
    otras librerías del sistema :/

--- frontend/server/controllers/RunController.php
+++ frontend/server/controllers/RunController.php
@@ -772,7 +772,7 @@ class RunController extends Controller {
             2 => ['pipe', 'w']
         ];
         $proc = proc_open(
-            "/usr/bin/aws s3 cp s3://omegaup-runs/${guid}.zip -",
+            AWS_CLI_BINARY . " s3 cp s3://omegaup-runs/${guid}.zip -",
             $descriptorspec,
             $pipes,
             '/tmp',
