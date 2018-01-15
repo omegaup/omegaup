4e1a5f8a1ca0abdd5b8ef07b3a153110404351a3
Juan Pablo Gomez <gomez_aguilar_juan_pablo@hotmail.com>
2018-01-15 17:02:35 +0000

    Aplicando correcciones en prueba de crear curso

--- frontend/tests/ui/test_course.py
+++ frontend/tests/ui/test_course.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-'''Run Selenium end-to-end tests.'''
-
-import os
+'''Run Selenium course tests.'''
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
-_OMEGAUP_ROOT = os.path.normpath(os.path.join(__file__, '../../../..'))
 
 
 def test_create_course(driver):
@@ -22,14 +18,17 @@ def test_create_course(driver):
         driver.browser.find_element_by_xpath(
             '//a[contains(@href, "/schools/")]').click()
 
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, "/course/")]').click()
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, ('//a[contains(@href, "/course/")]')))).click()
 
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, "/course/new/")]').click()
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, ('//a[contains(@href, "/course/new/")]')))).click()
 
-        driver.browser.find_element_by_class_name('name').send_keys(
-            course_alias)
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.CLASS_NAME, ('name')))).send_keys(course_alias)
         driver.browser.find_element_by_class_name('alias').send_keys(
             course_alias)
         driver.browser.find_element_by_class_name('tt-hint').send_keys(
@@ -45,25 +44,33 @@ def test_create_course(driver):
         assert (('/course/%s/edit/' % course_alias) in
                 driver.browser.current_url), driver.browser.current_url
 
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, "students")]').click()
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, ('//a[contains(@href, "students")]')))).click()
 
-        driver.browser.find_element_by_css_selector(
-            '.omegaup-course-addstudent .tt-hint').send_keys('user')
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, (
+                    '.omegaup-course-addstudent .tt-hint')))).send_keys('user')
         driver.browser.find_element_by_css_selector(
             '.omegaup-course-addstudent .tt-input').send_keys('user')
 
         driver.browser.find_element_by_css_selector(
             '.omegaup-course-addstudent form button[type=submit]').click()
 
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, "assignments")]').click()
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, (
+                    '//a[contains(@href, "assignments")]')))).click()
 
-        driver.browser.find_element_by_css_selector(
-            '#assignments .new button').click()
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, ('#assignments .new button')))).click()
 
-        driver.browser.find_element_by_css_selector(
-            '.schedule .name').send_keys(assignment_alias)
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, ('.schedule .name')))).send_keys(
+                    assignment_alias)
         driver.browser.find_element_by_css_selector(
             '.schedule .alias').send_keys(assignment_alias)
         driver.browser.find_element_by_css_selector(
@@ -72,8 +79,9 @@ def test_create_course(driver):
         driver.browser.find_element_by_css_selector(
             '#assignments .schedule button[type=submit]').click()
 
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, "problems")]').click()
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, ('//a[contains(@href, "problems")]')))).click()
 
         driver.wait.until(
             EC.element_to_be_clickable(
@@ -81,11 +89,14 @@ def test_create_course(driver):
                  ('//select[@name="assignments"]/option[contains(text(), %s)]'
                   % assignment_alias)))).click()
 
-        driver.browser.find_element_by_css_selector(
-            '#problems .problemlist button').click()
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, ('#problems .problemlist button')))).click()
 
-        driver.browser.find_element_by_css_selector(
-            '.problems-dropdown.tt-hint').send_keys('sumas')
+        driver.wait.until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, (
+                    '.problems-dropdown.tt-hint')))).send_keys('sumas')
         driver.browser.find_element_by_css_selector(
             '.problems-dropdown.tt-input').send_keys('sumas')
 
@@ -99,11 +110,17 @@ def test_create_course(driver):
         driver.browser.find_element_by_xpath(
             '//a[contains(@href, "/schools/")]').click()
 
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, "/course/")]').click()
+        driver.wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH,
+                 ('//a[contains(@href, "/course/")]')))).click()
 
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, %s)]' % course_alias).click()
+        driver.wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH,
+                 ('//a[contains(@href, %s)]' % course_alias)))).click()
 
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, %s)]' % assignment_alias).click()
+        driver.wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH,
+                 ('//a[contains(@href, %s)]' % assignment_alias)))).click()
