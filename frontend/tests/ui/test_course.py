c93775a3d2152a59f3dd829ccf9f0b2d05e3687e
Juan Pablo Gomez <gomez_aguilar_juan_pablo@hotmail.com>
2018-01-16 03:57:45 +0000

    Aplicando correcciones en test de crear curso en selenium

--- frontend/tests/ui/test_course.py
+++ frontend/tests/ui/test_course.py
@@ -13,114 +13,149 @@ def test_create_course(driver):
     course_alias = 'unittest_course_%s' % driver.id
     school_name = 'unittest_school_%s' % driver.id
     assignment_alias = 'unittest_homework_%s' % driver.id
+    user = 'user'
+    problem = 'sumas'
 
     with driver.login_admin():
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, "/schools/")]').click()
-
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, ('//a[contains(@href, "/course/")]')))).click()
-
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, ('//a[contains(@href, "/course/new/")]')))).click()
-
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.CLASS_NAME, ('name')))).send_keys(course_alias)
-        driver.browser.find_element_by_class_name('alias').send_keys(
-            course_alias)
-        driver.browser.find_element_by_class_name('tt-hint').send_keys(
-            school_name)
-        driver.browser.find_element_by_class_name('tt-input').send_keys(
-            school_name)
-        driver.browser.find_element_by_tag_name('textarea').send_keys(
-            'course description')
-
-        with driver.ajax_page_transition():
-            driver.browser.find_element_by_tag_name('form').submit()
+        create_course(driver, course_alias, school_name)
 
         assert (('/course/%s/edit/' % course_alias) in
                 driver.browser.current_url), driver.browser.current_url
 
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, ('//a[contains(@href, "students")]')))).click()
+        add_students(driver, user)
 
+        add_assignment(driver, assignment_alias)
+
+        add_problem_to_assignment(driver, assignment_alias, problem)
+
+    with driver.login('contestant', 'contestant'):
+        enter_to_course(driver, course_alias, assignment_alias)
+
+
+def create_course(driver, course_alias, school_name):
+    '''Creates one course with a new school.'''
+
+    driver.browser.find_element_by_xpath(
+        '//a[contains(@href, "/schools/")]').click()
+
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.XPATH, ('//a[contains(@href, "/course/")]')))).click()
+
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.XPATH, ('//a[contains(@href, "/course/new/")]')))).click()
+
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.CLASS_NAME, ('name')))).send_keys(course_alias)
+    driver.browser.find_element_by_class_name('alias').send_keys(
+        course_alias)
+    driver.browser.find_element_by_class_name('tt-hint').send_keys(
+        school_name)
+    driver.browser.find_element_by_class_name('tt-input').send_keys(
+        school_name)
+    driver.browser.find_element_by_tag_name('textarea').send_keys(
+        'course description')
+
+    with driver.ajax_page_transition():
+        driver.browser.find_element_by_tag_name('form').submit()
+
+
+def add_students(driver, users):
+    '''Add students to a recently created course.'''
+
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.XPATH, ('//a[contains(@href, "students")]')))).click()
+
+    for user in users:
         driver.wait.until(
             EC.visibility_of_element_located(
                 (By.CSS_SELECTOR, (
-                    '.omegaup-course-addstudent .tt-hint')))).send_keys('user')
+                    '.omegaup-course-addstudent .tt-hint')))).send_keys(user)
         driver.browser.find_element_by_css_selector(
-            '.omegaup-course-addstudent .tt-input').send_keys('user')
+            '.omegaup-course-addstudent .tt-input').send_keys(user)
 
         driver.browser.find_element_by_css_selector(
             '.omegaup-course-addstudent form button[type=submit]').click()
 
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, (
-                    '//a[contains(@href, "assignments")]')))).click()
 
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, ('#assignments .new button')))).click()
+def add_assignment(driver, assignment_alias):
+    '''Add assignments to a recently created course.'''
 
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, ('.schedule .name')))).send_keys(
-                    assignment_alias)
-        driver.browser.find_element_by_css_selector(
-            '.schedule .alias').send_keys(assignment_alias)
-        driver.browser.find_element_by_css_selector(
-            '.schedule textarea').send_keys('homework description')
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.XPATH, (
+                '//a[contains(@href, "assignments")]')))).click()
 
-        driver.browser.find_element_by_css_selector(
-            '#assignments .schedule button[type=submit]').click()
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, ('.tab-pane.active .new button')))).click()
 
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, ('//a[contains(@href, "problems")]')))).click()
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, ('.schedule .name')))).send_keys(
+                assignment_alias)
+    assignments_tab = driver.browser.find_element_by_css_selector(
+        '.tab-pane.active')
+    new_assignment_form = assignments_tab.find_element_by_css_selector(
+        '.schedule')
+    new_assignment_form.find_element_by_css_selector('.alias').send_keys(
+        assignment_alias)
+    new_assignment_form.find_element_by_css_selector('textarea').send_keys(
+        'homework description')
 
-        driver.wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH,
-                 ('//select[@name="assignments"]/option[contains(text(), %s)]'
-                  % assignment_alias)))).click()
+    new_assignment_form.find_element_by_css_selector(
+        'button[type=submit]').click()
 
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, ('#problems .problemlist button')))).click()
 
-        driver.wait.until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, (
-                    '.problems-dropdown.tt-hint')))).send_keys('sumas')
-        driver.browser.find_element_by_css_selector(
-            '.problems-dropdown.tt-input').send_keys('sumas')
+def add_problem_to_assignment(driver, assignment_alias, problem):
+    '''Add problems to an assignment given.'''
 
-        driver.browser.find_element_by_css_selector(
-            '.omegaup-course-problemlist form button[type=submit]').click()
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.XPATH, ('//a[contains(@href, "#problems")]')))).click()
 
-        driver.browser.find_element_by_css_selector(
-            '#problems .problemlist button').click()
+    driver.wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH,
+             ('//select[@name="assignments"]/option[contains(text(), %s)]'
+              % assignment_alias)))).click()
 
-    with driver.login_user():
-        driver.browser.find_element_by_xpath(
-            '//a[contains(@href, "/schools/")]').click()
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, (
+                '.tab-pane.active .problemlist button')))).click()
 
-        driver.wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH,
-                 ('//a[contains(@href, "/course/")]')))).click()
+    driver.wait.until(
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, (
+                '.problems-dropdown.tt-hint')))).send_keys(problem)
+    driver.browser.find_element_by_css_selector(
+        '.problems-dropdown.tt-input').send_keys(problem)
 
-        driver.wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH,
-                 ('//a[contains(@href, %s)]' % course_alias)))).click()
+    driver.browser.find_element_by_css_selector(
+        '.omegaup-course-problemlist form button[type=submit]').click()
 
-        driver.wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH,
-                 ('//a[contains(@href, %s)]' % assignment_alias)))).click()
+
+def enter_to_course(driver, course_alias, assignment_alias):
+    '''Enter to course previously created.'''
+
+    driver.browser.find_element_by_xpath(
+        '//a[contains(@href, "/schools/")]').click()
+
+    driver.wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH,
+             ('//a[contains(@href, "/course/")]')))).click()
+
+    driver.wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH,
+             ('//a[contains(@href, %s)]' % course_alias)))).click()
+
+    driver.wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH,
+             ('//a[contains(@href, %s)]' % assignment_alias)))).click()
