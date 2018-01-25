138adc7348c661d0c95d87b91448467e4d1d3e65
pablobatun <pablobatun@hotmail.com>
2018-01-24 22:43:26 -0500

    Update UserController.php (#1699)
    OMI 2018 Quintana Roo

--- frontend/server/controllers/UserController.php
+++ frontend/server/controllers/UserController.php
@@ -903,9 +903,8 @@ class UserController extends Controller {
                 throw new ForbiddenAccessException();
             }
             $keys = [
-                'OMIROO-18' => 500,
-                'ROOP-18' => 300,
-                'ROOS-18' => 300,
+                'OMIROO-D1-18' => 70,
+                'OMIROO-D2-18' => 70
             ];
         } elseif ($r['contest_type'] == 'TEBAEV') {
             if ($r['current_user']->username != 'lacj20'
