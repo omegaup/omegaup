874d3e7e90e3875c17aadaea17e5de5b287f01d6
pabo99 <gomez_aguilar_juan_pablo@hotmail.com>
2018-02-16 14:09:12 +0000

    Aplicando correcciones

--- frontend/www/problemmine.php
+++ frontend/www/problemmine.php
@@ -6,11 +6,10 @@ $private_problems_alert = 0;
 $is_admin = false;
 
 if ($session['valid']) {
-    if (!isset($_SESSION['private_problems_alert'])) {
-        if (ProblemsDAO::getPrivateCount($session['user']) > 0) {
-            $_SESSION['private_problems_alert'] = 1;
-            $private_problems_alert = 1;
-        }
+    if (!isset($_SESSION['private_problems_alert']) &&
+        ProblemsDAO::getPrivateCount($session['user']) > 0) {
+        $_SESSION['private_problems_alert'] = 1;
+        $private_problems_alert = 1;
     }
     $is_admin = Authorization::isSystemAdmin($session['user']->user_id);
 }
