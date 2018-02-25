8855dcb75086722877a887d146402a88ba94b9d8
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2018-02-25 15:41:21 -0600

    Guardando timestamp de concursos que pasan de privados a públicos (#1745)
    Se crea una tabla para almacenar el timestamp de todos los concursos que cambiaron de ser privados a públicos para que sean los primeros en mostrarse en las listas de concursos activos.
    
    Fixes #572

---
+++ frontend/database/47_contest_log.sql
@@ -0,0 +1,23 @@
+-- Table Contest_Log
+
+CREATE TABLE `Contest_Log` (
+  `public_contest_id` int(11) NOT NULL AUTO_INCREMENT,
+  `contest_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `from_visibility` tinyint(1) NOT NULL DEFAULT '0',
+  `to_visibility` tinyint(1) NOT NULL DEFAULT '1',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`public_contest_id`),
+  KEY `contest_id` (`contest_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `fk_cl_contest_id` FOREIGN KEY (`contest_id`) REFERENCES `Contests` (`contest_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cl_user_id` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Esta tabla funcionará para poder ordenar los concursos que se vuelven públicos y no se pierdan entre el resto';
+
+-- Table Contests
+
+ALTER TABLE `Contests`
+  ADD COLUMN `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Indica la hora en que se actualizó de privado a público un concurso o viceversa' AFTER `finish_time`;
+
+UPDATE `Contests`
+  SET `last_updated` = `start_time`;
