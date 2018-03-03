c70a863fa2ae61fa99cb378ccbdb5710a929da7c
lhchavez <lhchavez@lhchavez.com>
2018-03-03 02:46:41 +0000

    Ninja fix para las pruebas de Selenium

--- stuff/travis/selenium.sh
+++ stuff/travis/selenium.sh
@@ -53,5 +53,5 @@ stage_before_script() {
 
 stage_script() {
 	# TODO(https://github.com/omegaup/omegaup/issues/1798): Reenable Firefox
-	/usr/bin/python3 -m pytest "${OMEGAUP_ROOT}/frontend/tests/ui/" -s --browsers=chrome
+	/usr/bin/python3 -m pytest "${OMEGAUP_ROOT}/frontend/tests/ui/" -s --browser=chrome
 }
