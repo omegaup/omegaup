41788178e9e9bf1219afbbcb48fe2e3c299822e8
pabo99 <gomez_aguilar_juan_pablo@hotmail.com>
2018-03-05 21:18:07 +0000

    Agregando opcion de que equipo de soporte pueda cambiar passwords

--- frontend/www/js/omegaup/admin/support.js
+++ frontend/www/js/omegaup/admin/support.js
@@ -3,17 +3,37 @@ import {API, UI, OmegaUp, T} from '../omegaup.js';
 import Vue from 'vue';
 
 OmegaUp.on('ready', function() {
-  var payload = JSON.parse(document.getElementById('payload').innerText);
-
   var adminSupport = new Vue({
     el: '#admin-support',
     render: function(createElement) {
       return createElement('omegaup-admin-support', {
-        props: {},
-        on: {},
+        props: {
+          valid: this.valid,
+        },
+        on: {
+          'search-username': function(username) {
+            adminSupport.valid = false;
+            omegaup.API.User.profile({username: username})
+                .then(function(data) { adminSupport.valid = true; })
+                .fail(omegaup.UI.apiError);
+          },
+          'change-password': function(password, username) {
+            omegaup.API.User.changePassword({
+                              username: username,
+                              password: password,
+                            })
+                .then(function() {
+                  omegaup.UI.success(T.passwordResetResetSuccess);
+                })
+                .fail(omegaup.UI.apiError);
+          },
+          'reset': function() { adminSupport.valid = false; }
+        },
       });
     },
-    data: {},
+    data: {
+      valid: false,
+    },
     components: {
       'omegaup-admin-support': admin_Support,
     },
--- frontend/www/js/omegaup/components/admin/Support.vue
+++ frontend/www/js/omegaup/components/admin/Support.vue
@@ -1,10 +1,60 @@
 <template>
-  <div class="omegaup-admin-user panel-primary panel">
+  <div class="omegaup-admin-support panel-primary panel">
     <div class="panel-heading">
       <h2 class="panel-title">{{ T.omegaupTitleSupportDashboard }}</h2>
     </div>
     <div class="panel-body">
-      Support Dashboard Empty
+      <form class="form bottom-margin"
+            v-on:submit.prevent="onSearchUsername">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="input-group">
+              <input class="form-control"
+                   name="username"
+                   type="text"
+                   v-bind:disabled="valid"
+                   v-bind:placeholder="T.wordsUser"
+                   v-model="username"> <span class="input-group-btn"><button class=
+                   "btn btn-default"
+                      type="button"
+                      v-bind:disabled="valid"
+                      v-on:click.prevent="onSearchUsername">{{ T.wordsSearch }}</button></span>
+            </div>
+          </div>
+        </div>
+      </form>
+      <form class="form bottom-margin"
+            v-on:submit.prevent="onChangePassword"
+            v-show="valid">
+        <div class="row bottom-margin">
+          <div class="col-md-6">
+            <div class="input-group">
+              <input class="form-control"
+                   name="password"
+                   type="text"
+                   v-bind:placeholder="T.passwordResetPassword"
+                   v-model="password"> <span class="input-group-btn"><button class=
+                   "btn btn-default"
+                      type="button"
+                      v-bind:aria-label="T.passwordGenerateRandom"
+                      v-bind:title="T.passwordGenerateRandom"
+                      v-on:click.prevent="onGeneratePassword"><span aria-hidden="true"
+                    class="glyphicon glyphicon-random"></span></button> <button class=
+                    "btn btn-default"
+                      type="button"
+                      v-on:click.prevent="onChangePassword">{{ T.userEditChangePassword
+                      }}</button></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-right">
+            <button class="btn btn-primary submit"
+                 type="reset"
+                 v-on:click.prevent="onReset">{{ T.wordsCancel }}</button>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -13,12 +63,27 @@
 import {T} from '../../omegaup.js';
 
 export default {
-  props: {},
-  data: function() {
-    return {
-      T: T,
-    };
+  props: {
+    valid: Boolean,
   },
-  methods: {},
+  data: function() { return {T: T, username: '', password: ''};},
+  methods: {
+    onSearchUsername: function() {
+      this.$emit('search-username', this.username);
+    },
+    onChangePassword: function() {
+      this.$emit('change-password', this.password, this.username);
+    },
+    onGeneratePassword: function() {
+      let chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+      let length = 8;
+      var newPassword = '';
+      for (var i = 0; i < length; i++) {
+        newPassword += chars[Math.floor(Math.random() * chars.length)];
+      }
+      this.password = newPassword;
+    },
+    onReset: function() { this.$emit('reset');}
+  }
 };
 </script>
