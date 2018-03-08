852ea876e333de859aa7efe2abba89ea26e84a76
randika96 <rpjayasekara.16@cse.mrt.ac.lk>
2018-03-08 04:52:55 +0000

    MOdify the test case for new validation

--- frontend/tests/factories/UserFactory.php
+++ frontend/tests/factories/UserFactory.php
@@ -16,7 +16,7 @@ class UserFactory {
     * @param string $email optional
     * @return user (DAO)
     */
-    public static function createUser($username = null, $password = null, $email = null, $verify = true, $is_private = false) {
+    public static function createUser($username = null, $password = null, $email = null, $scholar_degree = 'none', $verify = true, $is_private = false) {
         // If data is not provided, generate it randomly
         if (is_null($username)) {
             $username = Utils::CreateRandomString();
@@ -37,6 +37,7 @@ class UserFactory {
             'name' => $username,
             'password' => $password,
             'email' => $email,
+            'scholar_degree' => $scholar_degree,
             'is_private' => $is_private,
             'permission_key' => UserController::$permissionKey
         ]);
