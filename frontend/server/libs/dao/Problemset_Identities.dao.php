d17379f44cb0754eb69110ad06fef66786b3b218
pabo99 <gomez_aguilar_juan_pablo@hotmail.com>
2018-03-21 04:33:25 +0000

    Eliminando DAO que ya no se utiliza

--- frontend/server/libs/dao/Problemset_Identities.dao.php
+++ frontend/server/libs/dao/Problemset_Identities.dao.php
@@ -10,7 +10,12 @@ include_once('base/Problemset_Identities.vo.base.php');
   *
   */
 class ProblemsetIdentitiesDAO extends ProblemsetIdentitiesDAOBase {
-    public static function CheckAndSaveFirstTimeAccess($identity_id, $problemset_id, $grant_access = false) {
+    public static function CheckAndSaveFirstTimeAccess(
+        $identity_id,
+        $problemset_id,
+        $grant_access = false,
+        $share_user_information = false
+    ) {
         $problemset_identity = self::getByPK($identity_id, $problemset_id);
         if (is_null($problemset_identity)) {
             if (!$grant_access) {
@@ -23,10 +28,12 @@ class ProblemsetIdentitiesDAO extends ProblemsetIdentitiesDAOBase {
             $problemset_identity->access_time = date('Y-m-d H:i:s');
             $problemset_identity->score = 0;
             $problemset_identity->time = 0;
+            $problemset_identity->share_user_information = $share_user_information;
             ProblemsetIdentitiesDAO::save($problemset_identity);
         } elseif (is_null($problemset_identity->access_time)) {
             // If its set to default time, update it
             $problemset_identity->access_time = date('Y-m-d H:i:s');
+            $problemset_identity->share_user_information = $share_user_information;
             ProblemsetIdentitiesDAO::save($problemset_identity);
         }
         return $problemset_identity;
