43a571a13fb7b28e00982a7075b387f63561c154
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2018-03-25 22:13:16 -0600

    Agregando opcion de que equipo de soporte pueda cambiar passwords (#1807)
    Se agrega la funcionalidad para que un miembro del equipo de soporte pueda cambiar la contraseña de un usuario, dado su correo.
    
    Fixes #1770

--- frontend/www/js/omegaup/admin/support.js
+++ frontend/www/js/omegaup/admin/support.js
@@ -3,17 +3,41 @@ import {API, UI, OmegaUp, T} from '../omegaup.js';
 import Vue from 'vue';
 
 OmegaUp.on('ready', function() {
-  var payload = JSON.parse(document.getElementById('payload').innerText);
-
-  var adminSupport = new Vue({
+  let adminSupport = new Vue({
     el: '#admin-support',
     render: function(createElement) {
       return createElement('omegaup-admin-support', {
-        props: {},
-        on: {},
+        props: {link: this.link, username: this.username},
+        on: {
+          'search-email': function(email) {
+            adminSupport.link = null;
+            adminSupport.username = null;
+            omegaup.API.User.passwordChangeRequest({email: email})
+                .then(function(data) { adminSupport.username = data.username; })
+                .fail(omegaup.UI.apiError);
+          },
+          'generate-token': function(email) {
+            omegaup.API.Reset.generateToken({
+                               email: email,
+                             })
+                .then(function(data) {
+                  omegaup.UI.success(
+                      T.passwordResetTokenWasGeneratedSuccessfully);
+                  adminSupport.link = data.link;
+                })
+                .fail(omegaup.UI.apiError);
+          },
+          'copy-token': function() {
+            omegaup.UI.success(T.passwordResetLinkCopiedToClipboard);
+          },
+          'reset': function() {
+            adminSupport.link = '';
+            adminSupport.username = null;
+          }
+        },
       });
     },
-    data: {},
+    data: {link: null, username: null},
     components: {
       'omegaup-admin-support': admin_Support,
     },
--- frontend/www/js/omegaup/api.js
+++ frontend/www/js/omegaup/api.js
@@ -429,6 +429,8 @@ export default {
   Reset: {
     create: _call('/api/reset/create/'),
 
+    generateToken: _call('/api/reset/generateToken/'),
+
     update: _call('/api/reset/update/'),
   },
 
@@ -510,6 +512,8 @@ export default {
     list: _call('/api/user/list/'),
 
     listUnsolvedProblems: _call('/api/user/listUnsolvedProblems/'),
+
+    passwordChangeRequest: _call('/api/user/passwordChangeRequest/'),
 
     problemsSolved: _call('/api/user/problemssolved/'),
 
