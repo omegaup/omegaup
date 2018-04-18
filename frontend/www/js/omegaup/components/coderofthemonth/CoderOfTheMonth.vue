1b29e99694aeb4abefef4deccf95f729c3a67c2f
SpaceWhite <space_white@yahoo.com>
2018-04-18 02:29:15 +0000

    Fixed /opt/omegaup/stuff/hook_tools/lint.py lints

--- frontend/www/js/omegaup/components/coderofthemonth/CoderOfTheMonth.vue
+++ frontend/www/js/omegaup/components/coderofthemonth/CoderOfTheMonth.vue
@@ -1,40 +1,46 @@
 <template>
-    <div class="wait_for_ajax panel panel-default" >
-	<div class="panel-heading">
-        <ul class="nav nav-tabs">
-            <li class="active" v-on:click="show = true"><a data-toggle="tab">{{UI.formatString(T.codersOfTheMonth)}}</a></li>
-            <li v-on:click="show = false"><a data-toggle="tab">{{UI.formatString(T.codersOfTheMonthList)}}</a></li>
-        </ul>
-	</div>
-	<div class="panel-body">
-	</div>
-		<table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>{{UI.formatString(T.codersOfTheMonthCountry)}}</th>
-                    <th>{{UI.formatString(T.codersOfTheMonthUser)}}</th>
-                    <th>{{UI.formatString(T.codersOfTheMonthDate)}}</th>
-                </tr>
-            </thead>
-            <tbody v-if="show">
-                <tr v-for="coder in coders">
-                    <td><img v-bind:src="coder.gravatar_32"/></td>
-                    <td><img v-bind:src="'/media/flags/' + coder.country_id.toLowerCase() + '.png'" v-if="coder.country_id != null"/></td>
-                    <td>{{coder.username}}</td>
-                    <td>{{coder.date}}</td>
-                </tr>
-            </tbody>
-            <tbody v-if="!show">
-                <tr v-for="coder in coders_monthly">
-                    <td><img v-bind:src="coder.gravatar_32"/></td>
-                    <td><img v-bind:src="'/media/flags/' + coder.country_id.toLowerCase() + '.png'" v-if="coder.country_id != null"/></td>
-                    <td>{{coder.username}}</td>
-                    <td>{{coder.date}}</td>
-                </tr>
-            </tbody>
-        </table>
+  <div class="wait_for_ajax panel panel-default">
+    <div class="panel-heading">
+      <ul class="nav nav-tabs">
+        <li class="active"
+            v-on:click="show = true">
+          <a data-toggle="tab">{{UI.formatString(T.codersOfTheMonth)}}</a>
+        </li>
+        <li v-on:click="show = false">
+          <a data-toggle="tab">{{UI.formatString(T.codersOfTheMonthList)}}</a>
+        </li>
+      </ul>
     </div>
+    <div class="panel-body"></div>
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th></th>
+          <th>{{UI.formatString(T.codersOfTheMonthCountry)}}</th>
+          <th>{{UI.formatString(T.codersOfTheMonthUser)}}</th>
+          <th>{{UI.formatString(T.codersOfTheMonthDate)}}</th>
+        </tr>
+      </thead>
+      <tbody v-if="show">
+        <tr v-for="coder in coders">
+          <td><img v-bind:src="coder.gravatar_32"></td>
+          <td><img v-bind:src="'/media/flags/' + coder.country_id.toLowerCase() + '.png'"
+               v-if="coder.country_id != null"></td>
+          <td>{{coder.username}}</td>
+          <td>{{coder.date}}</td>
+        </tr>
+      </tbody>
+      <tbody v-if="!show">
+        <tr v-for="coder in coders_monthly">
+          <td><img v-bind:src="coder.gravatar_32"></td>
+          <td><img v-bind:src="'/media/flags/' + coder.country_id.toLowerCase() + '.png'"
+               v-if="coder.country_id != null"></td>
+          <td>{{coder.username}}</td>
+          <td>{{coder.date}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -42,38 +48,30 @@ import {API, T} from '../../omegaup.js';
 import UI from '../../ui.js';
 
 export default {
-    props: {},
-    computed: {
-    },
-    data: function() {
-        return {
-            T: T,
-            UI: UI,
-            coders: [],
-            coders_monthly: [],
-            show: true
-        };
-    },
-    created: function() {
-        //top coder of the month
-        var self = this;
-        API.User.coderOfTheMonthList().then(function(response) {
-            self.coders = response.coders;
-        });
+  props: {},
+  computed: {},
+  data: function() {
+    return {T: T, UI: UI, coders: [], coders_monthly: [], show: true};
+  },
+  created: function() {
+    // top coder of the month
+    var self = this;
+    API.User.coderOfTheMonthList().then(function(response) {
+      self.coders = response.coders;
+    }).fail(UI.apiError);
 
-        //coder of the month list
-        var today = new Date();
-        var mm = today.getMonth() + 1;
-        var dd = today.getDate();
+    // coder of the month list
+    var today = new Date();
+    var mm = today.getMonth() + 1;
+    var dd = today.getDate();
 
-        var todayString = [
-            today.getFullYear(),
-            (mm>9 ? '' : '0') + mm,
-            (dd>9 ? '' : '0') + dd
-        ].join('-');
-        API.User.coderOfTheMonthList({date: todayString}).then(function(response) {
-            self.coders_monthly = response.coders;
-        });
-    }
+    var todayString = [
+      today.getFullYear(),
+      (mm > 9 ? '' : '0') + mm,
+      (dd > 9 ? '' : '0') + dd
+    ].join('-');
+    API.User.coderOfTheMonthList({date: todayString})
+        .then(function(response) { self.coders_monthly = response.coders; }).fail(UI.apiError);
+  }
 };
 </script>
