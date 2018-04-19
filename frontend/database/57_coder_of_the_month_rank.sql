b3666bb4b51c1d22dfc0d00a27758dfbae1fbe1f
SpaceWhite <space_white@yahoo.com>
2018-04-19 12:44:15 +0000

    Clean code 2

--- frontend/database/57_coder_of_the_month_rank.sql
+++ frontend/database/57_coder_of_the_month_rank.sql
@@ -2,4 +2,4 @@
 ALTER TABLE Coder_Of_The_Month ADD COLUMN rank int(11) NOT NULL;
 
 -- Populate previous top coder of the month rank with 1
-UPDATE TABLE Coder_Of_The_Month SET rank=1;
+UPDATE Coder_Of_The_Month SET rank=1;
