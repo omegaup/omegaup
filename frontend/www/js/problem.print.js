3806b9ad8cb9dbb8ade9a7e760b2ec65a744030e
lhchavez <lhchavez@lhchavez.com>
2018-04-23 03:59:26 +0000

    Fase dos de la migración a Markdown de JavaScript
    Este cambio ahora puede convertir los templates de libinteractive
    correctamente. De paso se actualizó pagedown para obtener todas las
    actualizaciones que se han hecho hasta el momento, y se movieron todas
    las personalizaciones (que se pudieron) a omegaup.UI.

--- frontend/www/js/problem.print.js
+++ frontend/www/js/problem.print.js
@@ -1,8 +1,8 @@
 (function() {
   var payload = JSON.parse(document.getElementById('payload').innerText);
-  var converter = Markdown.getSanitizingConverter();
+  var markdownConverter = omegaup.UI.markdownConverter({preview: true});
 
   var statement = document.querySelector('div.statement');
-  statement.innerHTML = converter.makeHtml(payload.problem_statement);
+  statement.innerHTML = markdownConverter.makeHtml(payload.problem_statement);
   MathJax.Hub.Queue(['Typeset', MathJax.Hub, statement]);
 })();
