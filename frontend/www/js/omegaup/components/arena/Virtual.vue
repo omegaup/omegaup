f7a6e605f3c86ef50d7c6a459638147dc25b7ba8
SpaceWhite <space_white@yahoo.com>
2018-05-25 00:42:15 +0000

    Virtual create js

--- frontend/www/js/omegaup/components/arena/Virtual.vue
+++ frontend/www/js/omegaup/components/arena/Virtual.vue
@@ -3,20 +3,19 @@
         <div class="panel-body">
             <div class="text-center">
                 <h2>{{detail.title + ' - ' + T.wordsVirtual}}</h2>
-                    <span>{{detail.contestDurationString}}</span>
-                    <form>
-                        <div class="row">
-                            <div class="form-group col-md-4"></div>
-                            <div class="form-group col-md-4">
-                                <label>{{T.contestNewFormStartDate}}</label>
-                                <omegaup-datetimepicker v-model="start_time"></omegaup-datetimepicker>
-                            </div>
-                            <div class="form-group col-md-4"></div>
+                <span>{{detail.contestDurationString}}</span>
+                <form class="form" v-on:submit.prevent="onSubmit">
+                    <div class="row">
+                        <div class="form-group col-md-4"></div>
+                        <div class="form-group col-md-4">
+                            <label>{{T.contestNewFormStartDate}}</label>
+                            <omegaup-datetimepicker v-model="start_time"></omegaup-datetimepicker>
                         </div>
-                        <button type="submit" class="btn btn-primary">{{T.contestNewFormScheduleVirtualContest}}</button>
-                    </form>
-                </div>
-            </div> <!-- div contest-details -->
+                        <div class="form-group col-md-4"></div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">{{T.contestNewFormScheduleVirtualContest}}</button>
+                </form>
+            </div>
             <hr>
             <div class="">
                 <h1>{{T.registerForContestChallenges}}</h1>
@@ -29,7 +28,7 @@
                     <li>{{detail.submissionGapString}}</li>
                 </ul>
             </div>
-        </div><!-- panel-->
+        </div> <!-- div contest-details -->
     </div>
 </template>
 
@@ -46,14 +45,13 @@ export default {
             UI: UI,
             detail: {},
             start_time: new Date(),
+            contest_alias:  /\/arena\/([^\/]+)\/virtual/.exec(window.location.pathname)[1]
         }
     },
     created: function() {
         var self = this;
 
-        let contestAlias = /\/arena\/([^\/]+)\/virtual/.exec(window.location.pathname)[1];
-
-        API.Contest.details({contest_alias: contestAlias}).then(function(response){
+        API.Contest.details({contest_alias: this.contest_alias}).then(function(response){
             var detail = response
 
             //convert time to H:i:s
@@ -70,6 +68,14 @@ export default {
             self.detail = detail;
             self.start_time = new Date();
         });
+    },
+    methods: {
+        onSubmit: function() {
+            API.Contest.createVirtual({alias: this.contest_alias, start_time: this.start_time.getTime() / 1000}).then(function(response) {
+                let virtual_contest_alias = response.alias;
+                window.location = "/contest/" + virtual_contest_alias + "/edit/";
+            }).fail(UI.apiError);
+        }
     },
     components: {
         'omegaup-datetimepicker': DateTimePicker
