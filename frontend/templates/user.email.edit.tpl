e1f7b601a2b11f4ebf58fccb6e6448881117c520
sarvesh211999 <sarvesh211999@gmail.com>
2018-05-29 23:32:19 +0530

    user.email.edit.tpl vueficiation #2066

--- frontend/templates/user.email.edit.tpl
+++ frontend/templates/user.email.edit.tpl
@@ -1,7 +1,7 @@
 {extends file="profile.tpl"}
 {block name="content"}
 <script type="text/json" id="profile">{$profile.userinfo|json_encode}</script></script>
-<div id="userEmailEdit"></div>	
+<div id="userEmailEdit"></div>
 <script type="text/javascript" src="{version_hash src="/js/dist/user_edit_email_form.js"}"></script>
 {/block}
  
--- frontend/www/js/omegaup/components/user/useremailedit.vue
+++ frontend/www/js/omegaup/components/user/useremailedit.vue
@@ -1,46 +1,52 @@
 <template>
-		<div class="col-md-10">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h2 class="panel-title">{{ T.userEmailEditEmail }}</h2>
-				</div>
-				<div class="panel-body">
-					<form id="user_edit_email_form" class="form-horizontal" role="form">
-
-						<div class="form-group">
-							<label for="email" class="col-md-3 control-label">{{ T.userEmailEditEmail }}</label>
-							<div class="col-md-7">
-								<input id='email' name='email' v-bind:value=profile.email type='text' size='30' class="form-control">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-offset-3 col-md-7">
-								<button type='submit' class="btn btn-primary">{{ T.wordsSaveChanges }}</button>
-							</div>
-						</div>
-
-					</form>
-					<div id="wait" style="display: none;">{{ T.userEmailEditSaving }}<img src="/media/wait.gif" /></div>
-				</div>
-			</div>
-	</div>
+  <div class="col-md-10">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="panel-title">{{ T.userEmailEditEmail }}</h2>
+      </div>
+      <div class="panel-body">
+        <form class="form-horizontal"
+              role="form"
+              v-bind:id="email_edit">
+          <div class="form-group">
+            <label class="col-md-3 control-label"
+                 for="email">{{ T.userEmailEditEmail }}</label>
+            <div class="col-md-7">
+              <input class="form-control"
+                   name='email'
+                   size='30'
+                   type='text'
+                   v-bind:id="email"
+                   v-bind:value="profile.email">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-md-offset-3 col-md-7">
+              <button class="btn btn-primary"
+                   type='submit'>{{ T.wordsSaveChanges }}</button>
+            </div>
+          </div>
+        </form>
+        <div style="display: none;"
+             v-bind:id="wait">{{ T.userEmailEditSaving }}<img src="/media/wait.gif"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import {T} from '../../omegaup.js';
 
 export default {
-	props: {
-		profile: Object,
-	},
-	
-	data: function(){
-		return {
-			T:T,
-		}
-	}
+  props: {
+    profile: Object,
+  },
 
+  data: function() {
+    return {
+      T: T, wait: "wait", email_edit: "user_edit_email_form", email: "email",
+    }
+  }
 }
 
 </script>
