7957fe4e3a900171c5a96b4c7e4eb710e382c471
pabo99 <gomez_aguilar_juan_pablo@hotmail.com>
2018-06-01 04:17:47 +0000

    Almacenando user_id en log de concursos

--- frontend/server/controllers/RunController.php
+++ frontend/server/controllers/RunController.php
@@ -333,6 +333,7 @@ class RunController extends Controller {
             RunsDAO::save($run);
 
             SubmissionLogDAO::save(new SubmissionLog([
+                'user_id' => $r['current_user_id'],
                 'identity_id' => $r['current_identity_id'],
                 'run_id' => $run->run_id,
                 'problemset_id' => $run->problemset_id,
