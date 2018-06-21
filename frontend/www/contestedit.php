15b2c91a56110d3ddab238decaa5d9a34bf6233d
pabo99 <gomez_aguilar_juan_pablo@hotmail.com>
2018-06-21 05:06:22 +0000

    Arreglando bug que marcaba undefined en request de concursos

--- frontend/www/contestedit.php
+++ frontend/www/contestedit.php
@@ -1,6 +1,11 @@
 <?php
 
 require_once('../server/bootstrap.php');
+$r = new Request([
+    'auth_token' => array_key_exists('ouat', $_REQUEST) ? $_REQUEST['ouat'] : null,
+    'contest_alias' => $_REQUEST['contest'],
+]);
 $smarty->assign('LANGUAGES', array_keys(RunController::$kSupportedLanguages));
 $smarty->assign('IS_UPDATE', 1);
+$smarty->assign('REQUEST_PAYLOAD', ContestController::apiRequests($r));
 $smarty->display('../templates/contest.edit.tpl');
