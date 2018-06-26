d955e6360b7b00b4b7e1db4f3cd6b69bc436a78b
Carlos Córdova <carlosab1802@gmail.com>
2018-06-26 02:30:20 +0000

    Eliminando los atributos quality y difficulty.

--- frontend/www/js/omegaup/components/problem/Feedback.vue
+++ frontend/www/js/omegaup/components/problem/Feedback.vue
@@ -6,13 +6,13 @@
          v-if="qualityHistogram">
       <omegaup-problem-histogram v-bind:histogram="qualityHistogram"
            v-bind:score="qualityScore"
-           v-bind:type="quality"></omegaup-problem-histogram>
+           v-bind:type="`Quality`"></omegaup-problem-histogram>
     </div>
     <div v-bind:class="containerClass"
          v-if="difficultyHistogram">
       <omegaup-problem-histogram v-bind:histogram="difficultyHistogram"
            v-bind:score="difficultyScore"
-           v-bind:type="difficulty"></omegaup-problem-histogram>
+           v-bind:type="`Difficulty`"></omegaup-problem-histogram>
     </div>
   </div>
 </template>
@@ -39,7 +39,7 @@ export default {
     difficultyScore: Number,
   },
   data: function() {
-    return { T, quality: 'Quality', difficulty: 'Difficulty', }
+    return { T, }
   },
   computed: {
     containerClass: function() {
