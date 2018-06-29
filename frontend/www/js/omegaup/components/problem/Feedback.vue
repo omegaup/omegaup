46b1b991bd40a8a65fa8cdeff659ec7be0ed3869
Carlos Córdova <carlosab1802@gmail.com>
2018-06-29 04:35:44 +0000

    Fix.

--- frontend/www/js/omegaup/components/problem/Feedback.vue
+++ frontend/www/js/omegaup/components/problem/Feedback.vue
@@ -6,13 +6,13 @@
          v-if="qualityHistogram">
       <omegaup-problem-histogram v-bind:histogram="qualityHistogram"
            v-bind:score="qualityScore"
-           v-bind:type="`Quality`"></omegaup-problem-histogram>
+           v-bind:type="`quality`"></omegaup-problem-histogram>
     </div>
     <div v-bind:class="containerClass"
          v-if="difficultyHistogram">
       <omegaup-problem-histogram v-bind:histogram="difficultyHistogram"
            v-bind:score="difficultyScore"
-           v-bind:type="`Difficulty`"></omegaup-problem-histogram>
+           v-bind:type="`difficulty`"></omegaup-problem-histogram>
     </div>
   </div>
 </template>
