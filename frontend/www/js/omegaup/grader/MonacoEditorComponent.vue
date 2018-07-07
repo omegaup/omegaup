c788ec249ddeaed4531c174b0fdaa18428843ab5
lhchavez <lhchavez@lhchavez.com>
2018-07-07 08:34:46 -0700

    Moviendo los recursos del grader para el frontend (#2130)
    Esto simplifica significativamente el deployment del grader.

---
+++ frontend/www/js/omegaup/grader/MonacoEditorComponent.vue
@@ -0,0 +1,90 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import * as Util from './util';
+import * as monaco from 'monaco-editor';
+
+export default {
+  props: {
+    store: Object,
+    storeMapping: Object,
+    theme: {
+      type: String,
+      'default': 'vs-dark',
+    },
+    initialModule: {
+      type: String,
+      'default': null,
+    },
+    readOnly: {
+      type: Boolean,
+      'default': false,
+    },
+    extension: {
+      type: String,
+      'default': null,
+    },
+    initialLanguage: {
+      type: String,
+      'default': null,
+    },
+  },
+  mounted: function() {
+    this._editor = monaco.editor.create(this.$el, {
+      value: this.contents,
+      language: Util.languageMonacoModelMapping[this.language],
+      theme: this.theme,
+      readOnly: this.readOnly,
+    });
+    this._model = this._editor.getModel();
+    this._model.onDidChangeContent(
+        () => { this.contents = this._model.getValue(); });
+  },
+  methods: {
+    onResize: function() { this._editor.layout();},
+  },
+  computed: {
+    language: function() {
+      if (this.initialLanguage) return this.initialLanguage;
+      return Util.vuexGet(this.store, this.storeMapping.language);
+    },
+    module: function() {
+      if (this.initialModule) return this.initialModule;
+      return Util.vuexGet(this.store, this.storeMapping.module);
+    },
+    contents: {
+      get() { return Util.vuexGet(this.store, this.storeMapping.contents);},
+      set(value) {
+        Util.vuexSet(this.store, this.storeMapping.contents, value);
+      },
+    },
+    filename: function() {
+      return this.module + '.' +
+             (this.extension || Util.languageExtensionMapping[this.language]);
+    },
+    title: function() { return this.filename;},
+    visible: function() {
+      if (!this.storeMapping.visible) return true;
+      return Util.vuexGet(this.store, this.storeMapping.visible);
+    },
+  },
+  watch: {
+    language: function(value) {
+      monaco.editor.setModelLanguage(this._model,
+                                     Util.languageMonacoModelMapping[value]);
+    },
+    contents: function(value) {
+      if (this._model.getValue() != value) this._model.setValue(value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  width: 100%;
+  height: 100%;
+}
+</style>
