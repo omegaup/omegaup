f7cf674bc1d3739aeac7d6df233e25073bb316fd
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2018-07-27 03:51:55 +0000

    Creando grupo de creador de identidades (#2139)
    Primera parte de #1910, el cual incluye la creación del grupo de creadores de identidades

---
+++ frontend/database/79_create_organizer_role.sql
@@ -0,0 +1,35 @@
+-- omegaup:organizer
+INSERT INTO
+  `Roles` (`role_id`,`name`,`description`)
+VALUES
+  (6,'GroupIdentityCreator','Miembro del grupo que tiene privilegios para crear identidades a partir de un grupo');
+
+INSERT INTO `ACLs` (`owner_id`) VALUES (1);
+
+SET @acl_id = LAST_INSERT_ID();
+
+INSERT INTO `Groups` (`acl_id`, `alias`, `name`, `description`) VALUES (
+  @acl_id,
+  'omegaup:group-identity-creator',
+  'omegaup:group-identity-creator',
+  'Equipo de usuarios con privilegios para crear identidades a partir de un grupo'
+);
+
+SET @organizer_group_id = LAST_INSERT_ID();
+
+INSERT INTO `Group_Roles` VALUES(@organizer_group_id, 6, 6);
+
+-- Alter Identities table, adding and updating gender column
+
+ALTER TABLE `Identities`
+  ADD COLUMN `gender` enum('female','male','other','decline') COMMENT 'Género de la identidad';
+
+UPDATE
+  `Identities`
+INNER JOIN
+  `Users`
+ON
+  `Users`.`user_id` = `Identities`.`user_id`
+SET
+  `Identities`.`gender`= `Users`.`gender`;
+  
