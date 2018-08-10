60ff3c9f15392c89c5b290ab2adaa958b9970c65
Juan Pablo Gomez <gomez_aguilar_juan_pablo@hotmail.com>
2018-08-10 06:16:09 +0000

    Mostrando un banner si el browser no es compatible con omegaUp

--- frontend/www/js/status.dismiss.js
+++ frontend/www/js/status.dismiss.js
@@ -3,3 +3,16 @@ $('#alert-close').on('click', function() { $('#status').slideUp(); });
 $('#email-verification-alert-close')
     .on('click', function() { $('#email-verification-alert')
                                   .slideUp(); });
+
+function isBrowserSupported() {
+  if (typeof(window.history.replaceState) !== 'function') {
+    return false;
+  }
+
+  return true;
+}
+
+if (!isBrowserSupported()) {
+  omegaup.UI.error(
+      "The browser doesn't support all the features of this page. Please try to update.");
+}
