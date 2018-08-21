5cb937a4feeaff33d81f66f85e78d329bff8d37a
Sarvesh <sarvesh211999@gmail.com>
2018-08-21 05:19:03 +0000

    fixes #2053

--- frontend/www/schools/index.php
+++ frontend/www/schools/index.php
@@ -1,4 +1,12 @@
 <?php
 
 require_once('../../server/bootstrap.php');
-$smarty->display('../templates/schools.intro.tpl');
+$currentSession = SessionController::apiCurrentSession()['session'];
+$r = new Request($_REQUEST);
+$course = CourseController::apiListCourses($r);
+
+if ($course['student']!=0 or $course['admin']!=0) {
+    die(header('Location: /course'));
+} else {
+    $smarty->display('../templates/schools.intro.tpl');
+}
