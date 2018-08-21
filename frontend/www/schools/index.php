5397790b8baa6a23bd4e7c5fdb7a06ad66a5c87d
Sarvesh <sarvesh211999@gmail.com>
2018-08-21 23:04:32 +0000

    nits

--- frontend/www/schools/index.php
+++ frontend/www/schools/index.php
@@ -1,11 +1,10 @@
 <?php
 
 require_once('../../server/bootstrap.php');
-$currentSession = SessionController::apiCurrentSession()['session'];
 $r = new Request($_REQUEST);
 $course = CourseController::apiListCourses($r);
 
-if (count($course['student'])!=0 or count($course['admin'])!=0) {
+if (count($course['student']) != 0 || count($course['admin']) != 0) {
     die(header('Location: /course'));
 } else {
     $smarty->display('../templates/schools.intro.tpl');
