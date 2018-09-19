ce817eafe5a01b5ffe28352b01355f53931de7ef
lhchavez <lhchavez@lhchavez.com>
2018-09-18 20:43:47 -0700

    nit

--- frontend/www/groupedit.php
+++ frontend/www/groupedit.php
@@ -2,11 +2,6 @@
 
 require_once('../server/bootstrap.php');
 
-if (!$experiments->isEnabled(Experiments::IDENTITIES)) {
-    header('HTTP/1.1 404 Not Found');
-    die();
-}
-
 $r = new Request($_REQUEST);
 $session = SessionController::apiCurrentSession($r)['session'];
 if (is_null($session['identity'])) {
@@ -14,7 +9,8 @@ if (is_null($session['identity'])) {
     die();
 }
 
-$is_organizer = Authorization::canCreateGroupIdentities($session['identity']->identity_id);
+$is_organizer = $experiments->isEnabled(Experiments::IDENTITIES) &&
+    Authorization::canCreateGroupIdentities($session['identity']->identity_id);
 $smarty->assign('IS_UPDATE', 1);
 $smarty->assign('IS_ORGANIZER', $is_organizer);
 $smarty->display('../templates/group.edit.tpl');
