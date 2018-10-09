34afca3bae153837ca39e44dc6e301abada99513
Juan Pablo Gomez <gomez_aguilar_juan_pablo@hotmail.com>
2018-10-09 19:03:25 +0000

    Arreglando bug que no permitía recuperar la redacción en redaccion.php

--- frontend/www/js/redaccion.js
+++ frontend/www/js/redaccion.js
@@ -1,6 +1,43 @@
 omegaup.OmegaUp.on('ready', function() {
   var markdownConverter = omegaup.UI.markdownConverter({preview: true});
   var markdownEditor = new Markdown.Editor(markdownConverter);
+  var originalRedaction = `# Descripción
+
+Esta es la descripción del problema. Inventa una historia creativa.
+Puedes utilizar matemáticas inline para hacer $x_i, y_i$, o $z_i$ o incluso:
+$$x=\\frac{b\\pm \\sqrt{b^2 -4ac}}{2a}$$
+
+# Entrada
+
+Aquí va la descripción de la entrada del problema.
+
+# Salida
+
+Esta es la descripción de la salida esperada.
+
+# Ejemplo
+
+||input
+1
+2
+||output
+Case #1: 3
+||description
+Explicación
+||input
+5
+10
+||output
+Case #2: 15
+||end
+
+# Límites
+
+* Aquí
+* Van
+* Los
+* Límites`;
+
   markdownEditor.hooks.chain('onPreviewRefresh', function() {
     MathJax.Hub.Queue(['Typeset', MathJax.Hub, $('#wmd-preview').get(0)]);
   });
@@ -8,11 +45,17 @@ omegaup.OmegaUp.on('ready', function() {
 
   // Ask the user if they want to restore the last draft
   if (localStorage.getItem('wmdinput')) {
-    var r = confirm('¿Deseas restaurar tu última redacción?');
-    if (r == true) {
-      $('#wmd-input').val(localStorage.getItem('wmdinput'));
-    }
+    $('#wmd-input').val(localStorage.getItem('wmdinput'));
+  } else {
+    $('#wmd-input').val(originalRedaction);
   }
+
+  $('#restart-problem')
+      .on('click', function(evt) {
+        $('#wmd-input').val(originalRedaction);
+        markdownEditor.refreshPreview();
+        localStorage.setItem('wmdinput', $('#wmd-input').val());
+      });
 
   $('#wmd-input')
       .on('keyup', function() {
