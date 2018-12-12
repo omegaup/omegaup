fc5a0c4c75f802e1fed41c8912189a35d2b477ad
tvanessa <tvanessaap@gmail.com>
2018-12-12 07:44:16 +0000

    Quitar comentario

--- frontend/server/controllers/UserController.php
+++ frontend/server/controllers/UserController.php
@@ -1272,7 +1272,6 @@ class UserController extends Controller {
      * @throws InvalidDatabaseOperationException
      */
     public static function apiProfile(Request $r) {
-        //usarname is needed
         self::authenticateOrAllowUnauthenticatedRequest($r);
 
         $r['identity'] = self::resolveTargetIdentity($r);
