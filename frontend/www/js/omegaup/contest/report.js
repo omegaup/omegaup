3f20b7b4635a635c84ba876ed0f7c96525f56ab5
Sarvesh <sarvesh211999@gmail.com>
2018-12-25 09:40:25 +0000

    lint fixed

--- frontend/www/js/omegaup/contest/report.js
+++ frontend/www/js/omegaup/contest/report.js
@@ -2,24 +2,23 @@ import contest_report from '../components/contest/Report.vue';
 import {OmegaUp} from '../omegaup.js';
 import Vue from 'vue';
 
-OmegaUp.on('ready',function(){
-	const payload = JSON.parse(document.getElementById('payload').innerText);
+OmegaUp.on('ready', function() {
+  const payload = JSON.parse(document.getElementById('payload').innerText);
 
-	let contestReport = new Vue({
-		el: '#contest-report',
-		render: function(createElement){
-			return createElement('contestReport',{
-				props: {
-					contestReport: this.contestReport,
-				}
-			})
-		},
-		data: {
-			contestReport: payload.contestReport,
-		},
-		components :{
-			contestReport: contest_report,
-		}
-	})
-
+  let contestReport = new Vue({
+    el: '#contest-report',
+    render: function(createElement) {
+      return createElement('contestReport', {
+        props: {
+          contestReport: this.contestReport,
+        }
+      });
+    },
+    data: {
+      contestReport: payload.contestReport,
+    },
+    components: {
+      contestReport: contest_report,
+    }
+  });
 });
