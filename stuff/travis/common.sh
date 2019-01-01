d969d92e56880d5c7a4e13ec915aa922c3dde20a
lhchavez <lhchavez@lhchavez.com>
2019-01-01 13:02:07 -0800

    Usando Xenial para Travis (#2300)
    Llevamos años usando Xenial en la VM y en prod. Travis acaba de empezar
    a soportar Xenial, así que es momento de migrarlo también.

--- stuff/travis/common.sh
+++ stuff/travis/common.sh
@@ -39,14 +39,11 @@ install_yarn() {
 }
 
 install_omegaup_update_problem() {
-	DOWNLOAD_URL=$(\
-		curl --location https://api.github.com/repos/omegaup/gitserver/releases/latest |\
-		gawk 'match($0, /browser_download_url.*(https:\/\/.*omegaup-update-problem.xz)/, line) { print line[1] }')
-	TARGET="${HOME}/bin/omegaup-update-problem.xz"
-	mkdir -p $(dirname "${TARGET}")
-	curl --location "${DOWNLOAD_URL}" -o "${TARGET}"
-	unxz "${TARGET}"
-	chmod +x "${TARGET%.xz}"
+	DOWNLOAD_URL='https://github.com/omegaup/gitserver/releases/download/v1.2.0/omegaup-update-problem.xz'
+	TARGET="/usr/bin/omegaup-update-problem.xz"
+	sudo curl --location "${DOWNLOAD_URL}" -o "${TARGET}"
+	sudo xz --decompress "${TARGET}"
+	sudo chmod +x "${TARGET%.xz}"
 }
 
 setup_phpenv() {
