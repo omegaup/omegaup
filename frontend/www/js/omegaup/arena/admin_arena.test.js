78ec494e522993d543cf931d598190079c45f173
Vincent Fango <space_white@yahoo.com>
2019-01-28 02:12:38 +0900

    upgrade webpack 4 (#2184)
    1. Upgrade webpack from 3 to 4 and its dependencies
    2. Modify optimization code in `webpack.config.js`
    3. Merge all languages into the main bundle
    4. Some fixes for the new split chunk model.
    
    Fixes: #1783

--- frontend/www/js/omegaup/arena/admin_arena.test.js
+++ frontend/www/js/omegaup/arena/admin_arena.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('../../dist/commons.js');
 var omegaup = require('../../dist/omegaup.js');
 var Markdown =
     require('../../../third_party/js/pagedown/Markdown.Sanitizer.js');
