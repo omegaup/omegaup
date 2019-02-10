d30502d92e16f54c8c517a8ffd99c18da88e51b8
akki111singh <akhil111singhrajput@gmail.com>
2019-02-10 18:32:29 +0000

    Fixed edit profile error

--- frontend/www/js/user.edit.js
+++ frontend/www/js/user.edit.js
@@ -17,7 +17,12 @@ omegaup.OmegaUp.on('ready', function() {
         $('#state_id option').remove();
         $('#state_id').val('');
 
-        var country = iso3166.country($('#country_id').val());
+        try {
+          var country = iso3166.country($('#country_id').val());
+        } catch (err) {
+          country = '';
+        }
+        
         if (!country.sub) {
           $('#state_id').attr('disabled', 'disabled');
           return;
