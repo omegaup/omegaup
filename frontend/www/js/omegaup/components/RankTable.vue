3007f097c7b207a9c6e3ccec2420ff271b96d20a
akki111singh <akhil111singhrajput@gmail.com>
2019-02-14 13:00:44 +0000

    structured the code in RankTable.vue

--- frontend/www/js/omegaup/components/RankTable.vue
+++ frontend/www/js/omegaup/components/RankTable.vue
@@ -1,72 +1,71 @@
 <template>
   <div class="panel panel-default">
     <div class="panel-heading">
-      <h3 class="panel-title">{{ UI.formatString(T.rankRangeHeader,
-      {lowCount:(page-1)*length+1,highCount:page*length}) }}</h3>
+      <template v-if="isIndex">
+        <h3 class="panel-title">{{ UI.formatString(T.rankHeader,{count:length}) }}</h3>
+      </template>
+      <template v-else="">
+        <h3 class="panel-title">{{ UI.formatString(T.rankRangeHeader,
+        {lowCount:(page-1)*length+1,highCount:page*length}) }}</h3>
+      </template>
     </div>
-    <div class="panel-body no-padding">
+    <div class="panel-body no-padding"
+         v-if="!isIndex">
       <label><omegaup-autocomplete class="form-control"
                             v-bind:init="el =&gt; UI.userTypeahead(el)"
                             v-model="searchedUsername"></omegaup-autocomplete></label>
                             <button class="btn btn-primary"
            type="button"
            v-on:click="onSubmit">{{ T.searchUser }}</button>
-      <template v-if="!isIndex">
-        <template v-if="page &gt; 1">
-          <a class="prev"
-                    v-bind:href="prevPageFilter">{{ T.wordsPrevPage }}</a> <span class="delimiter"
-                    v-show="shouldShowNextPage">|</span>
-        </template><a class="next"
-                  v-bind:href="nextPageFilter"
-                  v-show="shouldShowNextPage">{{ T.wordsNextPage }}</a>
-        <template v-if="Object.keys(availableFilters).length &gt; 0">
-          <select class="filter"
-                    v-model="filter"
-                    v-on:change="filterChange">
-            <option value="">
-              {{ T.wordsSelectFilter }}
-            </option>
-            <option v-bind:value="key"
-                    v-for="(item,key,index) in availableFilters">
-              {{ item }}
-            </option>
-          </select>
-        </template>
-      </template>
-      <template v-else="">
-        <h3 class="panel-title">{{ UI.formatString(T.rankHeader,{count:length}) }}</h3>
+      <template v-if="page &gt; 1">
+        <a class="prev"
+                  v-bind:href="prevPageFilter">{{ T.wordsPrevPage }}</a> <span class="delimiter"
+                  v-show="shouldShowNextPage">|</span>
+      </template><a class="next"
+           v-bind:href="nextPageFilter"
+           v-show="shouldShowNextPage">{{ T.wordsNextPage }}</a>
+      <template v-if="Object.keys(availableFilters).length &gt; 0">
+        <select class="filter"
+                  v-model="filter"
+                  v-on:change="filterChange">
+          <option value="">
+            {{ T.wordsSelectFilter }}
+          </option>
+          <option v-bind:value="key"
+                  v-for="(item,key,index) in availableFilters">
+            {{ item }}
+          </option>
+        </select>
       </template>
     </div>
-    <div class="table-responsive">
-      <table class="table table-striped table-hover no-margin">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th colspan="2">{{ T.wordsUser }}</th>
-            <th class="numericColumn">{{ T.rankScore }}</th>
-            <th class="numericColumn"
-                v-if="!isIndex">{{ T.rankSolved }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="rank in ranking">
-            <td>{{ rank.rank }}</td>
-            <td><img height="11"
-                 v-bind:src="flagURL(rank)"
-                 v-bind:title="rank.country"
-                 v-if="rank.country"
-                 width="16"></td>
-            <td class="forcebreaks forcebreaks-top-5"><strong><a v-bind:href=
-            "`/profile/${rank.username}`">{{ rank.username }}</a></strong><span v-if=
-            "rank.name == null || length == 5">&nbsp;</span> <span v-else=""><br>
-            {{ rank.name }}</span></td>
-            <td class="numericColumn">{{ rank.score }}</td>
-            <td class="numericColumn"
-                v-if="!isIndex">{{ rank.problemsSolvedUser }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th colspan="2">{{ T.wordsUser }}</th>
+          <th class="numericColumn">{{ T.rankScore }}</th>
+          <th class="numericColumn"
+              v-if="!isIndex">{{ T.rankSolved }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="rank in ranking">
+          <td>{{ rank.rank }}</td>
+          <td><img height="11"
+               v-bind:src="flagURL(rank)"
+               v-bind:title="rank.country"
+               v-if="rank.country"
+               width="16"></td>
+          <td class="forcebreaks forcebreaks-top-5"><strong><a v-bind:href=
+          "`/profile/${rank.username}`">{{ rank.username }}</a></strong><span v-if=
+          "rank.name == null || length == 5">&nbsp;</span> <span v-else=""><br>
+          {{ rank.name }}</span></td>
+          <td class="numericColumn">{{ rank.score }}</td>
+          <td class="numericColumn"
+              v-if="!isIndex">{{ rank.problemsSolvedUser }}</td>
+        </tr>
+      </tbody>
+    </table>
     <div class="container-fluid">
       <div class="col-xs-12 vertical-padding">
         <template v-if="isIndex">
