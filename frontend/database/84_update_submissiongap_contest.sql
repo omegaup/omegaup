830851f376a95e0dd3c2d9fd8ab3e70e90490a1a
tvanessa <tvanessaap@gmail.com>
2019-04-04 06:11:07 +0000

    Fixes

--- frontend/database/84_update_submissiongap_contest.sql
+++ frontend/database/84_update_submissiongap_contest.sql
@@ -6,6 +6,6 @@ SET
 WHERE
   submissions_gap < 60;
 
--- Constest: update submission_gap default from 1 second to 60 seconds
+-- Contest: update submission_gap default from 1 second to 60 seconds
 ALTER TABLE `Contests`
   MODIFY COLUMN `submissions_gap` int(11) NOT NULL DEFAULT '60' COMMENT 'Tiempo mínimo en segundos que debe de esperar un usuario despues de realizar un envío para hacer otro';
