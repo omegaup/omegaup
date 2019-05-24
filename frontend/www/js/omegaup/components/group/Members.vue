e2ccd74142f72825fe516d7d67a5d3bb961a2d7a
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2019-05-23 19:26:53 -0500

    Convirtiendo a vue la sección de miembros de un grupo (#2515)
    Como parte de la tarea de vueficar el código de smarty se convierte la sección de Miembros de un grupo a vue. Además porque se agregarán cambios que tienen que ver con el refactor de identidad
    
    Part of: #1912

---
+++ frontend/www/js/omegaup/components/group/Members.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="panel panel-primary">
+    <div class="panel-body">
+      <form class="form"
+            v-on:submit.prevent="onAddMember">
+        <div class="form-group">
+          <label>{{ T.wordsMember }} <input autocomplete="off"
+                 class="form-control typeahead"
+                 name="username"
+                 size="20"
+                 type="text"></label>
+        </div><button class="btn btn-primary"
+              type="submit">{{ T.wordsAddMember }}</button>
+      </form>
+    </div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>{{ T.wordsUser }}</th>
+          <th>{{ T.contestEditRegisteredAdminDelete }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="identity in identities">
+          <td><omegaup-user-username v-bind:classname="identity.classname"
+                                 v-bind:linkify="true"
+                                 v-bind:username="identity.username"></omegaup-user-username></td>
+          <td>
+            <a class="glyphicon glyphicon-remove"
+                href="#"
+                v-bind:title="T.groupEditMembersRemove"
+                v-on:click="onRemove(identity.username)"></a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style>
+label {
+  display: inline;
+}
+</style>
+
+<script>
+import {T, UI} from '../../omegaup.js';
+import user_Username from '../user/Username.vue';
+export default {
+  props: {
+    identities: Array,
+  },
+  data: function() {
+    return {
+      T: T,
+      memberUsername: '',
+      username: '',
+    };
+  },
+  mounted: function() {
+    let self = this;
+    UI.userTypeahead($('input.typeahead', self.$el), function(event, item) {
+      self.memberUsername = item.value;
+    });
+  },
+  methods: {
+    onAddMember: function() {
+      let hintElem = $('input.typeahead.tt-hint', this.$el);
+      let hint = hintElem.val();
+      if (hint) {
+        // There is a hint currently visible in the UI, the user likely
+        // expects that hint to be used when trying to add someone, instead
+        // of what they've actually typed so far.
+        this.memberUsername = hint;
+      } else {
+        this.memberUsername = $('input.typeahead.tt-input', this.$el).val();
+      }
+      this.$emit('add-member', this, this.memberUsername);
+    },
+    onRemove: function(username) { this.$emit('remove', username);},
+    reset: function() {
+      this.memberUsername = '';
+      let inputElem = $('input.typeahead', this.$el);
+      inputElem.typeahead('close');
+      inputElem.val('');
+    },
+  },
+  components: {
+    'omegaup-user-username': user_Username,
+  },
+};
+</script>
