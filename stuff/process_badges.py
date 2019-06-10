caf490e661df5b22626be738ee8e5b2a751e9c70
Carlos Cordova <carlosab1802@gmail.com>
2019-06-10 15:11:24 +0000

    Haciendo que los nombres de los archivos sean constantes.

--- stuff/process_badges.py
+++ stuff/process_badges.py
@@ -16,22 +16,27 @@ import os
 OMEGAUP_BADGES_ROOT = os.path.abspath(os.path.join(__file__, '..', '..',
                                                    'frontend/badges'))
 _MAX_BADGE_SIZE = 15 * 1024
+ICON_FILE = 'icon.svg'
+LOCALIZATIONS_FILE = 'localizations.json'
+QUERY_FILE = 'query.sql'
+TEST_FILE = 'test.json'
 
 
 def process_badge(alias: str):
     '''Validates and processes badge information'''
     logging.info('BADGE %s', alias)
     try:
-        path = os.path.join(OMEGAUP_BADGES_ROOT, alias, 'icon.svg')
+        path = os.path.join(OMEGAUP_BADGES_ROOT, alias, ICON_FILE)
         filesize = os.stat(path).st_size
         if filesize > _MAX_BADGE_SIZE:
             raise ValueError('El tamaño de icon.svg es mayor a 15KB')
     except OSError:
-        logging.warning('No se encontró icon.svg, se usará el default')
+        logging.warning('No se encontró %s, se usará el default.',
+                        LOCALIZATIONS_FILE)
     # SVG must be OK or not exist for this to pass.
 
     try:
-        path = os.path.join(OMEGAUP_BADGES_ROOT, alias, 'localizations.json')
+        path = os.path.join(OMEGAUP_BADGES_ROOT, alias, LOCALIZATIONS_FILE)
         # Opens localizations json and adds the entries to:
         # /frontend/templates/es.lang
         # /frontend/templates/en.lang
@@ -52,16 +57,16 @@ def process_badge(alias: str):
             # add_entries_to_templates()
             logging.info('Las entradas serán cargadas a los archivos .lang')
     except OSError:
-        logging.error('No se encontró localizations.json')
+        logging.error('No se encontró %s', LOCALIZATIONS_FILE)
         raise
 
     if not os.path.isfile(os.path.join(OMEGAUP_BADGES_ROOT, alias,
-                                       'query.sql')):
-        raise OSError('No se encontró el archivo query.sql')
+                                       QUERY_FILE)):
+        raise OSError('No se encontró el archivo %s' % QUERY_FILE)
 
     if not os.path.isfile(os.path.join(OMEGAUP_BADGES_ROOT, alias,
-                                       'test.json')):
-        raise OSError('No se encontró el archivo test.json')
+                                       TEST_FILE)):
+        raise OSError('No se encontró el archivo %s' % TEST_FILE)
     # run_test_for_badge()
     logging.info('%s ha sido correctamente agregado/actualizado.\n', alias)
 
