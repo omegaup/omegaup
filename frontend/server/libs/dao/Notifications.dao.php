79aa72b6bea7f9312ebe651a243df290358088a8
Carlos Córdova <carlosab1802@gmail.com>
2019-06-20 19:52:10 +0000

    Agregando API para listar todas las notificaciones de un usuario.

---
+++ frontend/server/libs/dao/Notifications.dao.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('base/Notifications.dao.base.php');
+require_once('base/Notifications.vo.base.php');
+/** Notifications Data Access Object (DAO).
+  *
+  * Esta clase contiene toda la manipulacion de bases de datos que se necesita para
+  * almacenar de forma permanente y recuperar instancias de objetos {@link Notifications }.
+  * @author alanboy
+  * @access public
+  * @package docs
+  *
+  */
+class NotificationsDAO extends NotificationsDAOBase {
+    private static function getUserNotifications(Users $user) {
+        global $conn;
+        $sql = 'SELECT
+                    n.contents, n.timestamp
+                FROM
+                    Notifications n
+                WHERE
+                    n.user_id = ? AND n.read = 0
+                ORDER BY
+                    n.timestamp ASC;';
+        $args = [$user->user_id];
+        return $conn->GetAll($sql, $args);
+    }
+}
