9c279f67ab413bc09cbedf5cd5457523dea758bc
Carlos Córdova <carlosab1802@gmail.com>
2019-06-22 22:59:26 +0000

    Cambiando la fecha límite para el badge.

--- frontend/badges/legacyUser/query.sql
+++ frontend/badges/legacyUser/query.sql
@@ -1,9 +1,10 @@
 SELECT
     DISTINCT `u`.`user_id`
 FROM
-	`Submissions` AS `s`
+    `Submissions` AS `s`
 INNER JOIN
     `Identities` AS `i` ON `s`.`identity_id` = `i`.`identity_id`
 INNER JOIN
     `Users` AS `u` ON `u`.`main_identity_id` = `i`.`identity_id`
-WHERE YEAR(`s`.`time`) <= 2011;
+WHERE
+    `s`.`time` <= '2013-02-02 13:12:24';
