6c7f7a28f86e63f03722ed42133c667c271b933c
Carlos Córdova <carlosab1802@gmail.com>
2019-06-22 22:48:19 +0000

    Cambiando la lógica de legacyUser badge.

--- frontend/badges/legacyUser/query.sql
+++ frontend/badges/legacyUser/query.sql
@@ -1,9 +1,9 @@
 SELECT
     DISTINCT `u`.`user_id`
 FROM
-	`Identity_Login_Log` AS `ill`
+	`Submissions` AS `s`
 INNER JOIN
-    `Identities` AS `i` ON `ill`.`identity_id` = `i`.`identity_id`
+    `Identities` AS `i` ON `s`.`identity_id` = `i`.`identity_id`
 INNER JOIN
     `Users` AS `u` ON `u`.`main_identity_id` = `i`.`identity_id`
-WHERE YEAR(`ill`.`time`) <= 2011;
+WHERE YEAR(`s`.`time`) <= 2011;
