7032eca0cc233006fe853a2d33806945a419dc3c
Carlos Cordova <carlosab1802@gmail.com>
2019-07-08 17:02:06 +0000

    Agregando API para marcar las notificaciones como leidas.

--- frontend/server/controllers/NotificationController.php
+++ frontend/server/controllers/NotificationController.php
@@ -23,5 +23,28 @@ class NotificationController extends Controller {
         ];
     }
 
-    //TODO: apiReadNotifications
+    /**
+     * Updates notifications as read in database
+     *
+     * @param Request $r
+     * @return array
+     * @throws InvalidDatabaseOperationException
+     */
+    public static function apiReadNotifications(Request $r) {
+        self::authenticateRequest($r);
+        if (empty($r['notifications'])) {
+            throw new NotFoundException('notificationIdsNotProvided');
+        }
+        foreach ($r['notifications'] as $id) {
+            $notification = NotificationsDAO::getByPK($id);
+            if ($notification === null) {
+                throw new NotFoundException('notificationDoesntExist');
+            }
+            $notification->read = 1;
+            NotificationsDAO::update($notification);
+        }
+        return [
+            'status' => 'ok',
+        ];
+    }
 }
--- frontend/tests/controllers/NotificationTest.php
+++ frontend/tests/controllers/NotificationTest.php
@@ -29,4 +29,74 @@ class NotificationTest extends OmegaupTestCase {
         $this->assertEquals(1, sizeof($notifications));
         $this->assertEquals('testUnread', json_decode($notifications[0]['contents'])->badge);
     }
+
+    public function testReadNotifications() {
+        $user = UserFactory::createUser();
+        NotificationsDAO::create(new Notifications([
+            'user_id' => $user->user_id,
+            'contents' => json_encode(['type' => 'badge', 'badge' => 'testUnread'])
+        ]));
+        NotificationsDAO::create(new Notifications([
+            'user_id' => $user->user_id,
+            'contents' => json_encode(['type' => 'badge', 'badge' => 'testUnread2'])
+        ]));
+        NotificationsDAO::create(new Notifications([
+            'user_id' => $user->user_id,
+            'contents' => json_encode(['type' => 'badge', 'badge' => 'testUnread3'])
+        ]));
+
+        // Get all unread notifications (3) for user
+        $login = self::login($user);
+        $results = NotificationController::apiMyList(new Request([
+            'auth_token' => $login->auth_token,
+            'user' => $user,
+        ]));
+        $notifications = $results['notifications'];
+
+        $ids = [];
+        // Ignore the first unread notification
+        for ($i = 1; $i < sizeof($notifications); $i++) {
+            $ids[] = $notifications[$i]['notification_id'];
+        }
+
+        // Mark notifications as read
+        $results = NotificationController::apiReadNotifications(new Request([
+            'auth_token' => $login->auth_token,
+            'user' => $user,
+            'notifications' => $ids,
+        ]));
+
+        // Get all unread notifications (1) for user
+        $results = NotificationController::apiMyList(new Request([
+            'auth_token' => $login->auth_token,
+            'user' => $user,
+        ]));
+        $notifications = $results['notifications'];
+        $this->assertEquals(1, sizeof($notifications));
+    }
+
+    public function testReadNotificationsExceptions() {
+        $user = UserFactory::createUser();
+        $login = self::login($user);
+        try {
+            NotificationController::apiReadNotifications(new Request([
+                'auth_token' => $login->auth_token,
+                'user' => $user,
+                'notifications' => [],
+            ]));
+            $this->fail('Should have thrown NotFoundException');
+        } catch (NotFoundException $e) {
+            $this->assertEquals($e->getMessage(), 'notificationIdsNotProvided');
+        }
+        try {
+            NotificationController::apiReadNotifications(new Request([
+                'auth_token' => $login->auth_token,
+                'user' => $user,
+                'notifications' => ['10'],
+            ]));
+            $this->fail('Should have thrown NotFoundException');
+        } catch (NotFoundException $e) {
+            $this->assertEquals($e->getMessage(), 'notificationDoesntExist');
+        }
+    }
 }
