00b63b1642e1d5c83dd21fa0bfedecbf7aa7c34e
Carlos Córdova Sáenz <carlosab1802@gmail.com>
2019-07-21 17:22:28 -0500

    Migrando componentes de user pt. 1.2 (#2693)
    Fixes: #2600

--- frontend/www/js/omegaup/ui.d.ts
+++ frontend/www/js/omegaup/ui.d.ts
@@ -5,9 +5,18 @@ declare namespace omegaup {
     escape: (s: string) => string;
     formatDate: (date: Date) => string;
     formatString: (template: string, values: { [key: string]: string; }) => string;
+    markdownConverter: (options?: MarkdownConverterOptions) => Converter;
     navigateTo: (url: string) => void;
     userTypeahead: (elem: HTMLElement, cb: (event: HTMLEvent, val: any) => void) => void;
   };
+
+  interface MarkdownConverterOptions {
+    preview: boolean;
+  }
+
+  interface Converter {
+    makeHtml: (text: string) => string;
+  }
 }
 
 declare let UI: omegaup.UI;
