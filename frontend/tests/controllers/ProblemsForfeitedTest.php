27f57f76f8bdd584e8d758cda1530fc0c2a4d3c8
Carlos Córdova <carlosab1802@gmail.com>
2019-07-22 05:47:40 +0000

    Usando la cosntante en ProblemForfeitedController.

--- frontend/tests/controllers/ProblemsForfeitedTest.php
+++ frontend/tests/controllers/ProblemsForfeitedTest.php
@@ -7,13 +7,13 @@
  */
 
 class ProblemsForfeitedTest extends OmegaupTestCase {
-    const SOLVED_PROBLEMS_PER_ALLOWED_SOLUTION = 10;
-
     public function testGetCounts() {
         $user = UserFactory::createUser();
         $login = self::login($user);
 
-        for ($i = 0; $i < static::SOLVED_PROBLEMS_PER_ALLOWED_SOLUTION; $i++) {
+        for ($i = 0;
+             $i < ProblemForfeitedController::SOLVED_PROBLEMS_PER_ALLOWED_SOLUTION;
+             $i++) {
             $problem = ProblemsFactory::createProblem();
             $run = RunsFactory::createRunToProblem($problem, $user, $login);
             RunsFactory::gradeRun($run);
