0a84b992285747dced59f9b48d828bfc7155d069
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2019-07-22 21:36:43 -0500

    Migrando toda la lógica del archivo course.php a su controlador (#2680)
    Se migra toda la lógica del archivo `frontend/www/course.php` al método
    `CourseController::getIntroDetails` para que sea más fácil realizar las
    pruebas y evitar que falle algo en producción.
    
    Part of: #2665

--- frontend/www/course.php
+++ frontend/www/course.php
@@ -3,72 +3,18 @@ require_once('../server/bootstrap_smarty.php');
 
 UITools::redirectToLoginIfNotLoggedIn();
 
-$r = new Request($_REQUEST);
-$session = SessionController::apiCurrentSession($r)['session'];
-
-$show_assignment = false;
-$intro_details = [];
-
 try {
-    $intro_details = CourseController::apiIntroDetails($r);
-    if (isset($_REQUEST['assignment_alias'])) {
-        $show_assignment = true;
-    }
+    $details = CourseController::getCourseDetailsForSmarty(
+        new Request($_REQUEST)
+    );
 } catch (Exception $e) {
     Logger::getLogger('course')->error('APIException ' . $e);
     header('HTTP/1.1 404 Not Found');
-    die();
+    die(file_get_contents('404.html'));
 }
 
-if ($intro_details['shouldShowResults'] || $intro_details['showAcceptTeacher'] ||
-    ($intro_details['isFirstTimeAccess'] && $intro_details['requests_user_information'] != 'no')) {
-    $smarty->assign('course_payload', [
-        'name' => $intro_details['name'],
-        'description' => $intro_details['description'],
-        'alias' => $intro_details['alias'],
-        'currentUsername' => $session['user']->username,
-        'needsBasicInformation' => $intro_details['basic_information_required']
-            && !is_null($session['identity']) && (!$session['identity']->country_id
-                || !$session['identity']->state_id || !$session['identity']->school_id
-        ),
-        'requestsUserInformation' => $intro_details['requests_user_information'],
-        'showAcceptTeacher' => $intro_details['showAcceptTeacher'],
-        'statements' => [
-            'privacy' => [
-                'markdown' => $intro_details['privacy_statement_markdown'],
-                'gitObjectId' => $intro_details['git_object_id'],
-                'statementType' => $intro_details['statement_type'],
-            ],
-            'acceptTeacher' => [
-                'markdown' => $intro_details['accept_teacher_statement']['markdown'],
-                'gitObjectId' => $intro_details['accept_teacher_statement']['git_object_id'],
-                'statementType' => 'accept_teacher',
-            ],
-        ],
-    ]);
-
-    $smarty->display('../templates/arena.course.intro.tpl');
-} elseif ($show_assignment) {
-    $course = CoursesDAO::getByAlias($_REQUEST['course_alias']);
-    if (is_null($course)) {
-        header('HTTP/1.1 404 Not Found');
-        die();
-    }
-    $group = GroupsDAO::getByPK($course->group_id);
-    if (is_null($group)) {
-        header('HTTP/1.1 404 Not Found');
-        die();
-    }
-    $showScoreboard = $session['valid'] && CourseController::shouldShowScoreboard(
-        $session['identity'],
-        $course,
-        $group
-    );
-    $smarty->assign('showRanking', $showScoreboard);
-    $smarty->display('../templates/arena.contest.course.tpl');
-} else {
-    $course = CoursesDAO::getByAlias($_REQUEST['course_alias']);
-    $showScoreboard = $session['valid'] && Authorization::isCourseAdmin($session['identity'], $course);
-    $smarty->assign('showRanking', $showScoreboard);
-    $smarty->display('../templates/course.details.tpl');
+foreach ($details['smartyProperties'] as $key => $value) {
+    $smarty->assign($key, $value);
 }
+
+$smarty->display("../templates/{$details['template']}");
