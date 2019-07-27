50122bf8552aef33b95f1307a02405e2be9665ac
Juan Pablo Gomez <gomez_aguilar_juan_pablo@hotmail.com>
2019-07-27 02:34:23 +0000

    nit

--- frontend/server/controllers/ContestController.php
+++ frontend/server/controllers/ContestController.php
@@ -2145,8 +2145,8 @@ class ContestController extends Controller {
         $admin_infos = [];
         foreach ($db_results as $result) {
             $admin_id = $result['admin_id'];
-            if (!array_key_exists($admin_id, $admin_infos)) {
-                $data = IdentitiesDAO::findByUserId($admin_id ?? 0);
+            if (!empty($admin_id) && !array_key_exists($admin_id, $admin_infos)) {
+                $data = IdentitiesDAO::findByUserId($admin_id);
                 if (!is_null($data)) {
                     $admin_infos[$admin_id]['user_id'] = $data->user_id;
                     $admin_infos[$admin_id]['username'] = $data->username;
