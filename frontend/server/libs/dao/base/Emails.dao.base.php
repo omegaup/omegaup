99f9b9ae56c901c1f595141595850bffff06b354
lhchavez <lhchavez@lhchavez.com>
2019-08-04 07:41:51 -0700

    Reemplazando DAO::save() por DAO::replace() (#2740)
    Este cambio hace que dejemos de usar DAO::save() para siempre jamás.
    Ahora si se necesita que un registro se cree/reemplace, se puede usar
    DAO::replace(). Esto necesita que las llaves primarias estén ya pobladas
    en el objeto DAO.

--- frontend/server/libs/dao/base/Emails.dao.base.php
+++ frontend/server/libs/dao/base/Emails.dao.base.php
@@ -19,30 +19,6 @@
  */
 abstract class EmailsDAOBase {
     /**
-     * Guardar registros.
-     *
-     * Este metodo guarda el estado actual del objeto {@link Emails}
-     * pasado en la base de datos. La llave primaria indicará qué instancia va
-     * a ser actualizada en base de datos. Si la llave primara o combinación de
-     * llaves primarias que describen una fila que no se encuentra en la base de
-     * datos, entonces save() creará una nueva fila, insertando en ese objeto
-     * el ID recién creado.
-     *
-     * @static
-     * @throws Exception si la operacion fallo.
-     * @param Emails [$Emails] El objeto de tipo Emails
-     * @return Un entero mayor o igual a cero identificando el número de filas afectadas.
-     */
-    final public static function save(Emails $Emails) : int {
-        if (is_null($Emails->email_id) ||
-            is_null(self::getByPK($Emails->email_id))
-        ) {
-            return EmailsDAOBase::create($Emails);
-        }
-        return EmailsDAOBase::update($Emails);
-    }
-
-    /**
      * Actualizar registros.
      *
      * @static
@@ -87,7 +63,7 @@ abstract class EmailsDAOBase {
      * Este metodo eliminará el registro identificado por la llave primaria en
      * el objeto Emails suministrado. Una vez que se ha
      * eliminado un objeto, este no puede ser restaurado llamando a
-     * {@link save()}, ya que este último creará un nuevo registro con una
+     * {@link replace()}, ya que este último creará un nuevo registro con una
      * llave primaria distinta a la que estaba en el objeto eliminado.
      *
      * Si no puede encontrar el registro a eliminar, {@link Exception} será
--- frontend/server/libs/dao/base/Users.dao.base.php
+++ frontend/server/libs/dao/base/Users.dao.base.php
@@ -19,30 +19,6 @@
  */
 abstract class UsersDAOBase {
     /**
-     * Guardar registros.
-     *
-     * Este metodo guarda el estado actual del objeto {@link Users}
-     * pasado en la base de datos. La llave primaria indicará qué instancia va
-     * a ser actualizada en base de datos. Si la llave primara o combinación de
-     * llaves primarias que describen una fila que no se encuentra en la base de
-     * datos, entonces save() creará una nueva fila, insertando en ese objeto
-     * el ID recién creado.
-     *
-     * @static
-     * @throws Exception si la operacion fallo.
-     * @param Users [$Users] El objeto de tipo Users
-     * @return Un entero mayor o igual a cero identificando el número de filas afectadas.
-     */
-    final public static function save(Users $Users) : int {
-        if (is_null($Users->user_id) ||
-            is_null(self::getByPK($Users->user_id))
-        ) {
-            return UsersDAOBase::create($Users);
-        }
-        return UsersDAOBase::update($Users);
-    }
-
-    /**
      * Actualizar registros.
      *
      * @static
@@ -102,7 +78,7 @@ abstract class UsersDAOBase {
      * Este metodo eliminará el registro identificado por la llave primaria en
      * el objeto Users suministrado. Una vez que se ha
      * eliminado un objeto, este no puede ser restaurado llamando a
-     * {@link save()}, ya que este último creará un nuevo registro con una
+     * {@link replace()}, ya que este último creará un nuevo registro con una
      * llave primaria distinta a la que estaba en el objeto eliminado.
      *
      * Si no puede encontrar el registro a eliminar, {@link Exception} será
