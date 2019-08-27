57344a53825bd569669b1d15b02775834be928f1
Carlos Córdova <carlosab1802@gmail.com>
2019-08-27 00:54:54 +0000

    Migrando CodeView a Typescript.

--- frontend/www/js/omegaup/components/arena/CodeView.vue
+++ frontend/www/js/omegaup/components/arena/CodeView.vue
@@ -2,63 +2,88 @@
   <omegaup-arena-codemirror ref="cm-wrapper"
         v-bind:options="editorOptions"
         v-bind:value="value"
-        v-on:change="onChange"
-        v-on:input="onInput"></omegaup-arena-codemirror>
+        v-on:change="$emit('change', value)"
+        v-on:input="$emit('input', value)"></omegaup-arena-codemirror>
 </template>
 
-<script>
-import {T, API} from '../../omegaup.js';
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+import omegaup from '../../api.js';
+import { T } from '../../omegaup.js';
 import UI from '../../ui.js';
-import {codemirror} from 'vue-codemirror-lite';
+import { codemirror } from 'vue-codemirror-lite';
 
-const languageModeMap = {
-  'c': 'text/x-csrc',
-  'cpp': 'text/x-c++src',
-  'java': 'text/x-java',
-  'py': 'text/x-python',
-  'rb': 'text/x-ruby',
-  'pl': 'text/x-perl',
-  'cs': 'text/x-csharp',
-  'pas': 'text/x-pascal',
-  'cat': 'text/plain',
-  'hs': 'text/x-haskell',
-  'cpp11': 'text/x-c++src',
-  'lua': 'text/x-lua',
+const languageModeMap: {
+  [language: string]: string;
+} = {
+  c: 'text/x-csrc',
+  cpp: 'text/x-c++src',
+  java: 'text/x-java',
+  py: 'text/x-python',
+  rb: 'text/x-ruby',
+  pl: 'text/x-perl',
+  cs: 'text/x-csharp',
+  pas: 'text/x-pascal',
+  cat: 'text/plain',
+  hs: 'text/x-haskell',
+  cpp11: 'text/x-c++src',
+  lua: 'text/x-lua',
 };
 
 // Preload all language modes.
-const modeList =
-    ['clike', 'python', 'ruby', 'perl', 'pascal', 'haskell', 'lua'];
+const modeList: string[] = [
+  'clike',
+  'python',
+  'ruby',
+  'perl',
+  'pascal',
+  'haskell',
+  'lua',
+];
+
 for (const mode of modeList) {
-  require('codemirror/mode/' + mode + '/' + mode + '.js');
+  require(`codemirror/mode/${mode}/${mode}.js`);
 }
 
-export default {
-  props: {
-    language: String,
-    readonly: Boolean,
-    value: String,
-  },
-  data: function() {
-    return { mode: languageModeMap[this.language] }
-  },
-  computed: {
-    editorOptions: function() {
-      return {
-        tabSize: 2, lineNumbers: true, mode: this.mode, readOnly: this.readonly
-      }
-    }
-  },
-  methods: {
-    onChange: function(value) { this.$emit('change', value);},
-    onInput: function(value) { this.$emit('input', value);},
-  },
-  watch: {
-    language: function(newLanguage) { this.mode = languageModeMap[newLanguage];}
-  },
+interface EditorOptions {
+  tabSize: number;
+  lineNumbers: boolean;
+  mode?: string;
+  readOnly: boolean;
+}
+
+@Component({
   components: {
-    "omegaup-arena-codemirror": codemirror,
+    'omegaup-arena-codemirror': codemirror,
+  },
+})
+export default class ArenaCodeView extends Vue {
+  @Prop() language!: string;
+  @Prop() readonly!: boolean;
+  @Prop() value!: string;
+
+  T = T;
+  mode?: string = languageModeMap[this.language];
+
+  data(): { [key: string]: any } {
+    return {
+      mode: languageModeMap[this.language],
+    };
   }
-};
+
+  get editorOptions(): EditorOptions {
+    return {
+      tabSize: 2,
+      lineNumbers: true,
+      mode: this.mode,
+      readOnly: this.readonly,
+    };
+  }
+
+  @Watch('language')
+  onLanguageChange(newLanguage: string) {
+    this.mode = languageModeMap[newLanguage];
+  }
+}
 
 </script>
