43b865ee2897ea7c771a71c7d256aff6c2acd341
Carlos Córdova <carlosab1802@gmail.com>
2019-08-30 02:20:13 +0000

    Actualizando la manera de mapear 'yes' y 'no' a booleanos.

--- frontend/database/103_groups_identities_make_accept_teacher_boolean.sql
+++ frontend/database/103_groups_identities_make_accept_teacher_boolean.sql
@@ -5,12 +5,7 @@ ALTER TABLE `Groups_Identities`
   ADD `accept_teacher` tinyint(1) DEFAULT NULL COMMENT 'Almacena la respuesta del participante de un curso si acepta al organizador como su maestro.';
 
 UPDATE `Groups_Identities`
-  SET `accept_teacher` = 1
-  WHERE `accept_t` = 'yes';
-
-UPDATE `Groups_Identities`
-  SET `accept_teacher` = 0
-  WHERE `accept_t` = 'no';
+  SET `accept_teacher` = (`accept_t` = 'yes');
 
 ALTER TABLE `Groups_Identities`
   DROP COLUMN `accept_t`;
