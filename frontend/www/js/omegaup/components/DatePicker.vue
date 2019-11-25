87a605f0f7f70402a40ede6c6f1701ceca019d4c
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2019-11-25 14:49:34 -0600

    Revirtiendo cambio que arreglaba los DatePickers (#3105)
    Haciendo revert del cambio #3065 para que la página de Editar Cursos no se quede
    congelada.
    
    Partially Fixes: #3103

--- frontend/www/js/omegaup/components/DatePicker.vue
+++ frontend/www/js/omegaup/components/DatePicker.vue
@@ -1,17 +1,13 @@
 <template>
   <input class="form-control"
-        required="required"
         size="16"
-        type="date"
-        v-bind:disabled="!enabled"
-        v-bind:readonly="usedFallback"
-        v-model="stringValue">
+        type="text"
+        v-bind:disabled="!enabled">
 </template>
 
 <script lang="ts">
 import { Vue, Component, Watch, Prop } from 'vue-property-decorator';
 import { T } from '../omegaup.js';
-import UI from '../ui.js';
 
 @Component
 export default class DatePicker extends Vue {
@@ -21,21 +17,8 @@ export default class DatePicker extends Vue {
   @Prop({ default: true }) enabled!: boolean;
   @Prop({ default: T.datePickerFormat }) format!: string;
 
-  private usedFallback: boolean = false;
-  private stringValue: string = UI.formatDateLocal(this.value);
-
   mounted() {
-    if ((this.$el as HTMLInputElement).type === 'text') {
-      // Even though we declared the input as having date type,
-      // browsers that don't support it will silently change the type to ext'.
-      // In that case, use the bootstrap datepicker.
-      this.mountedFallback();
-    }
-  }
-
-  private mountedFallback() {
     let self = this;
-    self.usedFallback = true;
     $(self.$el)
       .datepicker({
         weekStart: 1,
@@ -47,21 +30,9 @@ export default class DatePicker extends Vue {
       .datepicker('setValue', self.value);
   }
 
-  @Watch('stringValue')
-  onStringValueChanged(newStringValue: string) {
-    if (this.usedFallback) {
-      // If the fallback was used, we don't need to update anything.
-      return;
-    }
-    this.$emit('input', new Date(newStringValue));
-  }
-
   @Watch('value')
-  onPropertyChanged(newValue: Date) {
-    this.stringValue = UI.formatDateLocal(newValue);
-    if (this.usedFallback) {
-      $(this.$el).datepicker('setValue', newValue);
-    }
+  onPropertyChanged(newValue: string) {
+    $(this.$el).datepicker('setValue', newValue);
   }
 }
 
