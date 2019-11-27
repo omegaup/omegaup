87c7bc3cb9a56f39a99a7fc6a800bd4a38283431
Carlos Córdova <carlosab1802@gmail.com>
2019-11-27 14:49:20 +0000

    Lint fixes.

--- frontend/www/schools/profile.php
+++ frontend/www/schools/profile.php
@@ -3,20 +3,20 @@ namespace OmegaUp;
 require_once(dirname(__DIR__, 2) . '/server/bootstrap.php');
 
 try {
-  $result = \OmegaUp\Controllers\School::apiProfileDetails(
-    new \OmegaUp\Request($_REQUEST)
-  );
+    $result = \OmegaUp\Controllers\School::apiProfileDetails(
+        new \OmegaUp\Request($_REQUEST)
+    );
 } catch (\OmegaUp\Exceptions\ApiException $e) {
-  \OmegaUp\ApiCaller::handleException($e);
+    \OmegaUp\ApiCaller::handleException($e);
 }
 
 \OmegaUp\UITools::getSmartyInstance()->assign('profile', $result);
 
 \OmegaUp\UITools::getSmartyInstance()->display(
-  sprintf(
-    '%s/templates/school.profile.tpl',
-    strval(
-        OMEGAUP_ROOT
+    sprintf(
+        '%s/templates/school.profile.tpl',
+        strval(
+            OMEGAUP_ROOT
+        )
     )
-  )
 );
