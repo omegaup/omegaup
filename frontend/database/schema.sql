cd1181be0fba24679761731342eb4b2edc1c1fce
Carlos Córdova Sáenz <carlosab1802@gmail.com>
2019-12-14 18:21:58 -0500

    Agregando migración para Escuela del Mes (#3174)
    Fixes: #3169

--- frontend/database/schema.sql
+++ frontend/database/schema.sql
@@ -778,6 +778,22 @@ CREATE TABLE `Runs` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `School_Of_The_Month` (
+  `school_of_the_month_id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `time` date NOT NULL DEFAULT '2000-01-01',
+  `rank` int(11) NOT NULL COMMENT 'El lugar que tuvo la escuela en el mes.',
+  `selected_by` int(11) DEFAULT NULL COMMENT 'Identidad que seleccionó a la escuela.',
+  PRIMARY KEY (`school_of_the_month_id`),
+  KEY `school_of_the_month_id` (`school_of_the_month_id`),
+  KEY `school_id` (`school_id`),
+  KEY `selected_by` (`selected_by`),
+  CONSTRAINT `fk_sotmi_identity_id` FOREIGN KEY (`selected_by`) REFERENCES `Identities` (`identity_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_sotms_school_id` FOREIGN KEY (`school_id`) REFERENCES `Schools` (`school_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Escuelas del Mes';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Schools` (
   `school_id` int(11) NOT NULL AUTO_INCREMENT,
   `country_id` char(3) DEFAULT NULL,
