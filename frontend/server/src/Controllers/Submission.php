93fddc2d91819ad98890313a2719bb43f15d0542
Carlos Córdova <carlosab1802@gmail.com>
2019-12-22 17:22:56 +0000

    Agregando API para retornar los últimos submissions.

--- frontend/server/src/Controllers/Submission.php
+++ frontend/server/src/Controllers/Submission.php
@@ -9,4 +9,22 @@ class Submission extends \OmegaUp\Controllers\Controller {
     public static function getSource(string $guid): string {
         return \OmegaUp\Grader::GetInstance()->getSource($guid);
     }
+
+    /**
+     * Returns the latest submissions
+     *
+     * @return array{submissions: list<array{time: int, username: string, school_id: int, school_name: string, alias: string, title: string, language: string, verdict: string, runtime: int, memory: int}>, totalRows: int}
+     */
+    public static function apiLatestSubmissions(\OmegaUp\Request $r) {
+        $r->ensureInt('offset', null, null, false);
+        $r->ensureInt('rowcount', null, null, false);
+
+        $offset = is_null($r['offset']) ? 1 : intval($r['offset']);
+        $rowCount = is_null($r['rowcount']) ? 100 : intval($r['rowcount']);
+
+        return \OmegaUp\DAO\Submissions::getLatestSubmissions(
+            $offset,
+            $rowCount
+        );
+    }
 }
