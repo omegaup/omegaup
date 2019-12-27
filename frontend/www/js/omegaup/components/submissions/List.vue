1d3d7d3cd5a39e05f2a32d00242993408c4654d5
Carlos Córdova <carlosab1802@gmail.com>
2019-12-27 15:56:22 +0000

    Avance parcial.

---
+++ frontend/www/js/omegaup/components/submissions/List.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="row">
+    <div class="page-header">
+      <h1 class="text-center">
+        {{ T.submissionsListTitle }}
+      </h1>
+    </div>
+    <div class="row">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            {{
+              UI.formatString(T.submissionsRangeHeader, {
+                lowCount: (page - 1) * length + 1,
+                highCount: page * length,
+              })
+            }}
+          </h3>
+        </div>
+        <div class="panel-body">
+          <template v-if="page > 1">
+            <a class="prev" v-bind:href="`/submissions?page=${page - 1}`">
+              {{ T.wordsPrevPage }}</a
+            >
+            <span class="delimiter" v-show="shouldShowNextPage">|</span>
+          </template>
+          <a
+            class="next"
+            v-show="shouldShowNextPage"
+            v-bind:href="`/submissions?page=${page + 1}`"
+            >{{ T.wordsNextPage }}
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { T } from '../../omegaup.js';
+import omegaup from '../../api.js';
+import UI from '../../ui.js';
+
+@Component
+export default class SubmissionsList extends Vue {
+  @Prop() page!: number;
+  @Prop() length!: number;
+  @Prop() includeUser!: boolean;
+  @Prop() totalRows!: number;
+  @Prop() submissions!: omegaup.Submission[];
+
+  T = T;
+  UI = UI;
+
+  get shouldShowNextPage(): boolean {
+    return this.length * this.page < this.totalRows;
+  }
+}
+</script>
