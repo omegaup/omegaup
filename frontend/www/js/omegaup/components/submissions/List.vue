7fb0e2c61fba1b863c463c18597039ac4c591521
Carlos Córdova <carlosab1802@gmail.com>
2019-12-27 16:11:56 +0000

    Agregando header a la tabla de envíos.

--- frontend/www/js/omegaup/components/submissions/List.vue
+++ frontend/www/js/omegaup/components/submissions/List.vue
@@ -5,32 +5,43 @@
         {{ T.submissionsListTitle }}
       </h1>
     </div>
-    <div class="row">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">
-            {{
-              UI.formatString(T.submissionsRangeHeader, {
-                lowCount: (page - 1) * length + 1,
-                highCount: page * length,
-              })
-            }}
-          </h3>
-        </div>
-        <div class="panel-body">
-          <template v-if="page > 1">
-            <a class="prev" v-bind:href="`/submissions?page=${page - 1}`">
-              {{ T.wordsPrevPage }}</a
-            >
-            <span class="delimiter" v-show="shouldShowNextPage">|</span>
-          </template>
-          <a
-            class="next"
-            v-show="shouldShowNextPage"
-            v-bind:href="`/submissions?page=${page + 1}`"
-            >{{ T.wordsNextPage }}
-          </a>
-        </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{
+            UI.formatString(T.submissionsRangeHeader, {
+              lowCount: (page - 1) * length + 1,
+              highCount: page * length,
+            })
+          }}
+        </h3>
+      </div>
+      <div class="panel-body">
+        <template v-if="page > 1">
+          <a class="prev" v-bind:href="`/submissions?page=${page - 1}`">
+            {{ T.wordsPrevPage }}</a
+          >
+          <span class="delimiter" v-show="shouldShowNextPage">|</span>
+        </template>
+        <a
+          class="next"
+          v-show="shouldShowNextPage"
+          v-bind:href="`/submissions?page=${page + 1}`"
+          >{{ T.wordsNextPage }}
+        </a>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>{{ T.wordsTime }}</th>
+              <th>{{ T.wordsUser }}</th>
+              <th>{{ T.wordsProblem }}</th>
+              <th>{{ T.wordsLanguage }}</th>
+              <th>{{ T.wordsVerdict }}</th>
+              <th>{{ T.wordsRuntime }}</th>
+              <th>{{ T.wordsMemory }}</th>
+            </tr>
+          </thead>
+        </table>
       </div>
     </div>
   </div>
