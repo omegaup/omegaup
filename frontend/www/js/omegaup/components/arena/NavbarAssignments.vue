547874f9c0585d7854477a3f7ce4d566eb505a24
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2020-01-04 02:13:32 +0000

    Revirtiendo traducciones que ya no se ocupan

--- frontend/www/js/omegaup/components/arena/NavbarAssignments.vue
+++ frontend/www/js/omegaup/components/arena/NavbarAssignments.vue
@@ -1,47 +1,38 @@
 <template>
   <div class="navbar-assignments">
-    <div class="text-left col-md-12 col-sm-12 col-xs-12">
-      <a
-        class="btn btn-primary btn-sm prev"
-        title=""
-        v-on:click="$emit('navigate-to-assignment', previousAssignment.alias)"
-        v-bind:class="{ disabled: previousAssignment === null }"
-        v-bind:title="
-          previousAssignment !== null ? previousAssignment.name : ''
-        "
-        role="button"
-      >
-        <span
-          class="glyphicon glyphicon-chevron-left"
-          aria-hidden="true"
-          v-if="previousAssignment !== null"
-        ></span
-        >{{ previousAssignment !== null ? previousAssignment.name : '-' }}</a
-      >
-    </div>
-    <div class="text-right col-md-12 col-sm-12 col-xs-12">
-      <a
-        class="btn btn-primary btn-sm next"
-        title=""
-        v-on:click="$emit('navigate-to-assignment', nextAssignment.alias)"
-        v-bind:class="{ disabled: nextAssignment === null }"
-        v-bind:title="nextAssignment !== null ? nextAssignment.name : ''"
-        role="button"
-      >
-        {{ nextAssignment !== null ? nextAssignment.name : '-'
-        }}<span
-          class="glyphicon glyphicon-chevron-right"
-          aria-hidden="true"
-          v-if="nextAssignment !== null"
-        ></span
-      ></a>
-    </div>
+    <button
+      class="btn btn-primary col-md-12 col-sm-12 col-xs-12"
+      v-on:click="$emit('navigate-to-assignment', previousAssignment.alias)"
+      v-bind:disabled="previousAssignment === null"
+      v-bind:title="previousAssignment !== null ? previousAssignment.name : ''"
+      role="button"
+    >
+      <span
+        class="glyphicon glyphicon-chevron-left"
+        aria-hidden="true"
+        v-if="previousAssignment !== null"
+      ></span
+      >{{ previousAssignment !== null ? previousAssignment.name : '-' }}
+    </button>
+    <button
+      class="btn btn-primary col-md-12 col-sm-12 col-xs-12"
+      v-on:click="$emit('navigate-to-assignment', nextAssignment.alias)"
+      v-bind:disabled="nextAssignment === null"
+      v-bind:title="nextAssignment !== null ? nextAssignment.name : ''"
+      role="button"
+    >
+      {{ nextAssignment !== null ? nextAssignment.name : '-'
+      }}<span
+        class="glyphicon glyphicon-chevron-right"
+        aria-hidden="true"
+        v-if="nextAssignment !== null"
+      ></span>
+    </button>
   </div>
 </template>
 
 <style>
-.navbar-assignments > div > a.btn-primary {
-  width: 100%;
+.navbar-assignments > button {
   margin-bottom: 0.5em;
   white-space: nowrap;
   overflow: hidden;
