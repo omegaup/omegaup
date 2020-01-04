5f51cbb59170d1bacc037ad5baf47e0c24cc85cc
lhchavez <lhchavez@lhchavez.com>
2020-01-04 13:56:55 -0800

    Arreglando las anotaciones de tipos de libinteractive (#3277)

--- frontend/server/src/Controllers/Problem.php
+++ frontend/server/src/Controllers/Problem.php
@@ -4059,4 +4059,146 @@ class Problem extends \OmegaUp\Controllers\Controller {
             $problemArtifacts->getByRevision()
         );
     }
+
+    /**
+     * @return array{smartyProperties: array{error?: string, error_field?: string}, template: string}
+     */
+    public static function getLibinteractiveGenForSmarty(\OmegaUp\Request $r): array {
+        if (count($r) === 0) {
+            // \OmegaUp\Request does not support empty().
+            return [
+                'smartyProperties' => [],
+                'template' => 'libinteractive.gen.tpl',
+            ];
+        }
+        try {
+            \OmegaUp\Validators::validateInEnum(
+                $r['language'],
+                'language',
+                ['c', 'cpp', 'java']
+            );
+            \OmegaUp\Validators::validateInEnum(
+                $r['os'],
+                'os',
+                ['unix', 'windows']
+            );
+            \OmegaUp\Validators::validateValidAlias(
+                $r['name'],
+                'name'
+            );
+            \OmegaUp\Validators::validateStringNonEmpty(
+                $r['idl'],
+                'idl'
+            );
+        } catch (\OmegaUp\Exceptions\InvalidParameterException $e) {
+            return [
+                'smartyProperties' => [
+                    'error' => \OmegaUp\Translations::getInstance()->get(
+                        'parameterInvalid'
+                    ) ?? 'parameterInvalid',
+                    'error_field' => strval($e->parameter),
+                ],
+                'template' => 'libinteractive.gen.tpl',
+            ];
+        }
+        $dirname = \OmegaUp\FileHandler::TempDir(
+            sys_get_temp_dir(),
+            'libinteractive'
+        );
+        try {
+            file_put_contents("{$dirname}/{$r['name']}.idl", $r['idl']);
+            $args = [
+                '/usr/bin/java',
+                '-jar',
+                '/usr/share/java/libinteractive.jar',
+                'generate',
+                "{$r['name']}.idl",
+                $r['language'],
+                $r['language'],
+                '--makefile',
+                "--{$r['os']}",
+            ];
+            $descriptorspec = [
+                0 => ['pipe', 'r'],
+                1 => ['pipe', 'w'],
+                2 => ['pipe', 'w']
+            ];
+            $cmd = join(' ', array_map('escapeshellarg', $args));
+            $proc = proc_open(
+                $cmd,
+                $descriptorspec,
+                $pipes,
+                $dirname,
+                ['LANG' => 'en_US.UTF-8']
+            );
+            if (!is_resource($proc)) {
+                return [
+                    'smartyProperties' => [
+                        'error' => strval(error_get_last()),
+                    ],
+                    'template' => 'libinteractive.gen.tpl',
+                ];
+            }
+            fclose($pipes[0]);
+            $output = stream_get_contents($pipes[1]);
+            fclose($pipes[1]);
+            $err = stream_get_contents($pipes[2]);
+            fclose($pipes[2]);
+            $retval = proc_close($proc);
+
+            if ($retval != 0) {
+                return [
+                    'smartyProperties' => [
+                        'error' => "{$output}{$err}",
+                    ],
+                    'template' => 'libinteractive.gen.tpl',
+                ];
+            }
+            $zip = new \ZipArchive();
+            $zip->open(
+                "{$dirname}/interactive.zip",
+                \ZipArchive::CREATE | \ZipArchive::OVERWRITE
+            );
+
+            $files = new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator($dirname),
+                \RecursiveIteratorIterator::LEAVES_ONLY
+            );
+
+            /** @var \SplFileInfo $file */
+            foreach ($files as $name => $file) {
+                if ($file->isDir()) {
+                    continue;
+                }
+                if ($file->getFilename() == 'interactive.zip') {
+                    continue;
+                }
+
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($dirname) + 1);
+
+                // Add current file to archive
+                $zip->addFile($filePath, $relativePath);
+            }
+
+            $zip->close();
+
+            header('Content-Type: application/zip');
+            header(
+                "Content-Disposition: attachment; filename={$r['name']}.zip"
+            );
+            readfile("{$dirname}/interactive.zip");
+            \OmegaUp\FileHandler::deleteDirRecursively($dirname);
+            die();
+        } catch (\Exception $e) {
+            return [
+                'smartyProperties' => [
+                    'error' => strval($e),
+                ],
+                'template' => 'libinteractive.gen.tpl',
+            ];
+        } finally {
+            \OmegaUp\FileHandler::deleteDirRecursively($dirname);
+        }
+    }
 }
