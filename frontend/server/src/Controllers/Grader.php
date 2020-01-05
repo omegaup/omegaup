cf5728f22803a9c65ba8bd17d8c00edde20caef3
lhchavez <lhchavez@lhchavez.com>
2020-01-05 02:45:05 +0000

    Arreglando anotaciones de tipos a \OmegaUp\Controllers\Grader
    Resulta que algunas anotaciones no fueron agregadas.

--- frontend/server/src/Controllers/Grader.php
+++ frontend/server/src/Controllers/Grader.php
@@ -11,10 +11,9 @@ class Grader extends \OmegaUp\Controllers\Controller {
     /**
      * Calls to /status grader
      *
-     * @param \OmegaUp\Request $r
-     * @return array
+     * @return array{grader: array{status: string, broadcaster_sockets: int, embedded_runner: bool, queue: array{running: list<array{name: string, id: int}>, run_queue_length: int, runner_queue_length: int, runners: list<string>}}}
      */
-    public static function apiStatus(\OmegaUp\Request $r) {
+    public static function apiStatus(\OmegaUp\Request $r): array {
         $r->ensureIdentity();
 
         if (!\OmegaUp\Authorization::isSystemAdmin($r->identity)) {
