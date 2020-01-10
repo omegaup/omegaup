79e47cee0b77f8c7dcf95026421b6928bd923284
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2020-01-10 15:34:43 +0000

    Arreglando un tipo que marcaba como incorrecto en psalm

--- frontend/database/122_recalculate_max_points_in_problems_with_no_submissions.sql
+++ frontend/database/122_recalculate_max_points_in_problems_with_no_submissions.sql
@@ -2,27 +2,27 @@
 UPDATE
   Problemset_Problems p
 INNER JOIN
-	(
-		SELECT
-		    p.title,
-		    psp.problemset_id,
-		    p.problem_id,
-		    psp.points,
-		    p.languages
-		FROM
-		    Problemsets ps
-		INNER JOIN
-		    Problemset_Problems psp
-		ON
-		    psp.problemset_id = ps.problemset_id
-		INNER JOIN
-		    Problems p
-		ON
-		    p.problem_id = psp.problem_id
-		WHERE
-		    ps.type = 'Assignment'
-		    AND p.languages = ''
-	) q
+  (
+    SELECT
+      p.title,
+      psp.problemset_id,
+      p.problem_id,
+      psp.points,
+      p.languages
+    FROM
+      Problemsets ps
+    INNER JOIN
+      Problemset_Problems psp
+    ON
+      psp.problemset_id = ps.problemset_id
+    INNER JOIN
+      Problems p
+    ON
+      p.problem_id = psp.problem_id
+    WHERE
+      ps.type = 'Assignment'
+    AND p.languages = ''
+  ) q
 ON
   p.problemset_id = q.problemset_id AND p.problem_id = q.problem_id
 SET
@@ -30,22 +30,22 @@ SET
 
 -- Recalculate max_points to all affected assignments
 UPDATE
-    Assignments a
+  Assignments a
 JOIN
-	(
-		SELECT
-			assignment_id,
-			sum(psp.points) AS max_points
-    	FROM
-			Assignments a
-		INNER JOIN
-			Problemset_Problems psp
-        ON
-        	a.problemset_id = psp.problemset_id
-        GROUP BY
-        	a.assignment_id
-    ) q
+  (
+    SELECT
+      assignment_id,
+      sum(psp.points) AS max_points
+    FROM
+      Assignments a
+    INNER JOIN
+      Problemset_Problems psp
+    ON
+      a.problemset_id = psp.problemset_id
+    GROUP BY
+      a.assignment_id
+  ) q
 ON
-    a.assignment_id = q.assignment_id
+  a.assignment_id = q.assignment_id
 SET
-    a.max_points = q.max_points;
+  a.max_points = q.max_points;
