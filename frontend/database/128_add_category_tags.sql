6f64ef6313b892a21127c03bb156023c42505b19
Carlos Córdova <carlosab1802@gmail.com>
2020-01-27 14:43:06 +0000

    Agregando categorías de problemas y sus traducciones.

---
+++ frontend/database/128_add_category_tags.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    `Tags` (`name`)
+VALUES
+    ('problemCategoryOpenAnswer'),
+    ('problemCategoryKarelEducation'),
+    ('problemCategoryIntroductionToProgramming'),
+    ('problemCategoryMathematicalProblems'),
+    ('problemCategoryElementalDataStructures'),
+    ('problemCategoryAlgorithmsAndNetworksOptimization'),
+    ('problemCategoryCompetitiveProgramming'),
+    ('problemCategorySpecializedTopics');
