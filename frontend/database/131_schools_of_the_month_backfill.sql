5c2fef05e17d1aca83dba0d9f3b4ce55009d0b1f
Carlos Córdova <carlosab1802@gmail.com>
2020-01-29 16:53:50 +0000

    Agregando backfill de escuelas del mes.

---
+++ frontend/database/131_schools_of_the_month_backfill.sql
@@ -0,0 +1,209 @@
+-- DICIS UGTO
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2018-10-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 2413;
+
+-- OMI_COBACH
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2018-11-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 2534;
+
+-- CONALEP 252 Veracruz
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2018-12-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 355;
+
+-- CECyT Zacatecas
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-01-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 916;
+
+-- CBTIS 217
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-02-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 25;
+
+-- FCFM, UANL
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-03-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 1284;
+
+-- CBTA 39 Morelos
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-04-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 200;
+
+-- Instituto Tecnologico de Lazaro Cardenas
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-05-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 587;
+
+-- UAS Sinaloa
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-06-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 693;
+
+-- UAM Azcapotzalco
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-07-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 537;
+
+-- ITSUR
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-08-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 53;
+
+-- COBACHI Chihuahua
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-09-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 2534;
+
+-- CBTIS 217
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-10-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 25;
+
+--  CECyT 18
+INSERT INTO `School_Of_The_Month` (
+  `school_id`,
+  `time`,
+  `rank`
+)
+SELECT
+    `s`.`school_id`,
+    '2019-11-01',
+    '1'
+FROM
+    `Schools` `s`
+WHERE
+    `s`.`school_id` = 916;
