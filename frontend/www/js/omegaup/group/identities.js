576ed4c3c7660be8dc6a9717695ca949a2a627f2
lhchavez <lhchavez@lhchavez.com>
2020-02-03 03:26:22 +0000

    Agregando un botón para descargar las identidades generadas
    Esto ahorra copiar y pegar cosas como cavernícola.
    
    Fixes: #3415

--- frontend/www/js/omegaup/group/identities.js
+++ frontend/www/js/omegaup/group/identities.js
@@ -21,6 +21,27 @@ OmegaUp.on('ready', function() {
               })
               .fail(UI.apiError);
           },
+          'download-identities': function(identities) {
+            const csv = CSV.serialize({
+              fields: [
+                { id: 'username' },
+                { id: 'name' },
+                { id: 'password' },
+                { id: 'country_id' },
+                { id: 'state_id' },
+                { id: 'gender' },
+                { id: 'school_name' },
+              ],
+              records: identities,
+            });
+            const hiddenElement = document.createElement('a');
+            hiddenElement.href = `data:text/csv;charset=utf-8,${window.encodeURI(
+              csv,
+            )}`;
+            hiddenElement.target = '_blank';
+            hiddenElement.download = 'identities.csv';
+            hiddenElement.click();
+          },
           'read-csv': function(identitiesComponent, fileUpload) {
             identitiesComponent.identities = [];
             CSV.fetch({
