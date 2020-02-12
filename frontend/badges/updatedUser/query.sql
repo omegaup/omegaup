ed14f983298bc82c987e206d5feb0b3fc2b27ba8
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-02-12 22:19:09 +0000

    Badge perfil actualizado

--- frontend/badges/updatedUser/query.sql
+++ frontend/badges/updatedUser/query.sql
@@ -1,10 +1,17 @@
 SELECT
-    `user_id`
+    DISTINCT `u`.`user_id`
 FROM
-    `Identities`
+    `Identities` As `i`
+INNER JOIN
+	`Users` AS `u` ON `i`.`user_id`=`u`.`user_id`
 where
-    `language_id` IS NOT NULL AND
-    `country_id`IS NOT NULL AND
-    `state_id` IS NOT NULL AND
-    `gender` IS NOT NULL AND
-    `current_identity_school_id` IS NOT NULL ;
+    `i`.`language_id` IS NOT NULL AND
+    `i`.`country_id`IS NOT NULL AND
+    `i`.`state_id` IS NOT NULL AND
+    `i`.`gender` IS NOT NULL AND
+    `i`.`current_identity_school_id` IS NOT NULL AND
+    `u`.`scholar_degree` IS NOT NULL AND
+    `u`.`preferred_language` IS NOT NULL AND
+    `u`.`birth_date` IS NOT NULL
+GROUP BY
+    `u`.`user_id`;
