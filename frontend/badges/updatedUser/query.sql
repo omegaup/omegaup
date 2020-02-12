ccfea7ea29e21c0393930ae6bfd13251a6aa15d8
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-02-12 16:15:44 +0000

    formato

--- frontend/badges/updatedUser/query.sql
+++ frontend/badges/updatedUser/query.sql
@@ -1,10 +1,10 @@
-SELECT 
+SELECT
     `user_id`
-FROM 
+FROM
     `Identities`
-where 
-    `language_id` IS NOT NULL AND 
-    `country_id`IS NOT NULL AND 
-    `state_id` IS NOT NULL AND 
-    `gender` IS NOT NULL AND 
+where
+    `language_id` IS NOT NULL AND
+    `country_id`IS NOT NULL AND
+    `state_id` IS NOT NULL AND
+    `gender` IS NOT NULL AND
     `current_identity_school_id` IS NOT NULL ;
