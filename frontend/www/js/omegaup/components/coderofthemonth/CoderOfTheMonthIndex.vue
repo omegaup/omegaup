9c55ec10da28840baeda3bfd1d9b4849c1e7c59b
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2020-02-13 05:16:34 +0000

    Agregando algunos componentes en Vue de la vista index

---
+++ frontend/www/js/omegaup/components/coderofthemonth/CoderOfTheMonthIndex.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">{{ T.index }}</h3>
+    </div>
+    <div class="panel-body">
+      <div class="rss_element">
+        <h4 class="text-center">
+          <a v-bind:href="profileLink">
+            {{ username }}
+          </a>
+          <omegaup-country-flag
+            v-bind:country="country_id"
+          ></omegaup-country-flag>
+        </h4>
+        <div class="text-center">
+          <a v-bind:href="profileLink">
+            <img v-bind:src="gravatar_92" />
+          </a>
+        </div>
+        <div>{{ name }}</div>
+        <div>{{ school }}</div>
+        <div v-if="state && country !== 'xx">{{ state }}, {{ country }}</div>
+      </div>
+    </div>
+    <div class="panel-body">
+      <a href="/coderofthemonth/">{{ T.coderOfTheMonthFullList }}</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { T } from '../../omegaup.js';
+import countryFlag from '../CountryFlag.vue';
+
+@Component({
+  components: {
+    'omegaup-country-flag': countryFlag,
+  },
+})
+export default class CoderOfTheMonthIndex extends Vue {
+  @Prop() username!: string;
+  @Prop() name!: string;
+  @Prop() country!: string;
+  @Prop() country_id!: string;
+  @Prop() state!: string;
+  @Prop() school!: string;
+  @Prop() gravatar_92!: string;
+
+  T = T;
+
+  get profileLink(): string {
+    return `/profile/${username}/`;
+  }
+}
+</script>
---
+++ frontend/www/js/omegaup/components/contest/Next.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">{{ T.frontPageNextContests }}</h3>
+    </div>
+    <ul class="list-group" v-for="contest in contests">
+      <a class="list-group-item" v-bind:href="contest.alias">{{
+        contest.title
+      }}</a>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { T } from '../../omegaup.js';
+import omegaup from '../../api.js';
+
+@Component
+export default class SocialMedia extends Vue {
+  @Prop() contests!: omegaup.Contest[];
+
+  T = T;
+}
+</script>
