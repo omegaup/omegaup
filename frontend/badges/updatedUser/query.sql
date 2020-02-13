5f2bd58cece109c5733a7fc3a407e2e31e130c6e
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-02-13 17:06:02 +0000

    Observaciones

--- frontend/badges/updatedUser/query.sql
+++ frontend/badges/updatedUser/query.sql
@@ -3,8 +3,10 @@ SELECT
 FROM
     `Identities` As `i`
 INNER JOIN
-	`Users` AS `u` ON `i`.`user_id`=`u`.`user_id`
-where
+    `Users` AS `u` ON `i`.`user_id` = `u`.`user_id`
+INNER JOIN
+    `Identities_Schools` AS `s` ON `s`.`identity_id` = `i`.`identity_id`
+WHERE
     `i`.`language_id` IS NOT NULL AND
     `i`.`country_id`IS NOT NULL AND
     `i`.`state_id` IS NOT NULL AND
@@ -12,6 +14,7 @@ where
     `i`.`current_identity_school_id` IS NOT NULL AND
     `u`.`scholar_degree` IS NOT NULL AND
     `u`.`preferred_language` IS NOT NULL AND
-    `u`.`birth_date` IS NOT NULL
+    `u`.`birth_date` IS NOT NULL AND
+    `s`.`graduation_date` IS NOT NULL
 GROUP BY
     `u`.`user_id`;
