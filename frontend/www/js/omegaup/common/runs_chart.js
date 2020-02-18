8594d62e835b6dc32c2747ed63a0f5ab6d674aa9
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2020-02-18 18:59:15 +0000

    Regresando runs_chart a highchart normalito

--- frontend/www/js/omegaup/common/runs_chart.js
+++ frontend/www/js/omegaup/common/runs_chart.js
@@ -1,72 +1,54 @@
-import common_RunsChart from '../components/common/RunsChart.vue';
-import { API, UI, OmegaUp, T } from '../omegaup.js';
-import Vue from 'vue';
+import { OmegaUp, T } from '../omegaup.js';
 
 OmegaUp.on('ready', function() {
   const payload = JSON.parse(
     document.getElementById('runs-chart-payload').innerText,
   );
 
-  if (payload.total.length === 0) return;
+  const minY = payload.total.length === 0 ? 0 : payload.total[0] / 2.0;
 
-  const minY = payload.total[0] - payload.total[0] / 2.0;
-
-  const commonRunsChart = new Vue({
-    el: '#runs-chart',
-    render: function(createElement) {
-      return createElement('omegaup-common-runschart', {
-        props: {
-          chartOptions: this.chartOptions,
-        },
-      });
+  window.chart = new Highcharts.Chart({
+    chart: {
+      type: 'area',
+      renderTo: 'runs-chart',
+      height: 300,
+      spacingTop: 20,
     },
-    data: {
-      chartOptions: {
-        chart: {
-          type: 'area',
-          height: 300,
-          spacingTop: 20,
-        },
-        title: { text: T.wordsTotalRuns },
-        xAxis: {
-          type: 'datetime',
-          title: { text: null },
-          categories: payload.date.reverse(),
-        },
-        yAxis: { title: { text: T.wordsRuns }, min: minY },
-        legend: { enabled: false },
-        plotOptions: {
-          area: {
-            lineWidth: 1,
-            marker: { enabled: false },
-            shadow: false,
-            states: { hover: { lineWidth: 1 } },
-            threshold: null,
-          },
-        },
-        series: [
-          {
-            type: 'area',
-            name: T.wordsRuns,
-            data: payload.total.reverse(),
-            fillColor: {
-              linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-              stops: [
-                [0, Highcharts.getOptions().colors[0]],
-                [
-                  1,
-                  Highcharts.Color(Highcharts.getOptions().colors[0])
-                    .setOpacity(0)
-                    .get('rgba'),
-                ],
-              ],
-            },
-          },
-        ],
+    title: { text: T.wordsTotalRuns },
+    xAxis: {
+      type: 'datetime',
+      title: { text: null },
+      categories: payload.date.reverse(),
+    },
+    yAxis: { title: { text: T.wordsRuns }, min: minY },
+    legend: { enabled: false },
+    plotOptions: {
+      area: {
+        lineWidth: 1,
+        marker: { enabled: false },
+        shadow: false,
+        states: { hover: { lineWidth: 1 } },
+        threshold: null,
       },
     },
-    components: {
-      'omegaup-common-runschart': common_RunsChart,
-    },
+    series: [
+      {
+        type: 'area',
+        name: T.wordsRuns,
+        data: payload.total.reverse(),
+        fillColor: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, Highcharts.getOptions().colors[0]],
+            [
+              1,
+              Highcharts.Color(Highcharts.getOptions().colors[0])
+                .setOpacity(0)
+                .get('rgba'),
+            ],
+          ],
+        },
+      },
+    ],
   });
 });
