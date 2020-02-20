e7d3d028f6d1079191d43c3421ac6f22252860c1
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-02-20 00:40:03 +0000

    docblock arreglo

--- frontend/server/src/DAO/CoderOfTheMonth.php
+++ frontend/server/src/DAO/CoderOfTheMonth.php
@@ -297,7 +297,7 @@ class CoderOfTheMonth extends \OmegaUp\DAO\Base\CoderOfTheMonth {
                 WHERE
                     `time` = ?;';
 
-        /** @var list<array{coder_of_the_month_id: int, description: null|string, interview_url: null|string, rank: int, school_id: int|null, category: string, selected_by: int|null, time: string, user_id: int}> */
+        /** @var list<array{ category: string, coder_of_the_month_id: int, description: null|string, interview_url: null|string, rank: int, school_id: int|null, selected_by: int|null, time: string, user_id: int}> */
         $rs = \OmegaUp\MySQLConnection::getInstance()->GetAll($sql, [$time]);
 
         $coders = [];
