878b26c4ec43a891f00843992ddb09ffe800eee8
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2020-02-24 23:58:04 +0000

    Agregando tablas para ingresar a curso por medio de un request

--- frontend/database/schema.sql
+++ frontend/database/schema.sql
@@ -174,6 +174,37 @@ CREATE TABLE `Countries` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Course_Identity_Request` (
+  `identity_id` int(11) NOT NULL COMMENT 'Identidad del usuario',
+  `course_id` int(11) NOT NULL COMMENT 'Curso al cual se necesita un request para ingresar',
+  `request_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Hora en la que se realizó el request',
+  `last_update` timestamp NULL DEFAULT NULL COMMENT 'Última fecha de actualización del request',
+  `accepted` tinyint(1) DEFAULT NULL COMMENT 'Indica si la respuesta del request fue aceptada',
+  PRIMARY KEY (`identity_id`,`course_id`),
+  KEY `course_id` (`course_id`),
+  KEY `identity_id` (`identity_id`),
+  CONSTRAINT `fk_circ_course_id` FOREIGN KEY (`course_id`) REFERENCES `Courses` (`course_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ciri_identity_id` FOREIGN KEY (`identity_id`) REFERENCES `Identities` (`identity_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Usado cuando un curso se registra con admission_mode = registration';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Course_Identity_Request_History` (
+  `history_id` int(11) NOT NULL AUTO_INCREMENT,
+  `identity_id` int(11) NOT NULL COMMENT 'Identidad del usuario',
+  `course_id` int(11) NOT NULL COMMENT 'Curso al cual se necesita un request para ingresar',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Hora en la que se realizó el request',
+  `accepted` tinyint(4) NOT NULL COMMENT 'Indica si la respuesta del request fue aceptada',
+  `admin_id` int(11) NOT NULL COMMENT 'Identidad que usuario aceptó / rechazo el request',
+  PRIMARY KEY (`history_id`),
+  KEY `course_id` (`course_id`),
+  KEY `identity_course_hist` (`identity_id`,`course_id`),
+  CONSTRAINT `fk_cirhc_curse_id` FOREIGN KEY (`course_id`) REFERENCES `Courses` (`course_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cirhi_identity_id` FOREIGN KEY (`identity_id`) REFERENCES `Identities` (`identity_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabla para almacenar la respuesta de cada una de las peticiones hechas al curso con admission_mode = registration';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Courses` (
   `course_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -260,6 +291,7 @@ CREATE TABLE `Groups_Identities` (
   `share_user_information` tinyint(1) DEFAULT NULL COMMENT 'Almacena la respuesta del participante de un curso si está de acuerdo en divulgar su información.',
   `privacystatement_consent_id` int(11) DEFAULT NULL COMMENT 'Id del documento con el consentimiento de privacidad',
   `accept_teacher` tinyint(1) DEFAULT NULL COMMENT 'Almacena la respuesta del participante de un curso si acepta al organizador como su maestro.',
+  `is_invited` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Indica si la identidad ingresará al curso por invitación o le fue compartido el link del curso abierto con registro',
   PRIMARY KEY (`identity_id`,`group_id`),
   KEY `group_id` (`group_id`),
   KEY `identity_id` (`identity_id`),
