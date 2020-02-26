9555501951ed306861a542af5de46a3a3e37f792
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-02-26 05:25:57 +0000

    soporte para que la coder y el coder pueda ser la misma persona

--- frontend/database/136_coder_of_the_month_add_index_rank_time_category.sql
+++ frontend/database/136_coder_of_the_month_add_index_rank_time_category.sql
@@ -1,6 +1,6 @@
-ALTER TABLE 
-    `Coder_Of_The_Month` 
-ADD INDEX 
+ALTER TABLE
+    `Coder_Of_The_Month`
+ADD INDEX
     `rank_time_category` (`category`, `rank`, `time`),
-DROP INDEX 
+DROP INDEX
     `rank_time` ;
