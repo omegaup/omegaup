a130b7cc4f8c3de6d1ee46b485fc4469cf20da52
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-03-19 00:49:23 +0000

    Query cambia nombre

---
+++ frontend/database/140_rename_columns_rank.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `Coder_Of_The_Month`
+RENAME COLUMN `rank` TO `ranking`;
+
+ALTER TABLE `School_Of_The_Month`
+RENAME COLUMN `rank` TO `ranking`;
+
+ALTER TABLE `Schools`
+RENAME COLUMN `rank` TO `ranking`;
+
+ALTER TABLE `User_Rank`
+RENAME COLUMN `rank` TO `ranking`;
