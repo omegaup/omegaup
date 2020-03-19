9febbb5ff4241ba6cbe0bfc92d588d54d007f863
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-03-19 05:55:04 +0000

    cambio de rename por change

--- frontend/database/140_rename_columns_rank.sql
+++ frontend/database/140_rename_columns_rank.sql
@@ -1,11 +1,11 @@
 ALTER TABLE `Coder_Of_The_Month`
-RENAME COLUMN `rank` TO `ranking`;
+CHANGE COLUMN `rank` `ranking` int NOT NULL COMMENT 'El lugar en el que el usuario estuvo durante ese mes' ;
 
 ALTER TABLE `School_Of_The_Month`
-RENAME COLUMN `rank` TO `ranking`;
+CHANGE COLUMN `rank` `ranking` int NOT NULL COMMENT 'El lugar que tuvo la escuela en el mes.';
 
 ALTER TABLE `Schools`
-RENAME COLUMN `rank` TO `ranking`;
+CHANGE COLUMN `rank` `ranking` int DEFAULT NULL;
 
 ALTER TABLE `User_Rank`
-RENAME COLUMN `rank` TO `ranking`;
+CHANGE COLUMN `rank` `ranking` int NOT NULL;
