7dff846b3546cc9c01865cd76d771f180e1e44ff
lhchavez <lhchavez@lhchavez.com>
2020-03-28 15:22:42 +0000

    Moviendo knockout a arena.js
    Por fin, hemos logrado aislar a knockout a sólo la arena. Pronto será
    posible incluso erradicarlo de aquí, y va a ser excelente.

--- frontend/www/js/omegaup/omegaup.js
+++ frontend/www/js/omegaup/omegaup.js
@@ -2,35 +2,6 @@ import { Experiments, EventListenerList, OmegaUp, T, UI } from './omegaup.ts';
 import API from './api.js';
 export { API, EventListenerList, Experiments, OmegaUp, T, UI };
 
-OmegaUp.on('ready', function() {
-  // ko.secureBindingsProvider.nodeHasBindings() has a bug in which if
-  // there happens to be a comment with no content (like `<!---->`), it
-  // tries to call .trim() on undefined, and crashes.
-  ko.secureBindingsProvider.prototype.nodeHasBindings = function(node) {
-    if (node.nodeType === node.ELEMENT_NODE) {
-      return (
-        node.getAttribute(this.attribute) ||
-        (ko.components && ko.components.getComponentNameForNode(node))
-      );
-    }
-    if (node.nodeType === node.COMMENT_NODE) {
-      if (this.noVirtualElements) {
-        return false;
-      }
-      // Ensures that `value` is not undefined.
-      let value = '' + node.nodeValue || node.text;
-      if (!value) {
-        return false;
-      }
-      // See also: knockout/src/virtualElements.js
-      return value.trim().indexOf('ko ') === 0;
-    }
-  };
-  ko.bindingProvider.instance = new ko.secureBindingsProvider({
-    attribute: 'data-bind',
-  });
-});
-
 if (
   document.readyState === 'complete' ||
   (document.readyState !== 'loading' && !document.documentElement.doScroll)
