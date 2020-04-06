d6516fae6c1aa7c28a1580485c96c29f7baf0d58
Carlos Córdova <carlosab1802@gmail.com>
2020-04-06 16:32:39 +0000

    Dejando de utilizar vue-agile y utilizando el carousel de BS4 en su lugar.

--- frontend/www/js/omegaup/components/homepage/Carousel.vue
+++ frontend/www/js/omegaup/components/homepage/Carousel.vue
@@ -1,25 +1,54 @@
 <template>
-  <vue-agile
-    v-bind:autoplay="true"
-    v-bind:autoplay-speed="5000"
-    v-bind:timing="'ease-in-out'"
-  >
-    <omegaup-homepage-slide
-      v-bind:title="T.carouselSlideWelcomeTitle"
-      v-bind:description="T.carouselSlideWelcomeDescription"
-      v-bind:image-src="'/media/homepage/carousel_slide_1.svg'"
-    ></omegaup-homepage-slide>
-    <omegaup-homepage-slide
-      v-bind:title="T.carouselSlideMentorsTitle"
-      v-bind:description="T.carouselSlideMentorsDescription"
-      v-bind:image-src="'/media/homepage/carousel_slide_2.svg'"
-    ></omegaup-homepage-slide>
-    <omegaup-homepage-slide
-      v-bind:title="T.carouselSlideSchoolsTitle"
-      v-bind:description="T.carouselSlideSchoolsDescription"
-      v-bind:image-src="'/media/homepage/carousel_slide_3.svg'"
-    ></omegaup-homepage-slide>
-  </vue-agile>
+  <!-- id-lint off -->
+  <div class="carousel slide" data-ride="carousel" id="carousel-display">
+    <!-- id-lint on -->
+    <ol class="carousel-indicators">
+      <li data-target=".carousel" data-slide-to="0" class="active"></li>
+      <li data-target=".carousel" data-slide-to="1"></li>
+      <li data-target=".carousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner text-center py-5 py-md-0">
+      <div class="carousel-item active">
+        <omegaup-homepage-slide
+          v-bind:title="T.carouselSlideWelcomeTitle"
+          v-bind:description="T.carouselSlideWelcomeDescription"
+          v-bind:image-src="'/media/homepage/carousel_slide_1.svg'"
+        ></omegaup-homepage-slide>
+      </div>
+      <div class="carousel-item">
+        <omegaup-homepage-slide
+          v-bind:title="T.carouselSlideMentorsTitle"
+          v-bind:description="T.carouselSlideMentorsDescription"
+          v-bind:image-src="'/media/homepage/carousel_slide_2.svg'"
+        ></omegaup-homepage-slide>
+      </div>
+      <div class="carousel-item">
+        <omegaup-homepage-slide
+          v-bind:title="T.carouselSlideSchoolsTitle"
+          v-bind:description="T.carouselSlideSchoolsDescription"
+          v-bind:image-src="'/media/homepage/carousel_slide_3.svg'"
+        ></omegaup-homepage-slide>
+      </div>
+    </div>
+    <a
+      class="carousel-control-prev"
+      href="#carousel-display"
+      role="button"
+      data-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a
+      class="carousel-control-next"
+      href="#carousel-display"
+      role="button"
+      data-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 </template>
 
 <style lang="scss">
@@ -27,67 +56,9 @@
 
 $omegaup-primary--darker: #466aaf;
 $omegaup-primary--accent: #0275d8;
-
-.agile {
+.carousel {
   background: $omegaup-primary--darker;
   color: $white;
-  &__nav-button {
-    background-color: green;
-    background: transparent;
-    border: none;
-    color: #fff;
-    cursor: pointer;
-    font-size: 24px;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    transition-duration: 0.3s;
-    width: 80px;
-
-    &:hover {
-      background-color: rgba(0, 0, 0, 0.5);
-      opacity: 1;
-    }
-
-    &--prev {
-      left: 0;
-    }
-
-    &--next {
-      right: 0;
-    }
-  }
-
-  &__dots {
-    bottom: 10px;
-    left: 50%;
-    position: absolute;
-    transform: translateX(-50%);
-  }
-
-  &__dot {
-    margin: 0 10px;
-
-    button {
-      background-color: transparent;
-      border: 1px solid #fff;
-      border-radius: 50%;
-      cursor: pointer;
-      display: block;
-      height: 10px;
-      font-size: 0;
-      line-height: 0;
-      margin: 0;
-      padding: 0;
-      transition-duration: 0.3s;
-      width: 10px;
-    }
-
-    &--current button,
-    &:hover button {
-      background-color: #fff;
-    }
-  }
 }
 </style>
 
--- frontend/www/js/omegaup/components/homepage/Slide.vue
+++ frontend/www/js/omegaup/components/homepage/Slide.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="slide">
-    <div class="container-lg">
-      <div class="row align-items-center">
-        <div class="slide-text col-md-6">
-          <h3>{{ title }}</h3>
-          <p>{{ description }}</p>
-          <div slot="buttons"></div>
-        </div>
-        <div class="col-md-6">
-          <img class="img-fluid slide-img" v-bind:src="imageSrc"/>
-        </div>
+  <div class="container-lg p-4">
+    <div
+      class="slide d-flex align-items-center justify-content-around flex-wrap flex-lg-nowrap"
+    >
+      <div>
+        <h3>{{ title }}</h3>
+        <p>{{ description }}</p>
+        <div slot="buttons"></div>
+      </div>
+      <div>
+        <img class="d-block" height="320" v-bind:src="imageSrc" />
       </div>
     </div>
   </div>
@@ -17,22 +17,8 @@
 
 <style lang="scss" scoped>
 .slide {
-  padding: 50px 20px;
-  text-align: center;
-  display: flex;
-  align-items: center;
-
   @media only screen and (min-width: 767px) {
-    height: 600px;
-    padding: 50px;
-  }
-
-  &-img {
-    max-height: 400px;
-    @media only screen and (max-width: 767px) {
-      height: 300px;
-      margin-top: 40px;
-    }
+    height: 500px;
   }
 }
 </style>
