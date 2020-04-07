d0e85674eca41c137a50f9018a15a076e0491611
lhchavez <lhchavez@lhchavez.com>
2020-04-07 16:52:15 -0700

    Creando el usuario de MySQL.

--- stuff/docker/usr/bin/db-setup.sh
+++ stuff/docker/usr/bin/db-setup.sh
@@ -3,6 +3,8 @@
 set -e
 
 if ! /opt/omegaup/stuff/db-migrate.py --mysql-config-file=/home/ubuntu/.my.cnf exists ; then
+  mysql --defaults-file=/home/ubuntu/.my.cnf \
+    -e "CREATE USER IF NOT EXISTS 'omegaup'@'localhost' IDENTIFIED BY 'omegaup';"
   exec /opt/omegaup/stuff/bootstrap-environment.py \
     --mysql-config-file=/home/ubuntu/.my.cnf \
     --purge --verbose --root-url=http://localhost:8000/
