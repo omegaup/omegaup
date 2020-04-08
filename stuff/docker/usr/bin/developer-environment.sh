b9c4d9d9afef66210dd4ce6a4d0867c58bcaa327
lhchavez <lhchavez@lhchavez.com>
2020-04-07 19:37:19 -0700

    Estabilizando Docker (#3701)
    Este cambio hace que Docker ya se pueda correr sin el `--build`. A
    partir de ahora, cada que se desee hacer un cambio en los archivos de
    `stuff/docker`, hay que re-construir los contenedores y volverlos a
    subir a Docker Hub.

--- stuff/docker/usr/bin/developer-environment.sh
+++ stuff/docker/usr/bin/developer-environment.sh
@@ -25,6 +25,8 @@ fi
 if ! /opt/omegaup/stuff/db-migrate.py --mysql-config-file=/home/ubuntu/.my.cnf exists ; then
   mysql --defaults-file=/home/ubuntu/.my.cnf \
     -e "CREATE USER IF NOT EXISTS 'omegaup'@'localhost' IDENTIFIED BY 'omegaup';"
+  mysql --defaults-file=/home/ubuntu/.my.cnf \
+    -e 'GRANT ALL PRIVILEGES ON `omegaup-test`.* TO "omegaup"@"%";'
   /opt/omegaup/stuff/bootstrap-environment.py \
     --mysql-config-file=/home/ubuntu/.my.cnf \
     --purge --verbose --root-url=http://localhost:8000/
