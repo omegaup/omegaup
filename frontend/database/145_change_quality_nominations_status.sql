f0ef4432875791759c0565ecf58709dfade3adc9
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-04-14 23:46:45 +0000

    status en Quality_Nomination_Log

--- frontend/database/145_change_quality_nominations_status.sql
+++ frontend/database/145_change_quality_nominations_status.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `QualityNominations`
 CHANGE COLUMN `status` `status` ENUM('open','approved','denied','warning') NOT NULL DEFAULT 'open' COMMENT 'El estado de la nominación' ;
+
+ALTER TABLE `QualityNomination_Log`
+CHANGE COLUMN `to_status` `to_status` ENUM('open','approved','denied','warning') NOT NULL DEFAULT 'open',
+CHANGE COLUMN `from_status` `from_status` ENUM('open','approved','denied','warning') NOT NULL DEFAULT 'open';
