31cb7e9eefbb68f1a03e1fec9664ac42e3349e7f
lhchavez <lhchavez@lhchavez.com>
2020-04-21 19:46:27 -0700

    Update frontend/server/src/DAO/QualityNominations.php

--- frontend/server/src/DAO/QualityNominations.php
+++ frontend/server/src/DAO/QualityNominations.php
@@ -346,7 +346,7 @@ class QualityNominations extends \OmegaUp\DAO\Base\QualityNominations {
             $sqlFrom .= " AND qn.status = '{$status}'";
         }
 
-        // TODO(...): Change to ASC once duplicates are removed.
+        // TODO(#3696): Change to ASC once duplicates are removed.
         $sqlOrder = ' ORDER BY p.alias ASC, qn.qualitynomination_id DESC';
         $sqlLimit = ' LIMIT ?, ?;';
 
