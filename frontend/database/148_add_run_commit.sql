a2b3f3af65bebafa9b950d7b0248895e56aa6773
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2020-05-08 16:44:44 +0000

    Arreglando script para evitar fallos en producción

--- frontend/database/148_add_run_commit.sql
+++ frontend/database/148_add_run_commit.sql
@@ -3,8 +3,7 @@
 ALTER TABLE
   `Runs`
 ADD COLUMN
-  `commit` char(40) NOT NULL
-    COMMENT 'El hash SHA1 del commit en la rama master del problema con el que se realizó el envío.'
+  `commit` char(40) NOT NULL DEFAULT 'published'
     AFTER `version`;
 
 -- Updating commit field
@@ -16,3 +15,11 @@ INNER JOIN
   `Problems` ON `Submissions`.`problem_id` = `Problems`.`problem_id`
 SET
   `Runs`.`commit` = `Problems`.`commit`;
+
+-- Alter table Runs, removing default value in commit column
+
+ALTER TABLE
+  `Runs`
+MODIFY COLUMN
+  `commit` char(40) NOT NULL
+    COMMENT 'El hash SHA1 del commit en la rama master del problema con el que se realizó el envío.';
