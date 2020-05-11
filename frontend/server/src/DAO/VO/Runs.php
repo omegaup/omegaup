33125fc70ddf2a6b20c68de925dd4e0b8e902e3d
lhchavez <lhchavez@lhchavez.com>
2020-05-10 22:12:00 -0700

    Revert "Agregando campo commit para la tabla de envíos (#3891)" (#3981)
    This reverts commit 92a3d4e0acdf03e74777a36aeef42d38ca60ddb6.
    
    Resulta que a la base de datos en producción no le gustó este cambio
    porque excede el tamaño máximo configurado de la tabla. Hay que hacer
    ajustes de configuración antes de reintentar :/

--- frontend/server/src/DAO/VO/Runs.php
+++ frontend/server/src/DAO/VO/Runs.php
@@ -19,7 +19,6 @@ class Runs extends \OmegaUp\DAO\VO\VO {
         'run_id' => true,
         'submission_id' => true,
         'version' => true,
-        'commit' => true,
         'status' => true,
         'verdict' => true,
         'runtime' => true,
@@ -54,11 +53,6 @@ class Runs extends \OmegaUp\DAO\VO\VO {
         if (isset($data['version'])) {
             $this->version = strval(
                 $data['version']
-            );
-        }
-        if (isset($data['commit'])) {
-            $this->commit = strval(
-                $data['commit']
             );
         }
         if (isset($data['status'])) {
@@ -140,13 +134,6 @@ class Runs extends \OmegaUp\DAO\VO\VO {
      * @var string|null
      */
     public $version = null;
-
-    /**
-     * El hash SHA1 del commit en la rama master del problema con el que se realizó el envío.
-     *
-     * @var string|null
-     */
-    public $commit = null;
 
     /**
      * [Campo no documentado]
