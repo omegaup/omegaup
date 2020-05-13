81d4554c5c89762de241dc9afa2a1306f2196add
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-05-13 16:11:39 -0500

    Simplificando los metodos

--- frontend/tests/badges/cppCourseGraduateTest.php
+++ frontend/tests/badges/cppCourseGraduateTest.php
@@ -6,11 +6,17 @@
  * @author RuizYugen
  */
 class CppCourseGraduate extends \OmegaUp\Test\BadgesTestCase {
-    public function testCourseCppUserWinBadge() {
+    protected $courseData;
+    protected $students;
+    protected $submissionSource;
+    protected $problems;
+
+    public function setUp(): void {
+        parent::setUp();
         $courseAlias = 'introduccion_a_cpp';
 
         //create course
-        $courseData = \OmegaUp\Test\Factories\Course::createCourseWithAssignments(
+        $this->courseData = \OmegaUp\Test\Factories\Course::createCourseWithAssignments(
             /*$nAssignments=*/ 2,
             $courseAlias
         );
@@ -18,56 +24,59 @@ class CppCourseGraduate extends \OmegaUp\Test\BadgesTestCase {
 
         // Prepare assignment. Create four problems: The first three accept
         // submissions and the last one does not.
-        $adminLogin = self::login($courseData['admin']);
-        $problems = [];
+        $adminLogin = self::login($this->courseData['admin']);
+        $this->problems = [];
         for ($i = 0; $i < 3; $i++) {
-            $problems[] = \OmegaUp\Test\Factories\Problem::createProblem();
+            $this->problems[] = \OmegaUp\Test\Factories\Problem::createProblem();
         }
-        $problems[] = \OmegaUp\Test\Factories\Problem::createProblem(
+        $this->problems[] = \OmegaUp\Test\Factories\Problem::createProblem(
             new \OmegaUp\Test\Factories\ProblemParams([
                 'languages' => '',
             ])
         );
 
-        foreach (array_slice($problems, 0, 2) as $problemData) {
+        foreach (array_slice($this->problems, 0, 2) as $problemData) {
             \OmegaUp\Controllers\Course::apiAddProblem(new \OmegaUp\Request([
                 'auth_token' => $adminLogin->auth_token,
-                'course_alias' => $courseData['course_alias'],
-                'assignment_alias' => $courseData['assignment_aliases'][0],
+                'course_alias' => $this->courseData['course_alias'],
+                'assignment_alias' => $this->courseData['assignment_aliases'][0],
                 'problem_alias' => $problemData['request']['problem_alias'],
             ]));
         }
-        foreach (array_slice($problems, 2, 2) as $problemData) {
+        foreach (array_slice($this->problems, 2, 2) as $problemData) {
             \OmegaUp\Controllers\Course::apiAddProblem(new \OmegaUp\Request([
                 'auth_token' => $adminLogin->auth_token,
-                'course_alias' => $courseData['course_alias'],
-                'assignment_alias' => $courseData['assignment_aliases'][1],
+                'course_alias' => $this->courseData['course_alias'],
+                'assignment_alias' => $this->courseData['assignment_aliases'][1],
                 'problem_alias' => $problemData['request']['problem_alias'],
             ]));
         }
 
         // Add students to course
-        $students = [];
+        $this->students = [];
         for ($i = 0; $i < $studentsInCourse; $i++) {
-            $students[] = \OmegaUp\Test\Factories\Course::addStudentToCourse(
-                $courseData
+            $this->students[] = \OmegaUp\Test\Factories\Course::addStudentToCourse(
+                $this->courseData
             );
         }
 
-        $submissionSource = "#include <stdio.h>\nint main() { printf(\"3\"); return 0; }";
+        $this->submissionSource = "#include <stdio.h>\nint main() { printf(\"3\"); return 0; }";
+    }
+
+    public function testCourseCppUserEarnBadge() {
         {
             // This user will recive the badge because he will resolve 4 differents problems of the course
             $studentLogin = \OmegaUp\Test\ControllerTestCase::login(
-                $students[0]
+                $this->students[0]
             );
 
             // Add one run to the first problem in the first assignment.
             $runResponse = \OmegaUp\Controllers\Run::apiCreate(new \OmegaUp\Request([
                 'auth_token' => $studentLogin->auth_token,
-                'problemset_id' => $courseData['assignment_problemset_ids'][0],
-                'problem_alias' => $problems[0]['problem']->alias,
+                'problemset_id' => $this->courseData['assignment_problemset_ids'][0],
+                'problem_alias' => $this->problems[0]['problem']->alias,
                 'language' => 'c11-gcc',
-                'source' => $submissionSource,
+                'source' => $this->submissionSource,
             ]));
             \OmegaUp\Test\Factories\Run::gradeRun(
                 /*$runData=*/ null,
@@ -78,10 +87,10 @@ class CppCourseGraduate extends \OmegaUp\Test\BadgesTestCase {
             );
             $runResponse = \OmegaUp\Controllers\Run::apiCreate(new \OmegaUp\Request([
                 'auth_token' => $studentLogin->auth_token,
-                'problemset_id' => $courseData['assignment_problemset_ids'][0],
-                'problem_alias' => $problems[1]['problem']->alias,
+                'problemset_id' => $this->courseData['assignment_problemset_ids'][0],
+                'problem_alias' => $this->problems[1]['problem']->alias,
                 'language' => 'c11-gcc',
-                'source' => $submissionSource,
+                'source' => $this->submissionSource,
             ]));
             \OmegaUp\Test\Factories\Run::gradeRun(
                 /*$runData=*/ null,
@@ -94,10 +103,10 @@ class CppCourseGraduate extends \OmegaUp\Test\BadgesTestCase {
             // Add one run to the third problem in the second assignment
             $runResponse = \OmegaUp\Controllers\Run::apiCreate(new \OmegaUp\Request([
                 'auth_token' => $studentLogin->auth_token,
-                'problemset_id' => $courseData['assignment_problemset_ids'][1],
-                'problem_alias' => $problems[2]['problem']->alias,
+                'problemset_id' => $this->courseData['assignment_problemset_ids'][1],
+                'problem_alias' => $this->problems[2]['problem']->alias,
                 'language' => 'c11-gcc',
-                'source' => $submissionSource,
+                'source' => $this->submissionSource,
             ]));
             \OmegaUp\Test\Factories\Run::gradeRun(
                 /*$runData=*/ null,
@@ -109,73 +118,25 @@ class CppCourseGraduate extends \OmegaUp\Test\BadgesTestCase {
         }
         $queryPath = static::OMEGAUP_BADGES_ROOT . '/cppCourseGraduate/' . static::QUERY_FILE;
         $results = self::getSortedResults(file_get_contents($queryPath));
-        $expected = [$students[0]->user_id];
+        $expected = [$this->students[0]->user_id];
         $this->assertEquals($expected, $results);
     }
 
-    public function testCourseCppUserDoNotWinBadge() {
-        $courseAlias = 'introduccion_a_cpp';
-
-        //create course
-        $courseData = \OmegaUp\Test\Factories\Course::createCourseWithAssignments(
-            /*$nAssignments=*/ 2,
-            $courseAlias
-        );
-        $studentsInCourse = 2;
-
-        // Prepare assignment. Create four problems: The first three accept
-        // submissions and the last one does not.
-        $adminLogin = self::login($courseData['admin']);
-        $problems = [];
-        for ($i = 0; $i < 3; $i++) {
-            $problems[] = \OmegaUp\Test\Factories\Problem::createProblem();
-        }
-        $problems[] = \OmegaUp\Test\Factories\Problem::createProblem(
-            new \OmegaUp\Test\Factories\ProblemParams([
-                'languages' => '',
-            ])
-        );
-
-        foreach (array_slice($problems, 0, 2) as $problemData) {
-            \OmegaUp\Controllers\Course::apiAddProblem(new \OmegaUp\Request([
-                'auth_token' => $adminLogin->auth_token,
-                'course_alias' => $courseData['course_alias'],
-                'assignment_alias' => $courseData['assignment_aliases'][0],
-                'problem_alias' => $problemData['request']['problem_alias'],
-            ]));
-        }
-        foreach (array_slice($problems, 2, 2) as $problemData) {
-            \OmegaUp\Controllers\Course::apiAddProblem(new \OmegaUp\Request([
-                'auth_token' => $adminLogin->auth_token,
-                'course_alias' => $courseData['course_alias'],
-                'assignment_alias' => $courseData['assignment_aliases'][1],
-                'problem_alias' => $problemData['request']['problem_alias'],
-            ]));
-        }
-
-        // Add students to course
-        $students = [];
-        for ($i = 0; $i < $studentsInCourse; $i++) {
-            $students[] = \OmegaUp\Test\Factories\Course::addStudentToCourse(
-                $courseData
-            );
-        }
-
-        $submissionSource = "#include <stdio.h>\nint main() { printf(\"3\"); return 0; }";
+    public function testCourseCppUserDoNotEarnBadge() {
         {
             // This user will not recibe the badge because he will resolve a problem with multiple solutions
             $studentLogin = \OmegaUp\Test\ControllerTestCase::login(
-                $students[1]
+                $this->students[1]
             );
 
         for ($i = 0; $i < 10; $i++) {
             // Add one run to the first problem in the first assignment.
             $runResponse = \OmegaUp\Controllers\Run::apiCreate(new \OmegaUp\Request([
                 'auth_token' => $studentLogin->auth_token,
-                'problemset_id' => $courseData['assignment_problemset_ids'][0],
-                'problem_alias' => $problems[0]['problem']->alias,
+                'problemset_id' => $this->courseData['assignment_problemset_ids'][0],
+                'problem_alias' => $this->problems[0]['problem']->alias,
                 'language' => 'c11-gcc',
-                'source' => $submissionSource,
+                'source' => $this->submissionSource,
             ]));
             \OmegaUp\Test\Factories\Run::gradeRun(
                 /*$runData=*/ null,
