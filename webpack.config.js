8290733a179dcb8f2281e392aef31c1fc4e7af8f
lhchavez <lhchavez@lhchavez.com>
2020-05-15 17:54:03 -0700

    Convirtiendo frontend/www/ux/assignment*.js a TypeScript
    Este cambio migra los cursos a TypeScript.

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,8 @@ let config = [
       admin_user: './frontend/www/js/omegaup/admin/user.js',
       arena: './frontend/www/js/omegaup/arena/arena.ts',
       arena_admin: './frontend/www/js/omegaup/arena/admin.ts',
+      arena_assignment: './frontend/www/js/omegaup/arena/assignment.ts',
+      arena_assignment_admin: './frontend/www/js/omegaup/arena/assignment_admin.ts',
       arena_contest: './frontend/www/js/omegaup/arena/contest.ts',
       arena_contest_list: './frontend/www/js/omegaup/arena/contest_list.ts',
       arena_scoreboard: './frontend/www/js/omegaup/arena/scoreboard.ts',
