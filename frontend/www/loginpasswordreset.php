91f0cc6dd47c132b842575177b5b57d03933c6b6
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-05-18 12:21:19 -0500

    Arreglo de redirect

--- frontend/www/loginpasswordreset.php
+++ frontend/www/loginpasswordreset.php
@@ -1,21 +1,21 @@
 <?php
 namespace OmegaUp;
 require_once(dirname(__DIR__, 1) . '/server/bootstrap.php');
-if (OMEGAUP_LOCKDOWN) {
-    header('Location: /');
-    die();
-}
 
-\OmegaUp\UITools::render(
-    function (\OmegaUp\Request $r) {
-        return [
-            'smartyProperties' => [
-                'payload' => [
-                    'email' => $_GET['email'],
-                    'resetToken' => $_GET['reset_token']
+if (isset($_GET['email']) && isset($_GET['reset_token'])) {
+    \OmegaUp\UITools::render(
+        function (\OmegaUp\Request $r) {
+            return [
+                'smartyProperties' => [
+                    'payload' => [
+                        'email' => $_GET['email'],
+                        'resetToken' => $_GET['reset_token']
+                    ],
                 ],
-            ],
-            'entrypoint' => 'login_password_reset',
-        ];
-    }
-);
+                'entrypoint' => 'login_password_reset',
+            ];
+        }
+    );
+} else {
+    die(header('Location: /'));
+}
