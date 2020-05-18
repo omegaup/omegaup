afb18b51b9b5a7ac5ac601d2e87bcc4234480272
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-05-18 12:07:32 -0500

    se quito comentario

--- frontend/www/loginpasswordreset.php
+++ frontend/www/loginpasswordreset.php
@@ -19,13 +19,3 @@ if (OMEGAUP_LOCKDOWN) {
         ];
     }
 );
-
-/*require_once('../server/bootstrap_smarty.php');
-if (isset($_GET['email']) && isset($_GET['reset_token'])) {
-    $smarty->assign('EMAIL', $_GET['email']);
-    $smarty->assign('RESET_TOKEN', $_GET['reset_token']);
-} else {
-    die(header('Location: /'));
-}
-
-$smarty->display('../templates/login.password.reset.tpl');*/
