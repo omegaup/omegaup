53e1bf2befc87b153902705ba3cd002a33a149a8
Carlos Córdova <carlosab1802@gmail.com>
2020-05-22 14:01:44 +0000

    Eliminando archivos que ya no se utilizan.

--- frontend/server/src/Controllers/Submission.php
+++ frontend/server/src/Controllers/Submission.php
@@ -15,58 +15,6 @@ class Submission extends \OmegaUp\Controllers\Controller {
     }
 
     /**
-     * Returns the latest submissions
-     *
-     * @return array{submissions: list<array{time: \OmegaUp\Timestamp, username: string, school_id: int|null, school_name: string|null, alias: string, title: string, language: string, verdict: string, runtime: int, memory: int}>, totalRows: int}
-     *
-     * @omegaup-request-param int $offset
-     * @omegaup-request-param int $rowcount
-     * @omegaup-request-param mixed $username
-     */
-    public static function apiLatestSubmissions(\OmegaUp\Request $r) {
-        $r->ensureOptionalInt('offset');
-        $r->ensureOptionalInt('rowcount');
-
-        $offset = is_null($r['offset']) ? 1 : intval($r['offset']);
-        $rowCount = is_null($r['rowcount']) ? 100 : intval($r['rowcount']);
-
-        $identityId = null;
-        if (!is_null($r['username'])) {
-            \OmegaUp\Validators::validateValidUsername(
-                $r['username'],
-                'username'
-            );
-
-            $identity = \OmegaUp\DAO\Identities::FindByUsername($r['username']);
-            if (is_null($identity)) {
-                throw new \OmegaUp\Exceptions\NotFoundException('userNotExist');
-            }
-
-            $user = \OmegaUp\DAO\Users::FindByUsername($r['username']);
-            if (
-                !is_null(
-                    $user
-                ) &&
-                ($user->main_identity_id == $identity->identity_id) &&
-                $user->is_private
-            ) {
-                // Only the user's main identity is private.
-                throw new \OmegaUp\Exceptions\ForbiddenAccessException(
-                    'userInformationIsPrivate'
-                );
-            }
-
-            $identityId = $identity->identity_id;
-        }
-
-        return \OmegaUp\DAO\Submissions::getLatestSubmissions(
-            $offset,
-            $rowCount,
-            $identityId
-        );
-    }
-
-    /**
      * Gets the details for the latest submissions with pagination
      *
      * @return array{smartyProperties: array{payload: SubmissionsListPayload, title: string}, entrypoint: string}
