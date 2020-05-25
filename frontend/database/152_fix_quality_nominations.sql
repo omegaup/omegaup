3ac89d53b9a951e36cc54b981c49f57392b74e06
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-05-25 14:19:26 -0500

    Cambio a update case

--- frontend/database/152_fix_quality_nominations.sql
+++ frontend/database/152_fix_quality_nominations.sql
@@ -1,131 +1,67 @@
-/*adding field to state because it needs to preserve correct values */
-ALTER TABLE
-    `QualityNominations`
-CHANGE COLUMN
-    `status` `status` ENUM('open','warning','resolved','banned','temporal') NOT NULL DEFAULT 'open' COMMENT 'El estado de la nominación' ;
-
-ALTER TABLE
-    `QualityNomination_Log`
-CHANGE COLUMN
-    `to_status` `to_status` ENUM('open','warning','resolved','banned','temporal') NOT NULL DEFAULT 'open',
-CHANGE COLUMN
-    `from_status` `from_status` ENUM('open','warning','resolved','banned','temporal') NOT NULL DEFAULT 'open';
 /*Need next line to disable safe update*/
 SET SQL_SAFE_UPDATES = 0;
 
-/*update QualityNominations banned to temporal only nominations
+/*update QualityNominations (banned to resolved and resolved to banned) only QualityNominations
 that were before del PR https://github.com/omegaup/omegaup/pull/3736*/
 UPDATE
-    `QualityNominations` AS `q`
+	`QualityNominations` AS `q`
 SET
-    `q`.`status` = 'temporal'
+	`q`.`status` = CASE
+WHEN
+	`q`.`status` = 'banned'
+THEN
+	'resolved'
+WHEN
+	`q`.`status` = 'resolved'
+THEN
+	'banned'
+END
 WHERE
-    `q`.`status` = 'banned' AND
     (
-		SELECT
-			`qualitynomination_id`
-		FROM
-			`QualityNomination_Log`
-		WHERE
-			`qualitynomination_id`= `q`.`qualitynomination_id` AND
-			`time`< '2020-04-26'
-		ORDER BY
-			`time` DESC
-		LIMIT 1
-	) != null;
+    	SELECT
+    		`qualitynomination_id`
+    	FROM
+    		`QualityNomination_Log`
+    	WHERE
+    		`qualitynomination_id`= `q`.`qualitynomination_id` AND
+    		`time`< '2020-04-26'
+    	ORDER BY
+    		`time` DESC
+    	LIMIT 1
+    ) != null;
 
-/*update QualityNominations resolved to banned  only nominations
+/*update QualityNominationLog resolved to banned  only nominations
 that were before del PR https://github.com/omegaup/omegaup/pull/3736*/
 UPDATE
-    `QualityNominations` AS `q`
+	`QualityNomination_Log`
 SET
-    `q`.`status` = 'banned'
-WHERE
-    `q`.`status` = 'resolved' AND
-    (
-		SELECT
-			`qualitynomination_id`
-		FROM
-			`QualityNomination_Log`
-		WHERE
-			`qualitynomination_id`= `q`.`qualitynomination_id` AND
-			`time`< '2020-04-26'
-		ORDER BY
-			`time` DESC
-		LIMIT 1
-	) != null;
-
-/*update QualityNominations temporal to resolved*/
-UPDATE
-    `QualityNominations`
-SET
-    `status` = 'resolved'
-WHERE
-    `status` = 'temporal';
-
-/*update QualityNominations banned to temporal only nominations
-that were before del PR https://github.com/omegaup/omegaup/pull/3736*/
-UPDATE
-    `QualityNomination_Log`
-SET
-    `from_status` = 'temporal'
-WHERE
-    `from_status` = 'banned' AND
-    `time`< '2020-04-26';
+	`from_status` = CASE
+WHEN
+	`from_status` = 'banned' AND
+    `time`< '2020-04-26'
+THEN
+	'resolved'
+WHEN
+	`from_status` = 'resolved' AND
+    `time`< '2020-04-26'
+THEN
+	'banned'
+END;
 
 UPDATE
-    `QualityNomination_Log`
+	`QualityNomination_Log`
 SET
-    `from_status` = 'temporal'
-WHERE
-    `from_status` = 'banned' AND
-    `time`< '2020-04-26';
+	`to_status` = CASE
+WHEN
+	`to_status` = 'banned' AND
+    `time`< '2020-04-26'
+THEN
+	'resolved'
+WHEN
+	`to_status` = 'resolved' AND
+    `time`< '2020-04-26'
+THEN
+	'banned'
+END;
 
-/*update QualityNominations resolved to banned  only nominations
-that were before del PR https://github.com/omegaup/omegaup/pull/3736*/
-UPDATE
-    `QualityNomination_Log`
-SET
-    `from_status` = 'banned'
-WHERE
-    `from_status` = 'resolved' AND
-    `time`< '2020-04-26';
-
-UPDATE
-    `QualityNomination_Log`
-SET
-    `from_status` = 'banned'
-WHERE
-    `from_status` = 'resolved' AND
-    `time`< '2020-04-26';
-
-/*update QualityNominations temporal to resolved*/
-UPDATE
-    `QualityNomination_Log`
-SET
-    `from_status` = 'resolved'
-WHERE
-    `from_status` = 'temporal' AND
-    `time`< '2020-04-26';
-
-UPDATE
-    `QualityNomination_Log`
-SET
-    `from_status` = 'resolved'
-WHERE
-    `from_status` = 'temporal' AND
-    `time`< '2020-04-26';
-
-/*leave everything as it was*/
-ALTER TABLE
-    `QualityNominations`
-CHANGE COLUMN
-    `status` `status` ENUM('open','warning','resolved','banned') NOT NULL DEFAULT 'open' COMMENT 'El estado de la nominación' ;
-
-ALTER TABLE
-    `QualityNomination_Log`
-CHANGE COLUMN
-    `to_status` `to_status` ENUM('open','warning','resolved','banned') NOT NULL DEFAULT 'open',
-CHANGE COLUMN
-    `from_status` `from_status` ENUM('open','warning','resolved','banned') NOT NULL DEFAULT 'open';
 SET SQL_SAFE_UPDATES = 1;
