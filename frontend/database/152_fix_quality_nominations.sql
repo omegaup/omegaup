2ff8ad274ede0fd603064103d2a341020669f191
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-05-25 16:30:54 -0500

    Todo en una sola query

--- frontend/database/152_fix_quality_nominations.sql
+++ frontend/database/152_fix_quality_nominations.sql
@@ -1,67 +1,30 @@
 /*Need next line to disable safe update*/
 SET SQL_SAFE_UPDATES = 0;
 
-/*update QualityNominations (banned to resolved and resolved to banned) only QualityNominations
+/*Updating QualityNominations and QualityNomination_Log status, from_status and to_status
+(banned to resolved and resolved to banned) only QualityNominations
 that were before del PR https://github.com/omegaup/omegaup/pull/3736*/
 UPDATE
 	`QualityNominations` AS `q`
+INNER JOIN
+	`QualityNomination_Log` AS `ql` ON `q`.`qualitynomination_id` = `ql`.`qualitynomination_id`
 SET
 	`q`.`status` = CASE
-WHEN
-	`q`.`status` = 'banned'
-THEN
-	'resolved'
-WHEN
-	`q`.`status` = 'resolved'
-THEN
-	'banned'
-END
+    WHEN `q`.`status` = 'banned' THEN 'resolved'
+    WHEN `q`.`status` = 'resolved' THEN 'banned'
+    ELSE `q`.`status`
+    END,
+    `ql`.`from_status` = CASE
+        WHEN `ql`.`from_status` = 'banned' THEN 'resolved'
+        WHEN `ql`.`from_status` = 'resolved' THEN 'banned'
+        ELSE `ql`.`from_status`
+    END,
+    `ql`.`to_status` = CASE
+        WHEN `ql`.`to_status` = 'banned' THEN 'resolved'
+        WHEN `ql`.`to_status` = 'resolved' THEN 'banned'
+        ELSE `ql`.`to_status`
+     END
 WHERE
-    (
-    	SELECT
-    		`qualitynomination_id`
-    	FROM
-    		`QualityNomination_Log`
-    	WHERE
-    		`qualitynomination_id`= `q`.`qualitynomination_id` AND
-    		`time`< '2020-04-26'
-    	ORDER BY
-    		`time` DESC
-    	LIMIT 1
-    ) != null;
-
-/*update QualityNominationLog resolved to banned  only nominations
-that were before del PR https://github.com/omegaup/omegaup/pull/3736*/
-UPDATE
-	`QualityNomination_Log`
-SET
-	`from_status` = CASE
-WHEN
-	`from_status` = 'banned' AND
-    `time`< '2020-04-26'
-THEN
-	'resolved'
-WHEN
-	`from_status` = 'resolved' AND
-    `time`< '2020-04-26'
-THEN
-	'banned'
-END;
-
-UPDATE
-	`QualityNomination_Log`
-SET
-	`to_status` = CASE
-WHEN
-	`to_status` = 'banned' AND
-    `time`< '2020-04-26'
-THEN
-	'resolved'
-WHEN
-	`to_status` = 'resolved' AND
-    `time`< '2020-04-26'
-THEN
-	'banned'
-END;
+	`ql`.`time`< '2020-04-26';
 
 SET SQL_SAFE_UPDATES = 1;
