97b59ba72366e1ee560827c2691caae2fe9d0f99
Carlos Córdova <carlosab1802@gmail.com>
2020-05-25 06:00:09 +0000

    Cambiando el orden de tipo de validador y lenguajes en la creación de un problema.

--- frontend/www/js/omegaup/components/problem/Settings.vue
+++ frontend/www/js/omegaup/components/problem/Settings.vue
@@ -2,18 +2,6 @@
   <div>
     <div class="row">
       <div class="form-group col-md-6">
-        <label>{{ T.problemEditFormValidatorType }}</label>
-        <select name="validator" class="form-control" v-model="validator">
-          <option
-            v-for="(validatorText, validatorIndex) in validatorTypes"
-            v-bind:value="validatorIndex"
-          >
-            {{ validatorText }}</option
-          >
-        </select>
-      </div>
-
-      <div class="form-group col-md-6">
         <label>{{ T.problemEditFormLanguages }}</label>
         <select name="languages" class="form-control" v-model="languages">
           <option
@@ -21,6 +9,22 @@
             v-bind:value="languageIndex"
           >
             {{ languageText }}</option
+          >
+        </select>
+      </div>
+      <div class="form-group col-md-6">
+        <label>{{ T.problemEditFormValidatorType }}</label>
+        <select
+          name="validator"
+          class="form-control"
+          v-model="validator"
+          v-bind:disabled="languages === ''"
+        >
+          <option
+            v-for="(validatorText, validatorIndex) in validatorTypes"
+            v-bind:value="validatorIndex"
+          >
+            {{ validatorText }}</option
           >
         </select>
       </div>
@@ -33,7 +37,7 @@
         <input
           name="validator_time_limit"
           v-bind:value="validatorTimeLimit"
-          v-bind:disabled="languages == ''"
+          v-bind:disabled="languages === ''"
           type="text"
           class="form-control"
         />
@@ -44,7 +48,7 @@
         <input
           name="time_limit"
           v-bind:value="timeLimit"
-          v-bind:disabled="languages == ''"
+          v-bind:disabled="languages === ''"
           type="text"
           class="form-control"
         />
@@ -59,7 +63,7 @@
         <input
           name="overall_wall_time_limit"
           v-bind:value="overallWallTimeLimit"
-          v-bind:disabled="languages == ''"
+          v-bind:disabled="languages === ''"
           type="text"
           class="form-control"
         />
@@ -70,7 +74,7 @@
         <input
           name="extra_wall_time"
           v-bind:value="extraWallTime"
-          v-bind:disabled="languages == ''"
+          v-bind:disabled="languages === ''"
           type="text"
           class="form-control"
         />
@@ -83,7 +87,7 @@
         <input
           name="memory_limit"
           v-bind:value="memoryLimit"
-          v-bind:disabled="languages == ''"
+          v-bind:disabled="languages === ''"
           type="text"
           class="form-control"
         />
@@ -94,7 +98,7 @@
         <input
           name="output_limit"
           v-bind:value="outputLimit"
-          v-bind:disabled="languages == ''"
+          v-bind:disabled="languages === ''"
           type="text"
           class="form-control"
         />
@@ -104,7 +108,7 @@
         <input
           name="input_limit"
           v-bind:value="inputLimit"
-          v-bind:disabled="languages == ''"
+          v-bind:disabled="languages === ''"
           type="text"
           class="form-control"
         />
