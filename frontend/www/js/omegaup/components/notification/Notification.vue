4f932c9786573c181d4fdf74f628e5d6cd6fdbaa
Carlos Córdova <carlosab1802@gmail.com>
2020-05-28 02:19:56 +0000

    Agregando correcciones.

--- frontend/www/js/omegaup/components/notification/Notification.vue
+++ frontend/www/js/omegaup/components/notification/Notification.vue
@@ -52,10 +52,13 @@ import { types } from '../../api_types';
 import T from '../../lang';
 import * as ui from '../../ui';
 import * as time from '../../time';
+import * as markdown from '../../markdown';
 
 @Component
 export default class Notification extends Vue {
   @Prop() notification!: types.Notification;
+
+  markdownConverter = markdown.markdownConverter();
 
   get iconUrl(): string {
     switch (this.notification.contents.type) {
@@ -85,31 +88,36 @@ export default class Notification extends Vue {
   }
 
   get htmlText(): string {
+    if (this.notification.contents.body) {
+      return this.markdownConverter.makeHtml(
+        ui.formatString(
+          T[this.notification.contents.body.localizationString],
+          this.notification.contents.body.localizationParams,
+        ),
+      );
+    }
     switch (this.notification.contents.type) {
       case 'badge':
-        return ui.formatString(T.notificationNewBadge, {
-          badgeLink: `/badge/${this.notification.contents.badge}/`,
-          badgeName: T[`badge_${this.notification.contents.badge}_name`],
-        });
-      case 'course-registration-request':
-        return this.notification.contents.body
-          ? ui.formatString(
-              T[this.notification.contents.body.localizationString],
-              this.notification.contents.body.localizationParams,
-            )
-          : '';
-        return '';
+        return this.markdownConverter.makeHtml(
+          ui.formatString(T.notificationNewBadge, {
+            badgeName: T[`badge_${this.notification.contents.badge}_name`],
+          }),
+        );
       default:
         return '';
     }
   }
 
   get url(): string {
+    if (this.notification.contents.body) {
+      return this.notification.contents.body.url;
+    }
+
     switch (this.notification.contents.type) {
       case 'general_notification':
         return this.notification.contents.url || '';
-      case 'course-registration-request':
-        return this.notification.contents.body?.url || '';
+      case 'badge':
+        return `/badge/${this.notification.contents.badge}/`;
       case 'demotion':
         // TODO: Add link to problem page.
         return '';
