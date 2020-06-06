daeeb5ba76d09b74bbfcd4b9795d6eb7447c1428
Carlos Córdova Sáenz <carlosab1802@gmail.com>
2020-06-06 16:36:09 -0300

    Migrando course.details.tpl a TS + BS4 (#4150)
    ![image](https://user-images.githubusercontent.com/20785082/83658255-5a7d6200-a598-11ea-8f12-a66cf12b4bf9.png)
    
    Fixes: #3492

--- frontend/www/js/omegaup/api_types.ts
+++ frontend/www/js/omegaup/api_types.ts
@@ -247,6 +247,39 @@ export namespace types {
       );
     }
 
+    export function CourseDetailsPayload(
+      elementId: string = 'payload',
+    ): types.CourseDetailsPayload {
+      return (x => {
+        x.details = (x => {
+          if (x.assignments)
+            x.assignments = (x => {
+              if (!Array.isArray(x)) {
+                return x;
+              }
+              return x.map(x => {
+                if (x.finish_time)
+                  x.finish_time = ((x: number) => new Date(x * 1000))(
+                    x.finish_time,
+                  );
+                x.start_time = ((x: number) => new Date(x * 1000))(
+                  x.start_time,
+                );
+                return x;
+              });
+            })(x.assignments);
+          if (x.finish_time)
+            x.finish_time = ((x: number) => new Date(x * 1000))(x.finish_time);
+          if (x.start_time)
+            x.start_time = ((x: number) => new Date(x * 1000))(x.start_time);
+          return x;
+        })(x.details);
+        return x;
+      })(
+        JSON.parse((<HTMLElement>document.getElementById(elementId)).innerText),
+      );
+    }
+
     export function CourseListPayload(
       elementId: string = 'payload',
     ): types.CourseListPayload {
@@ -910,6 +943,11 @@ export namespace types {
     show_scoreboard?: boolean;
     start_time?: Date;
     student_count?: number;
+  }
+
+  export interface CourseDetailsPayload {
+    details: types.CourseDetails;
+    progress: types.AssignmentProgress;
   }
 
   export interface CourseListPayload {
