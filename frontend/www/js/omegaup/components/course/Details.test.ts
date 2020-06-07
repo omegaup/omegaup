46644d2987580f85af2b2bcfeae0fdb311afb061
lhchavez <lhchavez@lhchavez.com>
2020-06-07 12:44:34 -0700

    Arreglando error al desplegar un curso con duración ilimitada
    Este cambio arregla un error de JavaScript al intentar desplegar un
    curso sin fecha de fin. Esto permite que el curso de C++ se pueda volver
    a mostrar.

--- frontend/www/js/omegaup/components/course/Details.test.ts
+++ frontend/www/js/omegaup/components/course/Details.test.ts
@@ -4,6 +4,7 @@ import Vue from 'vue';
 
 import T from '../../lang';
 import { omegaup } from '../../omegaup';
+import { types } from '../../api_types';
 
 import course_Details from './Details.vue';
 
@@ -12,26 +13,74 @@ describe('Details.vue', () => {
     const courseName = 'Test course';
     const wrapper = shallowMount(course_Details, {
       propsData: {
-        course: {
+        course: <omegaup.Course>{
           admission_mode: 'registration',
           alias: 'test-course',
-          assignments: [],
+          assignments: <omegaup.Assignment[]>[],
           basic_information_required: false,
           description: '# Test',
           finish_time: null,
           isCurator: true,
           is_admin: true,
           name: courseName,
+          public: true,
           requests_user_information: 'no',
-          school_id: 0,
+          school_name: '',
           show_scoreboard: false,
           start_time: new Date(),
           student_count: 1,
         },
-        progress: [],
+        progress: <types.AssignmentProgress>{},
       },
     });
 
     expect(wrapper.text()).toContain(courseName);
+  });
+
+  it('Should handle assignments without finish_time', () => {
+    const courseName = 'Test course';
+    const wrapper = shallowMount(course_Details, {
+      propsData: {
+        course: <omegaup.Course>{
+          admission_mode: 'registration',
+          alias: 'test-course',
+          assignments: <omegaup.Assignment[]>[
+            <omegaup.Assignment>{
+              alias: 'test-assignment',
+              assignment_type: 'homework',
+              description: '',
+              finish_time: null,
+              name: 'Test',
+              order: 1,
+              scoreboard_url: '',
+              scoreboard_url_admin: '',
+              start_time: new Date(0),
+            },
+          ],
+          basic_information_required: false,
+          description: '# Test',
+          finish_time: null,
+          isCurator: true,
+          is_admin: true,
+          name: courseName,
+          public: true,
+          requests_user_information: 'no',
+          school_name: '',
+          show_scoreboard: false,
+          start_time: new Date(),
+          student_count: 1,
+        },
+        progress: <types.AssignmentProgress>{
+          'test-assignment': <types.Progress>{
+            score: 0,
+            max_score: 1,
+          },
+        },
+      },
+    });
+
+    expect(
+      wrapper.find('[data-homework-alias="test-assignment"]').text(),
+    ).toContain('—');
   });
 });
