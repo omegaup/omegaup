e647d54d702ebb9ad07899428bc31649feed712a
mjayalam <martin.ayala0297@gmail.com>
2020-06-08 17:21:54 +0000

    Aplicando clases de forms de BS4 a datos de perfil

--- frontend/www/js/omegaup/components/user/BasicInfov2.vue
+++ frontend/www/js/omegaup/components/user/BasicInfov2.vue
@@ -1,104 +1,94 @@
 <template>
   <div class="card-body">
-    <div>
-      <ul class="profile-fields fields-border">
-        <li>
-          <div class="padding-field">
-            <div class="field-name">
-              <strong>{{ T.profileUsername }}</strong>
-            </div>
-            <div class="field-data">
-              https://omegaup.com/profile/<strong
-                ><omegaup-user-username
-                  v-bind:classname="profile.classname"
-                  v-bind:username="profile.username"
-                ></omegaup-user-username></strong
-              >/
-            </div>
+    <div class="profile-fields fields-border">
+      <div class="form-group row padding-field">
+        <div class="col-sm-3 field-name">
+          <strong>{{ T.profileUsername }}</strong>
+        </div>
+        <div class="col-sm-9 field-data">
+          https://omegaup.com/profile/<strong
+            ><omegaup-user-username
+              v-bind:classname="profile.classname"
+              v-bind:username="profile.username"
+            ></omegaup-user-username></strong
+          >/
+        </div>
+      </div>
+      <div class="form-group row padding-field">
+        <div class="col-sm-3 field-name">
+          <strong>{{ T.profile }}</strong>
+        </div>
+        <div class="col-sm-9 field-data">
+          {{ profile.name }}
+        </div>
+      </div>
+
+      <div v-if="profile.email">
+        <div class="form-group row padding-field">
+          <div class="col-sm-3 field-name">
+            <strong>{{ T.profileEmail }}</strong>
           </div>
-        </li>
-        <li>
-          <div class="padding-field">
-            <div class="field-name">
-              <strong>{{ T.profile }}</strong>
-            </div>
-            <div class="field-data">
-              {{ profile.name }}
-            </div>
+          <div class="col-sm-9 field-data">
+            Primary: <strong>{{ profile.email }}</strong
+            >&nbsp;
           </div>
-        </li>
-        <li v-if="profile.email">
-          <div class="padding-field">
-            <div class="field-name">
-              <strong>{{ T.profileEmail }}</strong>
-            </div>
-            <div class="field-data">
-              Primary: <strong>{{ profile.email }}</strong
-              >&nbsp;
-            </div>
+        </div>
+
+        <div class="form-group row padding-field">
+          <div class="col-sm-3 field-name">
+            <strong>{{ T.profileCountry }}</strong>
           </div>
-        </li>
-        <li>
-          <div class="padding-field">
-            <div class="field-name">
-              <strong>{{ T.profileCountry }}</strong>
-            </div>
-            <div class="field-data">
-              <strong>{{ profile.country }}</strong>
-            </div>
+          <div class="col-sm-9 field-data">
+            <strong>{{ profile.country }}</strong>
           </div>
-        </li>
-        <li>
-          <div class="padding-field">
-            <div class="field-name">
-              <strong>{{ T.profileState }}</strong>
-            </div>
-            <div class="field-data">
-              <strong>{{ profile.state }}</strong>
-            </div>
+        </div>
+
+        <div class="form-group row padding-field">
+          <div class="col-sm-3 field-name">
+            <strong>{{ T.profileState }}</strong>
           </div>
-        </li>
-        <li>
-          <div class="padding-field">
-            <div class="field-name">
-              <strong>{{ T.profileSchool }}</strong>
-            </div>
-            <div class="field-data">
-              <a v-bind:href="`/schools/profile/${profile.school_id}/`"
-                ><strong>{{ profile.school }}</strong></a
-              >
-            </div>
+          <div class="col-sm-9 field-data">
+            <strong>{{ profile.state }}</strong>
           </div>
-        </li>
-        <li>
-          <div class="padding-field">
-            <div class="field-name">
-              <strong>{{ T.profileGraduationDate }}</strong>
-            </div>
-            <div class="field-data">
-              <strong>{{ profile.graduation_date }}</strong>
-            </div>
+        </div>
+
+        <div class="form-group row padding-field">
+          <div class="col-sm-3 field-name">
+            <strong>{{ T.profileSchool }}</strong>
           </div>
-        </li>
-        <li>
-          <div class="padding-field">
-            <div class="field-name">
-              <strong>{{ T.profileRank }}</strong
-              ><a href="https://blog.omegaup.com/categorias/" target="_blank"
-                ><em class="glyphicon glyphicon-question-sign"></em
-              ></a>
-            </div>
-            <div class="field-data">
-              <strong
-                ><omegaup-user-username
-                  v-bind:classname="profile.classname"
-                  v-bind:username="rank"
-                ></omegaup-user-username
-              ></strong>
-            </div>
+          <div class="col-sm-9 field-data">
+            <a v-bind:href="`/schools/profile/${profile.school_id}/`"
+              ><strong>{{ profile.school }}</strong></a
+            >
           </div>
-        </li>
-      </ul>
+        </div>
+
+        <div class="form-group row padding-field">
+          <div class="col-sm-3 field-name">
+            <strong>{{ T.profileGraduationDate }}</strong>
+          </div>
+          <div class="col-sm-9 field-data">
+            <strong>{{ profile.graduation_date }}</strong>
+          </div>
+        </div>
+
+        <div class="form-group row padding-field">
+          <div class="col-sm-3 field-name">
+            <strong>{{ T.profileRank }}</strong
+            ><a href="https://blog.omegaup.com/categorias/" target="_blank"
+              ><em class="glyphicon glyphicon-question-sign"></em
+            ></a>
+          </div>
+          <div class="col-sm-9 field-data">
+            <strong
+              ><omegaup-user-username
+                v-bind:classname="profile.classname"
+                v-bind:username="rank"
+              ></omegaup-user-username
+            ></strong>
+          </div>
+        </div>
+      </div>
     </div>
     <a
       v-if="!profile.is_private"
@@ -110,24 +100,15 @@
 </template>
 
 <style>
-.profile-fields .field-name {
-  color: #333;
-  float: left;
-  width: 10rem;
-}
-
-.fields-border > li {
-  border-color: #e9e9e9;
-  border-style: solid;
-  border-width: 0 0 0.05rem 0;
+.fields-border > .form-group,
+.fields-border div > .form-group {
+  border-color: #e9e9e9 !important;
+  border-style: solid !important;
+  border-width: 0 0 0.05rem 0 !important;
 }
 
 .field-data {
   color: gray;
-}
-
-li:not(first-child) {
-  border-width: 0;
 }
 
 .padding-field {
