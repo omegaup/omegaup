295534b2915463cbaff43b27069868295d0721bf
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2020-06-09 06:40:47 +0000

    Aplicando correcciones

--- frontend/server/src/Controllers/Course.php
+++ frontend/server/src/Controllers/Course.php
@@ -8,7 +8,7 @@
  * @psalm-type Progress=array{score: float, max_score: float}
  * @psalm-type AssignmentProgress=array<string, Progress>
  * @psalm-type CourseAssignment=array{alias: string, assignment_type: string, description: string, finish_time: \OmegaUp\Timestamp|null, max_points: float, name: string, order: int, publish_time_delay: int|null, scoreboard_url: string, scoreboard_url_admin: string, start_time: \OmegaUp\Timestamp}
- * @psalm-type CourseDetails=array{admission_mode?: string, alias: string, assignments?: list<CourseAssignment>, basic_information_required: bool, description: string, finish_time?: \OmegaUp\Timestamp|null, isCurator?: bool, is_admin: bool, name: string, public?: bool, requests_user_information: string, school_id?: int|null, school_name?: null|string, show_scoreboard?: bool, start_time?: \OmegaUp\Timestamp, student_count?: int}
+ * @psalm-type CourseDetails=array{admission_mode: string, alias: string, assignments: list<CourseAssignment>, basic_information_required: bool, description: string, finish_time: \OmegaUp\Timestamp|null, is_admin: bool, is_curator: bool, name: string, requests_user_information: string, school_id: int|null, school_name?: null|string, show_scoreboard: bool, start_time: \OmegaUp\Timestamp, student_count?: int}
  * @psalm-type RunMetadata=array{verdict: string, time: float, sys_time: int, wall_time: float, memory: int}
  * @psalm-type Run=array{guid: string, language: string, status: string, verdict: string, runtime: int, penalty: int, memory: int, score: float, contest_score: float|null, time: \OmegaUp\Timestamp, submit_delay: int, type: null|string, username: string, classname: string, alias: string, country: string, contest_alias: null|string}
  * @psalm-type ScoreboardRankingProblem=array{alias: string, penalty: float, percent: float, pending?: int, place?: int, points: float, run_details?: array{cases?: list<array{contest_score: float, max_score: float, meta: RunMetadata, name: null|string, out_diff: string, score: float, verdict: string}>, details: array{groups: list<array{cases: list<array{meta: RunMetadata}>}>}}, runs: int}
@@ -2765,9 +2765,7 @@ class Course extends \OmegaUp\Controllers\Controller {
             'name' => strval($course->name),
             'description' => strval($course->description),
             'alias' => strval($course->alias),
-            'basic_information_required' => boolval(
-                $course->needs_basic_information
-            ),
+            'basic_information_required' => $course->needs_basic_information,
             'requests_user_information' => $course->requests_user_information,
         ];
     }
@@ -2795,13 +2793,11 @@ class Course extends \OmegaUp\Controllers\Controller {
             'start_time' => $course->start_time,
             'finish_time' => $course->finish_time,
             'is_admin' => $isAdmin,
-            'isCurator' => \OmegaUp\Authorization::canCreatePublicCourse(
+            'is_curator' => \OmegaUp\Authorization::canCreatePublicCourse(
                 $identity
             ),
             'admission_mode' => $course->admission_mode,
-            'basic_information_required' => boolval(
-                $course->needs_basic_information
-            ),
+            'basic_information_required' => $course->needs_basic_information,
             'show_scoreboard' => boolval($course->show_scoreboard),
             'requests_user_information' => $course->requests_user_information
         ];
