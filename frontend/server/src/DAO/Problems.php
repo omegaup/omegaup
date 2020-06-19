b65928f55b01dbae3d4e5884fd84c566e40204d3
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2020-06-19 00:40:28 -0500

    Agregando pruebas

--- frontend/server/src/DAO/Problems.php
+++ frontend/server/src/DAO/Problems.php
@@ -562,7 +562,8 @@ class Problems extends \OmegaUp\DAO\Base\Problems {
         $params = [
             \OmegaUp\ProblemParams::VISIBILITY_PUBLIC_WARNING,
             \OmegaUp\ProblemParams::VISIBILITY_PUBLIC_BANNED,
-            $identityId];
+            $identityId,
+        ];
 
         /** @var list<array{accepted: int, acl_id: int, alias: string, allow_user_add_tags: bool, commit: string, creation_date: \OmegaUp\Timestamp, current_version: string, deprecated: bool, difficulty: float|null, difficulty_histogram: null|string, email_clarifications: bool, input_limit: int, languages: string, order: string, problem_id: int, quality: float|null, quality_histogram: null|string, quality_seal: bool, show_diff: string, source: null|string, submissions: int, title: string, visibility: int, visits: int}> */
         $rs = \OmegaUp\MySQLConnection::getInstance()->GetAll($sql, $params);
