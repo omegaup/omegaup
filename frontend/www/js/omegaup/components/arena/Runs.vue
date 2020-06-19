ba4adfce44a8437675ad98b1a3b06bb51cd04938
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2020-06-19 11:02:17 -0500

    Arreglando los campos de autocomplete en las vistas de envíos (#4241)
    Se arregla el autocomplete de problemas y de usuarios en las vistas de envíos.
    
    Contests:
    ![AutocompleteContestRuns](https://user-images.githubusercontent.com/3230352/85101868-01811100-b1c9-11ea-8d63-cd24efe47d73.gif)
    
    Courses:
    ![AutocompleteCourseRuns](https://user-images.githubusercontent.com/3230352/85101885-0f369680-b1c9-11ea-9e6d-d7cbf1089f15.gif)
    
    Global:
    ![AutocompleteGlobalRuns](https://user-images.githubusercontent.com/3230352/85101900-15c50e00-b1c9-11ea-87ba-f622597c21fe.gif)
    
    
    Fixes: #4222

--- frontend/www/js/omegaup/components/arena/Runs.vue
+++ frontend/www/js/omegaup/components/arena/Runs.vue
@@ -110,9 +110,7 @@
               <label
                 >{{ T.wordsUser }}:
                 <omegaup-autocomplete
-                  v-bind:init="
-                    el => typeahead.userContestTypeahead(el, this.contestAlias)
-                  "
+                  v-bind:init="initUserAutocomplete"
                   v-model="filterUsername"
                 ></omegaup-autocomplete>
               </label>
@@ -368,8 +366,8 @@ export default class Runs extends Vue {
   }
 
   initProblemAutocomplete(el: JQuery<HTMLElement>) {
-    if (this.problemsetProblems !== null) {
-      typeahead.problemContestTypeahead(
+    if (this.problemsetProblems.length !== 0) {
+      typeahead.problemsetProblemTypeahead(
         el,
         () => this.problemsetProblems,
         (event: Event, item: { alias: string; title: string }) => {
@@ -378,6 +376,14 @@ export default class Runs extends Vue {
       );
     } else {
       typeahead.problemTypeahead(el);
+    }
+  }
+
+  initUserAutocomplete(el: JQuery<HTMLElement>) {
+    if (this.problemsetProblems.length !== 0 && this.contestAlias) {
+      typeahead.userContestTypeahead(el, this.contestAlias);
+    } else {
+      typeahead.userTypeahead(el);
     }
   }
 
