4f255a083507b8ac01fab3bb0015d035192075cb
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2020-06-24 02:30:58 +0000

    Arreglando las pruebas

--- frontend/tests/controllers/AssignmentRemoveTest.php
+++ frontend/tests/controllers/AssignmentRemoveTest.php
@@ -4,45 +4,6 @@
  * @author juan.pablo
  */
 class AssignmentRemoveTest extends \OmegaUp\Test\ControllerTestCase {
-    private static $admin = null;
-    private static $participant = null;
-    private static $login = null;
-    private static $loginParticipant = null;
-    private static $courseData = null;
-    private static $problemData = null;
-
-    public function setUp(): void {
-        parent::setUp();
-
-        ['identity' => self::$admin] = \OmegaUp\Test\Factories\User::createUser();
-        self::$login = self::login(self::$admin);
-
-        self::$courseData = \OmegaUp\Test\Factories\Course::createCourseWithOneAssignment(
-            self::$admin,
-            self::$login
-        );
-
-        self::$problemData = \OmegaUp\Test\Factories\Problem::createProblem();
-
-        \OmegaUp\Test\Factories\Course::addProblemsToAssignment(
-            self::$login,
-            self::$courseData['course_alias'],
-            self::$courseData['assignment_alias'],
-            [self::$problemData]
-        );
-
-        [
-            'identity' => self::$participant,
-        ] = \OmegaUp\Test\Factories\User::createUser();
-
-        \OmegaUp\Test\Factories\Course::addStudentToCourse(
-            self::$courseData,
-            self::$participant
-        );
-
-        self::$loginParticipant = self::login(self::$participant);
-    }
-
     public function testAssignmentRemove() {
         $courseData = \OmegaUp\Test\Factories\Course::createCourseWithAssignments(
             /*$numberOfAssignments=*/5
@@ -80,22 +41,48 @@ class AssignmentRemoveTest extends \OmegaUp\Test\ControllerTestCase {
      * by admin
      */
     public function testAssignmentRemoveWithAccessLog() {
-        $courseAlias = self::$courseData['course_alias'];
-        $assignmentAlias = self::$courseData['assignment_alias'];
+        ['identity' => $admin] = \OmegaUp\Test\Factories\User::createUser();
+        $login = self::login($admin);
+
+        $courseData = \OmegaUp\Test\Factories\Course::createCourseWithOneAssignment(
+            $admin,
+            $login
+        );
+
+        $problemData = \OmegaUp\Test\Factories\Problem::createProblem();
+
+        \OmegaUp\Test\Factories\Course::addProblemsToAssignment(
+            $login,
+            $courseData['course_alias'],
+            $courseData['assignment_alias'],
+            [$problemData]
+        );
+
+        ['identity' => $participant] = \OmegaUp\Test\Factories\User::createUser();
+
+        \OmegaUp\Test\Factories\Course::addStudentToCourse(
+            $courseData,
+            $participant
+        );
+
+        $loginParticipant = self::login($participant);
+
+        $courseAlias = $courseData['course_alias'];
+        $assignmentAlias = $courseData['assignment_alias'];
 
         $getAssignmentResponse = \OmegaUp\Controllers\Course::apiAssignmentDetails(
             new \OmegaUp\Request([
-                'auth_token' => self::$loginParticipant->auth_token,
+                'auth_token' => $loginParticipant->auth_token,
                 'course' => $courseAlias,
                 'assignment' => $assignmentAlias,
             ])
         );
 
-        self::$login = self::login(self::$admin);
+        $login = self::login($admin);
 
         \OmegaUp\Controllers\Course::apiRemoveAssignment(
             new \OmegaUp\Request([
-                'auth_token' => self::$login->auth_token,
+                'auth_token' => $login->auth_token,
                 'assignment_alias' => $assignmentAlias,
                 'course_alias' => $courseAlias,
             ])
@@ -103,7 +90,7 @@ class AssignmentRemoveTest extends \OmegaUp\Test\ControllerTestCase {
 
         $response = \OmegaUp\Controllers\Course::apiListAssignments(
             new \OmegaUp\Request([
-                'auth_token' => self::$login->auth_token,
+                'auth_token' => $login->auth_token,
                 'course_alias' => $courseAlias
             ])
         );
@@ -115,28 +102,54 @@ class AssignmentRemoveTest extends \OmegaUp\Test\ControllerTestCase {
      * assignment is removed by admin
      */
     public function testAssignmentRemoveWithProblemOpened() {
-        $courseAlias = self::$courseData['course_alias'];
-        $assignmentAlias = self::$courseData['assignment_alias'];
+        ['identity' => $admin] = \OmegaUp\Test\Factories\User::createUser();
+        $login = self::login($admin);
+
+        $courseData = \OmegaUp\Test\Factories\Course::createCourseWithOneAssignment(
+            $admin,
+            $login
+        );
+
+        $problemData = \OmegaUp\Test\Factories\Problem::createProblem();
+
+        \OmegaUp\Test\Factories\Course::addProblemsToAssignment(
+            $login,
+            $courseData['course_alias'],
+            $courseData['assignment_alias'],
+            [$problemData]
+        );
+
+        ['identity' => $participant] = \OmegaUp\Test\Factories\User::createUser();
+
+        \OmegaUp\Test\Factories\Course::addStudentToCourse(
+            $courseData,
+            $participant
+        );
+
+        $loginParticipant = self::login($participant);
+
+        $courseAlias = $courseData['course_alias'];
+        $assignmentAlias = $courseData['assignment_alias'];
 
         $getAssignmentResponse = \OmegaUp\Controllers\Course::apiAssignmentDetails(
             new \OmegaUp\Request([
-                'auth_token' => self::$loginParticipant->auth_token,
+                'auth_token' => $loginParticipant->auth_token,
                 'course' => $courseAlias,
                 'assignment' => $assignmentAlias,
             ])
         );
 
         \OmegaUp\Test\Factories\Course::openProblemInCourseAssignment(
-            self::$courseData,
-            self::$problemData,
-            self::$participant
+            $courseData,
+            $problemData,
+            $participant
         );
 
-        self::$login = self::login(self::$admin);
+        $login = self::login($admin);
 
         \OmegaUp\Controllers\Course::apiRemoveAssignment(
             new \OmegaUp\Request([
-                'auth_token' => self::$login->auth_token,
+                'auth_token' => $login->auth_token,
                 'assignment_alias' => $assignmentAlias,
                 'course_alias' => $courseAlias,
             ])
@@ -144,7 +157,7 @@ class AssignmentRemoveTest extends \OmegaUp\Test\ControllerTestCase {
 
         $response = \OmegaUp\Controllers\Course::apiListAssignments(
             new \OmegaUp\Request([
-                'auth_token' => self::$login->auth_token,
+                'auth_token' => $login->auth_token,
                 'course_alias' => $courseAlias
             ])
         );
@@ -152,22 +165,46 @@ class AssignmentRemoveTest extends \OmegaUp\Test\ControllerTestCase {
     }
 
     public function testAssignmentRemoveWithSubmittedRuns() {
+        ['identity' => $admin] = \OmegaUp\Test\Factories\User::createUser();
+        $login = self::login($admin);
+
+        $courseData = \OmegaUp\Test\Factories\Course::createCourseWithOneAssignment(
+            $admin,
+            $login
+        );
+
+        $problemData = \OmegaUp\Test\Factories\Problem::createProblem();
+
+        \OmegaUp\Test\Factories\Course::addProblemsToAssignment(
+            $login,
+            $courseData['course_alias'],
+            $courseData['assignment_alias'],
+            [$problemData]
+        );
+
+        ['identity' => $participant] = \OmegaUp\Test\Factories\User::createUser();
+
+        \OmegaUp\Test\Factories\Course::addStudentToCourse(
+            $courseData,
+            $participant
+        );
+
         $runData = \OmegaUp\Test\Factories\Run::createCourseAssignmentRun(
-            self::$problemData,
-            self::$courseData,
-            self::$participant
+            $problemData,
+            $courseData,
+            $participant
         );
 
         \OmegaUp\Test\Factories\Run::gradeRun($runData);
 
-        self::$login = self::login(self::$admin);
+        $login = self::login($admin);
 
         try {
             \OmegaUp\Controllers\Course::apiRemoveAssignment(
                 new \OmegaUp\Request([
-                    'auth_token' => self::$login->auth_token,
-                    'assignment_alias' => self::$courseData['assignment_alias'],
-                    'course_alias' => self::$courseData['course_alias'],
+                    'auth_token' => $login->auth_token,
+                    'assignment_alias' => $courseData['assignment_alias'],
+                    'course_alias' => $courseData['course_alias'],
                 ])
             );
             $this->fail('Should have thrown exception.');
