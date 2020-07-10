f38b3438e10193c4035216e24217296ce35af954
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2020-07-09 21:08:17 -0500

    Agregando un par de pruebas de Cursos (#4240)

---
+++ frontend/www/js/omegaup/components/course/Edit.test.ts
@@ -0,0 +1,55 @@
+import { shallowMount } from '@vue/test-utils';
+import expect from 'expect';
+import Vue from 'vue';
+
+import T from '../../lang';
+import { omegaup } from '../../omegaup';
+import { types } from '../../api_types';
+
+import course_Edit from './Edit.vue';
+
+describe('Edit.vue', () => {
+  it('Should handle empty assignments', async () => {
+    const courseName = 'Test course';
+    const wrapper = shallowMount(course_Edit, {
+      propsData: {
+        data: {
+          course: <omegaup.Course>{
+            admission_mode: 'registration',
+            alias: 'test-course',
+            assignments: <omegaup.Assignment[]>[],
+            basic_information_required: false,
+            description: '# Test',
+            finish_time: null,
+            is_curator: true,
+            is_admin: true,
+            name: courseName,
+            public: true,
+            requests_user_information: 'no',
+            school_name: '',
+            show_scoreboard: false,
+            start_time: new Date(),
+            student_count: 1,
+          },
+          assignmentProblems: [],
+          selectedAssignment: null,
+          students: [],
+          identityRequests: [],
+          admins: [],
+          groupsAdmins: [],
+        },
+      },
+    });
+
+    expect(wrapper.text()).toContain(courseName);
+
+    // All the links are available
+    await wrapper.find('a[data-tab-assignments]').trigger('click');
+    await wrapper.find('a[data-tab-problems]').trigger('click');
+    await wrapper.find('a[data-tab-admission-mode]').trigger('click');
+    await wrapper.find('a[data-tab-students]').trigger('click');
+    await wrapper.find('a[data-tab-admins]').trigger('click');
+    await wrapper.find('a[data-tab-clone]').trigger('click');
+    await wrapper.find('a[data-tab-course]').trigger('click');
+  });
+});
