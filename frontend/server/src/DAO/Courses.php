8e49082c6e3b7af97c214fc59aec47c6bc12e79e
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2020-07-31 07:55:46 +0000

    Nits

--- frontend/server/src/DAO/Courses.php
+++ frontend/server/src/DAO/Courses.php
@@ -209,7 +209,7 @@ class Courses extends \OmegaUp\DAO\Base\Courses {
             WHERE c.admission_mode = ?;
            ';
 
-        /** @var list<array{accept_teacher: null, admission_mode: string, alias: string, course_id: int, finish_time: \OmegaUp\Timestamp|null, is_open: bool, name: string, progress: float, school_name: null|string, start_time: \OmegaUp\Timestamp}> */
+        /** @var list<array{accept_teacher: int|null, admission_mode: string, alias: string, course_id: int, finish_time: \OmegaUp\Timestamp|null, is_open: int, name: string, progress: float, school_name: null|string, start_time: \OmegaUp\Timestamp}> */
         $rs = \OmegaUp\MySQLConnection::getInstance()->GetAll(
             $sql,
             [\OmegaUp\Controllers\Course::ADMISSION_MODE_PUBLIC]
@@ -223,6 +223,7 @@ class Courses extends \OmegaUp\DAO\Base\Courses {
             $row['counts'] = \OmegaUp\DAO\Assignments::getAssignmentCountsForCourse(
                 $row['course_id']
             );
+            $row['is_open'] = boolval($row['is_open']);
             unset($row['course_id']);
             $courses[] = $row;
         }
