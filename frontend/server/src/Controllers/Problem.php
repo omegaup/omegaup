d63a20a4314ca40abc932afaec4af583506618e0
Aaron Castillo Castro <s16120237@alumnos.itsur.edu.mx>
2020-08-28 00:13:57 +0000

    validación del campo level_Tag del lado del servidor.

--- frontend/server/src/Controllers/Problem.php
+++ frontend/server/src/Controllers/Problem.php
@@ -157,8 +157,8 @@ class Problem extends \OmegaUp\Controllers\Controller {
                 $r['overall_wall_time_limit']
             );
         }
-        if (!is_null($r['level_tag'])) {
-            $params['problem_level'] = strval($r['level_tag']);
+        if (!is_null($r['problem_level'])) {
+            $params['problem_level'] = strval($r['problem_level']);
         }
         if (!is_null($r['selected_tags'])) {
             $params['selected_tags'] = strval($r['selected_tags']);
@@ -5115,7 +5115,7 @@ class Problem extends \OmegaUp\Controllers\Controller {
                                 'languages' => strval($r['languages']),
                                 'validator' => strval($r['validator']),
                                 'tags' => $tags,
-                                'problem_level' => strval($r['level_tag']),
+                                'problem_level' => strval($r['problem_level']),
                                 'publicTags' => \OmegaUp\Controllers\Tag::getPublicTags(),
                                 'levelTags' => \OmegaUp\Controllers\Tag::getLevelTags(),
                             ],
@@ -5156,7 +5156,7 @@ class Problem extends \OmegaUp\Controllers\Controller {
                             $sortedLanguages
                         ),
                         'tags' => $tags,
-                        'problem_level' => '',
+                        'problem_level' => 'problemLevelBasicIntroductionToProgramming',
                         'publicTags' => \OmegaUp\Controllers\Tag::getPublicTags(),
                         'levelTags' => \OmegaUp\Controllers\Tag::getLevelTags(),
                     ],
--- frontend/tests/Factories/Problem.php
+++ frontend/tests/Factories/Problem.php
@@ -57,7 +57,7 @@ class ProblemParams {
     public $levelTags;
 
     /**
-     * @param array{allow_user_add_tags?: bool, zipName?: string, title?: string, visibility?: ('deleted'|'private_banned'|'public_banned'|'private_warning'|'private'|'public_warning'|'public'|'promoted'), author?: \OmegaUp\DAO\VO\Identities, authorUser?: \OmegaUp\DAO\VO\Users, languages?: string, show_diff?: string, level_tag?: string} $params
+     * @param array{allow_user_add_tags?: bool, zipName?: string, title?: string, visibility?: ('deleted'|'private_banned'|'public_banned'|'private_warning'|'private'|'public_warning'|'public'|'promoted'), author?: \OmegaUp\DAO\VO\Identities, authorUser?: \OmegaUp\DAO\VO\Users, languages?: string, show_diff?: string, problem_level?: string} $params
      */
     public function __construct($params = []) {
         $this->zipName = $params['zipName'] ?? (OMEGAUP_TEST_RESOURCES_ROOT . 'testproblem.zip');
@@ -66,7 +66,7 @@ class ProblemParams {
         $this->visibility = $params['visibility'] ?? 'public';
         $this->showDiff = $params['show_diff'] ?? 'none';
         $this->allowUserAddTags = $params['allow_user_add_tags'] ?? false;
-        $this->levelTags = $params['level_tag'] ?? 'problemLevelBasicIntroductionToProgramming';
+        $this->levelTags = $params['problem_level'] ?? 'problemLevelBasicIntroductionToProgramming';
         if (!empty($params['author']) && !empty($params['authorUser'])) {
             $this->author = $params['author'];
             $this->authorUser = $params['authorUser'];
@@ -139,7 +139,7 @@ class Problem {
             'languages' => $params->languages,
             'show_diff' => $params->showDiff,
             'allow_user_add_tags' => $params->allowUserAddTags,
-            'level_tag' => 'problemLevelBasicIntroductionToProgramming',
+            'problem_level' => 'problemLevelBasicIntroductionToProgramming',
         ]);
 
         // Set file upload context
