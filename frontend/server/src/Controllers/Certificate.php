781e9fdec5bb5baa78b2851627e8922f7518ebad
Rafa Díaz <racrux@gmail.com>
2020-09-22 09:20:22 -0700

    Fixing Certificate.php

--- frontend/server/src/Controllers/Certificate.php
+++ frontend/server/src/Controllers/Certificate.php
@@ -11,13 +11,13 @@ class Certificate extends \OmegaUp\Controllers\Controller {
     /**
      * @return array{smartyProperties: array{payload: CertificateDetailsPayload, title: \OmegaUp\TranslationString}, entrypoint: string}
      *
-     * @omegaup-request-param null|string $uuid
+     * @omegaup-request-param string $uuid
      */
     public static function getDetailsForSmarty(\OmegaUp\Request $r) {
         return [
             'smartyProperties' => [
                 'payload' => [
-                    'uuid' => strval($r['uuid']),
+                    'uuid' => $r['uuid'],
                 ],
                 'title' => new \OmegaUp\TranslationString(
                     'omegaupTitleCertificate'
