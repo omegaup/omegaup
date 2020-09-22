96d0347e431a08b5572897fab3f52ee3eb0723d0
Rafa Díaz <racrux@gmail.com>
2020-09-22 09:37:10 -0700

    Fixing Certificate.php

--- frontend/server/src/Controllers/Certificate.php
+++ frontend/server/src/Controllers/Certificate.php
@@ -17,7 +17,7 @@ class Certificate extends \OmegaUp\Controllers\Controller {
         return [
             'smartyProperties' => [
                 'payload' => [
-                    'uuid' => $r['uuid'],
+                    'uuid' => $r->ensureString('uuid'),
                 ],
                 'title' => new \OmegaUp\TranslationString(
                     'omegaupTitleCertificate'
