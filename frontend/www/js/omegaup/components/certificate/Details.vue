159a9ecbd92c4e7144dfb189a268e9c4c821ea4c
Rafa Díaz <racrux@gmail.com>
2020-09-23 10:51:52 -0700

    Last fix, hopefully

--- frontend/www/js/omegaup/components/certificate/Details.vue
+++ frontend/www/js/omegaup/components/certificate/Details.vue
@@ -2,7 +2,7 @@
   <div class="panel panel-default">
     <div class="panel-heading">
       <h4 class="panel-title">
-        T.certificateDetailsTitle
+        {{ T.certificateDetailsTitle }}
       </h4>
     </div>
     <div class="panel-body">
