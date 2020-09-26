3c1d5d9a3ad9f753695ed69f6ab1a012036adf5d
BlueBlockGrape <43051192+BlueBlockGrape@users.noreply.github.com>
2020-09-26 12:43:30 -0500

    Creación Página problem/collection (#4715)
    Creación de la página problem/collection donde se tendrá la parte de las colecciones de problemas
    
    ![image](https://user-images.githubusercontent.com/43051192/94215763-32956f80-fea3-11ea-8e70-6e97c3cca3bc.png)
    
    
    Fixes: #4579

---
+++ frontend/www/js/omegaup/components/problem/Collection.test.ts
@@ -0,0 +1,16 @@
+import { shallowMount } from '@vue/test-utils';
+import expect from 'expect';
+import Vue from 'vue';
+
+import T from '../../lang';
+import { omegaup } from '../../omegaup';
+
+import problem_collection from './Collection.vue';
+
+describe('Collection.vue', () => {
+  it('Should handle empty list of problems', async () => {
+    const wrapper = shallowMount(problem_collection, {});
+
+    expect(wrapper.text()).toContain(T.collectionTitle);
+  });
+});
