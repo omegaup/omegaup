c7667533adfa42944eb5140ca733697a766b6818
VegaMex <s16120245@alumnos.itsur.edu.mx>
2020-09-27 16:51:03 +0000

    Primeras pruebas

---
+++ frontend/tests/controllers/TagListTest.php
@@ -0,0 +1,32 @@
+<?php
+
+class TagListTest extends \OmegaUp\Test\ControllerTestCase {
+
+    public function testValidParameter(): array {
+        ['user' => $user, 'identity' => $identity] = \OmegaUp\Test\Factories\User::createUser();
+        
+        $login = self::login($identity);
+        try {
+            \OmegaUp\Controllers\Tag::apiFrequentTags(new \OmegaUp\Request([
+                'auth_token' => $login->auth_token,
+                'problemLevel' => 'problemLevelBasicIntroductionToProgramming'
+            ]));
+            $this->fail('Incorrect parameter');
+        } catch (\OmegaUp\Exceptions\InvalidParameterException $e) {
+            $this->assertEquals('parameterInvalid', $e->getMessage());
+        }
+    }
+
+    public function testGetListOfFrequentTags() {
+        ['user' => $user, 'identity' => $identity] = \OmegaUp\Test\Factories\User::createUser();
+
+        $login = self::login($identity);
+        $response = \OmegaUp\Controllers\Tag::apiFrequentTags(
+            new \OmegaUp\Request([
+                'auth_token' => $login->auth_token,
+                'problemLevel' => 'problemLevelBasicIntroductionToProgramming'
+            ])
+        );
+        $this->assertIsArray($response);
+    }
+}
