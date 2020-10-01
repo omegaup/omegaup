406d22a2b6db900b2bec9e39b0228f381564b57e
VegaMex <s16120245@alumnos.itsur.edu.mx>
2020-10-01 18:41:34 +0000

    Nueva página

---
+++ frontend/www/js/omegaup/components/problem/CollectionDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-header">{{ data.title }}</h5>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import T from '../../lang';
+import { types } from '../../api_types';
+
+@Component
+export default class CollectionDetails extends Vue {
+  @Prop() data!: types.CollectionDetailsPayload;
+
+  T = T;
+}
+</script>
---
+++ frontend/www/js/omegaup/problem/collection_details.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import collection_Details from '../components/problem/CollectionDetails.vue';
+import { types } from '../api_types';
+import { OmegaUp } from '../omegaup';
+
+OmegaUp.on('ready', () => {
+  const payload = types.payloadParsers.CollectionDetailsPayload();
+  const CollectionDetails = new Vue({
+    el: '#main-container',
+    render: function (createElement) {
+      return createElement('omegaup-collection-details', {
+        props: {
+          collection: payload.collection,
+        },
+      });
+    },
+    components: {
+      'omegaup-collection-details': collection_Details,
+    },
+  });
+});
