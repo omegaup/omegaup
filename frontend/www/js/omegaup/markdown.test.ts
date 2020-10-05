84ff3f06b0b9ea681d035cc7357198377b3b5bef
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2020-10-05 11:28:18 -0500

    Arreglando prueba

--- frontend/www/js/omegaup/markdown.test.ts
+++ frontend/www/js/omegaup/markdown.test.ts
@@ -56,10 +56,10 @@ describe('markdown', () => {
     it('Should handle valid iframe tag form facebook', () => {
       expect(
         converter.makeHtml(
-          '<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fomegaup%2Fvideos%2F291451792022031%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>',
+          '<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fomegaup%2Fvideos%2F291451792022031%2F&show_text=0&width=560" width="560" height="315" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>',
         ),
       ).toEqual(
-        '<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fomegaup%2Fvideos%2F291451792022031%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>',
+        '<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fomegaup%2Fvideos%2F291451792022031%2F&show_text=0&width=560" width="560" height="315" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>',
       );
     });
 
