66d9681bf2adfd9be84bfc85a77e8194ea510594
VegaMex <s16120245@alumnos.itsur.edu.mx>
2020-10-06 14:38:25 +0000

    Archivo del nuevo componente

---
+++ frontend/www/js/omegaup/components/problem/Filter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card mb-3 panel panel-primary">
+
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+import { omegaup } from '../../omegaup';
+import T from '../../lang';
+import * as typeahead from '../../typeahead';
+
+@Component({
+  components: {
+    'omegaup-autocomplete': Autocomplete,
+  },
+})
+
+export default class CollectionFilter extends Vue {
+
+}
+</script>
