2e2c1d590517d340b47f5bdfca1081569011872e
Aaron Castillo Castro <s16120237@alumnos.itsur.edu.mx>
2020-10-08 18:37:42 +0000

    correccion de errores

--- frontend/server/src/Controllers/Problem.php
+++ frontend/server/src/Controllers/Problem.php
@@ -5910,7 +5910,7 @@ class Problem extends \OmegaUp\Controllers\Controller {
     /**
      * @return array{alias: string}
      */
-    public static function apiRandomProblem(Request $r) {
+    public static function apiRandomProblem(\OmegaUp\Request $r) {
         return [
             'alias' => \OmegaUp\DAO\Problems::getRandomProblemAlias(),
         ];
