0f2064d462f3328b5486f43e93ccc3db30245979
Israel Teneda <israteneda@gmail.com>
2020-10-09 15:06:53 -0500

    aplicando cambios

--- frontend/www/js/omegaup/lang.es.ts
+++ frontend/www/js/omegaup/lang.es.ts
@@ -1289,7 +1289,7 @@ const translations: { [key: string]: string; } = {
   wordsExam: "Examen",
   wordsExperiments: "Experimentos",
   wordsExtraWallTimeMs: "Tiempo extra para libinteractive (ms)",
-  wordsFeedback: "Comentarios",
+  wordsFeedback: "Retroalimentaci\u00f3n de env\u00edos",
   wordsFilterByCountry: "Filtrar por mi pa\u00eds / regi\u00f3n",
   wordsFilterByLanguage: "Filtrar por idioma",
   wordsFilterBySchool: "Filtrar por mi escuela",
