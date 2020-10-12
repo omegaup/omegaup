1338e1c8ebc371abe2d46762d03120e4a185cf96
BlueBlockGrape <43051192+BlueBlockGrape@users.noreply.github.com>
2020-10-12 18:27:15 -0500

    Otras colecciones y arreglos al componente problemCollection (#4798)
    Se agrega información para la parte de otras colecciones
    
    ![image](https://user-images.githubusercontent.com/43051192/95609717-caa76300-0a24-11eb-903b-58271bc7ff54.png)
    
    Part of: #4577

--- frontend/www/js/omegaup/components/problem/CollectionProblem.vue
+++ frontend/www/js/omegaup/components/problem/CollectionProblem.vue
@@ -2,23 +2,21 @@
   <div class="col-sm-4 mt-2">
     <div class="border border-dark">
       <div class="row">
-        <div class="col-sm-4 text-center pt-2">
+        <div class="col-sm-4 d-flex align-items-center justify-content-end">
           <h1>
-            <font-awesome-icon
-              :icon="['fas', getProblemLevelIcon(levelTagAlias)]"
-            />
+            <slot name="icon"></slot>
           </h1>
         </div>
-        <div class="col-sm-8 text-center" style="height: 112px">
+        <div class="col-sm-8 d-flex align-items-center" style="height: 112px">
           <p>
-            <strong>{{ name }}</strong> <br />
-            {{ problemCount }} {{ T.wordsProblems }}
+            <strong>{{ title }}</strong> <br />
+            <slot name="problem-count"></slot>
           </p>
         </div>
       </div>
       <div class="row">
         <div class="col mt-1 mb-1 text-center">
-          <a class="btn btn-primary" href="/problem/">{{
+          <a class="btn btn-primary" :href="href">{{
             T.problemcollectionViewProblems
           }}</a>
         </div>
@@ -33,56 +31,14 @@ import T from '../../lang';
 import 'v-tooltip/dist/v-tooltip.css';
 import { VTooltip } from 'v-tooltip';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import {
-  faRobot,
-  faLaptopCode,
-  faSquareRootAlt,
-  faProjectDiagram,
-  faSitemap,
-  faTrophy,
-  faCode,
-} from '@fortawesome/free-solid-svg-icons';
-library.add(faRobot);
-library.add(faLaptopCode);
-library.add(faSquareRootAlt);
-library.add(faProjectDiagram);
-library.add(faSitemap);
-library.add(faTrophy);
-library.add(faCode);
-
-const problemLevelIcons: { [key: string]: string } = {
-  problemLevelBasicKarel: 'robot',
-  problemLevelBasicIntroductionToProgramming: 'laptop-code',
-  problemLevelIntermediateMathsInProgramming: 'square-root-alt',
-  problemLevelIntermediateDataStructuresAndAlgorithms: 'project-diagram',
-  problemLevelIntermediateAnalysisAndDesignOfAlgorithms: 'sitemap',
-  problemLevelAdvancedCompetitiveProgramming: 'trophy',
-  problemLevelAdvancedSpecializedTopics: 'code',
-};
-
 @Component({
   directives: {
     tooltip: VTooltip,
   },
-  components: {
-    FontAwesomeIcon,
-  },
 })
 export default class CollectionProblem extends Vue {
-  @Prop() levelTagAlias!: string;
-  @Prop() problemCount!: number;
+  @Prop() title!: string;
+  @Prop() href!: string;
   T = T;
-
-  get name(): string {
-    return T[this.levelTagAlias];
-  }
-
-  getProblemLevelIcon(problemLevel: string): string {
-    if (Object.prototype.hasOwnProperty.call(problemLevelIcons, problemLevel))
-      return problemLevelIcons[problemLevel];
-    return 'icon';
-  }
 }
 </script>
