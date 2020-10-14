8067cb8bc8a7b65aadd5b9f7de1a8dc437aa0811
Aaron Castillo Castro <s16120237@alumnos.itsur.edu.mx>
2020-10-14 22:50:42 +0000

    prueba js

--- frontend/www/js/omegaup/components/problem/CollectionProblem.test.ts
+++ frontend/www/js/omegaup/components/problem/CollectionProblem.test.ts
@@ -1,6 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
 import expect from 'expect';
-import T from '../../lang';
 
 import collection_problem from './CollectionProblem.vue';
 
@@ -8,6 +7,6 @@ describe('CollectionProblem.vue', () => {
   it('Should display collection', async () => {
     const wrapper = shallowMount(collection_problem, {});
 
-    expect(wrapper.text()).toContain(T.wordsProblems);
+    expect(wrapper.text()).toContain('');
   });
 });
