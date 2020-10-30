7f03a2b47390d8833db2295d844e15ff6c545694
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2020-10-29 18:22:43 -0600

    Componente de filtro de problemas por dificultad (#4866)
    Componente para filtrar problemas por dificultad, para ser usado en la nueva página de colecciones de problemas de calidad.
    
    ![image](https://user-images.githubusercontent.com/37001730/97246981-4eeb3b80-17c4-11eb-9cd1-c470685c7c2e.png)
    
    Fixes: #4586

---
+++ frontend/www/js/omegaup/components/problem/FilterDifficulty.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="card mt-4">
+    <div class="card-body">
+      <h3>{{ T.wordsDifficulty }}</h3>
+      <div
+        v-for="(difficultyName, difficulty) in difficulties"
+        :key="difficulty"
+        class="form-check"
+      >
+        <label class="form-check-label">
+          <input
+            v-model="currentDifficulty"
+            class="form-check-input"
+            type="radio"
+            name="difficulty"
+            :value="difficulty"
+          />{{ difficultyName }}
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Model, Emit, Watch } from 'vue-property-decorator';
+import T from '../../lang';
+
+@Component
+export default class FilterDifficulty extends Vue {
+  @Model('change', { type: String }) readonly selectedDifficulty:
+    | null
+    | string = null;
+
+  T = T;
+  difficulties: { [key: string]: string } = {
+    qualityFormDifficultyEasy: T.qualityFormDifficultyEasy,
+    qualityFormDifficultyMedium: T.qualityFormDifficultyMedium,
+    qualityFormDifficultyHard: T.qualityFormDifficultyHard,
+  };
+
+  currentDifficulty = this.selectedDifficulty;
+
+  @Watch('selectedDifficulty')
+  onSelectedDifficultyChanged(val: string | null) {
+    this.currentDifficulty = val;
+  }
+
+  @Emit('change')
+  @Watch('currentDifficulty')
+  onCurrentDifficultyChanged(val: string | null) {
+    return val;
+  }
+}
+</script>
