164927abce9a7e1b3fe65e57392c22cd98fc4be3
Aaron Castillo Castro <s16120237@alumnos.itsur.edu.mx>
2020-11-17 23:37:29 +0000

    corrección de errores

--- frontend/www/problems/random/language.php
+++ frontend/www/problems/random/language.php
@@ -1,6 +1,6 @@
 <?php
 namespace OmegaUp;
-require_once(dirname(__DIR__, 2) . '/server/bootstrap.php');
+require_once(dirname(__DIR__, 3) . '/server/bootstrap.php');
 $alias = \OmegaUp\DAO\Problems::getRandomLanguageProblemAlias();
 header('HTTP/1.1 303 See Other');
 header("Location: /arena/problem/$alias/");
