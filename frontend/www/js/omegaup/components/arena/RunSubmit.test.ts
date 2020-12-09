30fe368ac15c0b54ce8e6fe39d6ad08d5067e04d
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2020-12-09 15:38:40 -0600

    Haciendo menos flaky la prueba de RunSubmit (#4974)
    Actualmente la prueba de RunSubmit está presentando inconsistencias debido
    a que se calculan los tiempos para habilitar/deshabilitar el botón de _Enviar_. 
    Así que se agregan fechas estáticas para evitar dichas inconsistencias.
    
    Part of: #4957

--- frontend/www/js/omegaup/components/arena/RunSubmit.test.ts
+++ frontend/www/js/omegaup/components/arena/RunSubmit.test.ts
@@ -7,9 +7,11 @@ import * as ui from '../../ui';
 import arena_RunSubmit from './RunSubmit.vue';
 
 describe('RunSubmit.vue', () => {
+  const currentDate = new Date();
+  const now = currentDate.getTime();
+
   it('Should handle disabled button', () => {
-    const now = new Date();
-    const future = now.getTime() + 10 * 1000;
+    const future = now + 10 * 1000;
     const nextSubmission = new Date(future);
     const wrapper = mount(arena_RunSubmit, {
       propsData: {
@@ -34,6 +36,8 @@ describe('RunSubmit.vue', () => {
   });
 
   it('Should handle enable button', () => {
+    const past = now - 1 * 1000;
+    const nextSubmission = new Date(past);
     const wrapper = shallowMount(arena_RunSubmit, {
       propsData: {
         languages: [
@@ -41,7 +45,7 @@ describe('RunSubmit.vue', () => {
           { py3: 'Python 3.6' },
           { java: 'Java' },
         ],
-        nextSubmissionTimestamp: new Date(),
+        nextSubmissionTimestamp: nextSubmission,
         preferredLanguage: 'es',
       },
     });
