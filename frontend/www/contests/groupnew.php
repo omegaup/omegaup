5d31d785e9f497fc86c237ff233c49b683483098
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2020-12-10 10:24:25 -0600

    Migrando a la plantilla unificada la vista de Nuevo grupo (#4971)
    Se migra la vista de `group.new.tpl` a la plantilla unificada.
    
    ![NewGroupBS4](https://user-images.githubusercontent.com/3230352/101543639-1f565400-396a-11eb-95cc-513f92602c1c.gif)
    
    Fixes: #3496

--- frontend/www/contests/groupnew.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace OmegaUp;
-require_once(dirname(__DIR__, 2) . '/server/bootstrap.php');
-\OmegaUp\UITools::getSmartyInstance()->display(
-    \OmegaUp\UITools::templatePath('group.new')
-);
