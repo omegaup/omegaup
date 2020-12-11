0e6f77cc0d58cd268d17871ed369b2831c5b7a92
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2020-12-10 20:06:23 -0600

    Agregando runDetailsData en la prueba de Details.test.ts

--- frontend/www/js/omegaup/components/problem/Details.test.ts
+++ frontend/www/js/omegaup/components/problem/Details.test.ts
@@ -59,6 +59,43 @@ describe('Details.vue', () => {
     title: 'Triangulos',
     visibility: 2,
     input_limit: 1000,
+    guid: '80bbe93bc01c1d47ff9fb396dfaff741',
+    runDetailsData: <types.RunDetails>{
+      admin: false,
+      alias: 'sumas',
+      cases: {},
+      details: {
+        compile_meta: {
+          Main: {
+            memory: 12091392,
+            sys_time: 0.029124,
+            time: 0.174746,
+            verdict: 'OK',
+            wall_time: 0.51659,
+          },
+        },
+        contest_score: 5,
+        groups: [],
+        judged_by: 'localhost',
+        max_score: 100,
+        memory: 10407936,
+        score: 0.05,
+        time: 0.31891,
+        verdict: 'PA',
+        wall_time: 0.699709,
+      },
+      feedback: 'none',
+      groups: [],
+      guid: '80bbe93bc01c1d47ff9fb396dfaff741',
+      judged_by: '',
+      language: 'py3',
+      logs: '',
+      show_diff: 'none',
+      source: 'print(3)',
+      source_link: false,
+      source_name: 'Main.py3',
+      source_url: 'blob:http://localhost:8001/url',
+    },
   };
 
   const user = <types.UserInfoForProblem>{
