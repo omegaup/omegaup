a6d3992aeed71601c6c8e22c73227d198793dab6
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2020-12-15 12:38:24 -0600

    Dejando de usar sinonjs

--- frontend/www/js/omegaup/components/group/Identitiesv2.test.ts
+++ frontend/www/js/omegaup/components/group/Identitiesv2.test.ts
@@ -1,10 +1,11 @@
+jest.mock('./Identitiesv2.vue');
 import { mount, shallowMount } from '@vue/test-utils';
 import expect from 'expect';
-import * as sinon from 'sinon';
 
 import T from '../../lang';
 
 import group_Identitiesv2, { readFile } from './Identitiesv2.vue';
+const readFileMock = readFile as jest.Mock;
 
 describe('Identitiesv2.vue', () => {
   it('Should handle identities view', () => {
@@ -17,38 +18,32 @@ describe('Identitiesv2.vue', () => {
     expect(wrapper.text()).toContain(T.groupsUploadCsvFile);
   });
 
-  it('Should handle an invalid csv file', () => {
+  it('Should handle an invalid csv file', async () => {
     const wrapper = mount(group_Identitiesv2, {
       propsData: {
         groupAlias: 'Hello',
       },
     });
 
-    const invalid_csv_file = { type: 'text/html', name: 'fake.html' };
+    readFileMock.mockResolvedValue({ type: 'text/html', name: 'fake.html' });
 
-    const takeFile = sinon.fake.returns(invalid_csv_file);
-    readFile(takeFile);
-
-    const file_input = wrapper.find('input[type=file]');
-    file_input.trigger('change');
+    const fileInput = wrapper.find('input[type=file]');
+    await fileInput.trigger('change');
 
     expect(wrapper.emitted('invalid-file')).toBeDefined();
   });
 
-  it('Should handle a valid csv file', () => {
+  it('Should handle a valid csv file', async () => {
     const wrapper = mount(group_Identitiesv2, {
       propsData: {
         groupAlias: 'Hello',
       },
     });
 
-    const valid_csv_file = { type: 'text/csv', name: 'users.csv' };
+    readFileMock.mockResolvedValue({ type: 'text/csv', name: 'users.csv' });
 
-    const takeFile = sinon.fake.returns(valid_csv_file);
-    readFile(takeFile);
-
-    const file_input = wrapper.find('input[type=file]');
-    file_input.trigger('change');
+    const fileInput = wrapper.find('input[type=file]');
+    await fileInput.trigger('change');
 
     expect(wrapper.emitted('read-csv')).toBeDefined();
   });
