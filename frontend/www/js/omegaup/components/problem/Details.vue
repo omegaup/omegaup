534bfa7a32ce0407300cb4dacdb4068d34b013ef
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2020-12-16 21:43:35 -0600

    Conectando Arena con componente ProblemDetails (#4533)
    Este es el primer intento para conectar la arena con el componente `ProblemDetails`. 
    
    Fixes: #4493

--- frontend/www/js/omegaup/components/problem/Details.vue
+++ frontend/www/js/omegaup/components/problem/Details.vue
@@ -160,8 +160,8 @@
           @new-submission="onNewSubmission"
         ></omegaup-arena-runs>
         <omegaup-problem-feedback
-          :quality-histogram="histogram.qualityHistogram"
-          :difficulty-histogram="histogram.difficultyHistogram"
+          :quality-histogram="parsedQualityHistogram"
+          :difficulty-histogram="parsedDifficultyHistogram"
           :quality-score="histogram.quality"
           :difficulty-score="histogram.difficulty"
         ></omegaup-problem-feedback>
@@ -193,6 +193,9 @@
           :show-pager="true"
           :show-disqualify="true"
           :problemset-problems="[]"
+          @details="(run) => onShowRunDetails(run.guid)"
+          @rejudge="(run) => $emit('rejudge', run)"
+          @disqualify="(run) => $emit('disqualify', run)"
           @filter-changed="
             (filter, value) => $emit('apply-filter', filter, value)
           "
@@ -363,6 +366,14 @@ export default class ProblemDetails extends Vue {
     );
   }
 
+  get parsedQualityHistogram(): number[] {
+    return JSON.parse(this.histogram?.qualityHistogram ?? '');
+  }
+
+  get parsedDifficultyHistogram(): number[] {
+    return JSON.parse(this.histogram?.difficultyHistogram ?? '');
+  }
+
   onNewSubmission(): void {
     if (!this.user.loggedIn) {
       this.$emit('redirect-login-page');
@@ -377,6 +388,11 @@ export default class ProblemDetails extends Vue {
       return;
     }
     this.popupDisplayed = PopupDisplayed.Promotion;
+  }
+
+  onShowRunDetails(guid: string): void {
+    this.popupDisplayed = PopupDisplayed.RunDetails;
+    this.$emit('details', guid);
   }
 
   onNewPromotionAsReviewer(): void {
