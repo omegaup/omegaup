6a8d38c9dcd910e5073bb5f75cef76cee31f279b
Carlos Córdova <carlosab1802@gmail.com>
2021-01-12 10:38:24 -0500

    Migrando contestGroups a BS4.

--- frontend/www/js/omegaup/components/contest/Editv2.vue
+++ frontend/www/js/omegaup/components/contest/Editv2.vue
@@ -165,6 +165,11 @@
           @emit-accept-request="(username) => $emit('accept-request', username)"
           @emit-deny-request="(username) => $emit('deny-request', username)"
         ></omegaup-common-requests>
+        <omegaup-contest-groups
+          :initial-groups="groups"
+          @emit-add-group="(groupAlias) => $emit('add-group', groupAlias)"
+          @emit-remove-group="(groupAlias) => $emit('remove-group', groupAlias)"
+        ></omegaup-contest-groups>
       </div>
     </div>
   </div>
@@ -181,7 +186,7 @@ import contest_AddContestant from './AddContestant.vue';
 import contest_Admins from '../common/Admins.vue';
 import contest_Clone from './Clone.vue';
 import common_Requests from '../common/Requestsv2.vue';
-import contest_Groups from './Groups.vue';
+import contest_Groups from './Groupsv2.vue';
 import contest_GroupAdmins from '../common/GroupAdmins.vue';
 import contest_Links from './Links.vue';
 import contest_NewForm from './NewForm.vue';
