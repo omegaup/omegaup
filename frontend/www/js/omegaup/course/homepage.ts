3e5e70de0a9d82460a2d1122039629c454488c08
Carlos Córdova <carlosab1802@gmail.com>
2021-01-19 09:19:05 -0500

    Agregando el homepage de cursos.

---
+++ frontend/www/js/omegaup/course/homepage.ts
@@ -0,0 +1,16 @@
+import course_Homepage from '../components/course/Homepage.vue';
+import { OmegaUp } from '../omegaup';
+import Vue from 'vue';
+
+OmegaUp.on('ready', () => {
+  new Vue({
+    el: '#main-container',
+    components: {
+      'omegaup-course-homepage': course_Homepage,
+    },
+    data: () => {},
+    render: (createElement) => {
+      return createElement('omegaup-course-homepage');
+    },
+  });
+});
