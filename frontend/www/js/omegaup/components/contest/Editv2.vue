122cdfcc8dbdde83439389248cc3f7e0b768af49
Carlos Córdova Sáenz <carlosab1802@gmail.com>
2021-01-20 09:54:19 -0500

    Migrando a BS4 la pestaña de administradores. (#5026)
    Otro avance de #3921

--- frontend/www/js/omegaup/components/contest/Editv2.vue
+++ frontend/www/js/omegaup/components/contest/Editv2.vue
@@ -172,6 +172,21 @@
         ></omegaup-contest-groups>
       </div>
     </div>
+    <div v-if="showTab === 'admins'" class="tab-pane active">
+      <omegaup-contest-admins
+        :admins="admins"
+        @add-admin="(username) => $emit('add-admin', username)"
+        @remove-admin="(username) => $emit('remove-admin', username)"
+      ></omegaup-contest-admins>
+      <div class="mt-2"></div>
+      <omegaup-contest-group-admins
+        :group-admins="groupAdmins"
+        @add-group-admin="(groupAlias) => $emit('add-group-admin', groupAlias)"
+        @remove-group-admin="
+          (groupAlias) => $emit('remove-group-admin', groupAlias)
+        "
+      ></omegaup-contest-group-admins>
+    </div>
   </div>
 </template>
 
@@ -183,11 +198,11 @@ import * as ui from '../../ui';
 
 import contest_AddProblem from './AddProblemv2.vue';
 import contest_AddContestant from './AddContestant.vue';
-import contest_Admins from '../common/Admins.vue';
+import contest_Admins from '../common/Adminsv2.vue';
 import contest_Clone from './Clone.vue';
 import common_Requests from '../common/Requestsv2.vue';
+import contest_GroupAdmins from '../common/GroupAdminsv2.vue';
 import contest_Groups from './Groupsv2.vue';
-import contest_GroupAdmins from '../common/GroupAdmins.vue';
 import contest_Links from './Links.vue';
 import contest_NewForm from './NewForm.vue';
 import common_Publish from '../common/Publishv2.vue';
