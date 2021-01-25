87820677d3280ccfd2a66467f141c52fff113ad4
Carlos Córdova Sáenz <carlosab1802@gmail.com>
2021-01-24 21:41:29 -0500

    Agregando el homepage de cursos (#5031)
    Fixes: #5013 
    
    ![coursesHomepage](https://user-images.githubusercontent.com/20785082/105046747-79e9d200-5a37-11eb-9613-2d56af1b6846.gif)

---
+++ frontend/www/js/omegaup/components/course/Homepage.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="row p-4 text-center justify-content-around align-items-center">
+      <h2 class="col-12 mb-5 font-italic">{{ T.courseHomepageTitle }}</h2>
+      <a
+        class="col-xs-6 col-sm-4 btn btn-primary btn-lg mx-auto custom-button"
+        role="button"
+        href="/course/"
+        >{{ T.wordsGetToKnowOurCourses }}</a
+      >
+    </div>
+    <div class="dropdown-divider"></div>
+    <div class="container-lg">
+      <div
+        class="row p-4 text-center text-md-left justify-content-around align-items-center"
+      >
+        <div class="p-3 col-md-5 mt-2 mt-md-0">
+          <img
+            class="img-fluid"
+            src="/media/course_homepage/teachers_section.svg"
+          />
+        </div>
+        <div class="col-md-6 mt-2 mt-md-0">
+          <omegaup-markdown
+            :markdown="T.courseHomepageTeachersSectionContent"
+          ></omegaup-markdown>
+        </div>
+      </div>
+      <div
+        class="row p-4 text-center text-md-left justify-content-around align-items-center"
+      >
+        <div class="p-3 col-md-5 order-md-2 mt-2 mt-md-0">
+          <img
+            class="img-fluid"
+            src="/media/course_homepage/students_section.svg"
+          />
+        </div>
+        <div class="col-md-6 mt-2 mt-md-0">
+          <omegaup-markdown
+            :markdown="T.courseHomepageStudentsSectionContent"
+          ></omegaup-markdown>
+        </div>
+      </div>
+      <div class="row mt-2">
+        <a
+          class="col-xs-6 col-sm-4 btn btn-primary btn-lg mx-auto custom-button"
+          role="button"
+          href="/course/"
+          >{{ T.wordsGetToKnowOurCourses }}</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import T from '../../lang';
+import omegaup_Markdown from '../Markdown.vue';
+
+@Component({
+  components: {
+    'omegaup-markdown': omegaup_Markdown,
+  },
+})
+export default class Homepage extends Vue {
+  T = T;
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../../sass/main.scss';
+
+a.custom-button {
+  color: $omegaup-white;
+}
+</style>
