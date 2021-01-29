58f2219c10ad06440207bdfe642ac630c4553a7a
RodCross <rodrigocl1312@gmail.com>
2021-01-29 01:41:08 -0500

    Agregando badge para graduado de Curso de Intro a Algoritmos II

---
+++ frontend/tests/badges/Badge_introToAlgorithms2CourseGraduateTest.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Simple test for introToAlgorithms2CourseGraduate user Badge
+ *
+ * @author RodCross
+ */
+// phpcs:ignore Squiz.Classes.ValidClassName.NotCamelCaps
+class Badge_introToAlgorithms2CourseGraduateTest extends \OmegaUp\Test\BadgesTestCase {
+    protected $courseData;
+    protected $problems;
+    protected $students;
+
+    public function setUp(): void {
+        parent::setUp();
+        $courseAlias = 'introduccion_a_algoritmos_ii';
+
+        // Create four problems
+        $this->problems = [];
+        for ($i = 0; $i < 4; $i++) {
+            $this->problems[] = \OmegaUp\Test\Factories\Problem::createProblem();
+        }
+
+        // Create course
+        $this->courseData = \OmegaUp\Test\Factories\Course::createCourseWithOneAssignment(
+            null,
+            null,
+            \OmegaUp\Controllers\Course::ADMISSION_MODE_PRIVATE,
+            'no',
+            'false',
+            0,
+            120,
+            120,
+            $courseAlias
+        );
+        $assignmentAlias = $this->courseData['assignment_alias'];
+
+        // Login
+        $login = self::login($this->courseData['admin']);
+
+        // Add the problems to the assignment
+        \OmegaUp\Test\Factories\Course::addProblemsToAssignment(
+            $login,
+            $courseAlias,
+            $assignmentAlias,
+            $this->problems
+        );
+
+        // Create students
+        $this->students = [];
+        $this->students[0] = \OmegaUp\Test\Factories\User::createUser();
+        $this->students[1] = \OmegaUp\Test\Factories\User::createUser();
+
+        // Add students to course
+        \OmegaUp\Test\Factories\Course::addStudentToCourse(
+            $this->courseData,
+            $this->students[0]['identity']
+        );
+        \OmegaUp\Test\Factories\Course::addStudentToCourse(
+            $this->courseData,
+            $this->students[1]['identity']
+        );
+    }
+
+    public function testIntroToAlgorithms2CourseEarnBadge() {
+        // The student solves 3 out of 4 problems
+        for ($i = 0; $i < 3; $i++) {
+            $runData = \OmegaUp\Test\Factories\Run::createCourseAssignmentRun(
+                $this->problems[$i],
+                $this->courseData,
+                $this->students[0]['identity']
+            );
+            \OmegaUp\Test\Factories\Run::gradeRun($runData);
+        }
+
+        $queryPath = static::OMEGAUP_BADGES_ROOT . '/introToAlgorithms2CourseGraduate/' . static::QUERY_FILE;
+        $results = self::getSortedResults(file_get_contents($queryPath));
+        $expected = [$this->students[0]['user']->user_id];
+        $this->assertEquals($expected, $results);
+    }
+
+    public function testIntroToAlgorithms2CourseDoNotEarnBadge() {
+        // The student solves the same problem with multiple submissions
+        for ($i = 0; $i < 10; $i++) {
+            $runData = \OmegaUp\Test\Factories\Run::createCourseAssignmentRun(
+                $this->problems[0],
+                $this->courseData,
+                $this->students[1]['identity']
+            );
+            \OmegaUp\Test\Factories\Run::gradeRun($runData);
+        }
+
+        $queryPath = static::OMEGAUP_BADGES_ROOT . '/introToAlgorithms2CourseGraduate/' . static::QUERY_FILE;
+        $results = self::getSortedResults(file_get_contents($queryPath));
+        $expected = [];
+        $this->assertEquals($expected, $results);
+    }
+}
