25b31e945791abf5cc5dbb274e72dce8d2f0134c
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-01-29 03:51:03 -0600

    Agregando todos los lenguajes de programación en registros de Cursos

--- frontend/database/167_languages_course.sql
+++ frontend/database/167_languages_course.sql
@@ -1,3 +1,16 @@
 -- Courses
 ALTER TABLE `Courses`
   ADD COLUMN `languages` set('c','c11-gcc','c11-clang','cpp','cpp11','cpp11-gcc','cpp11-clang','cpp17-gcc','cpp17-clang','java','py','py2','py3','rb','pl','cs','pas','kp','kj','cat','hs','lua') DEFAULT NULL COMMENT 'Un filtro (opcional) de qué lenguajes se pueden usar en un curso';
+
+-- Update column languages in all the courses and assignments' problemsets
+UPDATE
+  `Courses`
+SET
+  `languages` = 'c,c11-gcc,c11-clang,cpp,cpp11,cpp11-gcc,cpp11-clang,cpp17-gcc,cpp17-clang,java,py,py2,py3,rb,pl,cs,pas,kp,kj,cat,hs,lua';
+
+UPDATE
+  `Problemsets`
+SET
+  `languages` = 'c,c11-gcc,c11-clang,cpp,cpp11,cpp11-gcc,cpp11-clang,cpp17-gcc,cpp17-clang,java,py,py2,py3,rb,pl,cs,pas,kp,kj,cat,hs,lua'
+WHERE
+  `type` = 'Assignment';
