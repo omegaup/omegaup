0580faf735b0bfef82f5084695c591201df3fc53
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2021-01-31 19:49:12 -0600

    Permitiendo que los banners de éxito al clonar cursos permanezcan visibles siempre (#5075)
    Se permite que el banner de éxito que aparece cuando se clona un curso 
    permanezca visible hasta que el usuario decida cerrarlo.  Se aplica la bandera 
    a los diferentes lugares donde se clona un curso:
    - Editar curso
    - Detalles del curso público
    - Clonar curso con token.
    
    ![CloneCourseButtonStaysVisible](https://user-images.githubusercontent.com/3230352/106342963-b18f2000-6268-11eb-988b-0314cd3ff95c.gif)
    
    Fixes: #5066

--- frontend/www/js/omegaup/course/details.ts
+++ frontend/www/js/omegaup/course/details.ts
@@ -34,6 +34,7 @@ OmegaUp.on('ready', () => {
                   ui.formatString(T.courseEditCourseClonedSuccessfully, {
                     course_alias: alias,
                   }),
+                  /*autoHide=*/ false,
                 );
               })
               .catch(ui.apiError);
