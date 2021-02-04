3d7654bc37f0dbf73ce6b33efc6eecccace858ac
Carlos Córdova <carlosab1802@gmail.com>
2021-02-04 06:33:57 -0500

    Dejando de usar una librería extra.

--- frontend/www/js/omegaup/components/course/CardsList.test.ts
+++ frontend/www/js/omegaup/components/course/CardsList.test.ts
@@ -2,9 +2,6 @@ import { mount } from '@vue/test-utils';
 import expect from 'expect';
 import type { types } from '../../api_types';
 
-// https://github.com/vuejs/vue-test-utils/issues/1532#issuecomment-633444080
-import '@testing-library/jest-dom';
-
 import course_CardsList from './CardsList.vue';
 
 const publicCoursesTitles = ['Curso público actual', 'Curso público pasado'];
@@ -122,9 +119,11 @@ describe('CardsList.vue', () => {
     expect(wrapper.text()).toContain(publicCoursesTitles[0]);
     expect(wrapper.text()).toContain(publicCoursesTitles[1]);
 
-    expect(wrapper.find('div.dropdown-divider').element).not.toBeVisible();
+    expect(wrapper.find('div.dropdown-divider').element.style.display).toBe(
+      'none',
+    );
     await wrapper.find('input[data-see-all]').trigger('click');
-    expect(wrapper.find('div.dropdown-divider').element).toBeVisible();
+    expect(wrapper.find('div.dropdown-divider').element.style.display).toBe('');
   });
 
   it('Should list student courses', async () => {
@@ -138,8 +137,10 @@ describe('CardsList.vue', () => {
     expect(wrapper.text()).toContain(studentCoursesTitles[0]);
     expect(wrapper.text()).toContain(studentCoursesTitles[1]);
 
-    expect(wrapper.find('div.dropdown-divider').element).not.toBeVisible();
+    expect(wrapper.find('div.dropdown-divider').element.style.display).toBe(
+      'none',
+    );
     await wrapper.find('input[data-see-all]').trigger('click');
-    expect(wrapper.find('div.dropdown-divider').element).toBeVisible();
+    expect(wrapper.find('div.dropdown-divider').element.style.display).toBe('');
   });
 });
