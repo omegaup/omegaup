599604311574b4fdcfddac7e4b4248e0fb38e083
Rodrigo Alonso <52440153+RodCross@users.noreply.github.com>
2021-02-06 15:47:24 -0500

    Agregando badge para graduado de Curso de Intro a Algoritmos II (#5065)
    Fixes: #4436

---
+++ frontend/tests/badges/Badge_introToAlgorithms2CourseGraduateTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Simple test for introToAlgorithms2CourseGraduate Badge
+ *
+ * @author RodCross
+ */
+// phpcs:ignore Squiz.Classes.ValidClassName.NotCamelCaps
+class Badge_introToAlgorithms2CourseGraduateTest extends \OmegaUp\Test\BadgesTestCase {
+    public function testIntroToAlgorithms2CourseGraduate() {
+        parent::courseGraduateTest(
+            'introduccion_a_algoritmos_ii',
+            'c11-gcc',
+            'introToAlgorithms2CourseGraduate'
+        );
+    }
+}
