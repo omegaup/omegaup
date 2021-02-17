7700ed69c3243582558adbd9a3d66f02d5d7f709
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-02-16 23:37:25 -0600

    Agregando lazy al v-model de las imagenes para que alcance a cargar correctamente

--- frontend/www/js/omegaup/components/problem/StatementEdit.vue
+++ frontend/www/js/omegaup/components/problem/StatementEdit.vue
@@ -28,13 +28,12 @@
           <div ref="markdownButtonBar" class="wmd-button-bar"></div>
           <textarea
             ref="markdownInput"
-            v-model="currentMarkdown"
+            v-model.lazy="currentMarkdown"
             class="wmd-input"
           ></textarea>
         </div>
         <div class="col-md-6">
           <h1 class="title text-center">{{ title }}</h1>
-          <div ref="markdownPreview" data-markdown-statement></div>
           <omegaup-markdown
             :markdown="currentMarkdown"
             :image-mapping="statement.images"
@@ -122,8 +121,7 @@ const markdownConverter = new markdown.Converter({
 })
 export default class ProblemStatementEdit extends Vue {
   @Ref() readonly markdownButtonBar!: HTMLDivElement;
-  @Ref() readonly markdownInput!: HTMLDivElement;
-  @Ref() readonly markdownPreview!: HTMLDivElement;
+  @Ref() readonly markdownInput!: HTMLTextAreaElement;
   @Prop() alias!: string;
   @Prop() title!: string;
   @Prop() source!: string;
@@ -145,7 +143,7 @@ export default class ProblemStatementEdit extends Vue {
     this.markdownEditor = new Markdown.Editor(markdownConverter.converter, '', {
       panels: {
         buttonBar: this.markdownButtonBar,
-        preview: this.markdownPreview,
+        preview: null,
         input: this.markdownInput,
       },
     });
@@ -208,168 +206,9 @@ export default class ProblemStatementEdit extends Vue {
 
 <style lang="scss">
 @import '../../../../third_party/js/pagedown/demo/browser/demo.css';
-@import '../../../../../../node_modules/prismjs/themes/prism.css';
-@import '../../../../sass/main.scss';
 
 .wmd-preview,
 .wmd-button-bar {
   background-color: #fff;
-}
-
-[data-markdown-statement] {
-  display: block;
-  max-width: 50em;
-  margin: 0 auto;
-
-  h1 {
-    font-size: 1.3em;
-    margin: 1em 0 0.5em 0;
-    font-weight: bold;
-  }
-  h2 {
-    font-size: 1.1em;
-    margin: 1em 0 0.5em 0;
-    font-weight: bold;
-  }
-  h3 {
-    font-size: 1em;
-    margin: 1em 0 0.5em 0;
-    font-weight: bold;
-  }
-
-  p,
-  li {
-    hyphens: auto;
-    line-height: 150%;
-    text-align: justify;
-    orphans: 2;
-    widows: 2;
-    page-break-inside: avoid;
-  }
-  p {
-    margin-bottom: 1em;
-  }
-
-  ul {
-    list-style: disc;
-  }
-  ol {
-    list-style: decimal;
-  }
-  ul li,
-  ol li {
-    margin-left: 0.25em;
-  }
-
-  pre {
-    padding: 16px;
-    background: #eee;
-    margin: 1em 0;
-    border-radius: 6px;
-    display: block;
-    line-height: 125%;
-  }
-  & > pre > button {
-    margin-right: -16px;
-    margin-top: -16px;
-    padding: 6px;
-    font-size: 90%;
-  }
-  td > button.clipboard {
-    float: right;
-    border-color: rgb(218, 224, 229);
-    margin-left: 0.5em;
-    margin-right: -6px;
-    margin-top: -6px;
-    padding: 3px;
-    font-size: 90%;
-  }
-
-  figure {
-    text-align: center;
-    page-break-inside: avoid;
-  }
-
-  details {
-    padding: 16px;
-    border: 1px solid #eee;
-    summary {
-      color: $omegaup-blue;
-    }
-    &[open] > summary {
-      margin-bottom: 24px;
-    }
-  }
-
-  table td {
-    border: 1px solid #000;
-    padding: 10px;
-  }
-  table th {
-    text-align: center;
-  }
-  table.sample_io {
-    margin: 5px;
-    padding: 5px;
-
-    tbody {
-      background: #eee;
-      border: 1px solid #000;
-
-      tr:nth-child(even) {
-        background: #f5f5f5;
-      }
-    }
-    th {
-      padding: 10px;
-      border-top: 0;
-    }
-    td {
-      vertical-align: top;
-      padding: 10px;
-      border: 1px solid #000;
-    }
-    pre {
-      white-space: pre;
-      word-break: keep-all;
-      word-wrap: normal;
-      background: transparent;
-      border: 0px;
-      padding: 0px;
-      margin: inherit;
-      & > button {
-        margin-left: 2em;
-        padding: 3px;
-        font-size: 80%;
-      }
-    }
-  }
-
-  iframe {
-    width: 100%;
-    height: 400px;
-  }
-
-  a.libinteractive-help {
-    display: inline-block;
-    float: right;
-  }
-  code.libinteractive-download,
-  code.output-only-download {
-    background: #eee;
-    color: #ccc;
-    margin: 1em 0;
-    border: 1px dotted #ccc;
-    display: block;
-    text-align: center;
-    font-size: 2em;
-    line-height: 3em;
-    min-height: 3em;
-  }
-
-  img {
-    max-width: 100%;
-    page-break-inside: avoid;
-  }
 }
 </style>
