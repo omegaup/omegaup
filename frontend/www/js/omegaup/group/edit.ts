3ee18af615641bbb25f039adfbb8f01d225f6989
Carlos Córdova <carlosab1802@gmail.com>
2021-02-18 08:08:41 -0500

    Agregando prueba unitaria.

--- frontend/www/js/omegaup/group/edit.ts
+++ frontend/www/js/omegaup/group/edit.ts
@@ -298,92 +298,92 @@ OmegaUp.on('ready', () => {
       }),
     );
   }
+});
 
-  function generatePassword(humanReadable: boolean): string {
-    if (humanReadable) {
-      const words = {
-        es: [
-          'Loro',
-          'Perro',
-          'Pollo',
-          'Lagarto',
-          'Gato',
-          'Toro',
-          'Vaca',
-          'Sapo',
-          'Oso',
-          'Zorro',
-        ],
-        en: [
-          'Parrot',
-          'Dog',
-          'Chicken',
-          'Lizard',
-          'Cat',
-          'Bull',
-          'Cow',
-          'Frog',
-          'Bear',
-          'Fox',
-        ],
-        pt: [
-          'Papagaio',
-          'Cão',
-          'Frango',
-          'Lagarto',
-          'Gato',
-          'Touro',
-          'Vaca',
-          'Sapo',
-          'Suportar',
-          'Raposa',
-        ],
-      };
-      const wordsNumber = 12;
-      const totalNumbers = 6;
+export function generatePassword(humanReadable: boolean): string {
+  if (humanReadable) {
+    const words = {
+      es: [
+        'Loro',
+        'Perro',
+        'Pollo',
+        'Lagarto',
+        'Gato',
+        'Toro',
+        'Vaca',
+        'Sapo',
+        'Oso',
+        'Zorro',
+      ],
+      en: [
+        'Parrot',
+        'Dog',
+        'Chicken',
+        'Lizard',
+        'Cat',
+        'Bull',
+        'Cow',
+        'Frog',
+        'Bear',
+        'Fox',
+      ],
+      pt: [
+        'Papagaio',
+        'Cão',
+        'Frango',
+        'Lagarto',
+        'Gato',
+        'Touro',
+        'Vaca',
+        'Sapo',
+        'Suportar',
+        'Raposa',
+      ],
+    };
+    const wordsNumber = 12;
+    const totalNumbers = 6;
 
-      let langWords: string[] = [];
-      switch (T.locale) {
-        case 'es':
-          langWords = words.es;
-          break;
-        case 'pt':
-          langWords = words.pt;
-          break;
-        default:
-          langWords = words.en;
-      }
-      let password = '';
-      for (let i = 0; i < wordsNumber; i++) {
-        password += langWords[Math.floor(Math.random() * langWords.length)];
-      }
-      for (let i = 0; i < totalNumbers; i++) {
-        password += Math.floor(Math.random() * 10); // 3 random numbers
-      }
-      return password;
+    let langWords: string[] = [];
+    switch (T.locale) {
+      case 'es':
+        langWords = words.es;
+        break;
+      case 'pt':
+        langWords = words.pt;
+        break;
+      default:
+        langWords = words.en;
     }
-
-    const validChars = 'acdefhjkmnpqruvwxyACDEFHJKLMNPQRUVWXY346';
-    const len = 8;
-    // Browser supports window.crypto
-    if (typeof window.crypto == 'object') {
-      const arr = new Uint8Array(2 * len);
-      window.crypto.getRandomValues(arr);
-      return Array.from(
-        arr.filter((value) => value <= 255 - (255 % validChars.length)),
-        (value) => validChars[value % validChars.length],
-      )
-        .join('')
-        .substr(0, len);
-    }
-
-    // Browser does not support window.crypto
     let password = '';
-    for (let i = 0; i < len; i++) {
-      password += validChars.charAt(
-        Math.floor(Math.random() * validChars.length),
-      );
+    for (let i = 0; i < wordsNumber; i++) {
+      password += langWords[Math.floor(Math.random() * langWords.length)];
+    }
+    for (let i = 0; i < totalNumbers; i++) {
+      password += Math.floor(Math.random() * 10); // 3 random numbers
     }
     return password;
   }
-});
+
+  const validChars = 'acdefhjkmnpqruvwxyACDEFHJKLMNPQRUVWXY346';
+  const len = 8;
+  // Browser supports window.crypto
+  if (typeof window.crypto == 'object') {
+    const arr = new Uint8Array(2 * len);
+    window.crypto.getRandomValues(arr);
+    return Array.from(
+      arr.filter((value) => value <= 255 - (255 % validChars.length)),
+      (value) => validChars[value % validChars.length],
+    )
+      .join('')
+      .substr(0, len);
+  }
+
+  // Browser does not support window.crypto
+  let password = '';
+  for (let i = 0; i < len; i++) {
+    password += validChars.charAt(
+      Math.floor(Math.random() * validChars.length),
+    );
+  }
+  return password;
+}
