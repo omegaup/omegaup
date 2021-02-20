bbd2868bc068a0ae8b4cdba11a3cbbe5587216fb
Carlos Córdova Sáenz <carlosab1802@gmail.com>
2021-02-20 09:34:42 -0500

    Agregando human-readable passwords a las identidades en concursos (#5143)
    Fixes: #4744

--- frontend/www/js/omegaup/group/edit.ts
+++ frontend/www/js/omegaup/group/edit.ts
@@ -238,9 +238,11 @@ OmegaUp.on('ready', () => {
           'read-csv': ({
             identities,
             file,
+            humanReadable,
           }: {
             identities: types.Identity[];
             file: File;
+            humanReadable: boolean;
           }) => {
             CSV.fetch({ file })
               .done((dataset: CSV.Dataset) => {
@@ -259,7 +261,9 @@ OmegaUp.on('ready', () => {
                   identities.push({
                     username: `${payload.groupAlias}:${username}`,
                     name,
-                    password: generatePassword(),
+                    password: humanReadable
+                      ? generateHumanReadablePassword()
+                      : generatePassword(),
                     country_id,
                     state_id,
                     gender,
@@ -296,29 +300,92 @@ OmegaUp.on('ready', () => {
       }),
     );
   }
-
-  function generatePassword(): string {
-    const validChars = 'acdefhjkmnpqruvwxyACDEFHJKLMNPQRUVWXY346';
-    const len = 8;
-    // Browser supports window.crypto
-    if (typeof window.crypto == 'object') {
-      const arr = new Uint8Array(2 * len);
-      window.crypto.getRandomValues(arr);
-      return Array.from(
-        arr.filter((value) => value <= 255 - (255 % validChars.length)),
-        (value) => validChars[value % validChars.length],
-      )
-        .join('')
-        .substr(0, len);
-    }
-
-    // Browser does not support window.crypto
-    let password = '';
-    for (let i = 0; i < len; i++) {
-      password += validChars.charAt(
-        Math.floor(Math.random() * validChars.length),
-      );
-    }
-    return password;
-  }
 });
+
+export function generatePassword(): string {
+  const validChars = 'acdefhjkmnpqruvwxyACDEFHJKLMNPQRUVWXY346';
+  const len = 8;
+  // Browser supports window.crypto
+  if (typeof window.crypto == 'object') {
+    const arr = new Uint8Array(2 * len);
+    window.crypto.getRandomValues(arr);
+    return Array.from(
+      arr.filter((value) => value <= 255 - (255 % validChars.length)),
+      (value) => validChars[value % validChars.length],
+    )
+      .join('')
+      .substr(0, len);
+  }
+
+  // Browser does not support window.crypto
+  let password = '';
+  for (let i = 0; i < len; i++) {
+    password += validChars.charAt(
+      Math.floor(Math.random() * validChars.length),
+    );
+  }
+  return password;
+}
+
+export function generateHumanReadablePassword() {
+  const words = {
+    es: [
+      'Loro',
+      'Perro',
+      'Pollo',
+      'Lagarto',
+      'Gato',
+      'Toro',
+      'Vaca',
+      'Sapo',
+      'Oso',
+      'Zorro',
+    ],
+    en: [
+      'Parrot',
+      'Dog',
+      'Chicken',
+      'Lizard',
+      'Cat',
+      'Bull',
+      'Cow',
+      'Frog',
+      'Bear',
+      'Fox',
+    ],
+    pt: [
+      'Papagaio',
+      'Cachorro',
+      'Frango',
+      'Lagarto',
+      'Gato',
+      'Touro',
+      'Vaca',
+      'Sapo',
+      'Urso',
+      'Raposa',
+    ],
+  };
+  const wordsNumber = 12;
+  const totalNumbers = 6;
+
+  let langWords: string[] = [];
+  switch (T.locale) {
+    case 'es':
+      langWords = words.es;
+      break;
+    case 'pt':
+      langWords = words.pt;
+      break;
+    default:
+      langWords = words.en;
+  }
+  let password = '';
+  for (let i = 0; i < wordsNumber; i++) {
+    password += langWords[Math.floor(Math.random() * langWords.length)];
+  }
+  for (let i = 0; i < totalNumbers; i++) {
+    password += Math.floor(Math.random() * 10); // random numbers
+  }
+  return password;
+}
