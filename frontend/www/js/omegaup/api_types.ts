016849371660a305c2f942a8f889fa26017e666e
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2021-02-22 21:42:52 -0600

    Aplicando algunos refactors a APIs en controlador de User (#5144)
    Para poder arreglar el bug de que no se muestra correctamente el perfil de un
    usuario cuando este lo marcó como privado hay que hacer un pequeño refactor
    de funciones para que no se manden llamar directamente las APIs.
    
    Part of: #5122

--- frontend/www/js/omegaup/api_types.ts
+++ frontend/www/js/omegaup/api_types.ts
@@ -1259,45 +1259,49 @@ export namespace types {
       elementId: string = 'payload',
     ): types.UserProfileDetailsPayload {
       return ((x) => {
-        x.contests = ((x) => {
-          if (x instanceof Object) {
-            Object.keys(x).forEach(
-              (y) =>
-                (x[y] = ((x) => {
-                  x.data = ((x) => {
-                    x.finish_time = ((x: number) => new Date(x * 1000))(
-                      x.finish_time,
-                    );
-                    x.last_updated = ((x: number) => new Date(x * 1000))(
-                      x.last_updated,
-                    );
-                    x.start_time = ((x: number) => new Date(x * 1000))(
-                      x.start_time,
-                    );
-                    return x;
-                  })(x.data);
-                  return x;
-                })(x[y])),
-            );
-          }
-          return x;
-        })(x.contests);
-        x.ownedBadges = ((x) => {
-          if (!Array.isArray(x)) {
+        if (x.extraProfileDetails)
+          x.extraProfileDetails = ((x) => {
+            x.contests = ((x) => {
+              if (x instanceof Object) {
+                Object.keys(x).forEach(
+                  (y) =>
+                    (x[y] = ((x) => {
+                      x.data = ((x) => {
+                        x.finish_time = ((x: number) => new Date(x * 1000))(
+                          x.finish_time,
+                        );
+                        x.last_updated = ((x: number) => new Date(x * 1000))(
+                          x.last_updated,
+                        );
+                        x.start_time = ((x: number) => new Date(x * 1000))(
+                          x.start_time,
+                        );
+                        return x;
+                      })(x.data);
+                      return x;
+                    })(x[y])),
+                );
+              }
+              return x;
+            })(x.contests);
+            x.ownedBadges = ((x) => {
+              if (!Array.isArray(x)) {
+                return x;
+              }
+              return x.map((x) => {
+                if (x.assignation_time)
+                  x.assignation_time = ((x: number) => new Date(x * 1000))(
+                    x.assignation_time,
+                  );
+                if (x.first_assignation)
+                  x.first_assignation = ((x: number) => new Date(x * 1000))(
+                    x.first_assignation,
+                  );
+                return x;
+              });
+            })(x.ownedBadges);
             return x;
-          }
-          return x.map((x) => {
-            if (x.assignation_time)
-              x.assignation_time = ((x: number) => new Date(x * 1000))(
-                x.assignation_time,
-              );
-            if (x.first_assignation)
-              x.first_assignation = ((x: number) => new Date(x * 1000))(
-                x.first_assignation,
-              );
-            return x;
-          });
-        })(x.ownedBadges);
+          })(x.extraProfileDetails);
         x.profile = ((x) => {
           if (x.birth_date)
             x.birth_date = ((x: number) => new Date(x * 1000))(x.birth_date);
@@ -2016,6 +2020,16 @@ export namespace types {
     courseName?: string;
     name: string;
     problem?: string;
+  }
+
+  export interface ExtraProfileDetails {
+    badges: string[];
+    contests: types.UserProfileContests;
+    createdProblems: types.Problem[];
+    ownedBadges: types.Badge[];
+    solvedProblems: types.Problem[];
+    stats: types.UserProfileStats[];
+    unsolvedProblems: types.Problem[];
   }
 
   export interface FilteredCourse {
@@ -3002,16 +3016,9 @@ export namespace types {
   }
 
   export interface UserProfileDetailsPayload {
-    badges: string[];
-    contests: types.UserProfileContests;
-    createdProblems: types.Problem[];
-    ownedBadges: types.Badge[];
+    extraProfileDetails?: types.ExtraProfileDetails;
     profile: types.UserProfileInfo;
-    programmingLanguages: { [key: string]: string };
-    solvedProblems: types.Problem[];
-    stats: types.UserProfileStats[];
     statusError?: string;
-    unsolvedProblems: types.Problem[];
   }
 
   export interface UserProfileInfo {
@@ -4006,9 +4013,7 @@ export namespace messages {
   export type UserSelectCoderOfTheMonthRequest = { [key: string]: any };
   export type UserSelectCoderOfTheMonthResponse = {};
   export type UserStatsRequest = { [key: string]: any };
-  export type UserStatsResponse = {
-    runs: { date?: string; runs: number; verdict: string }[];
-  };
+  export type UserStatsResponse = { runs: types.UserProfileStats[] };
   export type UserStatusVerifiedRequest = { [key: string]: any };
   export type UserStatusVerifiedResponse = {
     username: string;
