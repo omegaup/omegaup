ebcd9aed65f90e8ccd4666d07ad58660882ecd1a
Carlos Córdova <carlosab1802@gmail.com>
2021-03-01 08:58:32 -0500

    Avance parcial.

--- frontend/www/js/omegaup/arena/course.ts
+++ frontend/www/js/omegaup/arena/course.ts
@@ -1,5 +1,33 @@
 import { OmegaUp } from '../omegaup';
+import { types } from '../api_types';
+import * as api from '../api';
+import * as time from '../time';
+
+import Vue from 'vue';
+import arena_Course from '../components/arena/Course.vue';
 
 OmegaUp.on('ready', () => {
-  // TODO: Build the component
+  time.setSugarLocale();
+  const payload = types.payloadParsers.AssignmentDetailsPayload();
+  const activeTab = window.location.hash
+    ? window.location.hash.substr(1).split('/')[0]
+    : 'problems';
+  console.log(payload);
+  console.log(activeTab);
+
+  const arenaCourse = new Vue({
+    el: '#main-container',
+    components: {
+      'omegaup-arena-course': arena_Course,
+    },
+    data: () => ({
+      problemInfo: null as types.ProblemInfo | null,
+      problem: null as ActiveProblem | null,
+      problems: payload.problems as types.NavbarProblemsetProblem[],
+      clarifications: payload.clarifications,
+      popupDisplayed: PopupDisplayed.None,
+      showNewClarificationPopup: false,
+      guid: null as null | string,
+    }),
+  });
 });
