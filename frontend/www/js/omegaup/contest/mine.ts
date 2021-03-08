e3ed1adeb16b28be82ec9f1af10fa1854ab151a6
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2021-03-07 21:22:30 -0600

    Mostrando el listado de concursos archivados (#5209)
    Último cambio referente a poder archivar concursos. En este se agrega el listado
    de concursos en la vista de "Mis concursos".
    
    ![ArchivedContestsList](https://user-images.githubusercontent.com/3230352/110255836-651bbc00-7f5b-11eb-8c24-26a30a2d10d6.gif)
    
    Fixes: #5164

--- frontend/www/js/omegaup/contest/mine.ts
+++ frontend/www/js/omegaup/contest/mine.ts
@@ -10,6 +10,7 @@ import contest_Mine from '../components/contest/Mine.vue';
 OmegaUp.on('ready', () => {
   const payload = types.payloadParsers.ContestListMinePayload();
   let showAllContests = false;
+  let showArchivedContests = false;
   const contestMine = new Vue({
     el: '#main-container',
     components: {
@@ -25,9 +26,15 @@ OmegaUp.on('ready', () => {
           privateContestsAlert: payload.privateContestsAlert,
         },
         on: {
+          'change-show-archived-contests': (
+            shouldShowArchivedContests: boolean,
+          ) => {
+            showArchivedContests = shouldShowArchivedContests;
+            fillContestsTable({ showAllContests, showArchivedContests });
+          },
           'change-show-all-contests': (shouldShowAll: boolean) => {
             showAllContests = shouldShowAll;
-            fillContestsTable(shouldShowAll);
+            fillContestsTable({ showAllContests, showArchivedContests });
           },
           'change-admission-mode': (
             selectedContests: string[],
@@ -48,7 +55,7 @@ OmegaUp.on('ready', () => {
                 ui.error(ui.formatString(T.bulkOperationError, error));
               })
               .finally(() => {
-                fillContestsTable(showAllContests);
+                fillContestsTable({ showAllContests, showArchivedContests });
               });
           },
           'download-csv-users': (contestAlias: string) => {
@@ -100,8 +107,15 @@ OmegaUp.on('ready', () => {
     },
   });
 
-  function fillContestsTable(showAllContests: boolean): void {
-    (showAllContests ? api.Contest.adminList() : api.Contest.myList())
+  function fillContestsTable({
+    showAllContests,
+    showArchivedContests,
+  }: {
+    showAllContests: boolean;
+    showArchivedContests: boolean;
+  }): void {
+    const param = { show_archived: showArchivedContests };
+    (showAllContests ? api.Contest.adminList(param) : api.Contest.myList(param))
       .then((result) => {
         contestMine.contests = result.contests;
       })
