8231a49ed4ebdf381fd6f2143652a1875b32e571
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2021-03-14 20:03:31 -0600

    Agregando componente MultiTypeahead (#5226)
    Se separa el nuevo componente de `Typeahead` en `MultiTypeahead` para poder
    diferenciar los casos en los que sólo se puede elegir una opción a la que te 
    permita elegir una o mas opciones.
    
    Part of: #5220

---
+++ frontend/www/js/omegaup/components/common/MultiTypeahead.vue
@@ -0,0 +1,56 @@
+<template>
+  <tags-input
+    v-model="selectedOptions"
+    :existing-tags="existingOptions"
+    :typeahead="true"
+    :typeahead-style="'dropdown'"
+    :typeahead-max-results="maxResults"
+    :typeahead-activation-threshold="activationThreshold"
+    :placeholder="T.typeaheadSearchPlaceholder"
+    :limit="0"
+    :hide-input-on-limit="true"
+    :only-existing-tags="true"
+    :typeahead-hide-discard="true"
+    @change="updateExistingOptions"
+    @tag-added="$emit('update:value', selectedOptions)"
+    @tag-removed="$emit('update:value', selectedOptions)"
+  >
+  </tags-input>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import VoerroTagsInput from '@voerro/vue-tagsinput';
+import '@voerro/vue-tagsinput/dist/style.css';
+import T from '../../lang';
+import { types } from '../../api_types';
+
+@Component({
+  components: {
+    'tags-input': VoerroTagsInput,
+  },
+})
+export default class Typeahead extends Vue {
+  @Prop() existingOptions!: types.ListItem[];
+  @Prop({ default: 3 }) activationThreshold!: number;
+  @Prop({ default: 10 }) maxResults!: number;
+
+  T = T;
+  selectedOptions: types.ListItem[] = [];
+
+  updateExistingOptions(query: string): void {
+    if (query.length < this.activationThreshold) return;
+    this.$emit('update-existing-options', query);
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../../../../sass/main.scss';
+
+.tags-input-remove:before,
+.tags-input-remove:after,
+.tags-input-typeahead-item-highlighted-default {
+  background-color: $omegaup-primary--darker;
+}
+</style>
