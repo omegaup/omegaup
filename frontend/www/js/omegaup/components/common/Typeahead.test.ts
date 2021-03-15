a8ad052ac0834839d76d56f4fb91a6fd063b27f4
lhchavez <lhchavez@lhchavez.com>
2021-03-14 17:58:17 -0700

    Usando `update:value` en Typeahead.vue (#5227)
    Este cambio hace que Typeahead use un evento de nombre `update:value`
    para poder usar `:value.sync` en el componente padre.

--- frontend/www/js/omegaup/components/common/Typeahead.test.ts
+++ frontend/www/js/omegaup/components/common/Typeahead.test.ts
@@ -1,14 +1,64 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import common_Typeahead from './Typeahead.vue';
+import VoerroTagsInput from '@voerro/vue-tagsinput';
+import Vue from 'vue';
 
 describe('Typeahead.vue', () => {
-  it('Should handle empty existing options list', async () => {
-    const wrapper = shallowMount(common_Typeahead, {
+  it('Should not call update-existing-options with a short query', async () => {
+    const wrapper = mount(common_Typeahead, {
       propsData: {
         existingOptions: [],
       },
     });
 
-    expect(wrapper.vm.$data.selectedOptions).toStrictEqual([]);
+    const tagsInput = wrapper.findComponent(VoerroTagsInput);
+    tagsInput.vm.$emit('change', 'qu');
+    expect(wrapper.emitted()).toEqual({});
+  });
+
+  it('Should call update-existing-options with a longer query', async () => {
+    const wrapper = mount(common_Typeahead, {
+      propsData: {
+        existingOptions: [],
+      },
+    });
+
+    const tagsInput = wrapper.findComponent(VoerroTagsInput);
+    tagsInput.vm.$emit('change', 'query');
+    expect(wrapper.emitted()).toEqual({
+      'update-existing-options': [['query']],
+    });
+  });
+
+  it('Should call update:value with a non-empty tag', async () => {
+    const wrapper = mount(common_Typeahead, {
+      propsData: {
+        existingOptions: [],
+      },
+    });
+
+    const tagsInput = wrapper.findComponent(VoerroTagsInput);
+    tagsInput.vm.$emit('input', [{ key: 'key', value: 'value' }]);
+    tagsInput.vm.$emit('tag-added');
+    await Vue.nextTick();
+    expect(wrapper.emitted()).toEqual({
+      'update:value': [['key'], ['key']],
+    });
+  });
+
+  it('Should call update:value with an empty tag', async () => {
+    const wrapper = mount(common_Typeahead, {
+      propsData: {
+        existingOptions: [{ key: 'key', value: 'value' }],
+      },
+    });
+
+    const tagsInput = wrapper.findComponent(VoerroTagsInput);
+    tagsInput.vm.$emit('input', []);
+    tagsInput.vm.$emit('tag-removed');
+    await Vue.nextTick();
+    expect(wrapper.emitted()).toEqual({
+      'update:value': [[null]],
+    });
   });
 });
