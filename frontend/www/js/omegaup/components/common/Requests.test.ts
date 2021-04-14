5a13d4ebcc00033005cbb6b22dc8abee0e6231cd
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-04-14 10:32:10 -0500

    Agregando anotaciones de tipo en pruebas

--- frontend/www/js/omegaup/components/common/Requests.test.ts
+++ frontend/www/js/omegaup/components/common/Requests.test.ts
@@ -5,19 +5,21 @@ import T from '../../lang';
 import common_Requests from './Requests.vue';
 
 describe('Requests.vue', () => {
-  const propsData = {
-    data: [
-      {
-        accepted: false,
-        admin: {
-          username: 'test_user',
-        },
-        country: null,
-        last_update: new Date(),
-        request_time: new Date(),
+  const data: types.IdentityRequest[] = [
+    {
+      accepted: false,
+      admin: {
         username: 'test_user',
       },
-    ],
+      country: null,
+      last_update: new Date(),
+      request_time: new Date(),
+      username: 'test_user',
+    },
+  ];
+
+  const propsData = {
+    data,
     textAddParticipant: T.contestAdduserAddContestant,
   };
 
