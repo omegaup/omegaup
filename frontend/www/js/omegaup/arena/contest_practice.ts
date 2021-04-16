c53feef5ccdbe140892811d8aed636e16bde499d
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-04-16 00:13:38 -0500

    Mandando rowcount y offset como parámetros de clarificaciones

--- frontend/www/js/omegaup/arena/contest_practice.ts
+++ frontend/www/js/omegaup/arena/contest_practice.ts
@@ -174,6 +174,8 @@ OmegaUp.on('ready', () => {
     refreshContestClarifications({
       type: ContestClarificationType.AllProblems,
       contestAlias: payload.contest.alias,
+      rowcount: 20,
+      offset: 0,
     });
   }, 5 * 60 * 1000);
 });
