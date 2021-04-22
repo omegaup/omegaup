382b0f8ac8bf835dfe2bc6fecf42d0c78480edd9
Michael Serrato <mikebsg01@gmail.com>
2021-04-21 20:37:47 -0500

    Ordenar tabla de nominaciones de calidad por 'alias' y 'fecha de envío' (#5324)
    Agrega componente `SortControls.vue` en las columnas de la tabla en `List.vue` y permite ordenar la tabla de nominaciones de calidad por 'alias' y 'fecha de envío'.
    
    Fixes: #5087

--- frontend/www/js/omegaup/components/qualitynomination/List.test.ts
+++ frontend/www/js/omegaup/components/qualitynomination/List.test.ts
@@ -1,7 +1,32 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import T from '../../lang';
 import qualitynomination_List from './List.vue';
 import type { types } from '../../api_types';
+
+const nominations: types.NominationListItem[] = [1, 2, 3].map((x) => ({
+  author: {
+    name: 'nombre',
+    username: 'user',
+  },
+  contents: {
+    original: '',
+    rationale: 'N/A',
+    reason: 'wrong-test-cases',
+  },
+  nomination: 'demotion',
+  nominator: {
+    name: 'nominador',
+    username: 'user_nominator',
+  },
+  problem: {
+    alias: `Problem-${x}`,
+    title: `Problem ${x}`,
+  },
+  qualitynomination_id: x,
+  status: 'open',
+  time: new Date(`2021-02-0${x} 00:00:00`),
+  votes: [],
+}));
 
 describe('List.vue', () => {
   it('Should handle list of nominations', async () => {
@@ -96,5 +121,100 @@ describe('List.vue', () => {
     });
 
     expect(wrapper.find('[data-name="reason"]').exists()).toBe(false);
+  });
+
+  describe('Sort controls', () => {
+    it('Should sort by problem title by default', async () => {
+      const wrapper = shallowMount(qualitynomination_List, {
+        propsData: {
+          nominations,
+          pagerItems: [
+            {
+              class: 'disabled',
+              label: '1',
+              page: 1,
+            },
+          ],
+          pages: 1,
+          length: 100,
+          isAdmin: true,
+          myView: false,
+        },
+      });
+
+      expect(wrapper.vm.orderedNominations[0].problem.title).toBe('Problem 1');
+    });
+
+    it('Should sort by problem title in descending order', async () => {
+      const wrapper = mount(qualitynomination_List, {
+        propsData: {
+          nominations,
+          pagerItems: [
+            {
+              class: 'disabled',
+              label: '1',
+              page: 1,
+            },
+          ],
+          pages: 1,
+          length: 100,
+          isAdmin: true,
+          myView: false,
+        },
+      });
+
+      expect(wrapper.vm.orderedNominations[0].problem.title).toBe('Problem 1');
+
+      const sortControlByTitle = wrapper.findComponent({
+        ref: 'sortControlByTitle',
+      });
+      expect(sortControlByTitle.exists()).toBe(true);
+
+      const button = sortControlByTitle.find('a');
+      expect(button.exists()).toBe(true);
+
+      button.trigger('click');
+
+      expect(wrapper.vm.orderedNominations[0].problem.title).toBe('Problem 3');
+    });
+
+    it('Should sort by time', async () => {
+      const wrapper = mount(qualitynomination_List, {
+        propsData: {
+          nominations,
+          pagerItems: [
+            {
+              class: 'disabled',
+              label: '1',
+              page: 1,
+            },
+          ],
+          pages: 1,
+          length: 100,
+          isAdmin: true,
+          myView: false,
+        },
+      });
+
+      expect(wrapper.vm.orderedNominations[0].problem.title).toBe('Problem 1');
+      expect(wrapper.vm.orderedNominations[0].time.getTime()).toBe(
+        new Date('2021-02-01 00:00:00').getTime(),
+      );
+
+      const sortControlByTime = wrapper.findComponent({
+        ref: 'sortControlByTime',
+      });
+      expect(sortControlByTime.exists()).toBe(true);
+
+      const button = sortControlByTime.find('a');
+      expect(button.exists()).toBe(true);
+
+      button.trigger('click');
+
+      expect(wrapper.vm.orderedNominations[0].problem.title).toBe('Problem 3');
+      expect(wrapper.vm.orderedNominations[0].time.getTime()).toBe(
+        new Date('2021-02-03 00:00:00').getTime(),
+      );
+    });
   });
 });
