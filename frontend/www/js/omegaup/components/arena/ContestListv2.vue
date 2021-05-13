827eedc419d3cf0ed9dcde36465dc9d1e01e3268
Michael Serrato <mikebsg01@gmail.com>
2021-05-12 19:51:17 -0500

    [ContestListRedesign][Frontend] Agregar componente de Sidebar (#5370)
    Se realiza la implementación del componente **Sidebar** mediante el uso del Plugin `Tabs` de la librería `Bootstrap-Vue`, compatible con el Bootstrap global de la plataforma.
    
    Fixes: #5367

--- frontend/www/js/omegaup/components/arena/ContestListv2.vue
+++ frontend/www/js/omegaup/components/arena/ContestListv2.vue
@@ -1,6 +1,41 @@
 <template>
   <div>
-    <span>{{ contests }}</span>
+    <div class="col-sm-12">
+      <h1 class="title">{{ T.wordsContests }}</h1>
+    </div>
+    <b-card no-body>
+      <b-tabs
+        v-model="activeTab"
+        class="sidebar"
+        pills
+        card
+        vertical
+        nav-wrapper-class="contest-list-nav col-sm-4 col-md-2"
+      >
+        <b-tab
+          ref="currentContestTab"
+          :title="T.contestListCurrent"
+          :title-link-class="titleLinkClass(ContestTab.Current)"
+          active
+        >
+          {{ contests.current }}
+        </b-tab>
+        <b-tab
+          ref="futureContestTab"
+          :title="T.contestListFuture"
+          :title-link-class="titleLinkClass(ContestTab.Future)"
+        >
+          {{ contests.future }}
+        </b-tab>
+        <b-tab
+          ref="pastContestTab"
+          :title="T.contestListPast"
+          :title-link-class="titleLinkClass(ContestTab.Past)"
+        >
+          {{ contests.past }}
+        </b-tab>
+      </b-tabs>
+    </b-card>
   </div>
 </template>
 
@@ -8,11 +43,61 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
 import { types } from '../../api_types';
 import T from '../../lang';
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Import Only Required Plugins
+import { TabsPlugin, CardPlugin } from 'bootstrap-vue';
+Vue.use(TabsPlugin);
+Vue.use(CardPlugin);
+
+export enum ContestTab {
+  Current = 0,
+  Future = 1,
+  Past = 2,
+}
+
 @Component({
   components: {},
 })
 export default class ArenaContestList extends Vue {
   @Prop() contests!: types.ContestList;
   T = T;
+  ContestTab = ContestTab;
+  activeTab: ContestTab = ContestTab.Current;
+
+  titleLinkClass(tab: ContestTab) {
+    if (this.activeTab === tab) {
+      return ['text-center', 'active-title-link'];
+    } else {
+      return ['text-center', 'title-link'];
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../../../../sass/main.scss';
+
+.sidebar {
+  /deep/ .contest-list-nav {
+    background-color: var(
+      --arena-contest-list-sidebar-tab-list-background-color
+    );
+
+    .active-title-link {
+      background-color: var(
+        --arena-contest-list-sidebar-tab-list-link-background-color--active
+      ) !important;
+    }
+
+    .title-link {
+      color: var(
+        --arena-contest-list-sidebar-tab-list-link-font-color
+      ) !important;
+    }
+  }
+}
+</style>
