4318974e8e81241a99505cb9c053732882191168
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-05-24 18:37:53 -0500

    Agregando un par de pruebas extra para completar la cobertura

--- frontend/tests/controllers/TeamGroupsTest.php
+++ frontend/tests/controllers/TeamGroupsTest.php
@@ -39,4 +39,67 @@ class TeamGroupsTest extends \OmegaUp\Test\ControllerTestCase {
         $this->assertNotNull($teamGroup);
         $this->assertEquals($description, $teamGroup->description);
     }
+
+    public function testCreateTeamGroupForNonAuthorizedUser() {
+        ['identity' => $identity] = \OmegaUp\Test\Factories\User::createUser();
+        [
+            'identity' => $nonAuthorizedIdentity,
+        ] = \OmegaUp\Test\Factories\User::createUser();
+        $name = \OmegaUp\Test\Utils::createRandomString();
+        $description = \OmegaUp\Test\Utils::createRandomString();
+        $alias = \OmegaUp\Test\Utils::createRandomString();
+
+        $login = self::login($identity);
+        $response = \OmegaUp\Controllers\Group::apiCreateTeamGroup(
+            new \OmegaUp\Request([
+                'auth_token' => $login->auth_token,
+                'name' => $name,
+                'alias' => $alias,
+                'description' => $description
+            ])
+        );
+
+        $teamGroup = \OmegaUp\DAO\TeamGroups::getByAlias($alias);
+        $this->assertFalse(
+            \OmegaUp\Authorization::isTeamGroupAdmin(
+                $nonAuthorizedIdentity,
+                $teamGroup
+            )
+        );
+    }
+
+    public function testCreateTeamGroupWithDuplictedAlias() {
+        ['identity' => $identity] = \OmegaUp\Test\Factories\User::createUser();
+        $name = \OmegaUp\Test\Utils::createRandomString();
+        $description = \OmegaUp\Test\Utils::createRandomString();
+        $alias = \OmegaUp\Test\Utils::createRandomString();
+
+        $login = self::login($identity);
+        $response = \OmegaUp\Controllers\Group::apiCreateTeamGroup(
+            new \OmegaUp\Request([
+                'auth_token' => $login->auth_token,
+                'name' => $name,
+                'alias' => $alias,
+                'description' => $description
+            ])
+        );
+
+        $this->assertEquals('ok', $response['status']);
+
+        try {
+            $response = \OmegaUp\Controllers\Group::apiCreateTeamGroup(
+                new \OmegaUp\Request([
+                    'auth_token' => $login->auth_token,
+                    'name' => $name,
+                    'alias' => $alias,
+                    'description' => $description
+                ])
+            );
+            $this->fail(
+                'Teams group creation should have failed because alias is already in use'
+            );
+        } catch (\OmegaUp\Exceptions\DuplicatedEntryInDatabaseException $e) {
+            $this->assertEquals('aliasInUse', $e->getMessage());
+        }
+    }
 }
