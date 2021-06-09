b2b1e6871e9863408ce02b5807ae9a6d11977699
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-06-09 14:39:47 -0500

    Agregando pruebas para aumentar la cobertura del codecov

--- frontend/www/js/omegaup/components/teamsgroup/Form.test.ts
+++ frontend/www/js/omegaup/components/teamsgroup/Form.test.ts
@@ -4,7 +4,7 @@ import T from '../../lang';
 import teamsgroup_Form from './Form.vue';
 
 describe('Form.vue', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     const div = document.createElement('div');
     div.id = 'root';
     document.body.appendChild(div);
@@ -17,15 +17,17 @@ describe('Form.vue', () => {
     }
   });
 
+  const propsData = {
+    teamsGroupAlias: 'Hello',
+    teamsGroupName: 'Hello omegaUp',
+    teamsGroupDescription: 'Hello omegaUp Description',
+    isUpdate: false,
+  };
+
   it('Should handle new form', async () => {
     const wrapper = shallowMount(teamsgroup_Form, {
       attachTo: '#root',
-      propsData: {
-        teamsGroupAlias: 'Hello',
-        teamsGroupName: 'Hello omegaUp',
-        teamsGroupDescription: 'Hello omegaUp Description',
-        isUpdate: false,
-      },
+      propsData,
     });
 
     expect(wrapper.find('div[class="card-header"]').text()).toBe(
@@ -39,6 +41,54 @@ describe('Form.vue', () => {
           alias: 'Hello',
           description: 'Hello omegaUp Description',
           name: 'Hello omegaUp',
+        },
+      ],
+    ]);
+
+    wrapper.destroy();
+  });
+
+  it('Should handle changes in name field', async () => {
+    const wrapper = shallowMount(teamsgroup_Form, {
+      propsData,
+    });
+
+    wrapper.setData({
+      name: 'some new name',
+    });
+
+    await wrapper.find('button[type="submit"]').trigger('click');
+
+    expect(wrapper.emitted('validate-unused-alias')).toEqual([
+      ['some-new-name'],
+    ]);
+    expect(wrapper.vm.$data.alias).toBe('some-new-name');
+  });
+
+  it('Should handle edit form', async () => {
+    const wrapper = shallowMount(teamsgroup_Form, {
+      attachTo: '#root',
+      propsData: { ...propsData, ...{ isUpdate: true } },
+    });
+
+    expect(wrapper.find('div[class="card"]').text()).not.toContain(
+      T.omegaupTitleTeamsGroupNew,
+    );
+
+    wrapper.setData({
+      name: 'some updated name',
+    });
+
+    await wrapper.find('button[type="submit"]').trigger('click');
+
+    // In edit mode, alias can not change its value
+    expect(wrapper.vm.$data.alias).toBe('Hello');
+    expect(wrapper.emitted('validate-unused-alias')).toBeFalsy();
+    expect(wrapper.emitted('update-teams-group')).toEqual([
+      [
+        {
+          description: 'Hello omegaUp Description',
+          name: 'some updated name',
         },
       ],
     ]);
