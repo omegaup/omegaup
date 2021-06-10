45642ce87fe0f729401be0751545371a9af9560b
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-06-09 21:58:33 -0500

    Agregando componente en vue de TeamsGroupUpload

--- webpack.config-frontend.js
+++ webpack.config-frontend.js
@@ -103,6 +103,7 @@ module.exports = {
     school_profile: './frontend/www/js/omegaup/schools/profile.ts',
     schools_rank: './frontend/www/js/omegaup/schools/rank.ts',
     submissions_list: './frontend/www/js/omegaup/submissions/list.ts',
+    teams_group_edit: './frontend/www/js/omegaup/teamsgroup/edit.ts',
     user_basic_edit: './frontend/www/js/omegaup/user/basicedit.js',
     user_edit_email_form: './frontend/www/js/omegaup/user/emailedit.js',
     user_manage_identities:
