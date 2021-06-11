e9b92eb3dc3352bd646849b9d437eb1081338ba8
Pablo Aguilar <pablo.aguilar@gmail.com>
2021-06-11 00:21:40 -0700

    Agregando link a discord en la seccion de redes sociales

--- frontend/www/js/omegaup/components/common/Footerv2.vue
+++ frontend/www/js/omegaup/components/common/Footerv2.vue
@@ -83,14 +83,19 @@
             </li>
           </ul>
           <div class="social-icons my-0 mx-auto">
-            <a href="https://github.com/omegaup/omegaup/">
+            <a class="text-nowrap" href="https://github.com/omegaup/omegaup/">
               <font-awesome-icon :icon="['fab', 'github']" />
               GitHub
             </a>
             |
-            <a href="https://www.facebook.com/omegaup/">
+            <a class="text-nowrap" href="https://www.facebook.com/omegaup/">
               <font-awesome-icon :icon="['fab', 'facebook']" />
               Facebook
+            </a>
+            |
+            <a class="text-nowrap" href="https://discord.com/channels/832682734115094638">
+              <font-awesome-icon :icon="['fab', 'discord']" />
+              Discord
             </a>
           </div>
         </div>
@@ -130,8 +135,8 @@ import { reportAnIssueURL } from '../../errors';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
-library.add(faFacebook, faGithub);
+import { faFacebook, faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
+library.add(faFacebook, faGithub, faDiscord);
 
 @Component({
   components: {
