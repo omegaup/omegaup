2c8bc0963a70c70f16cfa3b071ec2fe6a6ca9401
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-06-16 12:09:45 -0500

    Agregando más pruebas del rol de creador de identidades

--- frontend/tests/controllers/UserPrivilegesTest.php
+++ frontend/tests/controllers/UserPrivilegesTest.php
@@ -166,4 +166,82 @@ class UserPrivilegesTest extends \OmegaUp\Test\ControllerTestCase {
         );
         $this->assertNotContains('omegaup:mentor', $systemGroups);
     }
+
+    /*
+     * Test API addRole for users intended to upload identities
+     */
+    public function testAddGroupIdentityCreatorRole() {
+        // Create an admin identity
+        ['identity' => $admin] = \OmegaUp\Test\Factories\User::createAdminUser();
+
+        // Create group identity creator user
+        ['identity' => $creator] = \OmegaUp\Test\Factories\User::createUser();
+
+        $adminLogin = self::login($admin);
+        // Call to API Add Role
+        $response = \OmegaUp\Controllers\User::apiAddRole(new \OmegaUp\Request([
+            'auth_token' => $adminLogin->auth_token,
+            'username' => $creator->username,
+            'role' => 'GroupIdentityCreator'
+        ]));
+
+        $systemRoles = \OmegaUp\DAO\UserRoles::getSystemRoles(
+            $creator->user_id
+        );
+        $this->assertContains('GroupIdentityCreator', $systemRoles);
+
+        $creatorLogin = self::login($creator);
+        ['group' => $group] = \OmegaUp\Test\Factories\Groups::createGroup(
+            $creator,
+            null,
+            null,
+            null,
+            $creatorLogin
+        );
+
+        // Call api using identity creator group member
+        \OmegaUp\Controllers\Identity::apiBulkCreate(new \OmegaUp\Request([
+            'auth_token' => $creatorLogin->auth_token,
+            'identities' => \OmegaUp\Test\Factories\Identity::getCsvData(
+                'identities.csv',
+                $group->alias,
+            ),
+            'group_alias' => $group->alias,
+        ]));
+        $response = \OmegaUp\Controllers\Group::apiMembers(
+            new \OmegaUp\Request([
+                'auth_token' => $creatorLogin->auth_token,
+                'group_alias' => $group->alias,
+            ])
+        );
+        $this->assertCount(5, $response['identities']);
+
+        // Call to API Remove Role
+        $response = \OmegaUp\Controllers\User::apiRemoveRole(
+            new \OmegaUp\Request([
+                'auth_token' => $adminLogin->auth_token,
+                'username' => $creator->username,
+                'role' => 'GroupIdentityCreator'
+            ])
+        );
+        $systemRoles = \OmegaUp\DAO\UserRoles::getSystemRoles(
+            $creator->user_id
+        );
+        $this->assertNotContains('GroupIdentityCreator', $systemRoles);
+
+        try {
+            \OmegaUp\Controllers\Identity::apiBulkCreate(new \OmegaUp\Request([
+                'auth_token' => $creatorLogin->auth_token,
+                'identities' => \OmegaUp\Test\Factories\Identity::getCsvData(
+                    'identities.csv',
+                    $group->alias,
+                ),
+                'group_alias' => $group->alias,
+            ]));
+            $this->fail('should not have been able to create identites');
+        } catch (\OmegaUp\Exceptions\ForbiddenAccessException $e) {
+            // Exception expected
+            $this->assertEquals($e->getMessage(), 'userNotAllowed');
+        }
+    }
 }
