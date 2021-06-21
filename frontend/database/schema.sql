4c43d58537b67e55c93654633ccdaaffedf6fc5b
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2021-06-20 21:37:14 -0500

    Agregando tabla de Teams_Group_Roles para ligar concurso con un grupo de equipos (#5488)
    Este cambio servirá para agregar una tabla en la base de datos que no se
    había considerado anteriormente. La cual servirá para relacionar el grupo
    de equipos con el concurso para equipos que corresponda.
    
    Una vez aprobado este cambio, ahora si se procederá a agregar los cambios
    en los controladores.
    
    Part of: #5412

--- frontend/database/schema.sql
+++ frontend/database/schema.sql
@@ -1057,6 +1057,21 @@ CREATE TABLE `Teams` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Teams_Group_Roles` (
+  `team_group_id` int NOT NULL,
+  `role_id` int NOT NULL,
+  `acl_id` int NOT NULL,
+  PRIMARY KEY (`team_group_id`,`role_id`,`acl_id`),
+  KEY `team_group_id` (`team_group_id`),
+  KEY `role_id` (`role_id`),
+  KEY `acl_id` (`acl_id`),
+  CONSTRAINT `fk_tgra_acl_id` FOREIGN KEY (`acl_id`) REFERENCES `ACLs` (`acl_id`),
+  CONSTRAINT `fk_tgrg_team_group_id` FOREIGN KEY (`team_group_id`) REFERENCES `Team_Groups` (`team_group_id`),
+  CONSTRAINT `fk_tgrr_role_id` FOREIGN KEY (`role_id`) REFERENCES `Roles` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Establece los roles que se pueden dar a los grupos de equipos.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `User_Rank` (
   `user_id` int NOT NULL,
   `ranking` int DEFAULT NULL,
