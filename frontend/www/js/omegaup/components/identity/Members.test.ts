f87f02acfc616131dec792cf3eb9a3027d301878
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-06-25 14:38:43 -0500

    Agregando el componente Member.vue para equipos

---
+++ frontend/www/js/omegaup/components/identity/Members.test.ts
@@ -0,0 +1,97 @@
+import { mount, shallowMount } from '@vue/test-utils';
+
+import { types } from '../../api_types';
+import T from '../../lang';
+import * as ui from '../../ui';
+
+import identity_Members from './Members.vue';
+
+describe('Members.vue', () => {
+  beforeEach(() => {
+    const div = document.createElement('div');
+    div.id = 'root';
+    document.body.appendChild(div);
+  });
+
+  afterAll(() => {
+    const rootDiv = document.getElementById('root');
+    if (rootDiv) {
+      document.removeChild(rootDiv);
+    }
+  });
+
+  it('Should handle an empty list of members', async () => {
+    const teamUsername = 'teams:group_alias:team_1';
+    const wrapper = shallowMount(identity_Members, {
+      attachTo: '#root',
+      propsData: {
+        searchResultUsers: [] as types.ListItem[],
+        teamUsername,
+        teamsMembers: [] as types.TeamMember[],
+      },
+    });
+
+    expect(wrapper.find('.card-title').text()).toBe(
+      ui.formatString(T.groupEditMembersTitle, {
+        username: teamUsername,
+      }),
+    );
+
+    await wrapper.setData({
+      typeaheadContestants: [
+        { key: 'user_1', value: 'User 1' },
+        { key: 'user_2', value: 'User 2' },
+      ],
+    });
+
+    expect(wrapper.find('form button[type="submit"]').text()).toBe(
+      T.wordsAddMember,
+    );
+    await wrapper.find('form button[type="submit"]').trigger('click');
+    expect(wrapper.emitted('add-members')).toEqual([
+      [
+        {
+          teamUsername,
+          usersToAdd: ['user_1', 'user_2'],
+        },
+      ],
+    ]);
+
+    wrapper.destroy();
+  });
+
+  it('Should handle a list of members', async () => {
+    const teamUsername = 'teams:group_alias:team_1';
+    const wrapper = mount(identity_Members, {
+      attachTo: '#root',
+      propsData: {
+        searchResultUsers: [] as types.ListItem[],
+        teamUsername,
+        teamsMembers: [
+          {
+            classname: 'user-rank-unranked',
+            name: 'user 1',
+            team_alias: teamUsername,
+            team_name: 'team 1',
+            username: 'user_1',
+          },
+        ] as types.TeamMember[],
+      },
+    });
+
+    expect(wrapper.find('table[data-table-members] tbody').text()).toContain(
+      'user_1',
+    );
+    await wrapper.find('button[data-table-remove-member]').trigger('click');
+    expect(wrapper.emitted('remove-member')).toEqual([
+      [
+        {
+          teamUsername,
+          username: 'user_1',
+        },
+      ],
+    ]);
+
+    wrapper.destroy();
+  });
+});
