070e47d671afa52380bc47101c7b52352c17c4b7
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2021-06-29 21:33:18 -0500

    Agregando apiRemoveMember en el controlador de TeamsGroup (#5505)
    Para hacer más pequeño el cambio de #5499, se separa en este nuevo PR la
    creación de `apiRemoveMember` en el controlador de `TeamsGroups` para 
    eliminar miembros de un equipo de acuerdo a un grupo de equipos dado.
    
    Part of: #5418

--- frontend/www/js/omegaup/api_types.ts
+++ frontend/www/js/omegaup/api_types.ts
@@ -4249,6 +4249,8 @@ export namespace messages {
   };
   export type TeamsGroupListRequest = { [key: string]: any };
   export type TeamsGroupListResponse = types.ListItem[];
+  export type TeamsGroupRemoveMemberRequest = { [key: string]: any };
+  export type TeamsGroupRemoveMemberResponse = {};
   export type TeamsGroupRemoveTeamRequest = { [key: string]: any };
   export type TeamsGroupRemoveTeamResponse = {};
   export type TeamsGroupTeamsRequest = { [key: string]: any };
@@ -5005,6 +5007,9 @@ export namespace controllers {
     list: (
       params?: messages.TeamsGroupListRequest,
     ) => Promise<messages.TeamsGroupListResponse>;
+    removeMember: (
+      params?: messages.TeamsGroupRemoveMemberRequest,
+    ) => Promise<messages.TeamsGroupRemoveMemberResponse>;
     removeTeam: (
       params?: messages.TeamsGroupRemoveTeamRequest,
     ) => Promise<messages.TeamsGroupRemoveTeamResponse>;
