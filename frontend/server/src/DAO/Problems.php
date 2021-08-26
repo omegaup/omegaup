4e137a3794da2207ee6aaaaeee56dba19e44b0c0
luis-8 <luis_zavala8@outlook.com>
2021-08-26 13:50:34 -0500

    Modifiqué consulta de problemas resultos en el perfil para que no considere los del owner

--- frontend/server/src/DAO/Problems.php
+++ frontend/server/src/DAO/Problems.php
@@ -624,8 +624,30 @@ class Problems extends \OmegaUp\DAO\Base\Problems {
                 Submissions s ON s.problem_id = p.problem_id
             INNER JOIN
                 Runs r ON r.run_id = s.current_run_id
+            INNER JOIN
+                Identities i ON i.identity_id = s.identity_id
+            INNER JOIN
+                Users u ON u.user_id = i.user_id
             WHERE
                 r.verdict = "AC" AND s.type = "normal" AND s.identity_id = ?
+                AND NOT EXISTS (
+                    SELECT
+                        `pf`.`problem_id`, `pf`.`user_id`
+                    FROM
+                        `Problems_Forfeited` AS `pf`
+                    WHERE
+                        `pf`.`problem_id` = `p`.`problem_id` AND
+                        `pf`.`user_id` = `u`.`user_id`
+                )
+                AND NOT EXISTS (
+                    SELECT
+                        `a`.`acl_id`
+                    FROM
+                        `ACLs` AS `a`
+                    WHERE
+                        `a`.`acl_id` = `p`.`acl_id` AND
+                        `a`.`owner_id` = `u`.`user_id`
+                )
             GROUP BY
                 p.problem_id
             ORDER BY
