46d4430c05550d41a9f1751b1280c40b0eb982f7
Carlos Córdova <carlosab1802@gmail.com>
2021-08-31 20:33:35 -0500

    Avance parcial.

--- frontend/www/js/omegaup/components/course/Tabs.vue
+++ frontend/www/js/omegaup/components/course/Tabs.vue
@@ -4,6 +4,12 @@
     <li class="nav-item" role="presentation">
       <a class="nav-link" :class="{ active: showTab === 'studying' }" @click="showTab = 'studying'" data-toggle="tab" role="tab">Home</a>
     </li>
+    <li class="nav-item" role="presentation">
+      <a class="nav-link" :class="{ active: showTab === 'studying' }" @click="showTab = 'studying'" data-toggle="tab" role="tab">Home</a>
+    </li>
+    <li class="nav-item" role="presentation">
+      <a class="nav-link" :class="{ active: showTab === 'studying' }" @click="showTab = 'studying'" data-toggle="tab" role="tab">Home</a>
+    </li>
   </ul>
   <div class="tab-content">
     <div class="tab-pane" role="tabpanel" :class="{ active: showTab === 'studying' }">Jxjxjx</div>
@@ -13,10 +19,25 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
+import T from '../../lang';
 
 @Component
 export default class CourseTabs extends Vue {
 
-  showTab = 'studying';
+  tabs = {
+    enrolled: {
+      id: 'enrolled',
+      name: T.courseTabEnrolled,
+    },
+    general: {
+      id: 'general',
+      name: T.courseTabGeneral,
+    },
+    past: {
+      id: 'past',
+      name: T.courseTabPast,
+    }
+  };
+  selectedTab = this.tabs.enrolled.id;
 }
 </script>
