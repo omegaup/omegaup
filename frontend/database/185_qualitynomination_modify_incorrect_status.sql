44a2b0323474098dfe2979d4d0ca15a09fb33ca8
luis-8 <luis_zavala8@outlook.com>
2021-09-01 20:22:31 -0500

    especificando 'demotion' como tipo de nominación

--- frontend/database/185_qualitynomination_modify_incorrect_status.sql
+++ frontend/database/185_qualitynomination_modify_incorrect_status.sql
@@ -1,11 +1,13 @@
 UPDATE `QualityNominations` AS `qn`
 INNER JOIN `Problems` AS `p` ON `qn`.`problem_id` = `p`.`problem_id`
 SET `qn`.`status` = 'banned'
-WHERE `qn`.`status` = 'resolved'
+WHERE `qn`.`nomination` = 'demotion'
+AND `qn`.`status` = 'resolved'
 AND `p`.`visibility` <= -2;
 
 UPDATE `QualityNominations` AS `qn`
 INNER JOIN `Problems` AS `p` ON `qn`.`problem_id` = `p`.`problem_id`
 SET `qn`.`status` = 'resolved'
-WHERE `qn`.`status` = 'banned'
+WHERE `qn`.`nomination` = 'demotion'
+AND `qn`.`status` = 'banned'
 AND `p`.`visibility` >= -1;
