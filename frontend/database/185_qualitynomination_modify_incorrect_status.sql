fc22d24d36067e6612ea391c835f1cf452babf71
luis-8 <luis_zavala8@outlook.com>
2021-09-01 20:44:54 -0500

    corrección: no considerar VISIBILITY_DELETED

--- frontend/database/185_qualitynomination_modify_incorrect_status.sql
+++ frontend/database/185_qualitynomination_modify_incorrect_status.sql
@@ -3,7 +3,7 @@ INNER JOIN `Problems` AS `p` ON `qn`.`problem_id` = `p`.`problem_id`
 SET `qn`.`status` = 'banned'
 WHERE `qn`.`nomination` = 'demotion'
 AND `qn`.`status` = 'resolved'
-AND `p`.`visibility` <= -2;
+AND (`p`.`visibility` = -3 OR `p`.`visibility` = -2);
 
 UPDATE `QualityNominations` AS `qn`
 INNER JOIN `Problems` AS `p` ON `qn`.`problem_id` = `p`.`problem_id`
