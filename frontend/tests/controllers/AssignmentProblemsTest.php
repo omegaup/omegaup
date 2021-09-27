98b411516e30cf838145a48c7dee98bf8e9d21cd
magno309 <luisabrahamsanabriamelecio@gmail.com>
2021-09-27 21:26:34 +0000

    Fixed /hook_tools/lint.py lints

--- frontend/tests/controllers/AssignmentProblemsTest.php
+++ frontend/tests/controllers/AssignmentProblemsTest.php
@@ -522,7 +522,7 @@ class AssignmentProblemsTest extends \OmegaUp\Test\ControllerTestCase {
         );
 
         //First student will solve problem0, second student will only get 90%
-        
+
 
         $this->assertEquals($assignmentAlias, $results[0]['assignment_alias']);
         // Variance of the first problem must be greater than 0
