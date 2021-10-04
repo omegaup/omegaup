0c05ec0b8817027b2943767c958a1bc705c8a70e
Carlos Córdova <carlosab1802@gmail.com>
2021-10-04 16:08:20 -0500

    Dejando el list.ts como estaba.

--- frontend/www/js/omegaup/course/list.ts
+++ frontend/www/js/omegaup/course/list.ts
@@ -1,22 +1,20 @@
-import course_List from '../components/course/Tabs.vue';
+import course_List from '../components/course/List.vue';
 import { OmegaUp } from '../omegaup';
 import { types } from '../api_types';
 import Vue from 'vue';
 
 OmegaUp.on('ready', () => {
-  const payload = types.payloadParsers.CourseTabsPayload();
+  const payload = types.payloadParsers.CourseListPayload();
   const headerPayload = types.payloadParsers.CommonPayload();
   new Vue({
     el: '#main-container',
     components: {
-      'omegaup-course-list': course_List,
+      'omegaup-course-cards-list': course_List,
     },
     render: function (createElement) {
-      return createElement('omegaup-course-list', {
+      return createElement('omegaup-course-cards-list', {
         props: {
-          enrolledCourses: payload.courses.enrolled,
-          publicCourses: payload.courses.public,
-          finishedCourses: payload.courses.finished,
+          courses: payload.courses,
           loggedIn: headerPayload.isLoggedIn,
         },
       });
