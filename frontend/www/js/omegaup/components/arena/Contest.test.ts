421e9042ac6832fe8d0a5772e92d13ecdbc4809a
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-10-12 07:58:38 -0500

    Agregando campo nuevo en los objetos de .ts

--- frontend/www/js/omegaup/components/arena/Contest.test.ts
+++ frontend/www/js/omegaup/components/arena/Contest.test.ts
@@ -43,6 +43,7 @@ describe('Contest.vue', () => {
     problemset_id: 1,
     scoreboard: 100,
     show_penalty: true,
+    show_all_contestants_at_first_time_in_scoreboard: false,
     show_scoreboard_after: true,
     start_time: currentDate,
     submissions_gap: 1200,
