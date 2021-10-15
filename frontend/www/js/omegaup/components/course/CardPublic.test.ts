8e6d4670912102b9225448ff9358feaef192ff1b
Carlos Córdova <carlosab1802@gmail.com>
2021-10-15 09:27:21 -0500

    Separando en subcomponentes.

--- frontend/www/js/omegaup/components/course/CardPublic.test.ts
+++ frontend/www/js/omegaup/components/course/CardPublic.test.ts
@@ -1,11 +1,11 @@
-import { shallowMount, mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import { types } from '../../api_types';
 import * as ui from '../../ui';
 import T from '../../lang';
 
-import course_Card from './Cardv2.vue';
+import course_CardPublic from './CardPublic.vue';
 
-describe('Card.vue', () => {
+describe('CardPublic.vue', () => {
   const publicCourse: types.CourseCardPublic = {
     alias: 'test-course',
     lessonCount: 2,
@@ -15,18 +15,10 @@ describe('Card.vue', () => {
     studentCount: 2000,
   };
 
-  const enrolledCourse: types.CourseCardEnrolled = {
-    alias: 'test-enrolled-course',
-    name: 'Enrolled course name',
-    progress: 25,
-    school_name: 'Test course school',
-  };
-
   it('Should render information for public course', () => {
-    const wrapper = shallowMount(course_Card, {
+    const wrapper = shallowMount(course_CardPublic, {
       propsData: {
         course: publicCourse,
-        type: 'public',
       },
     });
 
@@ -38,17 +30,5 @@ describe('Card.vue', () => {
         studentCount: '2.0k',
       }),
     );
-  });
-
-  it('Should render information for enrolled course', () => {
-    const wrapper = mount(course_Card, {
-      propsData: {
-        course: enrolledCourse,
-        type: 'enrolled',
-      },
-    });
-
-    expect(wrapper.text()).toContain(enrolledCourse.name);
-    expect(wrapper.text()).toContain(enrolledCourse.school_name);
   });
 });
--- frontend/www/js/omegaup/components/course/Tabs.vue
+++ frontend/www/js/omegaup/components/course/Tabs.vue
@@ -2,19 +2,19 @@
   <div class="container-fluid p-5">
     <ul class="nav nav-tabs" role="tablist">
       <li
-        v-for="tab in tabs"
-        :key="tab.id"
+        v-for="(tabName, tabKey) in tabNames"
+        :key="tabKey"
         class="nav-item"
         role="presentation"
       >
         <a
           class="nav-link"
-          :href="`#${tab.id}`"
-          :class="{ active: selectedTab === tab.id }"
+          :href="`#${tabKey}`"
+          :class="{ active: selectedTab === tabKey }"
           data-toggle="tab"
           role="tab"
-          @click="selectedTab = tab.id"
-          >{{ tab.name }}</a
+          @click="selectedTab = tabKey"
+          >{{ tabName }}</a
         >
       </li>
       <li class="ml-auto">
@@ -26,22 +26,30 @@
     <div class="tab-content">
       <!-- TODO: Add search input. -->
       <div
-        v-for="tab in tabs"
-        :key="tab.id"
+        v-for="(tabName, tabKey) in tabNames"
+        :key="tabKey"
         class="tab-pane fade py-4 px-2"
         :class="{
-          show: selectedTab === tab.id,
-          active: selectedTab === tab.id,
+          show: selectedTab === tabKey,
+          active: selectedTab === tabKey,
         }"
         role="tabpanel"
       >
         <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
-          <omegaup-course-card
-            v-for="course in courses[tab.id]"
-            :key="course.alias"
-            :course="course"
-            :type="tab.id"
-          ></omegaup-course-card>
+          <template v-if="tabKey === Tab.Public">
+            <omegaup-course-card-public
+              v-for="course in courses[tabKey]"
+              :key="course.alias"
+              :course="course"
+            ></omegaup-course-card-public>
+          </template>
+          <template v-if="tabKey === Tab.Enrolled">
+            <omegaup-course-card-enrolled
+              v-for="course in courses[tabKey]"
+              :key="course.alias"
+              :course="course"
+            ></omegaup-course-card-enrolled>
+          </template>
         </div>
       </div>
     </div>
@@ -54,9 +62,10 @@ import { types } from '../../api_types';
 import T from '../../lang';
 
 import omegaup_Markdown from '../Markdown.vue';
-import course_Card from './Cardv2.vue';
+import course_CardPublic from './CardPublic.vue';
+import course_CardEnrolled from './CardEnrolled.vue';
 
-export enum Tabs {
+export enum Tab {
   Enrolled = 'enrolled',
   Public = 'public',
   Finished = 'finished',
@@ -64,7 +73,8 @@ export enum Tabs {
 
 @Component({
   components: {
-    'omegaup-course-card': course_Card,
+    'omegaup-course-card-public': course_CardPublic,
+    'omegaup-course-card-enrolled': course_CardEnrolled,
     'omegaup-markdown': omegaup_Markdown,
   },
 })
@@ -76,21 +86,42 @@ export default class CourseTabs extends Vue {
   };
 
   T = T;
-  Tabs = Tabs;
-  tabs = {
-    public: {
-      id: Tabs.Public,
-      name: T.courseTabPublic,
-    },
-    enrolled: {
-      id: Tabs.Enrolled,
-      name: T.courseTabEnrolled,
-    },
-    finished: {
-      id: Tabs.Finished,
-      name: T.courseTabFinished,
-    },
+  Tab = Tab;
+  tabNames: Record<Tab, string> = {
+    [Tab.Public]: T.courseTabPublic,
+    [Tab.Enrolled]: T.courseTabEnrolled,
+    [Tab.Finished]: T.courseTabFinished,
   };
-  selectedTab = this.tabs.public.id;
+  selectedTab = Tab.Public;
 }
 </script>
+
+<style lang="scss">
+@import '../../../../sass/main.scss';
+
+.card > .row.no-gutters {
+  background-color: $omegaup-white;
+  height: 12.5rem;
+  overflow-y: auto;
+
+  .course-data p {
+    font-size: 0.9rem;
+  }
+
+  .public-course-card {
+    background-color: $omegaup-blue;
+  }
+
+  .enrolled-course-card {
+    background-color: $omegaup-pink--lighter;
+  }
+
+  .finished-course-card {
+    background-color: $omegaup-grey--lighter;
+  }
+
+  .progress-bar {
+    background-color: $omegaup-yellow;
+  }
+}
+</style>
