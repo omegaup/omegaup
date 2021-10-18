a1735cf8c3d44f64d24d462375cce9716aec797f
egessel <egessel100@gmail.com>
2021-10-17 20:01:44 -0500

    Correcciones realizadas y se agrego test.

--- stuff/pipelines/test_verification_code.py
+++ stuff/pipelines/test_verification_code.py
@@ -1,25 +1,26 @@
+#!/usr/bin/python3
+
+'''test verification_code module.'''
+
 import unittest
-from verification_code import  generate_code
+from verification_code import generate_code
 
-class test_verification_code(unittest.TestCase):
-    def test1(self):
-        """Test if the checksum digit in created code 
-        is calculated correctly"""
-        
-        indexes_in_alfaphabet = [0,1,2,3,4,5,6,7,8]
-        code = generate_code(indexes_in_alfaphabet)
-        self.assertEqual(code,'23456789C2')
 
-    def test2(self):
-        """Test if the checksum digit in created code 
+class TestVerificationCode(unittest.TestCase):
+    """Test if the checksum digit in created code
         is calculated correctly"""
-        
-        indexes_in_alfaphabet = [2,4,7,8,10,12,14,10,16]
+    def test1(self) -> None:
+        """Test1"""
+        indexes_in_alfaphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8]
         code = generate_code(indexes_in_alfaphabet)
-        self.assertEqual(code,'469CGJPGR9')
+        self.assertEqual(code, '23456789C2')
+
+    def test2(self) -> None:
+        """Test2"""
+        indexes_in_alfaphabet = [2, 4, 7, 8, 10, 12, 14, 10, 16]
+        code = generate_code(indexes_in_alfaphabet)
+        self.assertEqual(code, '469CGJPGR9')
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
-    
