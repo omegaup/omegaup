3bb210eb0c5fe09b3de5a622dfba9a006ea8e783
egessel <egessel100@gmail.com>
2021-10-21 13:17:01 -0500

    Se cambio la funcion del test para usar pruebas parametrizadas, se instalo pytest-stub adentro del contenedor de hook_tools

--- stuff/pipelines/test_verification_code.py
+++ stuff/pipelines/test_verification_code.py
@@ -2,22 +2,15 @@
 
 '''test verification_code module.'''
 
-import unittest
+from typing import List
+import pytest
 from verification_code import generate_code
 
 
-class TestChecksumDigit(unittest.TestCase):
-    """Test checksum digit creation."""
-    def test1(self) -> None:
-        """Test1"""
-        code = generate_code([0, 1, 2, 3, 4, 5, 6, 7, 8])
-        self.assertEqual(code, '23456789C2')
-
-    def test2(self) -> None:
-        """Test2"""
-        code = generate_code([2, 4, 7, 8, 10, 12, 14, 10, 16])
-        self.assertEqual(code, '469CGJPGR9')
-
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize("params, expected",
+                         [([0, 1, 2, 3, 4, 5, 6, 7, 8], '23456789C2'),
+                          ([2, 4, 7, 8, 10, 12, 14, 10, 16],
+                           '469CGJPGR9')])  # type: ignore
+def test_checksum_digit(params: List[int], expected: str) -> None:
+    'Test checksum digit'
+    assert generate_code(params) == expected
