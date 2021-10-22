c1c47c6bade6f6bcf739ec4410863a37b1da7700
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2021-10-22 18:54:42 -0500

    Agregando los cambios en el frontend para el scoreboard de cursos (#5844)
    Se agrega el componente de `Course/Scoreboard` para poder mostrar la tabla 
    de posiciones en su vista independiente.
    
    ![image](https://user-images.githubusercontent.com/3230352/137556611-71591603-0a81-4125-a21f-8073c335c3e3.png)
    
    Part of: #3915

---
+++ frontend/www/js/omegaup/course/scoreboard.ts
@@ -0,0 +1,90 @@
+import arena_Scoreboard from '../components/arena/Scoreboard.vue';
+import { OmegaUp } from '../omegaup';
+import { EventsSocket } from '../arena/events_socket';
+import socketStore from '../arena/socketStore';
+import * as api from '../api';
+import * as ui from '../ui';
+import * as time from '../time';
+import Vue from 'vue';
+import { types } from '../api_types';
+import { onRankingChanged } from '../arena/ranking';
+import rankingStore from '../arena/rankingStore';
+
+OmegaUp.on('ready', () => {
+  const payload = types.payloadParsers.CourseScoreboardPayload();
+  const commonPayload = types.payloadParsers.CommonPayload();
+  const getRankingByTokenRefresh = 5 * 60 * 1000; // 5 minutes
+
+  let ranking: types.ScoreboardRankingEntry[] = [];
+  let lastTimeUpdated: null | Date = null;
+  if (payload.scoreboard) {
+    const rankingInfo = onRankingChanged({
+      scoreboard: payload.scoreboard,
+      currentUsername: commonPayload.currentUsername,
+      navbarProblems: payload.problems,
+    });
+    ranking = rankingInfo.ranking;
+    lastTimeUpdated = rankingInfo.lastTimeUpdated;
+    rankingStore.commit('updateRanking', ranking);
+    rankingStore.commit('updateLastTimeUpdated', lastTimeUpdated);
+  }
+
+  new Vue({
+    el: '#main-container',
+    components: {
+      'omegaup-arena-scoreboard': arena_Scoreboard,
+    },
+    data: () => ({
+      problems: payload.problems,
+    }),
+    render: function (createElement) {
+      return createElement('omegaup-arena-scoreboard', {
+        props: {
+          problems: this.problems,
+          title: payload.assignment.name,
+          finishTime: payload.assignment.finishTime,
+          socketStatus: socketStore.state.socketStatus,
+          ranking: rankingStore.state.ranking,
+          lastUpdated: rankingStore.state.lastTimeUpdated,
+          showInvitedUsersFilter: false,
+        },
+      });
+    },
+  });
+
+  const socket = new EventsSocket({
+    disableSockets: false,
+    problemsetAlias: payload.assignment.alias,
+    locationProtocol: window.location.protocol,
+    locationHost: window.location.host,
+    problemsetId: payload.assignment.problemsetId,
+    scoreboardToken: null,
+    clarificationsOffset: 1,
+    clarificationsRowcount: 30,
+    navbarProblems: payload.problems,
+    currentUsername: commonPayload.currentUsername,
+    intervalInMilliseconds: 5 * 60 * 1000,
+  });
+
+  socket.connect();
+
+  setInterval(() => {
+    api.Problemset.scoreboard({
+      problemset_id: payload.assignment.problemsetId,
+      token: payload.scoreboardToken,
+    })
+      .then(time.remoteTimeAdapter)
+      .then((scoreboard) => {
+        const rankingInfo = onRankingChanged({
+          scoreboard,
+          currentUsername: commonPayload.currentUsername,
+          navbarProblems: payload.problems,
+        });
+        ranking = rankingInfo.ranking;
+        lastTimeUpdated = rankingInfo.lastTimeUpdated;
+        rankingStore.commit('updateRanking', ranking);
+        rankingStore.commit('updateLastTimeUpdated', lastTimeUpdated);
+      })
+      .catch(ui.ignoreError);
+  }, getRankingByTokenRefresh);
+});
