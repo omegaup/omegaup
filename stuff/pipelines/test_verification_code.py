f449e074e2adf82d389e2238fbd46808c96967c2
egessel <egessel100@gmail.com>
2021-10-28 00:15:31 -0500

    Correcciones 2

--- stuff/pipelines/test_verification_code.py
+++ stuff/pipelines/test_verification_code.py
@@ -7,10 +7,14 @@ import pytest
 from verification_code import generate_code
 
 
-@pytest.mark.parametrize("params, expected",
-                         [([0, 1, 2, 3, 4, 5, 6, 7, 8], '23456789C2'),
-                          ([2, 4, 7, 8, 10, 12, 14, 10, 16],
-                           '469CGJPGR9')])  # type: ignore
+# mypy has conflict with pytest decorations
+@pytest.mark.parametrize(
+    "params, expected",
+    [
+        ([0, 1, 2, 3, 4, 5, 6, 7, 8], '23456789C2'),
+        ([2, 4, 7, 8, 10, 12, 14, 10, 16], '469CGJPGR9'),
+    ],
+)  # type: ignore
 def test_checksum_digit(params: List[int], expected: str) -> None:
-    'Test checksum digit'
+    '''Test checksum digit'''
     assert generate_code(params) == expected
