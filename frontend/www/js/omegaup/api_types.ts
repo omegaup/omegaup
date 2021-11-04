78bf102b3017fa68140e427e587d8e8f6a598ce2
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-11-04 14:41:13 -0600

    Migrando a plantilla unificada la página de VerifyEmail

--- frontend/www/js/omegaup/api_types.ts
+++ frontend/www/js/omegaup/api_types.ts
@@ -3170,9 +3170,10 @@ export namespace types {
   }
 
   export interface LoginDetailsPayload {
-    facebookUrl: string;
+    facebookUrl?: string;
     statusError?: string;
     validateRecaptcha: boolean;
+    verifyEmailSuccessfully?: string;
   }
 
   export interface MergedScoreboardEntry {
