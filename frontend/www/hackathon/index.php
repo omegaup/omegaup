1597e97a3e57b3fef1477c804d98af2b4148fdd1
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-11-04 20:02:46 -0600

    Eliminando la página de hackathon

--- frontend/www/hackathon/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once('../../server/bootstrap_smarty.php');
-if (OMEGAUP_LOCKDOWN) {
-    header('Location: /arena/');
-    die();
-}
-
-    $smarty->display('../templates/hackathon.tpl');
