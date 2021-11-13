6476178b6dd7fa079c241996b1a0653c9fca81a1
Carlos Córdova <carlosab1802@gmail.com>
2021-11-12 19:21:25 -0500

    Eliminando el viejo paginador.

---
+++ frontend/www/js/omegaup/components/common/Paginator.test.ts
@@ -0,0 +1,37 @@
+import { types } from '../../api_types';
+import { shallowMount } from '@vue/test-utils';
+
+import T from '../../lang';
+
+import common_Paginator from './Paginator.vue';
+
+describe('Paginator.vue', () => {
+  const pagerItems: types.PageItem[] = [
+    {
+      class: 'disabled',
+      label: '«',
+      page: 0,
+    },
+    {
+      class: 'disabled',
+      label: '1',
+      page: 1,
+    },
+    {
+      class: 'disabled',
+      label: '»',
+      page: 2,
+    },
+  ];
+  it('Should handle pager items', async () => {
+    const wrapper = shallowMount(common_Paginator, {
+      propsData: {
+        pagerItems,
+      },
+    });
+
+    expect(wrapper.text()).toContain(pagerItems[0].label);
+    expect(wrapper.text()).toContain(pagerItems[1].label);
+    expect(wrapper.text()).toContain(pagerItems[2].label);
+  });
+});
