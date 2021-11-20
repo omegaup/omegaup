ba3eb7bbe390eeabe368a48b558aff2795a50fd4
Mau MD <j.mauricio.munoz1@gmail.com>
2021-11-20 12:17:10 -0800

    Agregando BS4

--- frontend/www/js/omegaup/problem/creator/creator.ts
+++ frontend/www/js/omegaup/problem/creator/creator.ts
@@ -1,8 +1,15 @@
 import problem_creator from '../../components/problem/creator/Creator.vue';
 import { OmegaUp } from '../../omegaup';
 import Vue from 'vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 OmegaUp.on('ready', () => {
+  Vue.use(BootstrapVue);
+  Vue.use(BootstrapVueIcons);
+
   new Vue({
     el: '#main-container',
     components: {
