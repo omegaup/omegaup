c70782f22cf1ac90bf3f573d223c3b9ab0d88d22
Mau MD <j.mauricio.munoz1@gmail.com>
2021-11-20 12:42:22 -0800

    Agregado traducciones a los componentes

--- frontend/www/js/omegaup/components/problem/creator/Tabs.vue
+++ frontend/www/js/omegaup/components/problem/creator/Tabs.vue
@@ -3,19 +3,25 @@
     <b-tab>
       <template #title>
         <BIconPencil class="mr-1" />
-        <span> Redacción </span>
+        <span> {{ T.problemCreatorWriting }}</span>
       </template>
     </b-tab>
     <b-tab>
       <template #title>
         <BIconFileCode class="mr-1" />
-        <span> Solución </span>
+        <span> {{ T.problemCreatorCode }}</span>
       </template>
     </b-tab>
     <b-tab>
       <template #title>
         <BIconCheckCircle class="mr-1" />
-        <span> Casos de prueba </span>
+        <span> {{ T.problemCreatorTestCases }}</span>
+      </template>
+    </b-tab>
+    <b-tab>
+      <template #title>
+        <BIconArrowUpRight class="mr-1" />
+        <span> {{ T.problemCreatorSolution }}</span>
       </template>
     </b-tab>
   </b-tabs>
@@ -23,6 +29,9 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import T from '../../../lang';
 @Component({})
-export default class Tabs extends Vue {}
+export default class Tabs extends Vue {
+  T = T;
+}
 </script>
