a5a08619aed8013d276541563fb4e98d63c6c78d
Mau MD <j.mauricio.munoz1@gmail.com>
2021-11-20 12:16:34 -0800

    Agregando Header y Tabs

--- frontend/www/js/omegaup/components/problem/creator/Creator.vue
+++ frontend/www/js/omegaup/components/problem/creator/Creator.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>Creator Path Test</div>
+  <b-container fluid="lg">
+    <Header />
+    <Tabs />
+  </b-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-@Component({})
+import Header from './Header.vue';
+import Tabs from './Tabs.vue';
+@Component({
+  components: {
+    Header,
+    Tabs,
+  },
+})
 export default class Creator extends Vue {}
 </script>
---
+++ frontend/www/js/omegaup/components/problem/creator/Tabs.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-tabs small>
+    <b-tab>
+      <template #title>
+        <BIconPencil class="mr-1" />
+        <span> Redacción </span>
+      </template>
+    </b-tab>
+    <b-tab>
+      <template #title>
+        <BIconFileCode class="mr-1" />
+        <span> Solución </span>
+      </template>
+    </b-tab>
+    <b-tab>
+      <template #title>
+        <BIconCheckCircle class="mr-1" />
+        <span> Casos de prueba </span>
+      </template>
+    </b-tab>
+  </b-tabs>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component({})
+export default class Tabs extends Vue {}
+</script>
