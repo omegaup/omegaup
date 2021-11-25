055d9fc647bbdeae367a2db04b82ea950099e385
Carlos Córdova <carlosab1802@gmail.com>
2021-11-24 23:16:03 -0500

    Agregando los archivos para la nueva arena de cursos.

---
+++ frontend/www/js/omegaup/components/arena/Coursev2.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import T from '../../lang';
+
+@Component
+export default class ArenaCourse extends Vue {
+  T = T;
+}
+</script>
