a00566610ff1d57d9accacbedc0108d5eb09a7ce
Juan Pablo Gómez <gomez_aguilar_juan_pablo@hotmail.com>
2021-11-30 18:34:15 -0600

    Aplicando correcciones

--- stuff/pipelines/client_contest.py
+++ stuff/pipelines/client_contest.py
@@ -7,10 +7,13 @@ import logging
 import os
 import sys
 import json
+from typing import Any, List, Optional, Tuple
 import MySQLdb
 import MySQLdb.cursors
 import pika
 from verification_code import generate_code
+from rabbit_connection import Rabbit
+import rabbit_connection
 
 sys.path.insert(
     0,
@@ -20,31 +23,25 @@ import lib.db   # pylint: disable=wrong-import-position
 import lib.logs  # pylint: disable=wrong-import-position
 
 
-def receive_contest_messages(
+def certificate_contests_receive_messages(
         cur: MySQLdb.cursors.BaseCursor,
         dbconn: MySQLdb.connections.Connection,
-        rabbit_user: str,
-        rabbit_password: str) -> None:
+        channel: pika.adapters.blocking_connection.BlockingChannel) -> None:
     '''Receive contest messages'''
-
-    credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
-    parameters = pika.ConnectionParameters('rabbitmq', 5672, '/', credentials)
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.exchange_declare(exchange='logs_exchange', exchange_type='direct')
     result = channel.queue_declare(queue='', exclusive=True)
     queue_name = result.method.queue
     assert queue_name is not None
     channel.queue_bind(
-        exchange='logs_exchange',
+        exchange='certificates',
         queue=queue_name,
         routing_key="ContestQueue")
     logging.info('[*] waiting for the messages')
 
-    def callback(_channel: pika.adapters.blocking_connection.BlockingChannel,
-                 _method: pika.spec.Basic.Deliver,
-                 _properties: pika.spec.BasicProperties,
-                 body: bytes) -> None:
+    def certificate_contests_callback(
+            _channel: pika.adapters.blocking_connection.BlockingChannel,
+            _method: pika.spec.Basic.Deliver,
+            _properties: pika.spec.BasicProperties,
+            body: bytes) -> None:
         data = json.loads(body.decode())
         cur.execute('''
                 SELECT
@@ -103,19 +100,19 @@ def receive_contest_messages(
                 WHERE
                     certificate_id IS NULL;
                 ''', (data['contest_id'],))
-        certificates = []
+        certificates: List[Tuple[Any, str, Any, str, Optional[Any]]] = []
         for row in cur:
             contest_place = None
-            if row['certificate_cutoff'] is None:
+            if data['certificate_cutoff'] is None:
                 logging.info('The contest has no places')
-            elif row['place'] > row['certificate_cutoff']:
+            elif row['place'] > data['certificate_cutoff']:
                 logging.info('The user did not reach the place to be reported')
             else:
                 contest_place = row['place']
-            code_verification = generate_code()
+            verification_code = generate_code()
             certificates.append((
                 row['identity_id'], 'contest', data['contest_id'],
-                code_verification, contest_place
+                verification_code, contest_place
             ))
         cur.executemany('''
             INSERT INTO
@@ -130,9 +127,12 @@ def receive_contest_messages(
         dbconn.commit()
     channel.basic_consume(
         queue=queue_name,
-        on_message_callback=callback,
-        auto_ack=True)
-    channel.start_consuming()
+        on_message_callback=certificate_contests_callback,
+        auto_ack=False)
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.stop_consuming()
 
 
 def main() -> None:
@@ -140,19 +140,21 @@ def main() -> None:
     parser = argparse.ArgumentParser(description=__doc__)
     lib.db.configure_parser(parser)
     lib.logs.configure_parser(parser)
-    parser.add_argument('--rabbitmq-user')
-    parser.add_argument('--rabbitmq-password')
+
+    rabbit_connection.configure_parser(parser)
+
     args = parser.parse_args()
     lib.logs.init(parser.prog, args)
     logging.info('Started')
     dbconn = lib.db.connect(args)
+    rabbit_conn = Rabbit(args)
     try:
         with dbconn.cursor(cursorclass=MySQLdb.cursors.DictCursor) as cur:
-            receive_contest_messages(
-                cur, dbconn, args.rabbitmq_user,
-                args.rabbitmq_password)
+            certificate_contests_receive_messages(cur, dbconn,
+                                                  rabbit_conn.channel)
     finally:
         dbconn.close()
+        rabbit_conn.close()
         logging.info('Done')
 
 
