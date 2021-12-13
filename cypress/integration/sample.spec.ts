d655bb398b113c84f745cac4f3f5642e6f7ee307
Mau MD <j.mauricio.munoz1@gmail.com>
2021-12-13 12:37:33 -0800

    agregando file-upload y wait-for a cypress

--- cypress/integration/sample.spec.ts
+++ cypress/integration/sample.spec.ts
@@ -3,13 +3,37 @@ describe('Test', () => {
     cy.clearCookies();
     cy.clearLocalStorage();
   });
-  it('Check if contests exist', () => {
-    cy.visit('/');
-    cy.get('[data-nav-problems]').click();
-    cy.get('[data-nav-problems-collection]').click();
-    cy.get('[data-nav-problems-all]').click();
-    cy.get('[href="/arena/problem/karel-helloworld/"]').contains(
-      'Karel Hello World',
+
+  it('Should create a new problem (Cypress Studio Test)', function () {
+    cy.login('user', 'user');
+    cy.waitUntil(() =>
+      cy.get('.active > .container-lg > .slide > :nth-child(1) > h2'),
     );
+    cy.get('.nav-problems > .nav-link').click();
+    cy.get('.dropdown-menu > [href="/problem/new/"]').click();
+    cy.get(
+      ':nth-child(1) > .collapse > :nth-child(1) > :nth-child(1) > .form-control',
+    ).clear();
+    cy.get(
+      ':nth-child(1) > .collapse > :nth-child(1) > :nth-child(1) > .form-control',
+    ).type('name');
+    cy.get(
+      ':nth-child(1) > .collapse > :nth-child(1) > :nth-child(2) > .form-control',
+    ).click();
+    cy.get('.collapse > :nth-child(2) > :nth-child(1) > .form-control').clear();
+    cy.get('.collapse > :nth-child(2) > :nth-child(1) > .form-control').type(
+      'name',
+    );
+    cy.get('input[type="file"]').attachFile(
+      '../../frontend/tests/resources/testproblem.zip',
+    );
+    cy.get(':nth-child(2) > .card-header > .mb-0 > .btn').click();
+    cy.get('[required="required"] > .input-group > .form-control').clear();
+    cy.get('[required="required"] > .input-group > .form-control').type('rec');
+    cy.get('.list-group > :nth-child(1) > :nth-child(1)').click();
+    cy.get(
+      '.tags > .card > .card-body > .row > .form-group > .form-control',
+    ).select('problemLevelBasicIntroductionToProgramming');
+    cy.get('.form-group > .btn').click();
   });
 });
