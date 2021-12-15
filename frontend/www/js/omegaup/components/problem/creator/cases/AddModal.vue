5dbf3192f8b618ddc250ff8f6cd67db35d28a0f4
Mau MD <j.mauricio.munoz1@gmail.com>
2021-12-15 12:39:06 -0800

    agregando modal

---
+++ frontend/www/js/omegaup/components/problem/creator/cases/AddModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-modal
+    ref="addModal"
+    :title="T.problemCreatorAdd"
+    :ok-title="T.problemCreatorAdd"
+    :cancel-title="T.wordsCancel"
+    hide-header-close
+  >
+    <b-tabs v-model="tabIndex" small pills>
+      <b-tab :title="T.problemCreatorCase" name="modal-form"></b-tab>
+      <b-tab :title="T.problemCreatorMulticase" name="modal-form"> </b-tab>
+      <b-tab :title="T.problemCreatorGroup" name="modal-form"> </b-tab>
+    </b-tabs>
+  </b-modal>
+</template>
+
+<script lang="ts">
+import { BModal } from 'bootstrap-vue';
+import { Component, Ref, Vue } from 'vue-property-decorator';
+import T from '../../../../lang';
+
+@Component
+export default class AddModal extends Vue {
+  @Ref('addModal') addCaseModalRef!: BModal;
+
+  tabIndex = 0;
+
+  T = T;
+
+  openModal() {
+    this.addCaseModalRef.show();
+  }
+}
+</script>
--- frontend/www/js/omegaup/components/problem/creator/cases/Sidebar.vue
+++ frontend/www/js/omegaup/components/problem/creator/cases/Sidebar.vue
@@ -6,7 +6,7 @@
         <b-button size="sm" variant="primary" class="mr-2">
           <BIconLayoutSidebar />
         </b-button>
-        <b-button size="sm" variant="success" class="mr-2">
+        <b-button size="sm" variant="success" class="mr-2" @click="openModal">
           <span class="d-none d-xl-inline">{{ T.problemCreatorAdd }}</span>
           <BIconPlusCircle class="d-inline d-xl-none" />
         </b-button>
@@ -14,15 +14,26 @@
           <BIconThreeDotsVertical />
         </b-button>
       </div>
+      <!-- Modals -->
+      <problem-creator-add-modal ref="add-case-modal" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Ref, Vue } from 'vue-property-decorator';
+import problemCreatorAddModal from './AddModal.vue';
 import T from '../../../../lang';
-@Component
+
+@Component({
+  components: { 'problem-creator-add-modal': problemCreatorAddModal },
+})
 export default class Sidebar extends Vue {
   T = T;
+  @Ref('add-case-modal') addCaseModalRef!: problemCreatorAddModal;
+
+  openModal() {
+    this.addCaseModalRef.openModal();
+  }
 }
 </script>
