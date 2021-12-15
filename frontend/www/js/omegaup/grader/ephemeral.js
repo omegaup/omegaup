4024fe7be5974ad7c10c6744de79b9e3a988695d
lhchavez <lhchavez@lhchavez.com>
2021-12-15 05:43:50 -0800

    Update frontend/www/js/omegaup/grader/ephemeral.js

--- frontend/www/js/omegaup/grader/ephemeral.js
+++ frontend/www/js/omegaup/grader/ephemeral.js
@@ -356,11 +356,13 @@ let store = new Vuex.Store({
     },
     showSubmitButton(state, value) {
       state.problemsetId = value;
+      const submitButton = document.querySelector(
+        'button[data-submit-button]',
+      );
       if (value) {
-        const submitButton = document.querySelector(
-          'button[data-submit-button]',
-        );
         submitButton.classList.remove('d-none');
+      } else {
+        submitButton.classList.add('d-none');
       }
     },
     currentCase(state, value) {
