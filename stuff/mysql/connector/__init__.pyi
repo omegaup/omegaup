da74d42da97b09ed5f8edf10c4e62231888ea732
lhchavez <lhchavez@lhchavez.com>
2021-12-19 18:10:37 -0800

    Dejando de usar MySQLdb en Python (#6177)
    Ahora vamos a empezar a usar mysql.connector, que tiene mucho mejor
    soporte y sí está siendo actualizada.

---
+++ stuff/mysql/connector/__init__.pyi
@@ -0,0 +1,59 @@
+from typing import overload, Literal, Text
+
+from . import cursor
+
+
+def connect(
+    user: Text,
+    password: Text,
+    database: Text,
+    host: Text,
+    port: int = 3306,
+) -> MySQLConnection:
+    ...
+
+
+class MySQLConnection:
+    get_warnings: bool
+
+    def close(self) -> None:
+        ...
+
+    @overload
+    def cursor(self, *, buffered: Literal[True],
+               dictionary: Literal[True]) -> cursor.MySQLCursorBufferedDict:
+        ...
+
+    @overload
+    def cursor(self,
+               *,
+               buffered: Literal[False] = ...,
+               dictionary: Literal[True]) -> cursor.MySQLCursorDict:
+        ...
+
+    @overload
+    def cursor(self,
+               *,
+               buffered: Literal[True],
+               dictionary: Literal[False] = ...) -> cursor.MySQLCursorBuffered:
+        ...
+
+    @overload
+    def cursor(self,
+               *,
+               buffered: Literal[False] = ...,
+               dictionary: Literal[False] = ...) -> cursor.MySQLCursor:
+        ...
+
+    @overload
+    def cursor(self,
+               *,
+               buffered: bool = False,
+               dictionary: bool = False) -> cursor.BaseCursor:
+        ...
+
+    def commit(self) -> None:
+        ...
+
+    def rollback(self) -> None:
+        ...
