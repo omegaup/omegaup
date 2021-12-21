097321dfaca1f147c167399688d3b9cc4e6b8902
Mau MD <j.mauricio.munoz1@gmail.com>
2021-12-21 12:15:00 -0800

    Cambiando login y register para que usen la API

--- cypress/integration/basic_commands.spec.ts
+++ cypress/integration/basic_commands.spec.ts
@@ -6,13 +6,41 @@ describe('Basic Commands Test', () => {
     cy.clearLocalStorage();
   });
 
+  it('Should register an user using the API', () => {
+    cy.visit('/');
+    const username = uuid();
+    const password = uuid();
+    cy.register({ username, password, shouldLogin: true });
+  });
+
   it('Should register an user', () => {
     const username = uuid();
     const password = uuid();
-    cy.register({ username, password });
+    cy.visit('/');
+    cy.get('[data-login-btn]').click();
+    cy.get('[data-signup-username]').type(username);
+    cy.get('[data-signup-password]').type(password);
+    cy.get('[data-signup-repeat-password]').type(password);
+    cy.get('[data-signup-email]').type(`${username}@omegaup.com`);
+    cy.get('[data-signup-submit]').click();
+    cy.waitUntil(() => cy.get('.username').should('have.text', username));
+  });
+
+  it('Should login an user using the API', () => {
+    cy.visit('/');
+    const username = 'user';
+    const password = 'user';
+    cy.login({ username, password });
   });
 
   it('Should login an user', () => {
-    cy.login({ username: 'omegaup', password: 'omegaup' });
+    const username = 'user';
+    const password = 'user';
+    cy.visit('/');
+    cy.get('[data-login-btn]').click();
+    cy.get('[data-login-username]').type(username);
+    cy.get('[data-login-password]').type(password);
+    cy.get('[data-login-submit]').click();
+    cy.waitUntil(() => cy.get('.username').should('have.text', username));
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,29 @@
 // https://on.cypress.io/custom-commands
 import 'cypress-wait-until';
 import 'cypress-file-upload';
-import 'cypress-xpath';
-import { v4 as uuid } from 'uuid';
+
+import { expect } from 'chai';
 
 // Logins the user given a username and password
 Cypress.Commands.add('login', ({ username, password }) => {
-  cy.visit('/');
-  cy.get('[data-login-btn]').click();
-  cy.get('[data-login-username]').type(username);
-  cy.get('[data-login-password]').type(password);
-  cy.get('[data-login-submit]').click();
-  cy.waitUntil(() =>
-    cy
-      .xpath('//*[@id="__BVID__11___BV_modal_header_"]/button')
-      .should('be.visible')
-      .click(),
-  );
+  cy.request(
+    `/api/user/login?usernameOrEmail=${username}&password=${password}`,
+  ).then((response) => {
+    expect(response.status).to.eq(200);
+    cy.reload();
+    cy.get('.username').should('have.text', username);
+  });
 });
 
-// Register a new user given a username and password. If they are not specified, a random one is generated and returned
-Cypress.Commands.add('register', ({ username, password }) => {
-  cy.visit('/');
-  cy.get('[data-login-btn]').click();
-  cy.get('[data-signup-username]').type(username);
-  cy.get('[data-signup-password]').type(password);
-  cy.get('[data-signup-repeat-password]').type(password);
-  cy.get('[data-signup-email]').type(`${username}@omegaup.com`);
-  cy.get('[data-signup-submit]').click();
-  cy.waitUntil(() =>
-    cy
-      .xpath('//*[@id="__BVID__11___BV_modal_header_"]/button')
-      .should('be.visible')
-      .click(),
-  );
-});
+// Register a new user given a username and password. If shouldLogin is true, then the user will be logged in after the registration
+Cypress.Commands.add(
+  'register',
+  ({ username, password, shouldLogin = false }) => {
+    cy.request(
+      `/api/user/create?username=${username}&password=${password}&email=${username}@omegaup.com`,
+    ).then((response) => {
+      expect(response.status).to.eq(200);
+      if (shouldLogin) cy.login({ username, password });
+    });
+  },
+);
