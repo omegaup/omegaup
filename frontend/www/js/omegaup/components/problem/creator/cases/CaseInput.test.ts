4cb590a1c5a84625b4fec2698f743207973812f7
Mauricio Muñoz <j.mauricio.munoz1@gmail.com>
2021-12-29 08:45:16 -0800

    Agregando formulario para poder subir casos dentro del creador (#6198)
    Se agrego el formulario de casos, dentro del panel de `Agregar`. No contiene ningun tipo de logica, solo los elementos necesarios. 
    
    Tambien se agregaron las traducciones y pruebas correspondientes
     
    Fixes: #6197
    
    ![image](https://user-images.githubusercontent.com/74751751/147172822-1c846918-414f-47a5-ac39-f84ec549edbc.png)

---
+++ frontend/www/js/omegaup/components/problem/creator/cases/CaseInput.test.ts
@@ -0,0 +1,35 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+
+import CaseInput from './CaseInput.vue';
+import BootstrapVue, { IconsPlugin } from 'bootstrap-vue';
+import T from '../../../../lang';
+import Vue from 'vue';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+localVue.use(IconsPlugin);
+
+describe('AddPanel.vue', () => {
+  it('Should contain all 4 inputs', async () => {
+    const wrapper = shallowMount(CaseInput, {
+      localVue,
+    });
+
+    const expectedTextInputText = [
+      T.problemCreatorCaseName,
+      T.problemCreatorGroupName,
+      T.problemCreatorPoints,
+      T.problemCreatorAutomaticPoints,
+    ];
+
+    await Vue.nextTick();
+
+    const inputElements = wrapper.findAll('[label]');
+
+    expect(inputElements.length).toBe(expectedTextInputText.length);
+
+    inputElements.wrappers.forEach((element, index) => {
+      expect(element.attributes('label')).toBe(expectedTextInputText[index]); // We need to make it like this because that's how Vue-Bootstrap input element works
+    });
+  });
+});
---
+++ frontend/www/js/omegaup/components/problem/creator/cases/CaseInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mt-3">
+    <b-form-group
+      :description="T.problemCreatorCaseGroupNameHelper"
+      :label="T.problemCreatorCaseName"
+      label-for="case-name"
+      class="mb-4"
+    >
+      <b-form-input v-model="caseName" required autocomplete="off" />
+    </b-form-group>
+    <b-form-group :label="T.problemCreatorGroupName" label-for="case-group">
+      <b-form-select v-model="caseGroup" :options="options" />
+    </b-form-group>
+
+    <b-form-group
+      v-show="casePoints !== null"
+      :label="T.problemCreatorPoints"
+      label-for="case-points"
+    >
+      <b-form-input
+        v-model="casePoints"
+        type="number"
+        number
+        min="0"
+        max="100"
+      />
+    </b-form-group>
+    <b-form-group
+      :label="T.problemCreatorAutomaticPoints"
+      :description="T.problemCreatorAutomaticPointsHelper"
+    >
+      <b-form-checkbox
+        :checked="!casePoints"
+        name="auto-points"
+        @change="casePoints = casePoints === null ? 0 : null"
+      >
+      </b-form-checkbox>
+    </b-form-group>
+  </div>
+</template>
+
+<script lang="ts">
+import { NIL } from 'uuid';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import T from '../../../../lang';
+
+@Component
+export default class CaseInput extends Vue {
+  @Prop({ default: '' }) name!: string;
+  @Prop({ default: NIL }) group!: string;
+  @Prop({ default: null }) points!: number | null;
+
+  caseName = this.name;
+  caseGroup = this.group;
+  casePoints: number | null = this.points;
+
+  T = T;
+}
+</script>
