4639edec7e6aa7e0763962032c310a88a89d85e1
Mau MD <j.mauricio.munoz1@gmail.com>
2021-12-29 15:01:16 -0800

    Escapando parameters

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,12 +1,13 @@
 // https://on.cypress.io/custom-commands
 import 'cypress-wait-until';
 import 'cypress-file-upload';
+import { buildURLQuery } from '../../frontend/www/js/omegaup/ui';
 
 // Logins the user given a username and password
 Cypress.Commands.add('login', ({ username, password }) => {
-  cy.request(
-    `/api/user/login?usernameOrEmail=${username}&password=${password}`,
-  ).then((response) => {
+  const URL =
+    '/api/user/login?' + buildURLQuery({ usernameOrEmail: username, password });
+  cy.request(URL).then((response) => {
     expect(response.status).to.equal(200);
     cy.reload();
   });
@@ -14,9 +15,10 @@ Cypress.Commands.add('login', ({ username, password }) => {
 
 // Registers and logs in a new user given a username and password.
 Cypress.Commands.add('register', ({ username, password }) => {
-  cy.request(
-    `/api/user/create?username=${username}&password=${password}&email=${username}@omegaup.com`,
-  ).then((response) => {
+  const URL =
+    '/api/user/create?' +
+    buildURLQuery({ username, password, email: username + '@omegaup.com' });
+  cy.request(URL).then((response) => {
     expect(response.status).to.equal(200);
     cy.login({ username, password });
   });
