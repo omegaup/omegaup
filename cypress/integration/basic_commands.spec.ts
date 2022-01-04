37ea5293ed57ecb73b955b77800e1091f613c278
Mau MD <j.mauricio.munoz1@gmail.com>
2022-01-04 11:13:06 -0800

    Haciendo createProblem realmente un comando y no una prueba

--- cypress/integration/basic_commands.spec.ts
+++ cypress/integration/basic_commands.spec.ts
@@ -1,3 +1,4 @@
+import { copyToClipboard } from '@/js/omegaup/ui';
 import { v4 as uuid } from 'uuid';
 
 describe('Basic Commands Test', () => {
@@ -56,35 +57,11 @@ describe('Basic Commands Test', () => {
     const problemLevelIndex = 0;
 
     cy.register({ username, password });
-    // Select problem nav
-    cy.get('[data-nav-problems]').click();
-    cy.get('[data-nav-problems-create]').click();
-    // Fill basic problem form
-    cy.get('[name="title"]').type(problemAlias).blur();
-
-    // Alias should be the same as title.
-    cy.get('[name="problem_alias"]').should('have.value', problemAlias);
-
-    cy.get('[name="source"]').type(problemAlias);
-    cy.get('[name="problem_contents"]').attachFile('testproblem.zip');
-    cy.get('[data-tags-input]').type(autoCompleteTextTag);
-
-    // Tags panel
-    cy.waitUntil(() =>
-      cy
-        .get('[data-tags-input] .vbt-autcomplete-list a.vbst-item:first')
-        .should('have.text', tag) // Maybe theres another way to avoid to hardcode this
-        .click(),
-    );
-
-    cy.get('[name="problem-level"]').select(problemLevelIndex); // How can we assert this with the real text?
-
-    cy.get('button[type="submit"]').click(); // Submit
-
-    // Assert problem has been created
-    cy.location('href').should('include', problemAlias); // Url
-    cy.get('[name="title"]').should('have.value', problemAlias); // Title
-    cy.get('[name="problem_alias"]').should('have.value', problemAlias);
-    cy.get('[name="source"]').should('have.value', problemAlias);
+    cy.createProblem({
+      problemAlias,
+      tag,
+      autoCompleteTextTag,
+      problemLevelIndex,
+    });
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,3 +23,40 @@ Cypress.Commands.add('register', ({ username, password }) => {
     cy.login({ username, password });
   });
 });
+
+Cypress.Commands.add(
+  'createProblem',
+  ({ problemAlias, tag, autoCompleteTextTag, problemLevelIndex }) => {
+    cy.visit('/');
+    // Select problem nav
+    cy.get('[data-nav-problems]').click();
+    cy.get('[data-nav-problems-create]').click();
+    // Fill basic problem form
+    cy.get('[name="title"]').type(problemAlias).blur();
+
+    // Alias should be the same as title.
+    cy.get('[name="problem_alias"]').should('have.value', problemAlias);
+
+    cy.get('[name="source"]').type(problemAlias);
+    cy.get('[name="problem_contents"]').attachFile('testproblem.zip');
+    cy.get('[data-tags-input]').type(autoCompleteTextTag);
+
+    // Tags panel
+    cy.waitUntil(() =>
+      cy
+        .get('[data-tags-input] .vbt-autcomplete-list a.vbst-item:first')
+        .should('have.text', tag) // Maybe theres another way to avoid to hardcode this
+        .click(),
+    );
+
+    cy.get('[name="problem-level"]').select(problemLevelIndex); // How can we assert this with the real text?
+
+    cy.get('button[type="submit"]').click(); // Submit
+
+    // Assert problem has been created
+    cy.location('href').should('include', problemAlias); // Url
+    cy.get('[name="title"]').should('have.value', problemAlias); // Title
+    cy.get('[name="problem_alias"]').should('have.value', problemAlias);
+    cy.get('[name="source"]').should('have.value', problemAlias);
+  },
+);
