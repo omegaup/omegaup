7ec873bab2e6c38daf97a9962775b3efd762e2f9
Carlos Córdova <carlosab1802@gmail.com>
2022-01-06 09:08:35 -0500

    Agregando más pruebas.

--- frontend/www/js/omegaup/components/arena/Runsv2.test.ts
+++ frontend/www/js/omegaup/components/arena/Runsv2.test.ts
@@ -57,7 +57,22 @@ describe('Runsv2.vue', () => {
     },
   ];
 
-  it('Should handle runs', () => {
+  it('Should handle empty runs', () => {
+    const wrapper = mount(arena_Runs, {
+      propsData: {
+        runs: [] as types.Run[],
+        problemAlias: 'test-problem-1',
+      },
+      localVue,
+    });
+
+    expect(wrapper.find('h5').text()).toBe(T.wordsSubmissions);
+    const tableComponent = wrapper.findComponent(BTable);
+    expect(tableComponent.exists()).toBe(true);
+    expect(tableComponent.findAll('table tbody tr').length).toBe(0);
+  });
+
+  it('Should handle AC runs', () => {
     const wrapper = mount(arena_Runs, {
       propsData: {
         runs,
@@ -66,10 +81,7 @@ describe('Runsv2.vue', () => {
       localVue,
     });
 
-    expect(wrapper.find('h5').text()).toBe(T.wordsSubmissions);
-
     const tableComponent = wrapper.findComponent(BTable);
-    expect(tableComponent.exists()).toBe(true);
     expect(tableComponent.findAll('table tbody tr').length).toBe(runs.length);
     expect(tableComponent.find('td.table-success').exists()).toBe(true);
     for (const run of runs) {
@@ -77,6 +89,81 @@ describe('Runsv2.vue', () => {
       expect(tableComponent.text()).toContain(
         `${(run.score * 100).toFixed(2)}%`,
       );
+      expect(tableComponent.text()).toContain(
+        `${(run.memory / (1024 * 1024)).toFixed(2)} MB`,
+      );
+      expect(tableComponent.text()).toContain(
+        `${(run.runtime / 1000).toFixed(2)} s`,
+      );
     }
+  });
+
+  it('Should handle MLE and TLE runs', () => {
+    const runs = [
+      {
+        ...baseRunData,
+        guid: '122000',
+        verdict: 'MLE',
+        time: new Date('1/1/2020, 12:00:00 AM'),
+      },
+      {
+        ...baseRunData,
+        guid: '122001',
+        verdict: 'TLE',
+        time: new Date('1/1/2020, 12:05:00 AM'),
+      },
+    ];
+    const wrapper = mount(arena_Runs, {
+      propsData: {
+        runs,
+        problemAlias: 'test-problem-1',
+      },
+      localVue,
+    });
+
+    const tableComponent = wrapper.findComponent(BTable);
+    expect(tableComponent.findAll('table tbody tr').length).toBe(runs.length);
+    expect(tableComponent.find('td.table-success').exists()).toBe(false);
+
+    // MLE run
+    expect(tableComponent.text()).toContain(runs[0].guid);
+    expect(tableComponent.text()).toContain(
+      `>${(runs[0].memory / (1024 * 1024)).toFixed(2)} MB`,
+    );
+
+    // TLE run
+    expect(tableComponent.text()).toContain(runs[1].guid);
+    expect(tableComponent.text()).toContain(
+      `>${(runs[1].runtime / 1000).toFixed(2)} s`,
+    );
+  });
+
+  it('Should handle JE and CE runs', () => {
+    const runs = [
+      {
+        ...baseRunData,
+        guid: '122000',
+        verdict: 'JE',
+        time: new Date('1/1/2020, 12:00:00 AM'),
+      },
+      {
+        ...baseRunData,
+        guid: '122001',
+        verdict: 'CE',
+        time: new Date('1/1/2020, 12:05:00 AM'),
+      },
+    ];
+    const wrapper = mount(arena_Runs, {
+      propsData: {
+        runs,
+        problemAlias: 'test-problem-1',
+      },
+      localVue,
+    });
+
+    const tableComponent = wrapper.findComponent(BTable);
+    expect(tableComponent.findAll('table tbody tr').length).toBe(runs.length);
+    expect(tableComponent.find('td.table-warning').exists()).toBe(true);
+    expect(tableComponent.find('td.table-danger').exists()).toBe(true);
   });
 });
