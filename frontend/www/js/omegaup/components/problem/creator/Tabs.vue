c7cfb68c2e23435d87086c500cb7292888f322fd
Mau MD <j.mauricio.munoz1@gmail.com>
2022-01-10 14:35:12 -0800

    Arreglando otro test

--- frontend/www/js/omegaup/components/problem/creator/Tabs.vue
+++ frontend/www/js/omegaup/components/problem/creator/Tabs.vue
@@ -3,26 +3,26 @@
     <b-tab>
       <template #title>
         <BIconPencil class="mr-1" />
-        <span name="writing"> {{ T.problemCreatorStatement }}</span>
+        <span name="writing" data-tab> {{ T.problemCreatorStatement }}</span>
       </template>
     </b-tab>
     <b-tab>
       <template #title>
         <BIconFileCode class="mr-1" />
-        <span name="code"> {{ T.problemCreatorCode }}</span>
+        <span name="code" data-tab> {{ T.problemCreatorCode }}</span>
       </template>
     </b-tab>
     <b-tab>
       <template #title>
         <BIconCheckCircle class="mr-1" />
-        <span name="testcases"> {{ T.problemCreatorTestCases }}</span>
+        <span name="testcases" data-tab> {{ T.problemCreatorTestCases }}</span>
       </template>
       <cases-tab />
     </b-tab>
     <b-tab>
       <template #title>
         <BIconArrowUpRight class="mr-1" />
-        <span name="solution"> {{ T.problemCreatorSolution }}</span>
+        <span name="solution" data-tab> {{ T.problemCreatorSolution }}</span>
       </template>
     </b-tab>
   </b-tabs>
