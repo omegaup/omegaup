514f39a9b74f587302b2eb4abc02a36b0279962b
Mauricio Muñoz <j.mauricio.munoz1@gmail.com>
2022-01-16 11:00:21 -0800

    Agregando comando para hacer un run dentro de Cypress (#6300)
    Se agregó el comando para hacer un run dentro de cypress, también se agregaron múltiples tipos de apoyo.
    
    Part of: #6149

--- cypress/support/types.d.ts
+++ cypress/support/types.d.ts
@@ -24,6 +24,27 @@ export interface CourseOptions {
   description?: string;
 }
 
-export type RequestParticipantInformation = 'no' | 'optional' | 'required';
+export interface RunOptions {
+  problemAlias: string;
+  fixturePath: string;
+  language: Language;
+}
 
+export type RequestParticipantInformation = 'no' | 'optional' | 'required';
 export type ProblemLevel = 'introductory' | 'intermediate' | 'advanced';
+export type Language =
+  | 'c11-gcc'
+  | 'c11-clang'
+  | 'cpp11-gcc'
+  | 'cpp11-clang'
+  | 'cpp17-gcc'
+  | 'cpp17-clang'
+  | 'java'
+  | 'py2'
+  | 'py3'
+  | 'rb'
+  | 'cs'
+  | 'pas'
+  | 'hs'
+  | 'lua';
+export type Status = 'AC' | 'TLE' | 'MLE' | 'PA';
