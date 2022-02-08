f034065a598acf60a4ceb93206c687e3b67a6548
egessel <egessel100@gmail.com>
2022-02-07 23:28:19 -0600

    Implementation of rabbitmq producer

---
+++ stuff/pipelines/rabbitmq_producer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''Implementation of rabbitmq producer.'''
+
+import pika
+
+
+def send_message(
+    queue: str, exchange: str, routing_key: str,
+    channel: pika.adapters.blocking_connection.BlockingChannel,
+    message: str) -> None:
+    '''Send message to any queue'''
+    channel.queue_declare(queue, passive=False,
+                          durable=False, exclusive=False,
+                          auto_delete=False)
+    channel.exchange_declare(exchange=exchange,
+                             auto_delete=False,
+                             durable=True,
+                             exchange_type='direct')
+    body = message.encode()
+    channel.basic_publish(exchange=exchange,
+                          routing_key=routing_key,
+                          body=body)
