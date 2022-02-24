f6f50963d0939cbbcb5c02196344c23ea39d3626
Pablo Aguilar <pablo.aguilar@gmail.com>
2022-02-24 00:47:23 -0800

    Arreglando mas errores de lint

--- frontend/tests/GitServerTestSuiteListener.php
+++ frontend/tests/GitServerTestSuiteListener.php
@@ -5,61 +5,60 @@ namespace OmegaUp\Test;
 class GitServerTestSuiteListener implements \PHPUnit\Framework\TestListener {
     public function addWarning(
         \PHPUnit\Framework\Test $test,
-        \Throwable $e,
-        $time
+        \PHPUnit\Framework\Warning $e,
+        float $time
     ): void {
     }
 
     public function addError(
         \PHPUnit\Framework\Test $test,
-        \Throwable $e,
-        $time
+        \Throwable $t,
+        float $time
     ): void {
     }
 
     public function addFailure(
         \PHPUnit\Framework\Test $test,
         \PHPUnit\Framework\AssertionFailedError $e,
-        $time
+        float $time
     ): void {
     }
 
     public function addIncompleteTest(
         \PHPUnit\Framework\Test $test,
-        \Throwable $e,
-        $time
+        \Throwable $t,
+        float $time
     ): void {
     }
 
     public function addRiskyTest(
         \PHPUnit\Framework\Test $test,
-        \Throwable $e,
-        $time
+        \Throwable $t,
+        float $time
     ): void {
     }
 
     public function addSkippedTest(
         \PHPUnit\Framework\Test $test,
-        \Throwable $e,
-        $time
+        \Throwable $t,
+        float $time
     ): void {
     }
 
     public function startTest(\PHPUnit\Framework\Test $test): void {
     }
 
-    public function endTest(\PHPUnit\Framework\Test $test, $time): void {
+    public function endTest(\PHPUnit\Framework\Test $test, float $time): void {
     }
 
-    private $openSuiteCount = 0;
+    private int $openSuiteCount = 0;
     public function startTestSuite(\PHPUnit\Framework\TestSuite $suite): void {
-      /**
-       * @psalm-suppress UndefinedConstant OMEGAUP_TEST_SHARD is only
-       * defined in the test bootstrap.php file
-       */
+        /**
+         * @psalm-suppress UndefinedConstant OMEGAUP_TEST_SHARD is only
+         * defined in the test bootstrap.php file
+         */
         $this->openSuiteCount += 1;
         if ($this->openSuiteCount == 1) {
-            echo('Starting it up! ' . $suite->getName());
             $scriptFilename = __DIR__ . '/controllers/gitserver-start.sh ' .
             OMEGAUP_GITSERVER_PORT . ' ' . OMEGAUP_TEST_ROOT .
             ' /tmp/omegaup/problems-' . OMEGAUP_TEST_SHARD . '.git';
@@ -76,7 +75,6 @@ class GitServerTestSuiteListener implements \PHPUnit\Framework\TestListener {
     public function endTestSuite(\PHPUnit\Framework\TestSuite $suite): void {
         $this->openSuiteCount -= 1;
         if ($this->openSuiteCount == 0) {
-            echo('Shutting it down! ' . $suite->getName());
             $scriptFilename = __DIR__ . '/controllers/gitserver-stop.sh ' . OMEGAUP_TEST_ROOT;
             exec($scriptFilename, $output, $returnVar);
             if ($returnVar != 0) {
