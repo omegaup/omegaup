eb132cb95eb6d83e9099c5e8136d01c2a95c7781
Jorge Sanchez <sandix390@gmail.com>
2022-03-11 20:42:52 -0600

    feature runs_groups table created

--- frontend/database/00001_initial_schema.sql
+++ frontend/database/00001_initial_schema.sql
@@ -728,6 +728,24 @@ CREATE TABLE IF NOT EXISTS `Run_Counts` (
 --
 
 --
+-- Estructura de tabla para la tabla `Runs_Groups`
+--
+CREATE TABLE IF NOT EXISTS `Runs_Groups` (
+  case_run_id int NOT NULL AUTO_INCREMENT,
+  run_id int NOT NULL,
+  group_name char(40) NOT NULL,
+  score double NOT NULL DEFAULT '0',
+  verdict enum('AC','PA','PE','WA','TLE','OLE','MLE','RTE','RFE','CE','JE','VE') NOT NULL,
+  PRIMARY KEY (case_run_id),
+  UNIQUE KEY (run_id, group_name),
+  FOREIGN KEY (run_id) REFERENCES Runs (run_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Guarda los grupos de runs hasta la fecha.';
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
 -- Filtros para la tabla `Announcement`
 --
 ALTER TABLE `Announcement`
