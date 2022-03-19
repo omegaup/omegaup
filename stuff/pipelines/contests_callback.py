b34bd8666f5ea81b27027ad18555b3d13bc8edd5
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2022-03-18 18:49:16 -0600

    Agregando consutlas para obtener los concursos y para insertar los certificados de concursos

---
+++ stuff/pipelines/contests_callback.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+
+'''Processing contest messages.'''
+
+import dataclasses
+import json
+import logging
+
+from typing import List, Optional
+import omegaup.api
+import mysql.connector
+import mysql.connector.cursor
+from mysql.connector import errors
+from mysql.connector import errorcode
+import pika
+
+from verification_code import generate_code
+
+
+@dataclasses.dataclass
+class Certificate:
+    '''A dataclass for certificate.'''
+    certificate_type: str
+    contest_id: int
+    verification_code: str
+    contest_place: Optional[int]
+    username: str
+
+
+@dataclasses.dataclass
+class ContestCertificate:
+    '''A dataclass for contest certificate.'''
+    certificate_cutoff: int
+    alias: str
+    scoreboard_url: str
+    contest_id: int
+
+
+class ContestsCallback:
+    '''Contests callback'''
+    def __init__(self,
+                 dbconn: mysql.connector.MySQLConnection,
+                 api_token: str,
+                 url: str):
+        '''Contructor for contest callback'''
+        self.dbconn = dbconn
+        self.api_token = api_token
+        self.url = url
+
+    def __call__(self,
+                 _channel: pika.adapters.blocking_connection.BlockingChannel,
+                 _method: pika.spec.Basic.Deliver,
+                 _properties: pika.spec.BasicProperties,
+                 body: bytes) -> None:
+        '''Function to stores the certificates by a given contest'''
+        data = ContestCertificate(**json.loads(body.decode()))
+        client = omegaup.api.Client(api_token=self.api_token,
+                                    url=self.url)
+
+        scoreboard = client.contest.scoreboard(
+            contest_alias=data.alias,
+            token=data.scoreboard_url)
+        ranking = scoreboard['ranking']
+        certificates: List[Certificate] = []
+
+        for user in ranking:
+            contest_place: Optional[int] = None
+            if (data.certificate_cutoff
+                    and user['place'] <= data.certificate_cutoff):
+                contest_place = user['place']
+            verification_code = generate_code()
+            certificates.append(Certificate(
+                certificate_type='contest',
+                contest_id=data.contest_id,
+                verification_code=verification_code,
+                contest_place=contest_place,
+                username=str(user['username'])
+            ))
+        with self.dbconn.cursor(buffered=True, dictionary=True) as cur:
+            while True:
+                try:
+                    cur.executemany('''
+                        INSERT INTO
+                            `Certificates` (
+                                `identity_id`,
+                                `certificate_type`,
+                                `contest_id`,
+                                `verification_code`,
+                                `contest_place`)
+                        SELECT
+                            `identity_id`,
+                            %s,
+                            %s,
+                            %s,
+                            %s
+                        FROM
+                            `Identities`
+                        WHERE
+                            `username` = %s;
+                        ''',
+                                    [
+                                        dataclasses.astuple(
+                                            certificate
+                                        ) for certificate in certificates])
+                    self.dbconn.commit()
+                    break
+                except errors.IntegrityError as err:
+                    self.dbconn.rollback()
+                    if err.errno != errorcode.ER_DUP_ENTRY:
+                        raise
+                    for certificate in certificates:
+                        certificate.verification_code = generate_code()
+                    logging.exception(
+                        'At least one of the verification codes had a conflict'
+                    )
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
---
+++ stuff/pipelines/database/contest.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+'''Mysql queries to generate messages for contests'''
+
+import datetime
+from typing import Dict, List, NamedTuple
+
+import mysql.connector
+import mysql.connector.cursor
+
+
+class ContestCertificate(NamedTuple):
+    '''Relevant information for contests.'''
+    certificate_cutoff: str
+    alias: str
+    scoreboard_url: str
+    contest_id: str
+
+
+def get_contest_contestants(
+        *,
+        cur: mysql.connector.cursor.MySQLCursorDict,
+        date_lower_limit: datetime.date,
+        date_upper_limit: datetime.date,
+) -> List[Dict[str, str]]:
+    '''Get contest users to recieve a certificate'''
+
+    cur.execute(
+        '''
+        SELECT
+            certificate_cutoff,
+            c.contest_id,
+            alias,
+            scoreboard_url
+        FROM
+            Contests c
+        INNER JOIN
+            Problemsets p
+        ON
+            c.problemset_id = p.problemset_id
+        WHERE
+            finish_time BETWEEN %s AND %s;
+        ''', (date_lower_limit, date_upper_limit)
+    )
+    data: List[Dict[str, str]] = []
+    for row in cur:
+        contest = ContestCertificate(
+            certificate_cutoff=row['certificate_cutoff'],
+            alias=row['alias'],
+            scoreboard_url=row['scoreboard_url'],
+            contest_id=row['contest_id'],
+        )
+        data.append(contest._asdict())
+    return data
