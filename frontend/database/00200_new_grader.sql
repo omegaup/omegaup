081385cf81c3335e4c2602333605674a0d7ce4ef
Marlene Harms <marlene_harms@hotmail.com>
2022-03-24 00:51:45 -0600

    fixed table name

--- frontend/database/00200_new_grader.sql
+++ frontend/database/00200_new_grader.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `Contest`
+ALTER TABLE `Contests`
     ADD COLUMN `score_mode` enum('partial','all_or_nothing','max_per_group') NOT NULL COMMENT 'Indica el tipo de evaluación para el concurso'
