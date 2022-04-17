b4d6d7f3e26088dc04b88abdf8e4849697be8253
Pablo Aguilar <pablo.aguilar@gmail.com>
2022-04-17 00:18:01 -0700

    Segundo paso para solo cargar algunas listas de concursos (#6441)
    En este cambio se crea un `Vuex.Store` que guarda las listas de concursos para cada tab. El crear el store aplica tanto para Arena y ArenaV2.
    
    El store se va a encargar de pedir las listas que le falten de forma asíncrona. Cuando el componente `ContestList` se las "pida".
    
    Parte de #6357

---
+++ frontend/www/js/omegaup/arena/contestStore.ts
@@ -0,0 +1,50 @@
+import Vue from 'vue';
+import Vuex, { Commit } from 'vuex';
+import * as api from '../api';
+import { messages, types } from '../api_types';
+
+Vue.use(Vuex);
+
+export interface ContestState {
+  // The map of contest lists.
+  contests: types.TimeTypeContests;
+}
+
+export interface NamedContestListRequest {
+  name: string;
+  requestParams: messages.ContestListRequest;
+}
+
+interface NamedContestListResponse {
+  name: string;
+  response: messages.ContestListResponse;
+}
+
+export const contestStoreConfig = {
+  state: {
+    contests: {},
+  },
+  mutations: {
+    updateAll(state: ContestState, payloadContests: types.TimeTypeContests) {
+      state.contests = { ...state.contests, ...payloadContests };
+    },
+    updateList(
+      state: ContestState,
+      { name, response }: NamedContestListResponse,
+    ) {
+      Vue.set(state.contests, name, response);
+    },
+  },
+  actions: {
+    fetchContestList(
+      { commit }: { commit: Commit },
+      payload: NamedContestListRequest,
+    ) {
+      api.Contest.list(payload.requestParams).then((response) => {
+        commit('updateList', { name: payload.name, response });
+      });
+    },
+  },
+};
+
+export default new Vuex.Store<ContestState>(contestStoreConfig);
