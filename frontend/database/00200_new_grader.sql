44bf94abefe625a3cee4089301ff8152c45092eb
Marlene Harms <marlene_harms@hotmail.com>
2022-04-24 20:50:55 -0500

    deleted old file

--- frontend/database/00200_new_grader.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `Contests`
-    ADD COLUMN `score_mode` enum('partial','all_or_nothing','max_per_group') NOT NULL DEFAULT 'partial' COMMENT 'Indica el tipo de evaluación para el concurso'
