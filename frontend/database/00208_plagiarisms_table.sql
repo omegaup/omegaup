255670c643b5fed9f105d1de40510fdf61a4ba56
Mohit <63846699+MohitOhlyan@users.noreply.github.com>
2022-07-18 04:00:22 +0530

    Update frontend/database/00208_plagiarisms_table.sql
    Co-authored-by: Carlos Córdova Sáenz <carlosab1802@gmail.com>

--- frontend/database/00208_plagiarisms_table.sql
+++ frontend/database/00208_plagiarisms_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `Plagiarisms` (
     `guid_2` char(32) NOT NULL COMMENT 'La identificación de envío del segundo código plagiado',
     `contents` TEXT NOT NULL COMMENT 'Almacena los puntajes de similitud de los envíos y el rango de números de línea de esos puntajes',
     PRIMARY KEY (`plagiarisms_id`),
-    CONSTRAINT `fk_pci_contest_id` FOREIGN KEY (`contest_id`) REFERENCES `Contests` (`contest_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `fk_pc_contest_id` FOREIGN KEY (`contest_id`) REFERENCES `Contests` (`contest_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT `fk_pg_guid_1` FOREIGN KEY (`guid_1`) REFERENCES `Runs` (`guid`) ON DELETE NO ACTION ON UPDATE NO ACTION, 
     CONSTRAINT `fk_pg_guid_2` FOREIGN KEY (`guid_2`) REFERENCES `Runs` (`guid`) ON DELETE NO ACTION ON UPDATE NO ACTION
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='La lista de Códigos Plagiados para un concurso';
