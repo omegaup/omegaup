f3192683574fec537a774e6799a3a7f689318061
Mohit <63846699+MohitOhlyan@users.noreply.github.com>
2022-07-18 04:01:15 +0530

    Update frontend/database/00208_plagiarisms_table.sql
    Co-authored-by: Carlos Córdova Sáenz <carlosab1802@gmail.com>

--- frontend/database/00208_plagiarisms_table.sql
+++ frontend/database/00208_plagiarisms_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `Plagiarisms` (
     `plagiarism_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'El identificador único para cada potencial caso de plagio',
     `contest_id` int(11) NOT NULL,
     `submission_id_1` int(11) NOT NULL COMMENT 'El identificador del envío del primer código plagiado',
-    `guid_2` char(32) NOT NULL COMMENT 'La identificación de envío del segundo código plagiado',
+    `submission_id_2` int(11) NOT NULL COMMENT 'El identificador del envío del segundo código plagiado',
     `contents` TEXT NOT NULL COMMENT 'Almacena los puntajes de similitud de los envíos y el rango de números de línea de esos puntajes',
     PRIMARY KEY (`plagiarisms_id`),
     CONSTRAINT `fk_pc_contest_id` FOREIGN KEY (`contest_id`) REFERENCES `Contests` (`contest_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
