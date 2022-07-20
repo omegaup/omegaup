6d77777572a76fe489651d8dbea82c202301e131
lhchavez <lhchavez@lhchavez.com>
2022-07-20 06:23:10 -0700

    Update frontend/database/00208_add_SubmissionFeedbackThread_table.sql

--- frontend/database/00208_add_SubmissionFeedbackThread_table.sql
+++ frontend/database/00208_add_SubmissionFeedbackThread_table.sql
@@ -8,7 +8,7 @@ CREATE TABLE `Submission_Feedback_Thread` (
   PRIMARY KEY (`submission_feedback_thread_id`),
   CONSTRAINT `fk_sfs_submission_feedback_id` FOREIGN KEY (`submission_feedback_id`) REFERENCES `Submission_Feedback` (`submission_feedback_id`),
   CONSTRAINT `fk_sfti_identity_id` FOREIGN KEY (`identity_id`) REFERENCES `Identities` (`identity_id`)
-) COMMENT="Darle seguimiento a un comentario";
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT="Darle seguimiento a un comentario";
 
 ALTER TABLE `Submission_Feedback`
     ADD COLUMN `range_bytes_start` int NOT NULL DEFAULT 0 COMMENT 'Inicio de la subcadena seleccionada (en bytes) para agregarle el comentario',
