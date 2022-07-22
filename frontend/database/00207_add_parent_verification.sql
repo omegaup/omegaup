42f3c9edcb16a3a70dd84abbfff5bb4623cc8d63
Anmol Agrawal <anmol2002.aa1@gmail.com>
2022-07-22 14:23:56 +0530

    added comments and fix up

--- frontend/database/00207_add_parent_verification.sql
+++
@@ -1,6 +0,0 @@
--- Users table
-ALTER TABLE `Users`
-ADD COLUMN `parent_verified` boolean 
-ADD COLUMN `creation_timestamp` timestamp DEFAULT NULL
-ADD COLUMN `random_token_generated` VARCHAR(100) DEFAULT NULL
-ADD COLUMN `parent_email_verification_deadline` timestamp DEFAULT NULL;
