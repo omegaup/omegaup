a8ed8ea3323b96d9116baa5764708d23f6455853
Anmol Agrawal <anmol2002.aa1@gmail.com>
2022-07-24 09:43:44 +0530

    updated the schema

--- frontend/database/schema.sql
+++ frontend/database/schema.sql
@@ -1174,6 +1174,7 @@ CREATE TABLE `Users` (
   `parent_verified` tinyint(1) DEFAULT NULL COMMENT 'Almacena la respuesta del padre cuando este verifica la cuenta de su hijo',
   `creation_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Almacena la hora y fecha en que se creó la cuenta de usuario',
   `parental_verification_token` varchar(25) DEFAULT NULL COMMENT 'Token que se generará para los usuarios menores de 13 años al momento de registrar su cuenta, el cuál será enviado por correo electrónico al padre',
+  `parent_email_verification_initial` timestamp NULL DEFAULT NULL COMMENT 'Almacena la hora en que se envió el correo electrónico de verificación',
   `parent_email_verification_deadline` timestamp NULL DEFAULT NULL COMMENT 'Almacena la hora y fecha límite que tienen los padres para verificar la cuenta de su hijo menor a 13 años',
   PRIMARY KEY (`user_id`),
   KEY `fk_main_email_id` (`main_email_id`),
