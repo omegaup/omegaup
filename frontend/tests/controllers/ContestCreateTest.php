f96a8bfc50df323f0bf0b222bac78fb02ad52d35
Mohit <63846699+MohitOhlyan@users.noreply.github.com>
2022-07-30 04:59:31 +0530

    Update frontend/tests/controllers/ContestCreateTest.php
    Co-authored-by: Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>

--- frontend/tests/controllers/ContestCreateTest.php
+++ frontend/tests/controllers/ContestCreateTest.php
@@ -182,7 +182,7 @@ class ContestCreateTest extends \OmegaUp\Test\ControllerTestCase {
             $contestData['request']['alias']
         );
 
-        this->assertEquals(
+        $this->assertEquals(
             $checkPlagiarismDuplicate->plagiarism_threshold,
             $plagiarismThresholdExpected
         );
