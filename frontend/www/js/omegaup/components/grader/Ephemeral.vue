2d497ab7310b68d8e255a25906f11c34bdbca8cd
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2022-08-17 09:48:18 -0500

    Comenzando a usar el componente de GoldenLayout en lugar de VueGoldenLayout

--- frontend/www/js/omegaup/components/grader/Ephemeral.vue
+++ frontend/www/js/omegaup/components/grader/Ephemeral.vue
@@ -1,42 +1,114 @@
 <template>
-  <div class="test">
-    <golden-layout>
-      <gl-row>
-        <gl-component title="component1">
-          <h1>Component 1</h1>
-        </gl-component>
-        <gl-stack>
-          <gl-component title="component2">
-            <h1>Component 2</h1>
-          </gl-component>
-          <gl-component title="component3">
-            <h1>Component 3</h1>
-          </gl-component>
-        </gl-stack>
-      </gl-row>
-    </golden-layout>
-  </div>
+  <!-- id-lint off -->
+  <div id="layoutContainer"></div>
+  <!-- id-lint on -->
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
-import vgl, {
-  glComponent,
-  glRow,
-  glStack,
-  goldenLayout,
-} from 'vue-golden-layout';
-import 'golden-layout/src/css/goldenlayout-light-theme.css';
-
-Vue.use(vgl);
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import * as GoldenLayout from 'golden-layout';
+import CaseSelectorComponent from './CaseSelectorComponent.vue';
+import MonacoDiffComponent from './MonacoDiffComponent.vue';
+import MonacoEditorComponent from './MonacoEditorComponent.vue';
+import SettingsComponent from './SettingsComponent.vue';
+import TextEditorComponent from './TextEditorComponent.vue';
+import ZipViewerComponent from './ZipViewerComponent.vue';
 
 @Component({
-  components: {
-    glComponent,
-    goldenLayout,
-    glRow,
-    glStack,
-  },
+  components: {},
 })
-export default class GraderEphemeral extends Vue {}
+export default class GraderEphemeral extends Vue {
+  @Prop() goldenLayoutSettings!: any;
+  mounted() {
+    const layout = new GoldenLayout(
+      this.goldenLayoutSettings,
+      document.getElementById('layoutContainer'),
+    );
+
+    let componentMapping = {};
+    this.registerVueComponent(
+      layout,
+      'case-selector-component',
+      CaseSelectorComponent,
+      componentMapping,
+    );
+    this.registerVueComponent(
+      layout,
+      'monaco-editor-component',
+      MonacoEditorComponent,
+      componentMapping,
+    );
+    this.registerVueComponent(
+      layout,
+      'monaco-diff-component',
+      MonacoDiffComponent,
+      componentMapping,
+    );
+    this.registerVueComponent(
+      layout,
+      'settings-component',
+      SettingsComponent,
+      componentMapping,
+    );
+    this.registerVueComponent(
+      layout,
+      'text-editor-component',
+      TextEditorComponent,
+      componentMapping,
+    );
+    this.registerVueComponent(
+      layout,
+      'zip-viewer-component',
+      ZipViewerComponent,
+      componentMapping,
+    );
+  }
+
+  registerVueComponent(
+    layout: any,
+    componentName: string,
+    component: Vue,
+    componentMap: any,
+  ) {
+    layout.registerComponent(
+      componentName,
+      function (container: any, componentState: any) {
+        container.on('open', () => {
+          let vueComponents: { [key: string]: Vue } = {};
+          vueComponents[componentName] = component;
+          let props: { [key: string]: { store: any; storeMapping: any } } = {
+            //store,
+            storeMapping: componentState.storeMapping,
+          };
+          for (let k in componentState) {
+            if (k == 'id') continue;
+            if (!Object.prototype.hasOwnProperty.call(componentState, k))
+              continue;
+            props[k] = componentState[k];
+          }
+          let vue = new Vue({
+            el: container.getElement()[0],
+            components: vueComponents,
+            render: function (createElement) {
+              return createElement(componentName, {
+                props: props,
+              });
+            },
+          });
+          let vueComponent = vue.$children[0];
+          if (vueComponent.title) {
+            container.setTitle(vueComponent.title);
+            vueComponent.$watch('title', function (title) {
+              container.setTitle(title);
+            });
+          }
+          if (vueComponent.onResize) {
+            container.on('resize', () => vueComponent.onResize());
+          }
+          componentMap[componentState.id] = vueComponent;
+        });
+      },
+    );
+  }
+}
 </script>
