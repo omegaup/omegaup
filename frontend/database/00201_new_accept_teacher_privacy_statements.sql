bce7bf2b12887aa7c8fdf48ea570002db6725c9a
MikeMAMR <moreno.ruiz.miguel.ale@gmail.com>
2022-08-22 17:39:05 -0500

    Agrego PrivacyStatements con un nuevo id

--- frontend/database/00201_new_accept_teacher_privacy_statements.sql
+++ frontend/database/00201_new_accept_teacher_privacy_statements.sql
@@ -1,2 +1,6 @@
 INSERT INTO `PrivacyStatements` (`type`, `git_object_id`) VALUES
   ('accept_teacher', '5303d4881364e8ff814cfb51badcccc8fb7570bb');
+
+-- PrivacyStatements
+INSERT INTO `PrivacyStatements` (`type`, `git_object_id`) VALUES
+  ('accept_teacher', '4402300fc6f7a45a920edf49c46f8fd0e5c6d189');
