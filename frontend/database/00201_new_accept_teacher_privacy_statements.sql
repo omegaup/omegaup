b4607bf6fc6bdd540fa49c3b94f6d1748d9cf628
MikeMAMR <moreno.ruiz.miguel.ale@gmail.com>
2022-08-22 19:33:50 -0500

    Cambio a un nuevo archivo

--- frontend/database/00201_new_accept_teacher_privacy_statements.sql
+++ frontend/database/00201_new_accept_teacher_privacy_statements.sql
@@ -1,11 +1,2 @@
 INSERT INTO `PrivacyStatements` (`type`, `git_object_id`) VALUES
   ('accept_teacher', '5303d4881364e8ff814cfb51badcccc8fb7570bb');
-
--- PrivacyStatements
-INSERT INTO `PrivacyStatements` (`type`, `git_object_id`) VALUES
-  ('accept_teacher', '4402300fc6f7a45a920edf49c46f8fd0e5c6d189');
-
--- PrivacyStatements
-INSERT INTO `PrivacyStatements` (`type`, `git_object_id`) VALUES
-  ('accept_teacher', 'a15b0f83e498932d4d7c2f2ed1a91db770c26341');
-  
