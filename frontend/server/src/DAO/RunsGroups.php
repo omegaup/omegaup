8df443efb711d018c69e5e419603cf684c2bd96e
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2022-09-02 17:36:20 -0500

    Arreglando consulta que obtiene mayores puntajes por grupo

--- frontend/server/src/DAO/RunsGroups.php
+++ frontend/server/src/DAO/RunsGroups.php
@@ -13,111 +13,107 @@ namespace OmegaUp\DAO;
  */
 class RunsGroups extends \OmegaUp\DAO\Base\RunsGroups {
     /**
-     * @return list<array{contest_score: float, identity_id: int, penalty: int, problem_id: int, score: float, submission_count: int, type: string}>
+     * @return list<array{contest_score: float, identity_id: int, penalty: float, problem_id: int, score: float, submission_count: int, type: string}>
      */
     final public static function getProblemsetRunsGroups(
         int $problemsetId,
         ?\OmegaUp\Timestamp $scoreboardTimeLimit = null,
         string $penaltyType = 'MAX'
-    ): array {
-        $penaltyQuery = 'MAX(mspg_name.penalty) AS penalty';
-        $mainPenaltyQuery = 'MAX(mspg.penalty) AS penalty';
-        $timeQuery = '';
+    ) {
+        $penaltyQuery = 'MAX(mspg.penalty) AS penalty';
+        $timeQuery = 'TRUE';
         if ($penaltyType != 'MAX') {
-            $penaltyQuery = 'SUM(mspg_name.penalty) AS penalty';
-            $mainPenaltyQuery = 'MAX(mspg.penalty) AS penalty';
+            $penaltyQuery = 'SUM(mspg.penalty) AS penalty';
         }
         $params = [$problemsetId];
         if (!is_null($scoreboardTimeLimit)) {
-            $timeQuery = 'AND r.`time` < ?';
+            $timeQuery = 'mspg.`time` < ?';
             $params[] = $scoreboardTimeLimit->time;
         }
         $sql = "WITH mspg AS (
                     SELECT
                         rg.score,
                         r.penalty,
-                        r.run_id,
                         rg.group_name,
                         s.problem_id,
                         s.identity_id,
-                        s.`type`,
-                        s.problemset_id,
-                        r.`time`
-                    FROM Runs_Groups rg
-                    INNER JOIN Runs r ON rg.run_id = r.run_id
-                    INNER JOIN Submissions s ON s.submission_id = r.submission_id
+                        s.type,
+                        r.time,
+                        s.problemset_id
+                    FROM
+                        Runs_Groups rg
+                    INNER JOIN
+                        Runs r
+                    ON
+                        rg.run_id = r.run_id
+                    INNER JOIN
+                        Submissions s
+                    ON
+                        s.submission_id = r.submission_id
                     WHERE
-                        s.problemset_id = ?
-                        AND s.`status` = 'ready'
-                        AND s.`type` = 'normal'
-                        {$timeQuery}
+                        s.problemset_id = ? AND
+                        s.status = 'ready' AND
+                        s.type = 'normal'
                 )
                 SELECT
-                    IFNULL(ROUND(mspg_identity.score, 2), 0.00) AS score,
-                    IFNULL(ROUND(mspg_identity.score * pp.points, 2), 0.00) AS contest_score,
-                    IFNULL(mspg_identity.penalty, 0) AS penalty,
-                    mspg_identity.problem_id,
-                    mspg_identity.identity_id,
-                    IFNULL(mspg_identity.`type`, 'normal') AS `type`,
-                    mspg_all.submission_count
+                    IFNULL(ROUND(SUM(mspg_limit.score), 2), 0.00) AS score,
+                    IFNULL(ROUND(SUM(mspg_limit.score) * pp.points, 2), 0.00) AS contest_score,
+                    IFNULL(SUM(mspg_limit.penalty), 0) AS penalty,
+                    mspg_all.problem_id,
+                    mspg_all.identity_id,
+                    IFNULL(mspg_all.`type`, 'normal') AS `type`,
+                    IFNULL(MAX(mspg_all.submission_count), 0) AS submission_count
                 FROM
                     Problemset_Problems pp
-                INNER JOIN
-                    (
-                        SELECT
-                            COUNT(*) AS submission_count,
-                            mspg_count.problem_id,
-                            mspg_count.identity_id,
-                            mspg_count.problemset_id
-                        FROM
-                            (
-                            SELECT
-                                mspg.problem_id,
-                                mspg.identity_id,
-                                mspg.run_id,
-                                mspg.problemset_id
-                            FROM
-                                mspg
-                            GROUP BY
-                                problem_id, identity_id, run_id
-                            ) AS mspg_count
-                        GROUP BY problem_id, identity_id
-                    ) AS mspg_all
+                INNER JOIN (
+                    SELECT
+                        COUNT(*) AS submission_count,
+                        MAX(mspg.score) AS score,
+                        {$penaltyQuery},
+                        mspg.group_name,
+                        mspg.problem_id,
+                        mspg.identity_id,
+                        mspg.type,
+                        mspg.problemset_id
+                    FROM
+                        mspg
+                    GROUP BY
+                        problem_id,
+                        identity_id,
+                        group_name
+                ) AS mspg_all
                 ON
-                    mspg_all.problemset_id = pp.problemset_id
-                    AND mspg_all.problem_id = pp.problem_id
-                INNER JOIN
-                    (
-                        SELECT
-                            SUM(mspg_name.score) AS score,
-                            {$penaltyQuery},
-                            mspg_name.problem_id,
-                            mspg_name.identity_id,
-                            mspg_name.problemset_id,
-                            mspg_name.`type`
-                        FROM (
-                            SELECT
-                                IFNULL(MAX(mspg.score), 0.00) AS score,
-                                {$mainPenaltyQuery},
-                                mspg.problem_id,
-                                mspg.identity_id,
-                                IFNULL(mspg.`type`, 'normal') AS `type`,
-                                mspg.group_name,
-                                mspg.problemset_id
-                            FROM
-                                mspg
-                            GROUP BY
-                                problem_id, identity_id, group_name
-                        ) AS mspg_name
-                        GROUP BY problem_id, identity_id
-                    ) AS mspg_identity
+                    pp.problemset_id = mspg_all.problemset_id
+                    AND pp.problem_id = mspg_all.problem_id
+                INNER JOIN (
+                    SELECT
+                        MAX(mspg.score) AS score,
+                        {$penaltyQuery},
+                        mspg.group_name,
+                        mspg.problem_id,
+                        mspg.identity_id,
+                        mspg.type,
+                        mspg.problemset_id
+                    FROM
+                        mspg
+                    WHERE
+                        {$timeQuery}
+                    GROUP BY
+                        problem_id,
+                        identity_id,
+                        group_name
+                ) AS mspg_limit
                 ON
-                    mspg_identity.problemset_id = pp.problemset_id
-                    AND mspg_identity.problem_id = pp.problem_id
-                    AND mspg_all.identity_id = mspg_identity.identity_id;
+                    pp.problemset_id = mspg_limit.problemset_id
+                    AND pp.problem_id = mspg_limit.problem_id
+                    AND mspg_limit.identity_id = mspg_all.identity_id
+                    AND mspg_limit.group_name = mspg_all.group_name
+                GROUP BY
+                    problem_id,
+                    identity_id
         ";
 
-        /** @var list<array{contest_score: float, identity_id: int, penalty: int, problem_id: int, score: float, submission_count: int, type: string}> */
+        /** @var list<array{contest_score: float, identity_id: int, penalty: float, problem_id: int, score: float, submission_count: int, type: string}> */
         $result = \OmegaUp\MySQLConnection::getInstance()->GetAll(
             $sql,
             $params
