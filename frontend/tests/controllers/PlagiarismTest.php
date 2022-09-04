1919498dc9e00de39fe2a4a18f2305c45c83f7f4
MohitOhlyan <mohitpb1511@gmail.com>
2022-09-05 02:48:53 +0530

    PlagiarismTest check plagiarism added

--- frontend/tests/controllers/PlagiarismTest.php
+++ frontend/tests/controllers/PlagiarismTest.php
@@ -12,14 +12,14 @@ class PlagiarismTest extends \OmegaUp\Test\ControllerTestCase {
             new \OmegaUp\Test\Factories\ContestParams([
                 'startTime' => $originalTime - 60 * 60,
                 'finishTime' => $originalTime - 60 * 45,
-                // TO:DO Use check plagiarism flag here
+                'check_plagiarism' => 1,
             ])
         );
         \OmegaUp\Test\Factories\Contest::createContest(
             new \OmegaUp\Test\Factories\ContestParams([
                 'startTime' => $originalTime - 60 * 60,
                 'finishTime' => $originalTime - 60 * 30,
-                // TO:DO Use check plagiarism flag here
+                'check_plagiarism' => 1,
             ])
         );
         \OmegaUp\Test\Utils::runCheckPlagiarisms();
