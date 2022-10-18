c303f789193c34449a2ac43dac7cd7479f5d0cc3
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2022-10-18 13:09:03 -0500

    Utilizando un Ref único para el componente

--- frontend/www/js/omegaup/components/arena/Feedback.vue
+++ frontend/www/js/omegaup/components/arena/Feedback.vue
@@ -3,7 +3,7 @@
     <div class="card-header">{{ T.runDetailsNewFeedback }}</div>
     <div class="card-body">
       <textarea
-        :ref="`ta-${feedback.line}`"
+        ref="feedback-form"
         v-model="text"
         :placeholder="T.runDetailsFeedbackPlaceholder"
         class="w-100"
@@ -15,14 +15,22 @@
           data-button-submit
           :disabled="!text"
           class="btn btn-primary mx-2"
-          @click.prevent="$emit('submit', { ...feedback, text })"
+          @click.prevent="
+            $emit('submit', {
+              ...feedback,
+              text,
+              status: FeedbackStatus.InProgress,
+            })
+          "
         >
           {{ T.runDetailsFeedbackAddReview }}
         </button>
         <button
           data-button-cancel
           class="btn btn-danger mx-2"
-          @click.prevent="$emit('cancel', feedback)"
+          @click.prevent="
+            $emit('cancel', { ...feedback, status: FeedbackStatus.New })
+          "
         >
           {{ T.runDetailsFeedbackCancel }}
         </button>
@@ -32,7 +40,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop, Ref } from 'vue-property-decorator';
 import T from '../../lang';
 
 export enum FeedbackStatus {
@@ -42,7 +50,7 @@ export enum FeedbackStatus {
 }
 
 export interface ArenaCourseFeedback {
-  line: number;
+  line: null | number;
   text: null | string;
   status: FeedbackStatus;
 }
@@ -50,12 +58,14 @@ export interface ArenaCourseFeedback {
 @Component
 export default class Feedback extends Vue {
   @Prop() feedback!: ArenaCourseFeedback;
+  @Ref('feedback-form') feedbackForm!: HTMLTextAreaElement;
 
+  FeedbackStatus = FeedbackStatus;
   T = T;
   text = this.feedback.text;
 
   mounted() {
-    (this.$refs[`ta-${this.feedback.line}`] as HTMLTextAreaElement).focus();
+    this.feedbackForm.focus();
   }
 }
 </script>
