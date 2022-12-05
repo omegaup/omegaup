56d18f989e7317c1d739629801b5f7d468e859a0
AlexiaAM <alexia77755@gmail.com>
2022-12-05 08:59:39 -0600

    descomentar otras pruebas del archivo certificatestest

--- frontend/tests/controllers/CertificatesTest.php
+++ frontend/tests/controllers/CertificatesTest.php
@@ -8,7 +8,7 @@ class CertificatesTest extends \OmegaUp\Test\ControllerTestCase {
      * Generate certificates in a contest
      *
      */
-    /*public function testGenerateContestCertificates() {
+    public function testGenerateContestCertificates() {
         //Create user
         ['identity' => $identity] = \OmegaUp\Test\Factories\User::createUser();
 
@@ -46,7 +46,7 @@ class CertificatesTest extends \OmegaUp\Test\ControllerTestCase {
         );
 
         $this->assertEquals(3, $contest->certificate_cutoff);
-    }*/
+    }
 
     /**
      * Generate certificates in a contest
@@ -98,7 +98,7 @@ class CertificatesTest extends \OmegaUp\Test\ControllerTestCase {
      * try to generate certificates in a contest as a normal user
      *
      */
-    /*public function testGenerateContestCertificatesAsNormalUser() {
+    public function testGenerateContestCertificatesAsNormalUser() {
         //Create user
         ['identity' => $identity] = \OmegaUp\Test\Factories\User::createUser();
 
@@ -124,83 +124,5 @@ class CertificatesTest extends \OmegaUp\Test\ControllerTestCase {
         } catch (\OmegaUp\Exceptions\ForbiddenAccessException $e) {
             $this->assertSame('userNotAllowed', $e->getMessage());
         }
-    }*/
-
-    /**
-     * Generate certificates in a contest
-     *
-     */
-    /*public function testGetUserCertificates() {
-        //Create user
-        ['identity' => $certificateGenerator] = \OmegaUp\Test\Factories\User::createUser();
-
-        //login
-        $loginCertificateGenerator = self::login($certificateGenerator);
-
-        //add role certificate generator to identity user
-        \OmegaUp\Controllers\User::apiAddRole(new \OmegaUp\Request([
-            'auth_token' => $loginCertificateGenerator->auth_token,
-            'username' => $certificateGenerator->username,
-            'role' => 'CertificateGenerator'
-        ]));
-
-        // Get a problem
-        $problem = \OmegaUp\Test\Factories\Problem::createProblem();
-
-        // Create contest with 2 hours and a window length 30 of minutes
-        $contestData = \OmegaUp\Test\Factories\Contest::createContest(
-            new \OmegaUp\Test\Factories\ContestParams([
-                'alias' => 'pasado',
-            ])
-        );
-
-        // Add the problem to the contest
-        \OmegaUp\Test\Factories\Contest::addProblemToContest(
-            $problem,
-            $contestData
-        );
-
-        // Create a contestant
-        ['identity' => $identity] = \OmegaUp\Test\Factories\User::createUser();
-        error_log(print_r($identity->user_id, true));
-
-        // Add contestant to contest
-        \OmegaUp\Test\Factories\Contest::addUser($contestData, $identity);
-
-        // User creates a run in a valid time
-        $run = \OmegaUp\Test\Factories\Run::createRun(
-            $problem,
-            $contestData,
-            $identity
-        );
-        \OmegaUp\Test\Factories\Run::gradeRun($run, 1.0, 'AC', 10);
-
-        $certificatesCutoff = 3;
-
-        \OmegaUp\Controllers\Certificate::apiGenerateContestCertificates(
-            new \OmegaUp\Request([
-                'auth_token' => $loginCertificateGenerator->auth_token,
-                'contest_id' => $contestData['contest']->contest_id,
-                'certificates_cutoff' => $certificatesCutoff
-            ])
-        );
-
-        //login
-        $loginIdentity = self::login($identity);
-
-        $contest = \OmegaUp\DAO\Contests::getByAlias(
-            $contestData['request']['alias']
-        );
-
-        $this->assertEquals(3, $contest->certificate_cutoff);
-
-        ///checar los
-
-        \OmegaUp\Controllers\Certificate::apiGetUserCertificates(
-            new \OmegaUp\Request([
-                'auth_token' => $loginIdentity->auth_token,
-                'user_id' => $identity->user_id
-            ])
-        );
-    }*/
+    }
 }
