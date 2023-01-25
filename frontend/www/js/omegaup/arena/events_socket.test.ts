18c1cc89a468c338712947e604ba20c5f242731d
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2023-01-25 00:02:49 -0600

    Agregando un mock para onRankingEvents

--- frontend/www/js/omegaup/arena/events_socket.test.ts
+++ frontend/www/js/omegaup/arena/events_socket.test.ts
@@ -10,7 +10,7 @@ import { clarificationStoreConfig } from './clarificationsStore';
 import { createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 import fetchMock from 'jest-fetch-mock';
-import { onRankingChanged } from './ranking';
+import { onRankingChanged, onRankingEvents } from './ranking';
 import { mocked } from 'ts-jest/utils';
 
 const navbarProblems: types.NavbarProblemsetProblem[] = [
@@ -234,12 +234,33 @@ describe('EventsSocket', () => {
     await server?.connected;
 
     const onRankingChangedMock = mocked(onRankingChanged, true);
+    const onRankingEventsMock = mocked(onRankingEvents, true);
     onRankingChangedMock.mockReturnValueOnce({
       users: [],
       ranking: [],
       currentRanking: { omegaUp: 0 },
       maxPoints: 300,
       lastTimeUpdated: new Date(0),
+    });
+    onRankingEventsMock.mockReturnValueOnce({
+      series: [
+        {
+          type: 'line',
+          rank: 0,
+          data: [
+            [1674625311000, 0],
+            [1674625378000, 0],
+            [1674625378734, 100],
+          ],
+          name: 'test-user',
+          step: 'right',
+        },
+      ],
+      navigatorData: [
+        [1674625311000, 0],
+        [1674625378000, 0],
+        [1674625378734, 100],
+      ],
     });
     server?.send({
       message: '/scoreboard/update/',
