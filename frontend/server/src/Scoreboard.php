ca53b5dfeedadca8de3642d267e394c3c0431128
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2023-01-28 21:24:32 -0600

    Separando los cambios del feature CodeReview de controladores a un nuevo PR (#6862)
    Para poder hacer más pequeño el cambio en el PR #6733, se agrega este nuevo
    cambio el cual se encarga de aplicar todos los cambios del lado del servidor.
    
    Part of: #6827

--- frontend/server/src/Scoreboard.php
+++ frontend/server/src/Scoreboard.php
@@ -496,7 +496,6 @@ class Scoreboard {
         $totalPoints = 0.0;
         $totalPenalty = 0.0;
         // Get final scores
-        //error_log(print_r($scores, true));
         foreach ($scores as $score) {
             $totalPoints += $score['points'];
             if ($contestPenaltyCalcPolicy == 'sum') {
