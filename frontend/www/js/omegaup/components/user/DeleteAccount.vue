5118d593ce85f9ae24f440b753183bdc3a87b6d0
Anmol Agrawal <anmol2002.aa1@gmail.com>
2023-03-22 09:29:01 +0530

    lint fixed

--- frontend/www/js/omegaup/components/user/DeleteAccount.vue
+++ frontend/www/js/omegaup/components/user/DeleteAccount.vue
@@ -5,36 +5,40 @@
         <div class="alert alert-danger">
           <h4 class="alert-heading">{{ T.wordsDangerZone }}</h4>
           <hr />
-          <omegaup-markdown :markdown="T.wordsDangerZoneDesc"></omegaup-markdown>
+          <omegaup-markdown
+            :markdown="T.wordsDangerZoneDesc"
+          ></omegaup-markdown>
           <br /><br />
-          <button class="btn btn-danger" type="submit"  @click="showConfirmationModal = true">
+          <button
+            class="btn btn-danger"
+            type="submit"
+            @click="showConfirmationModal = true"
+          >
             {{ T.wordsDelete }}
           </button>
         </div>
       </div>
     </form>
     <b-modal
-    v-model="showConfirmationModal"
-    title="Confirmation required"
-    ok-title="Delete"
-    ok-variant="danger"
-    cancel-title="Cancel"
-  >
-    <p>Are you sure you want to delete your account?</p>
-  </b-modal>
-
+      v-model="showConfirmationModal"
+      title="Confirmation required"
+      ok-title="Delete"
+      ok-variant="danger"
+      cancel-title="Cancel"
+    >
+      <p>Are you sure you want to delete your account?</p>
+    </b-modal>
   </div>
-  </template>
-  
-  <script lang="ts">
-  import { Vue, Component } from 'vue-property-decorator';
-  import T from '../../lang';
-  import omegaup_Markdown from '../Markdown.vue';
-  
-  @Component({ components: {'omegaup-markdown': omegaup_Markdown}})
-  export default class UserDeleteAccount extends Vue {
-    T = T;
-    showConfirmationModal = false;
-  }
-  </script>
-  
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import T from '../../lang';
+import omegaup_Markdown from '../Markdown.vue';
+
+@Component({ components: { 'omegaup-markdown': omegaup_Markdown } })
+export default class UserDeleteAccount extends Vue {
+  T = T;
+  showConfirmationModal = false;
+}
+</script>
