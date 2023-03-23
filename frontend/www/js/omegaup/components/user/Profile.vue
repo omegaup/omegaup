023ab68c00bc8a8c4dbb29776e8a8e104e1f786e
Ingrid Margarita <margaret9197@gmail.com>
2023-03-22 19:48:14 -0500

    Mejorando el mensaje de consentimiento de privacidad en cursos.

--- frontend/www/js/omegaup/components/user/Profile.vue
+++ frontend/www/js/omegaup/components/user/Profile.vue
@@ -11,6 +11,7 @@
           {{ ui.info(T.userProfileIsPrivate) }}
         </h1>
       </template>
+      <h3>{hola</h3>
       <template #title>
         <h3>{{ currentTitle }}</h3>
       </template>
