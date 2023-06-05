7ef7c24a4eb44b90ded34287d54f8e3973bbefda
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2023-06-05 19:42:42 +0530

    formatted index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -6,17 +6,11 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-    on('task',{
+    on('task', {
+        log(message) {
+            console.log(message);
 
-        log(message){
-        
-        console.log(message)
-        
-        
-        
-        return null
-        
+            return null;
         },
-        
-        })
+    });
 };
