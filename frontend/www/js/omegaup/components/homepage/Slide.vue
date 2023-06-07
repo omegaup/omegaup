aad00ff059dee005628cf80f78cc44e04cdc5bff
Ingrid Margarita <margaret9197@gmail.com>
2023-06-07 00:05:06 -0500

    Cambiando la altura de px a rem.

--- frontend/www/js/omegaup/components/homepage/Slide.vue
+++ frontend/www/js/omegaup/components/homepage/Slide.vue
@@ -48,10 +48,10 @@ export default class Slide extends Vue {
 
 <style lang="scss" scoped>
 .slide {
-  height: 700px;
+  height: 43.5rem;
 
   @media only screen and (min-width: 767px) {
-    height: 500px;
+    height: 31rem;
   }
 
   h2 {
