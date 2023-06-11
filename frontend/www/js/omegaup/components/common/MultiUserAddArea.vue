2e029e144d972c478411524afe36dc00d271da1e
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2023-06-11 14:46:45 -0600

    feat: add multi-user-add-area component

---
+++ frontend/www/js/omegaup/components/common/MultiUserAddArea.vue
@@ -0,0 +1,156 @@
+<template>
+<div class="multi-user-add-area">
+    <textarea
+        v-if="isFocused || !usersList.length"
+        v-model="bulkContestants"
+        class="form-control contestants"
+        @input.prevent="onBulkContestantsChanged"
+        @blur.prevent="isFocused = false"
+    >
+    </textarea>
+    <div
+        v-else
+        class="form-control contestants"
+    >
+        <span href="#" class="edit-icon" @click.prevent="isFocused = true">Editar</span>
+        <div class="users-list">
+            <div v-for="user in usersList" :key="user" class="users-list__item">
+                <span class="tags-input-badge tags-input-badge-pill tags-input-badge-selected-default">{{ user }}</span>
+                <a href="#" class="tags-input-remove" @click.prevent="removeUser(user)"></a>
+            </div>
+        </div>
+    </div> 
+</div>
+</template>
+
+<script lang="ts">
+function debounce (fn: any, waitTime: number) {
+    let timer: any = null;
+
+    return function(...args: any[]) {
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
+        const context: any = this;
+
+        timer = setTimeout(() => {
+            fn.apply(context, args);
+        }, waitTime);
+    }
+}
+
+const WAIT_TIME = 1000;
+import { Vue, Component, Watch, Prop } from 'vue-property-decorator';
+
+@Component
+export default class MultiUserAddArea extends Vue {
+    @Prop() users!: string[];
+    isFocused: boolean = true;
+    bulkContestants: string | null = '';
+    usersList: string[] = [];
+
+    // TODO: Add debounce to this method
+    onBulkContestantsChanged = debounce(function (event: Event) {
+        const target = event.target as HTMLTextAreaElement;
+        const { value } = target;
+
+        // Split the value by lines, then by commas, then by spaces
+        // and convert into a single array
+        const users = value.split('\n').reduce((acc, line) => {
+            const lineUsers = line.split(',').reduce((acc, token) => {
+                const tokenUsers = token.split(' ').reduce((acc, token) => {
+                    if (token.trim() !== '') {
+                        acc.push(token.trim());
+                    }
+                    return acc;
+                }, [] as string[]);
+                return acc.concat(tokenUsers);
+            }, [] as string[]);
+            return acc.concat(lineUsers);
+        }, [] as string[]);
+
+        console.log({ users })
+
+        this.usersList = Array.from(new Set([...users])); // Removes duplicates
+        this.bulkContestants = this.usersList.join(',');
+    
+        this.isFocused = false;
+    }, WAIT_TIME);
+
+    removeUser(user: string) {
+        this.usersList = this.usersList.filter(u => u !== user);
+        this.bulkContestants = this.usersList.join(',');
+        this.isFocused = false;
+    }
+
+    @Watch('users')
+    onUsersChange() {
+        console.log({ propsUsers: this.users, usersList: this.usersList })
+        // We need to keep the usersList without any user that is part of the users prop
+        this.usersList = this.usersList.filter(user => !this.users.includes(user));
+        this.bulkContestants = this.usersList.join(',');
+    }
+
+    // When the usersList changes, emit the new value to the parent
+    @Watch('usersList')
+    onUsersListChange() {
+        this.$emit('update-users', this.usersList);
+    }
+}
+</script>
+
+<style scoped>
+.multi-user-add-area {
+    position: relative;
+}
+
+.form-control {
+    min-height: 4rem;
+    overflow: auto;
+}
+
+.users-list {
+    display: flex;
+    flex-wrap: wrap;
+
+    max-width: 95%;
+}
+
+.edit-icon {
+    position: absolute;
+    top: 0;
+    right: 1.5rem;
+
+    color: #8bb0d3;
+}
+
+.edit-icon:hover {
+    color: #666;
+    cursor: pointer;
+}
+
+.users-list__item {
+    position: relative;
+}
+
+.users-list > div {
+    margin: 0.25rem;
+}
+
+.tags-input-badge-pill {
+    padding-right: 1.2rem;
+}
+
+.tags-input-remove {
+    cursor: pointer;
+    right: 0.1rem;
+    top: 0.1rem;
+}
+
+.tags-input-remove:before,
+.tags-input-remove:after {
+    width: 10px;
+}
+</style>
