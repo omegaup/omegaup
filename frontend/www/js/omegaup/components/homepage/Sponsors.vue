d150fcf6aae2e32c8c2697363a574cade255b5f7
galovdev <contacto.detodounpocomx@gmail.com>
2023-07-14 13:40:49 -0600

    sponsors fix

--- frontend/www/js/omegaup/components/homepage/Sponsors.vue
+++ frontend/www/js/omegaup/components/homepage/Sponsors.vue
@@ -30,21 +30,11 @@ export default class Sponsors extends Vue {
 h3.display-4 {
   color: $omegaup-primary--darker;
   font-weight: normal;
-
+  font-size: 1.88rem;
+  margin-top: 1.88em;
 }
-
 img.img-fluid {
+  max-width: 200px;
   margin: -0.2rem !important;
-}
-
-@media only screen and (min-width: 767px) {
-  h3.display-4 {
-    font-size: 1.88rem;
-    margin-top: 1.88em;
-  }
-
-  img.img-fluid {
-    max-width: 200px;
-  }
 }
 </style>
