c9e6e5c40fd417c359e923717f229e6721dc4316
Ingrid Margarita <margaret9197@gmail.com>
2023-07-25 11:02:08 -0500

    Usando la tabla-responsive de Boostrap, agregando margenes en Ranking de alumnos.

--- frontend/www/js/omegaup/components/arena/Scoreboard.vue
+++ frontend/www/js/omegaup/components/arena/Scoreboard.vue
@@ -120,6 +120,234 @@
   </div>
 </template>
 
+<template>
+  <div class="omegaup-scoreboard px-2">
+    <slot name="scoreboard-header">
+      <div class="text-center mt-4 pt-2">
+        <h2 class="mb-4">
+          <span>{{ title }}</span>
+          <slot name="socket-status">
+            <sup :class="socketClass" :title="socketStatusTitle">{{
+              socketStatus
+            }}</sup>
+          </slot>
+        </h2>
+        <div v-if="!finishTime" class="clock">{{ INF }}</div>
+        <omegaup-countdown
+          v-else
+          class="clock"
+          :target-time="finishTime"
+        ></omegaup-countdown>
+      </div>
+    </slot>
+    <highcharts
+      v-if="rankingChartOptions"
+      :options="rankingChartOptions"
+    ></highcharts>
+    <label v-if="showInvitedUsersFilter">
+      <input
+        v-model="onlyShowExplicitlyInvited"
+        class="toggle-contestants"
+        type="checkbox"
+      />
+      {{ T.scoreboardShowOnlyInvitedIdentities }}</label
+    >
+    <label class="float-right"
+      >{{ T.scoreboardShowParticipantsNames }}:
+      <select
+        v-model="nameDisplayOptions"
+        class="form-control"
+        data-scoreboard-options
+      >
+        <option :value="ui.NameDisplayOptions.Name">{{ T.wordsName }}</option>
+        <option :value="ui.NameDisplayOptions.Username">
+          {{ T.wordsUser }}
+        </option>
+        <option :value="ui.NameDisplayOptions.NameAndUsername">
+          {{ T.arenaNameAndUsername }}
+        </option>
+      </select>
+    </label>
+    <div class="table-responsive">
+      <table data-table-scoreboard class="table">
+        <thead>
+          <tr>
+            <th><!-- legend --></th>
+            <th><!-- position --></th>
+            <th>{{ T.contestParticipant }}</th>
+            <th v-for="(problem, index) in problems" :key="problem.alias">
+              <a :href="'#problems/' + problem.alias" :title="problem.alias">{{
+                ui.columnName(index)
+              }}</a>
+            </th>
+            <th :colspan="2 + problems.length">{{ T.wordsTotal }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <template v-for="(user, userIndex) in ranking">
+            <tr
+              v-if="showUser(user.is_invited)"
+              :key="`${user.username}-${user.virtual}`"
+              :class="user.username"
+            >
+              <td class="legend" :class="legendClass(userIndex)"></td>
+              <td class="position" data-table-scoreboard-position>
+                {{ user.place || '—' }}
+              </td>
+              <td class="user" data-table-scoreboard-username>
+                {{ ui.rankingUsername(user, nameDisplayOptions) }}
+                <img
+                  v-if="user.country"
+                  alt=""
+                  height="11"
+                  :src="`/media/flags/${user.country.toLowerCase()}.png`"
+                  :title="user.country"
+                  width="16"
+                />
+              </td>
+
+              <td
+                v-for="(problem, problemIndex) in user.problems"
+                :key="problem.alias"
+                :class="problemClass(problem, problems[problemIndex].alias)"
+              >
+                <template v-if="problem.runs > 0">
+                  <div class="points">
+                    {{ renderPoints(problem) }}
+                  </div>
+                  <div class="penalty">
+                    <span v-if="showPenalty">{{ problem.penalty }}</span> ({{
+                      problem.runs
+                    }})
+                  </div>
+                </template>
+                <template v-else> - </template>
+              </td>
+              <td>
+                <div class="points">
+                  {{ user.total.points.toFixed(digitsAfterDecimalPoint) }}
+                </div>
+                <div class="penalty">
+                  {{ user.total.penalty }} ({{ totalRuns(user) }})
+                </div>
+              </td>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+    </div>
+    <div class="footer">
+      {{ lastUpdatedString }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+import * as Highcharts from 'highcharts/highstock';
+import { Chart } from 'highcharts-vue';
+import { types } from '../../api_types';
+import { omegaup } from '../../omegaup';
+import T from '../../lang';
+import * as ui from '../../ui';
+import * as time from '../../time';
+import omegaup_Countdown from '../Countdown.vue';
+import { SocketStatus } from '../../arena/events_socket';
+
+@Component({
+  components: {
+    highcharts: Chart,
+    'omegaup-countdown': omegaup_Countdown,
+  },
+})
+export default class ArenaScoreboard extends Vue {
+  @Prop({ default: 10 }) numberOfPositions!: number;
+  @Prop() problems!: omegaup.Problem[];
+  @Prop() ranking!: types.ScoreboardRankingEntry[];
+  @Prop({ default: null }) rankingChartOptions!: Highcharts.Options | null;
+  @Prop() lastUpdated!: Date;
+  @Prop({ default: true }) showInvitedUsersFilter!: boolean;
+  @Prop({ default: true }) showPenalty!: boolean;
+  @Prop({ default: false }) showAllContestants!: boolean;
+  @Prop({ default: 2 }) digitsAfterDecimalPoint!: number;
+  @Prop() title!: string;
+  @Prop({ default: null }) finishTime!: null | Date;
+  @Prop({ default: SocketStatus.Waiting }) socketStatus!: SocketStatus;
+
+  T = T;
+  ui = ui;
+  INF = '∞';
+  onlyShowExplicitlyInvited =
+    !this.showAllContestants && this.showInvitedUsersFilter;
+  nameDisplayOptions: ui.NameDisplayOptions =
+    ui.NameDisplayOptions.NameAndUsername;
+
+  get lastUpdatedString(): null | string {
+    if (!this.lastUpdated) return null;
+    return ui.formatString(T.scoreboardLastUpdated, {
+      datetime: time.formatDateTime(this.lastUpdated),
+    });
+  }
+
+  get socketClass(): string {
+    if (this.socketStatus === SocketStatus.Connected) {
+      return 'socket-status socket-status-ok';
+    }
+    if (this.socketStatus === SocketStatus.Failed) {
+      return 'socket-status socket-status-error';
+    }
+    return 'socket-status';
+  }
+
+  get socketStatusTitle(): string {
+    if (this.socketStatus === SocketStatus.Connected) {
+      return T.socketStatusConnected;
+    }
+    if (this.socketStatus === SocketStatus.Failed) {
+      return T.socketStatusFailed;
+    }
+    return T.socketStatusWaiting;
+  }
+
+  legendClass(idx: number): string {
+    return idx < this.numberOfPositions ? `legend-${idx + 1}` : '';
+  }
+
+  renderPoints(p: types.ScoreboardRankingProblem): string {
+    return (
+      (p.points > 0 ? '+' : '') + p.points.toFixed(this.digitsAfterDecimalPoint)
+    );
+  }
+
+  totalRuns(u: types.ScoreboardRankingEntry): number {
+    return u.problems.reduce(
+      (acc: number, val: types.ScoreboardRankingProblem) => acc + val.runs,
+      0,
+    );
+  }
+
+  problemClass(p: types.ScoreboardRankingProblem, alias: string): string {
+    if (p.percent === 100) {
+      return `${alias} accepted`;
+    } else if (p.pending) {
+      return `${alias} pending`;
+    } else if (p.percent === 0 && p.runs > 0) {
+      return `${alias} wrong`;
+    } else {
+      return alias;
+    }
+  }
+
+  showUser(userIsInvited: boolean): boolean {
+    // Invited users filter is only available in contests, in a course all users
+    // are visible in scoreboard.
+    if (!this.showInvitedUsersFilter) return true;
+    return userIsInvited || !this.onlyShowExplicitlyInvited;
+  }
+}
+</script>
+
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
 
@@ -353,233 +581,6 @@ export default class ArenaScoreboard extends Vue {
   }
 }
 </style>
-<template>
-  <div class="omegaup-scoreboard px-2">
-    <slot name="scoreboard-header">
-      <div class="text-center mt-4 pt-2">
-        <h2 class="mb-4">
-          <span>{{ title }}</span>
-          <slot name="socket-status">
-            <sup :class="socketClass" :title="socketStatusTitle">{{
-              socketStatus
-            }}</sup>
-          </slot>
-        </h2>
-        <div v-if="!finishTime" class="clock">{{ INF }}</div>
-        <omegaup-countdown
-          v-else
-          class="clock"
-          :target-time="finishTime"
-        ></omegaup-countdown>
-      </div>
-    </slot>
-    <highcharts
-      v-if="rankingChartOptions"
-      :options="rankingChartOptions"
-    ></highcharts>
-    <label v-if="showInvitedUsersFilter">
-      <input
-        v-model="onlyShowExplicitlyInvited"
-        class="toggle-contestants"
-        type="checkbox"
-      />
-      {{ T.scoreboardShowOnlyInvitedIdentities }}</label
-    >
-    <label class="float-right"
-      >{{ T.scoreboardShowParticipantsNames }}:
-      <select
-        v-model="nameDisplayOptions"
-        class="form-control"
-        data-scoreboard-options
-      >
-        <option :value="ui.NameDisplayOptions.Name">{{ T.wordsName }}</option>
-        <option :value="ui.NameDisplayOptions.Username">
-          {{ T.wordsUser }}
-        </option>
-        <option :value="ui.NameDisplayOptions.NameAndUsername">
-          {{ T.arenaNameAndUsername }}
-        </option>
-      </select>
-    </label>
-    <div class="table-responsive">
-      <table data-table-scoreboard class="table">
-        <thead>
-          <tr>
-            <th><!-- legend --></th>
-            <th><!-- position --></th>
-            <th>{{ T.contestParticipant }}</th>
-            <th v-for="(problem, index) in problems" :key="problem.alias">
-              <a :href="'#problems/' + problem.alias" :title="problem.alias">{{
-                ui.columnName(index)
-              }}</a>
-            </th>
-            <th :colspan="2 + problems.length">{{ T.wordsTotal }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <template v-for="(user, userIndex) in ranking">
-            <tr
-              v-if="showUser(user.is_invited)"
-              :key="`${user.username}-${user.virtual}`"
-              :class="user.username"
-            >
-              <td class="legend" :class="legendClass(userIndex)"></td>
-              <td class="position" data-table-scoreboard-position>
-                {{ user.place || '—' }}
-              </td>
-              <td class="user" data-table-scoreboard-username>
-                {{ ui.rankingUsername(user, nameDisplayOptions) }}
-                <img
-                  v-if="user.country"
-                  alt=""
-                  height="11"
-                  :src="`/media/flags/${user.country.toLowerCase()}.png`"
-                  :title="user.country"
-                  width="16"
-                />
-              </td>
-
-              <td
-                v-for="(problem, problemIndex) in user.problems"
-                :key="problem.alias"
-                :class="problemClass(problem, problems[problemIndex].alias)"
-              >
-                <template v-if="problem.runs > 0">
-                  <div class="points">
-                    {{ renderPoints(problem) }}
-                  </div>
-                  <div class="penalty">
-                    <span v-if="showPenalty">{{ problem.penalty }}</span> ({{
-                      problem.runs
-                    }})
-                  </div>
-                </template>
-                <template v-else> - </template>
-              </td>
-              <td>
-                <div class="points">
-                  {{ user.total.points.toFixed(digitsAfterDecimalPoint) }}
-                </div>
-                <div class="penalty">
-                  {{ user.total.penalty }} ({{ totalRuns(user) }})
-                </div>
-              </td>
-            </tr>
-          </template>
-        </tbody>
-      </table>
-    </div>
-    <div class="footer">
-      {{ lastUpdatedString }}
-    </div>
-  </div>
-</template>
-
-<script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
-
-import * as Highcharts from 'highcharts/highstock';
-import { Chart } from 'highcharts-vue';
-import { types } from '../../api_types';
-import { omegaup } from '../../omegaup';
-import T from '../../lang';
-import * as ui from '../../ui';
-import * as time from '../../time';
-import omegaup_Countdown from '../Countdown.vue';
-import { SocketStatus } from '../../arena/events_socket';
-
-@Component({
-  components: {
-    highcharts: Chart,
-    'omegaup-countdown': omegaup_Countdown,
-  },
-})
-export default class ArenaScoreboard extends Vue {
-  @Prop({ default: 10 }) numberOfPositions!: number;
-  @Prop() problems!: omegaup.Problem[];
-  @Prop() ranking!: types.ScoreboardRankingEntry[];
-  @Prop({ default: null }) rankingChartOptions!: Highcharts.Options | null;
-  @Prop() lastUpdated!: Date;
-  @Prop({ default: true }) showInvitedUsersFilter!: boolean;
-  @Prop({ default: true }) showPenalty!: boolean;
-  @Prop({ default: false }) showAllContestants!: boolean;
-  @Prop({ default: 2 }) digitsAfterDecimalPoint!: number;
-  @Prop() title!: string;
-  @Prop({ default: null }) finishTime!: null | Date;
-  @Prop({ default: SocketStatus.Waiting }) socketStatus!: SocketStatus;
-
-  T = T;
-  ui = ui;
-  INF = '∞';
-  onlyShowExplicitlyInvited =
-    !this.showAllContestants && this.showInvitedUsersFilter;
-  nameDisplayOptions: ui.NameDisplayOptions =
-    ui.NameDisplayOptions.NameAndUsername;
-
-  get lastUpdatedString(): null | string {
-    if (!this.lastUpdated) return null;
-    return ui.formatString(T.scoreboardLastUpdated, {
-      datetime: time.formatDateTime(this.lastUpdated),
-    });
-  }
-
-  get socketClass(): string {
-    if (this.socketStatus === SocketStatus.Connected) {
-      return 'socket-status socket-status-ok';
-    }
-    if (this.socketStatus === SocketStatus.Failed) {
-      return 'socket-status socket-status-error';
-    }
-    return 'socket-status';
-  }
-
-  get socketStatusTitle(): string {
-    if (this.socketStatus === SocketStatus.Connected) {
-      return T.socketStatusConnected;
-    }
-    if (this.socketStatus === SocketStatus.Failed) {
-      return T.socketStatusFailed;
-    }
-    return T.socketStatusWaiting;
-  }
-
-  legendClass(idx: number): string {
-    return idx < this.numberOfPositions ? `legend-${idx + 1}` : '';
-  }
-
-  renderPoints(p: types.ScoreboardRankingProblem): string {
-    return (
-      (p.points > 0 ? '+' : '') + p.points.toFixed(this.digitsAfterDecimalPoint)
-    );
-  }
-
-  totalRuns(u: types.ScoreboardRankingEntry): number {
-    return u.problems.reduce(
-      (acc: number, val: types.ScoreboardRankingProblem) => acc + val.runs,
-      0,
-    );
-  }
-
-  problemClass(p: types.ScoreboardRankingProblem, alias: string): string {
-    if (p.percent === 100) {
-      return `${alias} accepted`;
-    } else if (p.pending) {
-      return `${alias} pending`;
-    } else if (p.percent === 0 && p.runs > 0) {
-      return `${alias} wrong`;
-    } else {
-      return alias;
-    }
-  }
-
-  showUser(userIsInvited: boolean): boolean {
-    // Invited users filter is only available in contests, in a course all users
-    // are visible in scoreboard.
-    if (!this.showInvitedUsersFilter) return true;
-    return userIsInvited || !this.onlyShowExplicitlyInvited;
-  }
-}
-</script>
 
 <style lang="scss" scoped>
 @import '../../../../sass/main.scss';
