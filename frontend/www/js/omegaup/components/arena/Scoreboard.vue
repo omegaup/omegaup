4e40e32055f1764c8a2a92cc71c58113940097d4
Ingrid Margarita <margaret9197@gmail.com>
2023-07-31 11:23:05 -0500

    Agregando un mensaje sino hay información que mostrar en el Ranking.

--- frontend/www/js/omegaup/components/arena/Scoreboard.vue
+++ frontend/www/js/omegaup/components/arena/Scoreboard.vue
@@ -19,9 +19,12 @@
       </div>
     </slot>
     <highcharts
-      v-if="rankingChartOptions"
+      v-if="rankingChartOptions && Object.keys(rankingChartOptions).length"
       :options="rankingChartOptions"
     ></highcharts>
+    <div v-else class="bg-white text-center p-4 mb-3">
+      No tenemos datos para mostrar en el Ranking.
+    </div>
     <label v-if="showInvitedUsersFilter">
       <input
         v-model="onlyShowExplicitlyInvited"
