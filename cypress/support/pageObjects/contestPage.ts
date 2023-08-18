4a9babeea96ad0bcf02f9dbe8515deb93ce7b553
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2023-08-18 17:44:43 +0530

    Fix failure

--- cypress/support/pageObjects/contestPage.ts
+++ cypress/support/pageObjects/contestPage.ts
@@ -200,7 +200,7 @@ export class ContestPage {
     for (let i = 0; i < noOfProblems; i++) {
       const problemOptions: ProblemOptions = {
         problemAlias: uuid().slice(0, 10),
-        tag: 'Recursion',
+        tag: 'Recursión',
         autoCompleteTextTag: 'recur',
         problemLevelIndex: 0,
       };
