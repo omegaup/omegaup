49e475df1da31c05b310374e32fe6e283d180529
Karyme-Nava <s19120124@alumnos.itsur.edu.mx>
2023-09-06 10:42:48 -0600

    Agregando funciones que se usarán en apiGetCertificatePdf para generar certificados de cursos

--- frontend/server/src/Controllers/Certificate.php
+++ frontend/server/src/Controllers/Certificate.php
@@ -26,4 +26,35 @@ class Certificate extends \OmegaUp\Controllers\Controller {
             'entrypoint' => 'certificate_details',
         ];
     }
+
+    private static function createCertificatePdf(
+        string $title,
+        string $identityName,
+        string $description,
+        int $date
+    ): string {
+        return '';
+    }
+
+    private static function getCourseCertificate(string $verification_code): string {
+        $certificateData = \OmegaUp\DAO\Certificates::getCourseCertificateByVerificationCode(
+            $verification_code
+        );
+
+        if (is_null($certificateData)) {
+            return '';
+        }
+
+        $title = 'Graduación';
+        $identityName = $certificateData['identity_name'];
+        $description = 'por completar el curso "' . $certificateData['course_name'] . '" en la plataforma omegaUp.com';
+        $date = $certificateData['timestamp']->time;
+
+        return self::createCertificatePdf(
+            $title,
+            $identityName,
+            $description,
+            $date
+        );
+    }
 }
---
+++ frontend/server/src/DAO/Certificates.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace OmegaUp\DAO;
+
+/**
+ * Certificates Data Access Object (DAO).
+ *
+ * Esta clase contiene toda la manipulacion de bases de datos que se necesita
+ * para almacenar de forma permanente y recuperar instancias de objetos
+ * {@link \OmegaUp\DAO\VO\Certificates}.
+ * @access public
+ * @package docs
+ */
+class Certificates extends \OmegaUp\DAO\Base\Certificates {
+     /**
+     * Returns the data of the course certificate using its verification code
+     *
+     * @return array{course_name: string, identity_name: string, timestamp: \OmegaUp\Timestamp}|null
+     */
+    final public static function getCourseCertificateByVerificationCode(
+        string $verification_code
+    ) {
+        $sql = '
+            SELECT
+                co.name AS course_name,
+                i.name AS identity_name,
+                ce.timestamp
+            FROM
+                Certificates ce
+            INNER JOIN
+                Courses co
+            ON
+                ce.course_id = co.course_id
+            INNER JOIN
+                Identities i
+            ON
+                i.identity_id = ce.identity_id
+            WHERE
+                ce.verification_code = ?;
+        ';
+
+        /** @var array{course_name: string, identity_name: string, timestamp: \OmegaUp\Timestamp}|null */
+        $data = \OmegaUp\MySQLConnection::getInstance()->GetRow(
+            $sql,
+            [$verification_code]
+        );
+
+        return $data;
+    }
+}
