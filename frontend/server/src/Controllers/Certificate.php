d24239f0cafa0491d2e67fa44ac7de12ea6edb4f
Karyme-Nava <s19120124@alumnos.itsur.edu.mx>
2023-09-06 21:51:51 -0600

    Agregando traducciones para los PDFs de los certificados

--- frontend/server/src/Controllers/Certificate.php
+++ frontend/server/src/Controllers/Certificate.php
@@ -9,9 +9,6 @@ use setasign\Fpdi\Fpdi;
  * @psalm-type CertificateDetailsPayload=array{uuid: string}
  */
 class Certificate extends \OmegaUp\Controllers\Controller {
-    /** @var list<string> */
-    private static $months = ['Diciembre', 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-
     /**
      * @return array{templateProperties: array{payload: CertificateDetailsPayload, title: \OmegaUp\TranslationString}, entrypoint: string}
      *
@@ -31,49 +28,197 @@ class Certificate extends \OmegaUp\Controllers\Controller {
         ];
     }
 
+    private static function getMonthName(int $month): string {
+        $translator = \OmegaUp\Translations::getInstance();
+        switch ($month) {
+            case 1:
+                return $translator->get('certificatePdfMonth1');
+            case 2:
+                return $translator->get('certificatePdfMonth2');
+            case 3:
+                return $translator->get('certificatePdfMonth3');
+            case 4:
+                return $translator->get('certificatePdfMonth4');
+            case 5:
+                return $translator->get('certificatePdfMonth5');
+            case 6:
+                return $translator->get('certificatePdfMonth6');
+            case 7:
+                return $translator->get('certificatePdfMonth7');
+            case 8:
+                return $translator->get('certificatePdfMonth8');
+            case 9:
+                return $translator->get('certificatePdfMonth9');
+            case 10:
+                return $translator->get('certificatePdfMonth10');
+            case 11:
+                return $translator->get('certificatePdfMonth11');
+            default:
+                return $translator->get('certificatePdfMonth12');
+        }
+    }
+
     private static function createCertificatePdf(
         string $title,
         string $identityName,
         string $description,
         int $date
     ): string {
+        $translator = \OmegaUp\Translations::getInstance();
         $pdf = new FPDI('L');
 
         $pdf->setSourceFile('/opt/omegaup/stuff/CertificateTemplate.pdf');
         $templateId = $pdf->importPage(1);
-        $pdf->addPage();
+        $pdf->AddPage();
         $pdf->useTemplate($templateId);
+        $pdf->SetAutoPageBreak(false);
 
-        $pdf->SetFont('Arial', 'B', 39);
-        $pdf->SetXY(50, 76);
-        $pdf->Cell(215, 15, utf8_decode($title), 0, 1, 'C');
+        $x = 50;
+        $y = 0;
+        $width = 215;
+        $height = 15;
+        $border = 0;
+        $ln = 1;
+        $center = 'C';
+        $bold = 'B';
+        $italic = 'I';
 
-        $pdf->SetFont('Arial', 'B', 21);
-        $pdf->SetXY(50, 109);
-        $pdf->Cell(215, 15, utf8_decode($identityName), 0, 1, 'C');
-
-        $pdf->SetFont('Arial', '', 18);
-        $pdf->SetXY(50, 132);
-        $pdf->MultiCell(215, 10, utf8_decode($description), 0, 'C');
-
-        $pdf->SetFont('Arial', 'I', 14);
+        $pdf->SetFont('Arial', $italic, 14);
         $pdf->SetTextColor(153, 153, 153);
-        $pdf->SetXY(50, 151);
-        $day = date('j', $date);
-        $month = intval(date('n', $date));
-        $year = date('o', $date);
+
+        $y = 41;
+        $pdf->SetXY($x, $y);
         $pdf->Cell(
-            215,
-            10,
+            $width,
+            $height,
             utf8_decode(
-                'Bellevue, Washington a ' . $day . ' de ' . self::$months[$month] . ' de ' . $year
+                $translator->get(
+                    'certificatePdfHeader'
+                )
             ),
-            0,
-            1,
-            'C'
+            $border,
+            $ln,
+            $center
         );
 
+        $y = 148;
+        $pdf->SetXY($x, $y);
+        $day = intval(date('j', $date));
+        $month = intval(date('n', $date));
+        $year = intval(date('o', $date));
+        $pdf->Cell(
+            $width,
+            $height,
+            utf8_decode(
+                sprintf(
+                    $translator->get('certificatePdfPlaceAndDate'),
+                    $day,
+                    self::getMonthName($month),
+                    $year
+                )
+            ),
+            $border,
+            $ln,
+            $center
+        );
+
+        $y = 197;
+        $pdf->SetXY($x, $y);
+        $pdf->Cell(
+            $width,
+            $height,
+            utf8_decode(
+                $translator->get(
+                    'certificatePdfDirector'
+                )
+            ),
+            $border,
+            $ln,
+            $center
+        );
+
+        $pdf->SetFont('', $bold, 39);
+        $pdf->SetTextColor(0, 0, 0);
+
+        $y = 76;
+        $pdf->SetXY($x, $y);
+        $pdf->Cell($width, $height, $title, $border, $ln, $center);
+
+        $pdf->SetFont('', $bold, 21);
+
+        $y = 109;
+        $pdf->SetXY($x, $y);
+        $pdf->Cell(
+            $width,
+            $height,
+            $identityName,
+            $border,
+            $ln,
+            $center
+        );
+
+        $pdf->SetFont('', '', 18);
+
+        $y = 57;
+        $pdf->SetXY($x, $y);
+        $pdf->Cell(
+            $width,
+            $height,
+            utf8_decode(
+                $translator->get(
+                    'certificatePdfGrantsRecognition'
+                )
+            ),
+            $border,
+            $ln,
+            $center
+        );
+
+        $y = 92;
+        $pdf->SetXY($x, $y);
+        $pdf->Cell(
+            $width,
+            $height,
+            utf8_decode(
+                $translator->get(
+                    'certificatePdfPerson'
+                )
+            ),
+            $border,
+            $ln,
+            $center
+        );
+
+        $y = 132;
+        $height = 10;
+        $pdf->SetXY($x, $y);
+        $pdf->MultiCell(
+            $width,
+            $height,
+            $description,
+            $border,
+            $center
+        );
+
+        $pdf->Output('Certificate.pdf', 'F');
         return $pdf->Output('', 'S');
+    }
+
+    private static function getPlaceSuffix(int $n): string {
+        $translator = \OmegaUp\Translations::getInstance();
+        if ($n >= 11 && $n <= 13) {
+            return $translator->get('certificatePdfContestPlace0');
+        }
+        if (($n % 10) == 1) {
+            return $translator->get('certificatePdfContestPlace1');
+        }
+        if (($n % 10) == 2) {
+            return $translator->get('certificatePdfContestPlace2');
+        }
+        if (($n % 10) == 3) {
+            return $translator->get('certificatePdfContestPlace3');
+        }
+        return $translator->get('certificatePdfContestPlace0');
     }
 
     private static function getContestCertificate(string $verification_code): string {
@@ -85,9 +230,22 @@ class Certificate extends \OmegaUp\Controllers\Controller {
             return '';
         }
 
-        $title = $certificateData['contest_place'] . '° lugar';
-        $identityName = $certificateData['identity_name'];
-        $description = 'por su participación en el concurso "' . $certificateData['contest_title'] . '" que se llevó a cabo en la plataforma omegaUp.com';
+        $translator = \OmegaUp\Translations::getInstance();
+        $placeNumber = intval($certificateData['contest_place']);
+        $title = utf8_decode(
+            $placeNumber
+            . self::getPlaceSuffix($placeNumber)
+            . $translator->get(
+                'certificatePdfContestPlace'
+            )
+        );
+        $identityName = utf8_decode($certificateData['identity_name']);
+        $description = utf8_decode(
+            sprintf(
+                $translator->get('certificatePdfContestDescription'),
+                $certificateData['contest_title']
+            )
+        );
         $date = $certificateData['timestamp']->time;
 
         return self::createCertificatePdf(
