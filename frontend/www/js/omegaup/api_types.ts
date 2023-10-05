959d4a35e7d3c498269fa7dffd84ccdc6f2b2710
Karyme Jocelyn Nava Osorio <78821834+Karyme-Nava@users.noreply.github.com>
2023-10-05 15:14:53 -0600

    Agregando apiValidateCertificate (#7247)
    # Descripción
    
    Se agregó apiValidateCertificate que permite validar si un certificado
    existe mediante su código de verificación
    
    Fixes: #7248
    
    # Checklist:
    
    - [x] El código sigue la [guía de
    estilo](https://github.com/omegaup/omegaup/wiki/Coding-guidelines) de
    omegaUp.
    - [x] Se corrieron todas las pruebas y pasaron.
    - [x] Si se está agregando funcionalidad nueva, se agregaron pruebas.
    - [ ] Si el cambio es grande (> 200 líneas), hay que intentar partirlo
    en varios pull requests. De preferencia uno para los controladores +
    phpunit y luego otro para la interfaz.

--- frontend/www/js/omegaup/api_types.ts
+++ frontend/www/js/omegaup/api_types.ts
@@ -4855,6 +4855,8 @@ export namespace messages {
   // Certificate
   export type CertificateGetCertificatePdfRequest = { [key: string]: any };
   export type CertificateGetCertificatePdfResponse = { certificate: string };
+  export type CertificateValidateCertificateRequest = { [key: string]: any };
+  export type CertificateValidateCertificateResponse = { valid: boolean };
 
   // Clarification
   export type ClarificationCreateRequest = { [key: string]: any };
@@ -5750,6 +5752,9 @@ export namespace controllers {
     getCertificatePdf: (
       params?: messages.CertificateGetCertificatePdfRequest,
     ) => Promise<messages.CertificateGetCertificatePdfResponse>;
+    validateCertificate: (
+      params?: messages.CertificateValidateCertificateRequest,
+    ) => Promise<messages.CertificateValidateCertificateResponse>;
   }
 
   export interface Clarification {
