16383ff06d4823e6ee50c82e76c5352c324297dd
Karyme-Nava <s19120124@alumnos.itsur.edu.mx>
2023-10-18 15:51:51 -0600

    Corrigiendo prueba para que use el link de descarga

--- frontend/www/js/omegaup/components/certificate/Mine.test.ts
+++ frontend/www/js/omegaup/components/certificate/Mine.test.ts
@@ -11,19 +11,30 @@ describe('Mine.vue', () => {
       {
         certificate_type: 'contest',
         date: new Date(),
-        name: 'Contest',
+        name: 'Test contest',
         verification_code: 'k2s8OPl620',
       },
       {
         certificate_type: 'course',
         date: new Date(),
-        name: 'Course',
+        name: 'Test course',
         verification_code: '9soPa69jP3',
       },
+      {
+        certificate_type: 'coder_of_the_month',
+        date: new Date(),
+        verification_code: 'dodp874598',
+      },
+      {
+        certificate_type: 'coder_of_the_month_female',
+        date: new Date(),
+        verification_code: 'gkspa12345',
+      },
     ],
+    location: 'https://omegaup.com/',
   };
 
-  it('Should handle an empty table', async () => {
+  it('Should handle an empty table', () => {
     const wrapper = shallowMount(certificate_Mine, {
       propsData: {
         certificates: [] as types.CertificateListItem[],
@@ -35,40 +46,42 @@ describe('Mine.vue', () => {
     expect(wrapper.text()).toContain(T.certificateListMineCertificatesEmpty);
   });
 
-  it('Should handle a table with data', async () => {
+  it('Should handle a table with data', () => {
     const wrapper = mount(certificate_Mine, {
       propsData,
     });
 
     expect(wrapper.text()).toContain(T.certificateListMineTitle);
     expect(wrapper.find('table').exists()).toBeTruthy();
-    expect(wrapper.text()).toContain('Contest');
-    expect(wrapper.text()).toContain('Course');
+    expect(wrapper.text()).toContain('Test contest');
+    expect(wrapper.text()).toContain('Test course');
+    expect(wrapper.text()).toContain(T.certificateListMineCoderOfTheMonth);
+    expect(wrapper.text()).toContain(
+      T.certificateListMineCoderOfTheMonthFemale,
+    );
     expect(wrapper.text()).toContain(T.certificateListMineDownload);
     expect(wrapper.text()).toContain(T.certificateListMineCopyToClipboard);
   });
 
-  it('Should handle download pdf certificates', async () => {
+  it('Should download a file', async () => {
+    const clickSpy = jest.spyOn(
+      (certificate_Mine as any).options.methods,
+      'getDownloadLink',
+    );
+
     const wrapper = mount(certificate_Mine, {
       propsData,
     });
 
-    await wrapper.findAll('button').trigger('click');
-    expect(wrapper.emitted('download-pdf-certificate')).toBeDefined();
-    expect(wrapper.emitted('download-pdf-certificate')).toHaveLength(2);
-    expect(wrapper.emitted('download-pdf-certificate')).toEqual([
-      [
-        {
-          verificationCode: 'k2s8OPl620',
-          name: 'Contest',
-        },
-      ],
-      [
-        {
-          verificationCode: '9soPa69jP3',
-          name: 'Course',
-        },
-      ],
-    ]);
+    await wrapper.findAll('a').trigger('click');
+
+    await wrapper.vm.$nextTick();
+
+    expect(clickSpy).toHaveBeenCalledTimes(propsData['certificates'].length);
+    propsData['certificates'].forEach((certificate) => {
+      expect(clickSpy).toHaveBeenCalledWith(certificate.verification_code);
+    });
+
+    clickSpy.mockRestore();
   });
 });
