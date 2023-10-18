000498a8b5f45ff9a977005ef1d6241366497ab5
Ingrid Margarita <margaret9197@gmail.com>
2023-10-18 14:44:31 -0500

    Tratando de arreglar el código.

--- frontend/www/js/omegaup/components/user/Dependents.vue
+++ frontend/www/js/omegaup/components/user/Dependents.vue
@@ -1,19 +1,15 @@
 <template>
-    <div class="card">
-        HOlassssssssss
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import { Vue, Component } from 'vue-property-decorator';
-  import T from '../../lang';
+  <div class="card">HOlassssssssss</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import T from '../../lang';
 //   import * as ui from '../../ui';
-  
-  @Component
-  export default class UserDependents extends Vue {
-    T = T;
-    // ui = ui;
-  }
-  </script>
-  
-  
+
+@Component
+export default class UserDependents extends Vue {
+  T = T;
+  // ui = ui;
+}
+</script>
