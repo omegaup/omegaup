512413a00719a23c9d1b306a8a070640e3aa76ac
Ingrid Margarita <margaret9197@gmail.com>
2023-10-18 15:47:55 -0500

    Componente funcionando.

--- frontend/www/js/omegaup/components/user/Dependents.vue
+++ frontend/www/js/omegaup/components/user/Dependents.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="card">HOlassssssssss</div>
+  <div class="card">
+    <h5
+      class="card-header d-flex justify-content-between align-items-center rank-title"
+    >
+      klwlgledglk
+    </h5>
+    <table class="table mb-0">
+      <thead>
+        <tr>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">{{ T.contentCreator }}</th>
+          <th scope="col" class="text-center">{{ T.rankScore }}</th>
+          <th scope="col" class="text-center"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(author, index) in rankingData.ranking" :key="index">
+          <th scope="row" class="text-center">
+            {{ author.author_ranking || index }}
+          </th>
+          <td class="text-center"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
