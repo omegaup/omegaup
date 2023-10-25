ae0cd9132e5732658e3387676fae87bfe6b75602
galovdev <contacto.detodounpocomx@gmail.com>
2023-10-24 21:13:24 -0600

    conflict

--- frontend/www/js/omegaup/api_types.ts
+++ frontend/www/js/omegaup/api_types.ts
@@ -3899,6 +3899,7 @@ export namespace types {
     emailClarifications: boolean;
     extraWallTime: number | string;
     groupScorePolicy?: string;
+    hasVisitedSection?: boolean;
     inputLimit: number | string;
     languages: string;
     levelTags: string[];
