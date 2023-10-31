ec92e7f3a38a9e9e43300fd46c71fcc68ef1191f
Karyme-Nava <s19120124@alumnos.itsur.edu.mx>
2023-10-31 17:24:12 -0600

    Agregando excepciones

--- frontend/www/certificate/download.php
+++ frontend/www/certificate/download.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace OmegaUp;
 require_once(dirname(__DIR__, 2) . '/server/bootstrap.php');
 
 if (!isset($_GET['verification_code'])) {
-    die();
+    throw new \OmegaUp\Exceptions\InvalidParameterException(
+        'parameterEmpty',
+        'verification_code'
+    );
 }
 /** @var string */
 $verificationCode = $_GET['verification_code'];
@@ -12,7 +16,7 @@ $blobData = \OmegaUp\Controllers\Certificate::getCertificatePdf(
     $verificationCode
 );
 if (is_null($blobData)) {
-    die();
+    throw new \OmegaUp\Exceptions\NotFoundException('certificateNotFound');
 }
 header('Content-Type: application/pdf');
 header("Content-Disposition: attachment; filename={$fileName}");
