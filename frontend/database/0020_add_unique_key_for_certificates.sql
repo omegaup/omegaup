9855a726c207135899af278730bda3bd7ae0f3fc
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2023-11-01 09:59:31 -0600

    Eliminando archivo con nombre erroneo

--- frontend/database/0020_add_unique_key_for_certificates.sql
+++
@@ -1,4 +0,0 @@
--- Add unique keys to Certificates
-ALTER TABLE `Certificates`
-    ADD UNIQUE KEY `contest_identity_key` (`identity_id`, `contest_id`, `certificate_type`),
-    ADD UNIQUE KEY `course_identity_key` (`identity_id`, `course_id`, `certificate_type`);
