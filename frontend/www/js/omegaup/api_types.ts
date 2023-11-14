9d7a0bf1b496115a0fe295a5ebcbf6131242a3e7
Karyme Jocelyn Nava Osorio <78821834+Karyme-Nava@users.noreply.github.com>
2023-11-14 11:09:38 -0600

    Revert "Revert "Agregando primera parte de apiGenerateContestCertificates" (#7334)"
    This reverts commit d3071fd240151f5ce0a35f67731de18689b7cc73.

--- frontend/www/js/omegaup/api_types.ts
+++ frontend/www/js/omegaup/api_types.ts
@@ -4878,6 +4878,10 @@ export namespace messages {
   export type BadgeUserListResponse = { badges: types.Badge[] };
 
   // Certificate
+  export type CertificateGenerateContestCertificatesRequest = {
+    [key: string]: any;
+  };
+  export type CertificateGenerateContestCertificatesResponse = {};
   export type CertificateGetCertificatePdfRequest = { [key: string]: any };
   export type CertificateGetCertificatePdfResponse = { certificate?: string };
   export type CertificateGetUserCertificatesRequest = { [key: string]: any };
@@ -5779,6 +5783,9 @@ export namespace controllers {
   }
 
   export interface Certificate {
+    generateContestCertificates: (
+      params?: messages.CertificateGenerateContestCertificatesRequest,
+    ) => Promise<messages.CertificateGenerateContestCertificatesResponse>;
     getCertificatePdf: (
       params?: messages.CertificateGetCertificatePdfRequest,
     ) => Promise<messages.CertificateGetCertificatePdfResponse>;
