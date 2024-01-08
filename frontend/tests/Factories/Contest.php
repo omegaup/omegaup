8a6e52106849ca502d727d1d239dcaaa92d6f048
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2024-01-07 20:38:30 -0600

    Arreglando el  penalty cuando se cambia la versión de problema en concursos (#7387)
    # Descripción
    
    Se arregla el valor de `penalty` cuando se cambia la versión de un
    problema
    dentro de un concurso.
    
    Esto sucedía principalmente porque al cambiar la versión del problema,
    se crea
    un nuevo registro en la tabla de `Runs` para diferenciar los resultados
    del
    envío con ambas versiones. 
    
    El problema que estaba ocurriendo es que no se recalculaban los valores
    de
    penalty. Y tomando en cuenta que este no depende de la versión del
    problema,
    sino de la configuración del concurso, entonces cada que se actualiza la
    versión
    del problema, ahora se recalculan dichos valores de penalty para todos
    los
    envíos.
    
    También se arregló la consulta de recalular valores de penalty, ya que
    sólo estaba
    aplicando el cambio a los envíos originales, e ignoraba todos los envíos
    de
    problemas en donde se cambió su versión.
    
    
    ![RunPenaltyInUpdateProblem](https://github.com/omegaup/omegaup/assets/3230352/9efe5067-f7b4-4598-930e-28afa60aacc6)
    
    
    Fixes: #5805 
    
    # Checklist:
    
    - [x] El código sigue la [guía de
    estilo](https://github.com/omegaup/omegaup/wiki/Coding-guidelines) de
    omegaUp.
    - [x] Se corrieron todas las pruebas y pasaron.
    - [x] Si se está agregando funcionalidad nueva, se agregaron pruebas.

--- frontend/tests/Factories/Contest.php
+++ frontend/tests/Factories/Contest.php
@@ -123,7 +123,13 @@ class ContestParams {
     public $showScoreboardAfter;
 
     /**
-     * @param array{alias?: string, title?: string, admissionMode?: string, basicInformation?: bool, contestForTeams?: bool, teamsGroupAlias?: string, requestsUserInformation?: string, contestDirector?: \OmegaUp\DAO\VO\Identities, contestDirectorUser?: \OmegaUp\DAO\VO\Users, windowLength?: ?int, languages?: ?list<string>, startTime?: \OmegaUp\Timestamp, finishTime?: \OmegaUp\Timestamp, lastUpdated?: \OmegaUp\Timestamp, penaltyCalcPolicy?: string, feedback?: string, scoreMode?: string, checkPlagiarism?: bool, scoreboardPct?: int, showScoreboardAfter?: bool} $params
+     * @readonly
+     * @var string
+     */
+    public $penaltyType;
+
+    /**
+     * @param array{alias?: string, admissionMode?: string, basicInformation?: bool, checkPlagiarism?: bool, contestDirector?: \OmegaUp\DAO\VO\Identities, contestDirectorUser?: \OmegaUp\DAO\VO\Users, contestForTeams?: bool, feedback?: string, finishTime?: \OmegaUp\Timestamp, languages?: ?list<string>, lastUpdated?: \OmegaUp\Timestamp, penaltyCalcPolicy?: string, penaltyType?: string, requestsUserInformation?: string, scoreboardPct?: int, scoreMode?: string, showScoreboardAfter?: bool, startTime?: \OmegaUp\Timestamp, teamsGroupAlias?: string, title?: string, windowLength?: ?int} $params
      */
     public function __construct($params = []) {
         $this->title = $params['title'] ?? \OmegaUp\Test\Utils::createRandomString();
@@ -160,6 +166,7 @@ class ContestParams {
             new \OmegaUp\Timestamp(\OmegaUp\Time::get() + 60 * 60)
         );
         $this->penaltyCalcPolicy = $params['penaltyCalcPolicy'] ?? 'sum';
+        $this->penaltyType = $params['penaltyType'] ?? 'contest_start';
         $this->feedback = $params['feedback'] ?? 'detailed';
         $this->contestForTeams = $params['contestForTeams'] ?? false;
         $this->teamsGroupAlias = $params['teamsGroupAlias'] ?? null;
@@ -210,7 +217,7 @@ class Contest {
             'feedback' => $params->feedback,
             'penalty' => 100,
             'scoreboard' => $params->scoreboardPct,
-            'penalty_type' => 'contest_start',
+            'penalty_type' => $params->penaltyType,
             'languages' => $params->languages,
             'recommended' => 0, // This is just a default value, it is not honored by apiCreate.
             'needs_basic_information' => $params->basicInformation,
