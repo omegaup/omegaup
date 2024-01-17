16dc2bb0a870a344dbc5184b4835ff0d5c21a06b
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2024-01-17 09:58:48 -0600

    Nit

--- frontend/database/00223_add_certificate_id_in_coder_of_the_month_table.sql
+++ frontend/database/00223_add_certificate_id_in_coder_of_the_month_table.sql
@@ -4,3 +4,4 @@ ALTER TABLE `Coder_Of_The_Month`
   ADD COLUMN `certificate_id` int DEFAULT NULL COMMENT 'Id del certificado que pertenece al Coder del mes',
   ADD KEY `certificate_id` (`certificate_id`),
   ADD CONSTRAINT `fk_cotmc_certificate_id` FOREIGN KEY (`certificate_id`) REFERENCES `Certificates` (`certificate_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  
