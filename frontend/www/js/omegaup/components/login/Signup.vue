8db96ba244e064f82ca2b1cd0783f9be234e2c2b
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2024-02-16 01:28:16 -0600

    Términos y condiciones obligatorios en formulario de registro actual

--- frontend/www/js/omegaup/components/login/Signup.vue
+++ frontend/www/js/omegaup/components/login/Signup.vue
@@ -97,6 +97,7 @@
                     password,
                     passwordConfirmation,
                     recaptchaResponse,
+                    termsAndPolicies,
                   })
                 "
               >
