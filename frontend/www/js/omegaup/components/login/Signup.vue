753cd4bcad582f99d4eb6dd22240add433e60b3d
Ingrid Ruiz Bravo <110271913+IngridRB@users.noreply.github.com>
2024-02-16 12:13:01 -0500

    Agregando un selector de fecha en el formulario de registro. (#7255)
    # Descripción
    
    Se agrego al formulario de registro un selector de fecha y un checkbox .
    Y si es menor de 13 años, se solicita la dirección de correo electrónico
    de su figura parental en lugar de su propia dirección de correo
    electrónico.
    
    El checkbox por defecto esta vacío, y el input day solo permitirá
    ingresar fechas de cumpleaños para personas menores de 13 años. Y se
    solicitara el correo electrónico de la figura parental.
    
    ![image](https://github.com/omegaup/omegaup/assets/110271913/e751644e-3a60-4fa5-8727-8d9e57909e23)
    
    Si el checkbox esta seleccionado, el input day solo permitirá ingresar
    fechas de cumpleaños para personas mayores de 13 años. Y se solicitara
    el correo electrónico de la persona que usara la plataforma.
    
    ![image](https://github.com/omegaup/omegaup/assets/110271913/ad30ade4-c2c4-4a54-aa79-069f226d1544)
    
    Fixes: #7252 
    
    # Checklist:
    
    - [x] El código sigue la [guía de
    estilo](https://github.com/omegaup/omegaup/wiki/Coding-guidelines) de
          omegaUp.
    - [x] Se corrieron todas las pruebas y pasaron.
    - [x] Si se está agregando funcionalidad nueva, se agregaron pruebas.
    - [x] Si el cambio es grande (> 200 líneas), hay que intentar partirlo
    en
    varios pull requests. De preferencia uno para los controladores +
    phpunit
          y luego otro para la interfaz.
    
    ---------
    
    Co-authored-by: Carlos Córdova Sáenz <carlosab1802@gmail.com>
    Co-authored-by: Hugo Dueñas <heduenas@users.noreply.github.com>
    Co-authored-by: Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>

--- frontend/www/js/omegaup/components/login/Signup.vue
+++ frontend/www/js/omegaup/components/login/Signup.vue
@@ -4,7 +4,7 @@
       <h2 class="card-title">{{ T.loginSignupHeader }}</h2>
     </div>
     <div class="card-body">
-      <form>
+      <form v-if="!useSignupFormWithBirthDate">
         <div class="row justify-content-md-center">
           <div class="col-md-4 col-md-offset-2 introjs-username">
             <div class="form-group">
@@ -91,15 +91,167 @@
                 name="sign_up"
                 type="submit"
                 @click.prevent="
-                  $emit(
-                    'register-and-login',
+                  $emit('register-and-login', {
                     username,
                     email,
                     password,
                     passwordConfirmation,
                     recaptchaResponse,
                     termsAndPolicies,
-                  )
+                  })
+                "
+              >
+                {{ T.loginSignUp }}
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+
+      <form v-else>
+        <div class="row">
+          <div class="col-md-4 offset-md-2">
+            <div class="form-group">
+              <input
+                v-model="over13Checked"
+                type="checkbox"
+                data-over-thirteen-checkbox
+                @change="updateDateRestriction"
+              />
+              <label for="checkbox" class="pl-1">
+                <omegaup-markdown
+                  :markdown="T.over13yearsOld"
+                ></omegaup-markdown>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div v-if="isUnder13" class="col-md-8 offset-md-2">
+            <div class="form-group">
+              <label class="control-label">{{ T.loginParentEmail }}</label>
+              <input
+                v-model="parentEmail"
+                name="reg_parent_email"
+                type="email"
+                class="form-control"
+                autocomplete="parent-email"
+              />
+            </div>
+          </div>
+          <div v-else class="col-md-8 offset-md-2 introjs-email">
+            <div class="form-group">
+              <label class="control-label">{{ T.loginEmail }}</label>
+              <input
+                v-model="email"
+                data-signup-email
+                name="reg_email"
+                type="email"
+                class="form-control"
+                autocomplete="email"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-md-center">
+          <div class="col-md-4 col-md-offset-2 introjs-username">
+            <div class="form-group">
+              <label class="control-label">{{ T.loginAccountName }}</label>
+              <input
+                v-model="username"
+                data-signup-username
+                name="reg_username"
+                class="form-control"
+                autocomplete="username"
+              />
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label class="control-label">{{ T.loginDateOfBirth }}</label>
+              <input
+                v-model="dateOfBirth"
+                name="reg_date_of_birth"
+                type="date"
+                class="form-control"
+                autocomplete="date-of-birth"
+                :max="maxDateForTimepicker"
+                :min="minDateForTimepicker"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-md-center">
+          <div class="col-md-4 col-md-offset-2 introjs-password">
+            <div class="form-group">
+              <label class="control-label">{{ T.loginPasswordCreate }}</label>
+              <input
+                v-model="password"
+                data-signup-password
+                name="reg_password"
+                type="password"
+                class="form-control"
+                autocomplete="new-password"
+              />
+            </div>
+          </div>
+          <div class="col-md-4 introjs-confirmpassword">
+            <div class="form-group">
+              <label class="control-label">{{ T.loginRepeatPassword }}</label>
+              <input
+                v-model="passwordConfirmation"
+                data-signup-repeat-password
+                name="reg_password_confirmation"
+                type="password"
+                class="form-control"
+                autocomplete="new-password"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="row justify-content-md-center">
+          <div class="col-md-10 introjs-terms-and-conditions">
+            <input v-model="termsAndPolicies" type="checkbox" />
+            <label for="checkbox" class="pl-1">
+              <omegaup-markdown
+                :markdown="T.acceptPrivacyPolicy"
+              ></omegaup-markdown>
+            </label>
+          </div>
+        </div>
+
+        <div class="row justify-content-md-center">
+          <div v-if="validateRecaptcha" class="col-md-4">
+            <vue-recaptcha
+              name="recaptcha"
+              sitekey="6LfMqdoSAAAAALS8h-PB_sqY7V4nJjFpGK2jAokS"
+              @verify="verify"
+              @expired="expired"
+            ></vue-recaptcha>
+          </div>
+        </div>
+
+        <div class="row justify-content-md-center">
+          <div class="col-md-4 col-md-offset-6">
+            <div class="form-group introjs-register">
+              <button
+                data-signup-submit
+                class="btn btn-primary form-control"
+                name="sign_up"
+                @click.prevent="
+                  $emit('register-and-login', {
+                    over13Checked,
+                    username,
+                    email,
+                    dateOfBirth,
+                    parentEmail,
+                    password,
+                    passwordConfirmation,
+                    recaptchaResponse,
+                    termsAndPolicies,
+                  })
                 "
               >
                 {{ T.loginSignUp }}
@@ -129,13 +281,18 @@ Vue.use(VueCookies, { expire: -1 });
 export default class Signup extends Vue {
   @Prop() validateRecaptcha!: boolean;
   @Prop() hasVisitedSection!: boolean;
+  @Prop({ default: false }) useSignupFormWithBirthDate!: boolean;
 
   T = T;
   username: string = '';
   email: string = '';
+  dateOfBirth: string = '';
+  parentEmail: string = '';
   password: string = '';
   passwordConfirmation: string = '';
   recaptchaResponse: string = '';
+  isUnder13: boolean = true;
+  over13Checked: boolean = false;
   termsAndPolicies: boolean = false;
 
   mounted() {
@@ -200,9 +357,41 @@ export default class Signup extends Vue {
   expired(): void {
     this.recaptchaResponse = '';
   }
+
+  get maxDateForTimepicker() {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = (currentDate.getMonth() + 1)
+      .toString()
+      .padStart(2, '0');
+    const currentDay = currentDate.getDate().toString().padStart(2, '0');
+
+    return this.over13Checked
+      ? `${currentYear - 13}-${currentMonth}-${currentDay}`
+      : `${currentYear}-${currentMonth}-${currentDay}`;
+  }
+
+  get minDateForTimepicker() {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = (currentDate.getMonth() + 1)
+      .toString()
+      .padStart(2, '0');
+    const dayFollowingTheCurrent = (currentDate.getDate() + 1)
+      .toString()
+      .padStart(2, '0');
+
+    return this.over13Checked
+      ? '1900-01-01'
+      : `${currentYear - 13}-${currentMonth}-${dayFollowingTheCurrent}`;
+  }
+
+  updateDateRestriction() {
+    if (this.over13Checked) {
+      this.isUnder13 = false;
+      return;
+    }
+    this.isUnder13 = true;
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-@import '../../../../sass/main.scss';
-</style>
