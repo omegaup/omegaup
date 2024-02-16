75ab80dc5bafb23a7e291a68f3da503fa6c69e8d
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2024-02-16 01:22:36 -0600

    Mandando la fecha de nacimiento con el formato correcto

--- frontend/www/js/omegaup/login/signin.ts
+++ frontend/www/js/omegaup/login/signin.ts
@@ -131,7 +131,7 @@ OmegaUp.on('ready', () => {
               recaptcha: string;
             } = {
               username,
-              birth_date: dateOfBirth,
+              birth_date: new Date(dateOfBirth),
               password,
               recaptcha: recaptchaResponse,
             };
