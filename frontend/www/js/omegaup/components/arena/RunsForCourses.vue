ad1bd3d07498117a36f73cfd57cb4707fc86cb84
SubhashThenua <subhashthenua20082002@gmail.com>
2024-02-19 22:59:55 +0530

    fix-lint

--- frontend/www/js/omegaup/components/arena/RunsForCourses.vue
+++ frontend/www/js/omegaup/components/arena/RunsForCourses.vue
@@ -303,7 +303,9 @@
                 >
                 <a
                   problem-navigation-button
-                  :href="`/arena/problem/${run.alias}/`" class="ml-2">
+                  :href="`/arena/problem/${run.alias}/`"
+                  class="ml-2"
+                >
                   <font-awesome-icon :icon="['fas', 'external-link-alt']" />
                 </a>
               </td>
