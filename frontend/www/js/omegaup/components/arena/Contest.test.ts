0375cb349f06036ffd485627a529310f00038f21
Aritra Majumder <aritramajumder8438@gmail.com>
2024-04-05 11:54:02 +0530

    Update frontend/www/js/omegaup/components/arena/Contest.test.ts
    Co-authored-by: Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>

--- frontend/www/js/omegaup/components/arena/Contest.test.ts
+++ frontend/www/js/omegaup/components/arena/Contest.test.ts
@@ -32,7 +32,6 @@ describe('Contest.vue', () => {
     alias: 'omegaUp',
     description: 'hello omegaUp',
     director: 'omegaUpDirector',
-    extra_note: 'hello',
     feedback: 'detailed',
     finish_time: futureDate,
     languages: 'py3',
