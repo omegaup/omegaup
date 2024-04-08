d08e898a02d16e0b241caef8d48efc0d501c506b
Anas Iqbal <mohd.abd.6602@gmail.com>
2024-04-08 06:08:11 +0530

    Update problem list styling and search bar background color with variable

--- frontend/www/js/omegaup/components/problem/SearchBar.vue
+++ frontend/www/js/omegaup/components/problem/SearchBar.vue
@@ -127,7 +127,7 @@ export default class ProblemSearchBar extends Vue {
 .card-header {
   border: 1px solid var(--header-problem-card-color);
   border-bottom: none;
-  background-color: #f7f7f7;
+  background-color: var(--header-problem-card-color-no-opacity);
 }
 
 @media (max-width: 576px) {
