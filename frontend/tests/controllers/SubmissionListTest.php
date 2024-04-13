614f26b6888218193ce7f908f54c398a62511638
Ashish-003 <ashishgupta101061@gmail.com>
2024-04-14 02:08:55 +0530

    Fixed lint

--- frontend/tests/controllers/SubmissionListTest.php
+++ frontend/tests/controllers/SubmissionListTest.php
@@ -10,44 +10,44 @@ class SubmissionListTest extends \OmegaUp\Test\ControllerTestCase {
          *
          * - Page 2 should have 3 submissions given the row count is given
          *
-         * - 
+         * -
          */
 
          $usersCount = 4;
          $numSubmissions = 2; // number of submisisons for each problem
-         foreach (range(0, $usersCount - 1) as $_) {
-             [
-                 'identity' => $identity,
-             ] = \OmegaUp\Test\Factories\User::createUser();
-             $problem = \OmegaUp\Test\Factories\Problem::createProblem();
- 
-             for ($i = 0; $i < $numSubmissions; $i++) {
+        foreach (range(0, $usersCount - 1) as $_) {
+            [
+                'identity' => $identity,
+            ] = \OmegaUp\Test\Factories\User::createUser();
+            $problem = \OmegaUp\Test\Factories\Problem::createProblem();
+
+            for ($i = 0; $i < $numSubmissions; $i++) {
                 $runData = \OmegaUp\Test\Factories\Run::createRunToProblem(
                     $problem,
                     $identity
                 );
                 \OmegaUp\Test\Factories\Run::gradeRun($runData);
-             }
-         }
- 
-         $submissions = \OmegaUp\Controllers\Submission::apiList(
-             new \OmegaUp\Request([
+            }
+        }
+
+        $submissions = \OmegaUp\Controllers\Submission::apiList(
+            new \OmegaUp\Request([
                  'page' => 1,
                  'pageSize' => 5,
              ])
-         )['submissions'];
+        )['submissions'];
          $this->assertCount(5, $submissions);
- 
+
          // When visiting the second page, there should be 3 submissions left.
-         $submissions = \OmegaUp\Controllers\Submission::apiList(
-             new \OmegaUp\Request([
+        $submissions = \OmegaUp\Controllers\Submission::apiList(
+            new \OmegaUp\Request([
                  'page' => 2,
                  'pageSize' => 5,
              ])
-         )['submissions'];
+        )['submissions'];
          $this->assertCount(3, $submissions);
-     }
-     public function testUserSubmissionsList() {
+    }
+    public function testUserSubmissionsList() {
         /**
          * Create 2 users, 2 problems and and grades each of them 6 times.
          *
@@ -55,41 +55,40 @@ class SubmissionListTest extends \OmegaUp\Test\ControllerTestCase {
          *
          * - Page 2 should have 1 submission given the row count is given.
          *
-         * - 
+         * -
          */
-         $usersCount = 2;
-         $numSubmissions = 6;
-         $users = [];
-         for( $i = 0; $i < $usersCount; $i++) {
-             $users[] = \OmegaUp\Test\Factories\User::createUser();
-             $problem = \OmegaUp\Test\Factories\Problem::createProblem();
-             for ($j = 0; $j< $numSubmissions; $j++) {
+        $usersCount = 2;
+        $numSubmissions = 6;
+        $users = [];
+        for ($i = 0; $i < $usersCount; $i++) {
+            $users[] = \OmegaUp\Test\Factories\User::createUser();
+            $problem = \OmegaUp\Test\Factories\Problem::createProblem();
+            for ($j = 0; $j < $numSubmissions; $j++) {
                 $runData = \OmegaUp\Test\Factories\Run::createRunToProblem(
                     $problem,
                     $users[$i]['identity']
                 );
                 \OmegaUp\Test\Factories\Run::gradeRun($runData);
-             }
-         }
- 
-         $submissions = \OmegaUp\Controllers\Submission::apiList(
-             new \OmegaUp\Request([
-                 'page' => 1,
-                 'pageSize' => 5,
-                 'username' => $users[0]['identity']->username,
-             ])
-         )['submissions'];
-         $this->assertCount(5, $submissions);
- 
-         // When visiting the second page, there should be 1 submissions left.
-         $submissions = \OmegaUp\Controllers\Submission::apiList(
-             new \OmegaUp\Request([
-                 'page' => 2,
-                 'pageSize' => 5,
-                 'username' => $users[0]['identity']->username,
-             ])
-         )['submissions'];
-         $this->assertCount(1, $submissions);
-     }
+            }
+        }
 
+        $submissions = \OmegaUp\Controllers\Submission::apiList(
+            new \OmegaUp\Request([
+                'page' => 1,
+                'pageSize' => 5,
+                'username' => $users[0]['identity']->username,
+            ])
+        )['submissions'];
+        $this->assertCount(5, $submissions);
+
+        // When visiting the second page, there should be 1 submissions left.
+        $submissions = \OmegaUp\Controllers\Submission::apiList(
+            new \OmegaUp\Request([
+                'page' => 2,
+                'pageSize' => 5,
+                'username' => $users[0]['identity']->username,
+            ])
+        )['submissions'];
+        $this->assertCount(1, $submissions);
+    }
 }
