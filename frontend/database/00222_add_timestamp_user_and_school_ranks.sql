328a9c2d66d6186d83e18f58e6c748235e208aac
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2024-05-14 17:38:09 -0600

    Eliminando la tabla User_Rank para crearla con todos los campos

--- frontend/database/00222_add_timestamp_user_and_school_ranks.sql
+++ frontend/database/00222_add_timestamp_user_and_school_ranks.sql
@@ -1,6 +1,31 @@
--- User_Rank table
-ALTER TABLE `User_Rank`
-ADD COLUMN IF NOT EXISTS `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Almacena la hora y fecha en que se actualiza el rank de usuario';
+-- Drop User_Rank table
+DROP TABLE `User_Rank`;
+
+-- Create User_Rank table wih timestamp, it's because we have faced some issues
+-- with the script when we create the column timestamp in the table
+CREATE TABLE `User_Rank` (
+  `user_id` int NOT NULL,
+  `ranking` int DEFAULT NULL,
+  `problems_solved_count` int NOT NULL DEFAULT '0',
+  `score` double NOT NULL DEFAULT '0',
+  `username` varchar(50) NOT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  `country_id` char(3) DEFAULT NULL,
+  `state_id` char(3) DEFAULT NULL,
+  `school_id` int DEFAULT NULL,
+  `author_score` double NOT NULL DEFAULT '0',
+  `author_ranking` int DEFAULT NULL,
+  `classname` varchar(50) DEFAULT NULL COMMENT 'Almacena la clase precalculada para no tener que determinarla en tiempo de ejecucion.',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Almacena la hora y fecha en que se actualiza el rank de usuario',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `rank` (`ranking`),
+  KEY `fk_ur_state_id` (`country_id`,`state_id`),
+  KEY `fk_ur_school_id` (`school_id`),
+  CONSTRAINT `fk_ur_country_id` FOREIGN KEY (`country_id`) REFERENCES `Countries` (`country_id`),
+  CONSTRAINT `fk_ur_school_id` FOREIGN KEY (`school_id`) REFERENCES `Schools` (`school_id`),
+  CONSTRAINT `fk_ur_state_id` FOREIGN KEY (`country_id`, `state_id`) REFERENCES `States` (`country_id`, `state_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Guarda el ranking de usuarios por problemas resueltos.';
 
 -- Add School_Rank table
 CREATE TABLE `School_Rank` (
