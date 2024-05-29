bbcce9b545a3801d781309b251d1f6d4749fb5be
HNOONa-0 <hanyahmed11811@gmail.com>
2024-05-29 20:09:30 +0300

    import templates as necessary and format

--- frontend/www/js/omegaup/grader/GraderStore.js
+++ frontend/www/js/omegaup/grader/GraderStore.js
@@ -1,7 +1,9 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import * as Util from './util';
+import * as templates from './GraderTemplates';
 
+// persist function only used in the store when some data get updated
 const persistToSessionStorage = Util.throttle(({ alias, contents }) => {
   sessionStorage.setItem(
     `ephemeral-sources-${alias}`,
@@ -9,165 +11,15 @@ const persistToSessionStorage = Util.throttle(({ alias, contents }) => {
   );
 }, 10000);
 
-const languageExtensionMapping = Object.fromEntries(
-  Object.entries(Util.supportedLanguages).map(([key, value]) => [
-    key,
-    value.extension,
-  ]),
-);
-const defaultValidatorSource = `#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+// literal strings
+const defaultValidatorSource = templates.defaultValidatorSource;
+const defaultInteractiveIdlSource = templates.defaultInteractiveIdlSource;
+const defaultInteractiveMainSource = templates.defaultInteractiveMainSource;
 
-import logging
-import sys
-
-def _main() -> None:
-  # lee "data.in" para obtener la entrada original.
-  with open('data.in', 'r') as f:
-    a, b = [int(x) for x in f.read().strip().split()]
-  # lee "data.out" para obtener la salida esperada.
-  with open('data.out', 'r') as f:
-    suma = int(f.read().strip())
-
-  score = 0
-  try:
-    # Lee la salida del concursante
-    suma_concursante = int(input().strip())
-
-    # Determina si la salida es correcta
-    if suma_concursante != suma:
-      # Cualquier cosa que imprimas a sys.stderr se ignora, pero es útil
-      # para depurar con debug-rejudge.
-      logging.error('Salida incorrecta')
-      return
-    score = 1
-  except:
-    log.exception('Error leyendo la salida del concursante')
-  finally:
-    print(score)
-
-if __name__ == '__main__':
-  _main()`;
-
-const defaultInteractiveIdlSource = `interface Main {
-};
-
-interface sumas {
-    long sumas(long a, long b);
-};`;
-
-const defaultInteractiveMainSource = `#include <iostream>
-
-#include "sumas.h"
-
-int main(int argc, char* argv[]) {
-    long long a, b;
-    std::cin >> a >> b;
-    std::cout << sumas(a, b) << '\\n';
-}`;
-
-const sourceTemplates = {
-  c: `#include <stdio.h>
-#include <stdint.h>
-
-int main() {
-  // TODO: fixme.
-
-  return 0;
-}`,
-  cpp: `#include <iostream>
-
-int main() {
-  std::cin.tie(nullptr);
-  std::ios_base::sync_with_stdio(false);
-
-  // TODO: fixme.
-
-  return 0;
-}`,
-  cs: `using System.Collections.Generic;
-using System.Linq;
-using System;
-
-class Program
-{
-  static void Main(string[] args)
-  {
-    // TODO: fixme.
-  }
-}`,
-  java: `import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(
-                          new InputStreamReader(System.in));
-    // TODO: fixme.
-  }
-}`,
-  lua: `-- TODO: fixme.`,
-  py: `#!/usr/bin/python3
-
-def _main() -> None:
-  # TODO: fixme.
-  pass
-
-if __name__ == '__main__':
-  _main()`,
-  rb: `# TODO: fixme.`,
-};
-
+// objects
+const sourceTemplates = { ...templates.sourceTemplates };
 const originalInteractiveTemplates = {
-  c: `#include "sumas.h"
-
-long long sumas(long long a, long long b) {
-  // FIXME
-  return 0;
-}`,
-  cpp: `#include "sumas.h"
-
-long long sumas(long long a, long long b) {
-  // FIXME
-  return 0;
-}`,
-  cs: '// not supported',
-  java: `public class sumas {
-  public static long sumas(long a, long b) {
-    // FIXME
-    return 0;
-  }
-}`,
-  lua: '-- not supported',
-  pas: `unit sumas;
-{
- unit Main;
-}
-
-interface
-  function sumas(a: LongInt; b: LongInt): LongInt;
-
-implementation
-
-uses Main;
-
-function sumas(a: LongInt; b: LongInt): LongInt;
-begin
-  { FIXME }
-  sumas := 0;
-end;
-
-end.`,
-  py: `#!/usr/bin/python3
-
-import Main
-
-def sumas(a: int, b: int) -> int:
-    """ sumas """
-    # FIXME
-    return 0`,
-  rb: '# not supported',
+  ...templates.originalInteractiveTemplates,
 };
 const interactiveTemplates = { ...originalInteractiveTemplates };
 const languageExtensionMapping = Object.fromEntries(
@@ -177,6 +29,7 @@ const languageExtensionMapping = Object.fromEntries(
   ]),
 );
 
+// simple copy paste of the vuex store here
 Vue.use(Vuex);
 let store = new Vuex.Store({
   state: {
@@ -361,7 +214,7 @@ let store = new Vuex.Store({
       state.request.language = state.sessionStorageSources.language;
       state.request.source =
         state.sessionStorageSources.sources[
-        languageExtensionMapping[state.sessionStorageSources.language]
+          languageExtensionMapping[state.sessionStorageSources.language]
         ];
       document.getElementById('language').value = state.request.language;
     },
@@ -638,5 +491,7 @@ let store = new Vuex.Store({
   },
   strict: true,
 });
+// reset locally inside this file instead
+// because we may forget to do it outside
 store.commit('reset');
 export default store;
