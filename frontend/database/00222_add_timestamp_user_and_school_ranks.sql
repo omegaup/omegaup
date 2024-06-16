c8176969b063243edfb2619d806cb7f8cbd0046e
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2024-06-16 12:24:50 -0600

    Arreglando script de migración para evitar campos duplicados en `User_Rank` (#7647)
    # Description
    
    Nuevo intento para arreglar el script `00222` el cual está intentando
    agregar una columna que ya existe
    
    
    # Comments
    
    También elimino la creación de la tabla `School_Rank`, que por lo pronto
    no se va a usar y que al parecer también contiene un error.
    
    # Checklist:
    
    - [x] The code follows the [coding
    guidelines](https://github.com/omegaup/omegaup/wiki/Coding-guidelines)
    of omegaUp.
    - [x] The tests were executed and all of them passed.
    
    ---------
    
    Signed-off-by: Aritra Majumder <aritramajumder8438@gmail.com>
    Co-authored-by: Hugo Dueñas <heduenas@users.noreply.github.com>
    Co-authored-by: Aritra Majumder <aritramajumder8438@gmail.com>
    Co-authored-by: Carlos Córdova Sáenz <carlosab1802@gmail.com>

--- frontend/database/00222_add_timestamp_user_and_school_ranks.sql
+++ frontend/database/00222_add_timestamp_user_and_school_ranks.sql
@@ -1,18 +1,18 @@
 -- User_Rank table
-ALTER TABLE `User_Rank`
-ADD COLUMN `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Almacena la hora y fecha en que se actualiza el rank de usuario';
-
--- Add School_Rank table
-CREATE TABLE `School_Rank` (
-  `school_id` int NOT NULL,
-  `ranking` int DEFAULT NULL,
-  `score` double NOT NULL DEFAULT '0',
-  `country_id` char(3) DEFAULT NULL,
-  `state_id` char(3) DEFAULT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Almacena la hora y fecha en que se actualizó el rank de la escuela',
-  PRIMARY KEY (`school_id`),
-  KEY `rank` (`ranking`),
-  KEY `fk_sr_state_id` (`country_id`,`state_id`),
-  CONSTRAINT `fk_sr_country_id` FOREIGN KEY (`country_id`) REFERENCES `Countries` (`country_id`),
-  CONSTRAINT `fk_sr_state_id` FOREIGN KEY (`country_id`, `state_id`) REFERENCES `States` (`country_id`, `state_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Guarda el ranking de escuelas de acuerdo a su puntaje.';
+SET @dbname = DATABASE();
+SET @tablename = "User_Rank";
+SET @columnname = "timestamp";
+SET @preparedStatement = (SELECT IF(
+  (
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      (table_name = @tablename)
+      AND (table_schema = @dbname)
+      AND (column_name = @columnname)
+  ) > 0,
+  "SELECT 1",
+  CONCAT("ALTER TABLE ", @tablename, " ADD ", @columnname, " timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Almacena la hora y fecha en que se actualiza el rank de usuario';")
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
