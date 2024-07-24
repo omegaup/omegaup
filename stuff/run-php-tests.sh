a6814dea0d7a74450aca6888b02b2afb2016431c
Hugo Dueñas <heduenas@users.noreply.github.com>
2024-07-24 11:09:29 -0700

    Probando con 127.0.0.1

--- stuff/run-php-tests.sh
+++ stuff/run-php-tests.sh
@@ -25,11 +25,11 @@ else
 fi
 
 # Enable General Query Log
-mysql -h localhost -P 13306 -uroot -e "SET GLOBAL general_log = 'OFF';"
-mysql -h localhost -P 13306 -uroot -e "ALTER TABLE mysql.general_log ENGINE = MyISAM;"
-mysql -h localhost -P 13306 -uroot -e "TRUNCATE TABLE mysql.general_log;"
-mysql -h localhost -P 13306 -uroot -e "SET GLOBAL general_log = 'ON';"
-mysql -h localhost -P 13306 -uroot -e "SELECT argument FROM mysql.general_log;"
+mysql -h 127.0.0.1 -P 13306 -uroot -e "SET GLOBAL general_log = 'OFF';"
+mysql -h 127.0.0.1 -P 13306 -uroot -e "ALTER TABLE mysql.general_log ENGINE = MyISAM;"
+mysql -h 127.0.0.1 -P 13306 -uroot -e "TRUNCATE TABLE mysql.general_log;"
+mysql -h 127.0.0.1 -P 13306 -uroot -e "SET GLOBAL general_log = 'ON';"
+mysql -h 127.0.0.1 -P 13306 -uroot -e "SELECT argument FROM mysql.general_log;"
 
 exec "${OMEGAUP_ROOT}/vendor/bin/phpunit" \
 	--bootstrap "${OMEGAUP_ROOT}/frontend/tests/bootstrap.php" \
@@ -38,4 +38,4 @@ exec "${OMEGAUP_ROOT}/vendor/bin/phpunit" \
 	"${ARGS[@]}"
 
 # Disable General Query Log
-mysql -h localhost -P 13306 -uroot -e "SET GLOBAL general_log = 'OFF';"
+mysql -h 127.0.0.1 -P 13306 -uroot -e "SET GLOBAL general_log = 'OFF';"
