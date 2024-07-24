86b6b61b2d0eb07d179362f3b024dc4496ceda76
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2024-07-24 13:34:25 -0600

    Limpiando de código innecesario

--- stuff/mysql_types.sh
+++ stuff/mysql_types.sh
@@ -6,6 +6,7 @@ set -e
 # returns.
 
 OMEGAUP_ROOT=$(/usr/bin/git rev-parse --show-toplevel)
+MYSQL_TCP_PORT=13306
 
 # Clean up anything that might have been left from a previous run.
 if [[ -d "${OMEGAUP_ROOT}/frontend/tests/runfiles/" ]]; then
@@ -13,18 +14,13 @@ if [[ -d "${OMEGAUP_ROOT}/frontend/tests/runfiles/" ]]; then
 fi
 
 # Enable General Query Log
-mysql -h mysql -P 13306 -uroot -e "SET GLOBAL general_log = 'OFF';"
-mysql -h mysql -P 13306 -uroot -e "ALTER TABLE mysql.general_log ENGINE = MyISAM;"
-mysql -h mysql -P 13306 -uroot -e "TRUNCATE TABLE mysql.general_log;"
-mysql -h mysql -P 13306 -uroot -e "SET GLOBAL general_log = 'ON';"
+mysql -h mysql -P ${MYSQL_TCP_PORT} -uroot -e "TRUNCATE TABLE mysql.general_log;"
+mysql -h mysql -P ${MYSQL_TCP_PORT} -uroot -e "SET GLOBAL general_log = 'ON';"
 
 "${OMEGAUP_ROOT}/stuff/run-php-tests.sh"
 
-# Display General Query Log
-mysql -h mysql -P 13306 -uroot -e "SELECT CONVERT(argument USING utf8) AS argument FROM mysql.general_log WHERE command_type = 'Query' ORDER BY event_time DESC LIMIT 100;"
-
 # Disable General Query Log
-mysql -h mysql -P 13306 -uroot -e "SET GLOBAL general_log = 'OFF';"
+mysql -h mysql -P ${MYSQL_TCP_PORT} -uroot -e "SET GLOBAL general_log = 'OFF';"
 
 sort --unique \
 	--output "${OMEGAUP_ROOT}/frontend/tests/runfiles/mysql_types.log" \
