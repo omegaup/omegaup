e5e30f2350846ca2b89c53c1321f9d0af83a5d1b
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2024-08-22 09:08:19 -0600

    Quitando las clausulas ON DUPLICATE KEY UPDATE

--- frontend/database/00225_create_support_role.sql
+++ frontend/database/00225_create_support_role.sql
@@ -2,13 +2,7 @@
 INSERT INTO
   `Roles` (`role_id`, `name`, `description`)
 VALUES
-  (6, 'Support', 'Miembro del grupo de soporte')
-ON DUPLICATE KEY UPDATE
-  `name` = VALUES(`name`),
-  `description` = VALUES(`description`);
+  (6, 'Support', 'Miembro del grupo de soporte');
 
 INSERT INTO `Group_Roles` (`group_id`, `role_id`, `acl_id`)
-VALUES ((SELECT `group_id` FROM `Groups_` WHERE `alias` = 'omegaup:support'), 6, 6)
-ON DUPLICATE KEY UPDATE
-  `role_id` = VALUES(`role_id`),
-  `acl_id` = VALUES(`acl_id`);
+VALUES ((SELECT `group_id` FROM `Groups_` WHERE `alias` = 'omegaup:support'), 6, 6);
