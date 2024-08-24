5f5b8245515810b5478d46f8ae2c96da9c76f59b
Aritra <aritramajumder8438@gmail.com>
2024-08-25 01:50:42 +0530

    Add download and delete lines option to cases menu (#7705)
    This PR enables user to
    - [x] delete lines by batch. 
    - [x] download `.in` file.
    - [x] download `.txt` file.
    
    
    
    https://github.com/omegaup/omegaup/assets/64671908/431c8ea5-e489-49b6-bdbc-0550c53e240c
    
    # Checklist:
    
    - [x] The code follows the [coding
    guidelines](https://github.com/omegaup/omegaup/wiki/Coding-guidelines)
    of omegaUp.
    - [x] The tests were executed and all of them passed.
    - [x] If you are creating a feature, the new tests were added.
    - [x] If the change is large (> 200 lines), this PR was split into
    various Pull Requests. It's preferred to create one PR for changes in
    controllers + unit tests in PHPUnit, and then another Pull Request for
    UI + tests in Jest, Cypress or both.
    
    ---------
    
    Signed-off-by: Aritra Majumder <aritramajumder8438@gmail.com>
    Co-authored-by: Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
    Co-authored-by: Hugo Dueñas <heduenas@users.noreply.github.com>

--- frontend/www/js/omegaup/components/problem/creator/cases/CaseEdit.test.ts
+++ frontend/www/js/omegaup/components/problem/creator/cases/CaseEdit.test.ts
@@ -52,8 +52,17 @@ describe('CaseEdit.vue', () => {
     });
     await Vue.nextTick();
 
+    // There are currently 6 bootstrap buttons on the page.
+    // - Edit case
+    // - Delete case
+    // - Download .in
+    // - Download .txt
+    // - Delete lines
+    // - Add new line
+    const initialBButtonsCount = 6;
+
     const buttons = wrapper.findAllComponents(BButton);
-    expect(buttons.length).toBe(4);
+    expect(buttons.length).toBe(initialBButtonsCount);
 
     expect(wrapper.text()).toContain(newUngroupedCase.name);
     expect(wrapper.text()).toContain(newUngroupedCasegroup.name);
@@ -64,7 +73,7 @@ describe('CaseEdit.vue', () => {
     expect(
       wrapper.find('bicontrashfill-stub').element.parentElement?.textContent,
     ).toContain(T.problemCreatorDeleteCase);
-    expect(wrapper.find('biconthreedotsvertical-stub').exists()).toBeTruthy();
+    expect(wrapper.find('b-dropdown-stub').exists()).toBe(true);
   });
 
   it('Should show a grouped case', async () => {
@@ -78,9 +87,6 @@ describe('CaseEdit.vue', () => {
     });
     await Vue.nextTick();
 
-    const buttons = wrapper.findAllComponents(BButton);
-    expect(buttons.length).toBe(4);
-
     expect(wrapper.text()).toContain(newCase.name);
     expect(wrapper.text()).toContain(newGroup.name);
 
@@ -90,7 +96,7 @@ describe('CaseEdit.vue', () => {
     expect(
       wrapper.find('bicontrashfill-stub').element.parentElement?.textContent,
     ).toContain(T.problemCreatorDeleteCase);
-    expect(wrapper.find('biconthreedotsvertical-stub').exists()).toBeTruthy();
+    expect(wrapper.find('b-dropdown-stub').exists()).toBe(true);
   });
 
   it('Should add, modify and delete a line', async () => {
@@ -103,9 +109,6 @@ describe('CaseEdit.vue', () => {
       caseID,
     });
     await Vue.nextTick();
-
-    const buttons = wrapper.findAllComponents(BButton);
-    expect(buttons.length).toBe(4);
 
     expect(wrapper.text()).toContain(newCase.name);
     expect(wrapper.text()).toContain(newGroup.name);
@@ -124,8 +127,15 @@ describe('CaseEdit.vue', () => {
     expect(wrapper.vm.getLinesFromSelectedCase[0].data.value).toBe('testValue');
     expect(wrapper.vm.getLinesFromSelectedCase[0].data.kind).toBe('line');
 
+    // There are 4 dropdown items for each line:
+    // - Line
+    // - Multiline
+    // - Array
+    // - Matrix
+    const dropdownItemCount = 4;
+
     const dropdowns = wrapper.findAll('a.dropdown-item');
-    expect(dropdowns.length).toBe(4);
+    expect(dropdowns.length).toBe(dropdownItemCount);
 
     await dropdowns.at(1).trigger('click');
     expect(wrapper.vm.getLinesFromSelectedCase[0].data.kind).toBe('multiline');
@@ -140,6 +150,8 @@ describe('CaseEdit.vue', () => {
     await Vue.nextTick();
 
     const formInputsUpdated = wrapper.findAll('input');
+
+    // The input element should be replaced by textarea element
     expect(formInputsUpdated.length).toBe(0);
   });
 
@@ -185,6 +197,7 @@ describe('CaseEdit.vue', () => {
     wrapper.vm.caseInputRef.caseGroup = newGroup.groupID;
     wrapper.vm.updateCaseInfo();
 
+    // There's only one group added to the store.
     expect(wrapper.vm.groups.length).toBe(1);
 
     expect(wrapper.vm.groups[0].cases[0].name).toBe(modifiedName);
@@ -381,7 +394,6 @@ describe('CaseEdit.vue', () => {
     await Vue.nextTick();
 
     const dropdowns = wrapper.findAll('a.dropdown-item');
-    expect(dropdowns.length).toBe(4);
 
     await dropdowns.at(2).trigger('click');
     expect(wrapper.vm.getLinesFromSelectedCase[0].data.kind).toBe('array');
@@ -395,7 +407,6 @@ describe('CaseEdit.vue', () => {
     const modalBody = wrapper.find('div[data-array-modal]');
 
     const modalInputs = modalBody.findAll('input');
-    expect(modalInputs.length).toBe(5);
 
     const modalButton = modalBody.find('button[data-array-modal-generate]');
     expect(modalButton.exists).toBeTruthy();
@@ -428,8 +439,11 @@ describe('CaseEdit.vue', () => {
 
     const modalFooter = wrapper.find('footer.modal-footer');
 
+    // There should be two footer buttons: Save and Cancel.
+    const footerButtonsCount = 2;
+
     const footerButtons = modalFooter.findAll('button');
-    expect(footerButtons.length).toBe(2);
+    expect(footerButtons.length).toBe(footerButtonsCount);
 
     await modalFooter.find('button.btn-success').trigger('click');
 
@@ -572,4 +586,97 @@ describe('CaseEdit.vue', () => {
       });
     },
   );
+
+  it('deletes line, downloads .in and downloads .txt, when corresponding buttons are clicked', async () => {
+    const wrapper = mount(CaseEdit, { localVue, store });
+
+    const groupID = newGroup.groupID;
+    const caseID = newCase.caseID;
+    store.commit('casesStore/setSelected', {
+      groupID,
+      caseID,
+    });
+    store.dispatch('casesStore/deleteLinesForSelectedCase');
+    await Vue.nextTick();
+
+    const menuDropdown = wrapper.find('div[data-menu-dropdown]');
+    expect(menuDropdown.exists()).toBeTruthy();
+
+    // There are four buttons in the dropdown area.
+    // - Toggle dropdown
+    // - delete lines
+    // - Download .in
+    // - Download .txt
+    const dropdownAreaButtonsCount = 4;
+
+    const dropdownButtons = menuDropdown.findAll('button');
+    expect(dropdownButtons.length).toBe(dropdownAreaButtonsCount);
+
+    const deleteButton = menuDropdown.find('button[data-menu-delete-lines]');
+    const downloadInButton = menuDropdown.find('button[data-menu-download-in]');
+    const downloadTxtButton = menuDropdown.find(
+      'button[data-menu-download-txt]',
+    );
+
+    expect(deleteButton.text()).toBe(T.problemCreatorLinesDelete);
+    expect(downloadInButton.text()).toBe(T.problemCreatorCaseDownloadIn);
+    expect(downloadTxtButton.text()).toBe(T.problemCreatorCaseDownloadTxt);
+
+    wrapper.vm.addNewLine();
+    await Vue.nextTick();
+
+    // Only one line is added.
+    expect(wrapper.vm.getLinesFromSelectedCase.length).toBe(1);
+
+    await deleteButton.trigger('click');
+
+    expect(wrapper.vm.getLinesFromSelectedCase.length).toBe(0);
+
+    wrapper.vm.addNewLine();
+    wrapper.vm.addNewLine();
+    await Vue.nextTick();
+
+    // Two lines are added, so!
+    expect(wrapper.vm.getLinesFromSelectedCase.length).toBe(2);
+
+    const dropdownsMultiline = wrapper.findAll(
+      `a[data-array-modal-dropdown="${T.problemCreatorLineMultiline}"]`,
+    );
+
+    // There are two dropdown items for multiline, one for each line.
+    expect(dropdownsMultiline.length).toBe(2);
+
+    await dropdownsMultiline.at(1).trigger('click');
+
+    const formInputs = wrapper.findAll('input');
+    const formTextArea = wrapper.find('textarea');
+
+    await formInputs.at(0).setValue('testLabel');
+    await formInputs.at(1).setValue('ome g a');
+
+    await formInputs.at(2).setValue('testLabel');
+    await formTextArea.setValue('u\np');
+
+    expect(
+      wrapper.vm.getStringifiedLinesFromCaseGroupID({
+        groupID: groupID,
+        caseID: caseID,
+      }),
+    ).toBe('ome g a\nu\np');
+
+    const mockDownload = jest.spyOn(wrapper.vm, 'downloadInputFile');
+
+    await downloadInButton.trigger('click');
+    expect(mockDownload).toHaveBeenCalledWith('.in');
+
+    await downloadTxtButton.trigger('click');
+    expect(mockDownload).toHaveBeenCalledWith('.txt');
+
+    expect(wrapper.emitted()['download-input-file']).toStrictEqual([
+      [{ fileName: 'New Case.in', fileContent: 'ome g a\nu\np' }],
+      [{ fileName: 'New Case.txt', fileContent: 'ome g a\nu\np' }],
+    ]);
+
+    mockDownload.mockRestore();
+  });
 });
