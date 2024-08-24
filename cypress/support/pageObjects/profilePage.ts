41d4f3496c583dd788cd1a8e66811bbeafac31ff
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2024-08-24 13:24:43 -0600

    Resolviendo el error al actualizar un concurso por equipos (#7765)
    # Description
    
    Se arregla el formulario de "Actualizar concurso" para que envíe un
    string en el campo de `team-group-alias` en lugar de un objeto:
    
    
    ![FixedContestForTeamsUpdate](https://github.com/user-attachments/assets/ed14130f-bb1c-4630-994b-75c6a9dcdf0a)
    
    
    Fixes: #7178 
    Fixes: #6978 
    
    
    # Checklist:
    
    - [x] The code follows the [coding
    guidelines](https://github.com/omegaup/omegaup/wiki/Coding-guidelines)
    of omegaUp.
    - [x] The tests were executed and all of them passed.
    - [ ] If you are creating a feature, the new tests were added.
    - [ ] If the change is large (> 200 lines), this PR was split into
    various Pull Requests. It's preferred to create one PR for changes in
    controllers + unit tests in PHPUnit, and then another Pull Request for
    UI + tests in Jest, Cypress or both.
    
    ---------
    
    Co-authored-by: Hugo Dueñas <heduenas@users.noreply.github.com>

--- cypress/support/pageObjects/profilePage.ts
+++ cypress/support/pageObjects/profilePage.ts
@@ -52,51 +52,6 @@ export class ProfilePage {
     cy.get('.modal-footer>button.btn-danger').click();
   }
 
-  createTeamGroup(teamGroupOptions: TeamGroupOptions): void {
-    cy.get('[data-nav-user]').click();
-    cy.get('[data-nav-user-teams-groups]').click();
-    cy.get('[href="/teamsgroup/new/"]').click();
-
-    cy.get('[name="title"]').type(teamGroupOptions.groupTitle);
-    cy.get('[name="description"]').type(teamGroupOptions.groupDescription);
-    cy.get('[name="number-of-contestants"]')
-      .clear()
-      .type(teamGroupOptions.noOfContestants);
-
-    cy.get('[data-create-teams-group]').click();
-  }
-
-  uploadTeamGroups(): void {
-    cy.get('[href="#upload"]').click();
-    cy.get('[name="identities"]').attachFile('team_groups.csv');
-
-    cy.get('td[aria-colindex="2"]').then((rawHTMLElements) => {
-      const teamNames: Array<string> = [];
-      Cypress.$.makeArray(rawHTMLElements).forEach((element) => {
-        cy.task('log', element.innerText);
-        teamNames.push(element.innerText);
-      });
-
-      cy.wrap(teamNames).as('teamNamesList');
-    });
-
-    cy.get('[name="create-identities"]').click();
-    cy.get('#alert-close').click();
-    cy.waitUntil(() => {
-      return cy.get('#alert-close').should('not.be.visible');
-    });
-
-    cy.get('[href="#teams"]').click();
-    cy.get('@teamNamesList').then((textArray) => {
-      cy.get('[data-group-team-name]')
-        .should('have.length', textArray.length)
-        .then((rawHTMLElements) => {
-          return Cypress.$.makeArray(rawHTMLElements).map((el) => el.innerText);
-        })
-        .should('deep.equal', textArray);
-    });
-  }
-
   changePassword(oldPassword: string, newPassword: string): void {
     cy.get('[data-nav-user]').click();
     cy.get('[data-nav-profile]').click();
