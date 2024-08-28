1248dbff1f06155b5562250c87e519b97bc03dea
HNOONa-0 <hanyahmed11811@gmail.com>
2024-08-28 12:50:09 +0300

    use ephemeral.vue component

--- frontend/www/js/omegaup/components/arena/EphemeralGrader.vue
+++ frontend/www/js/omegaup/components/arena/EphemeralGrader.vue
@@ -1,91 +1,39 @@
 <template>
-  <iframe
-    ref="grader"
-    class="mt-2 border border-white"
-    src="/grader/ephemeral/index-light.html?embedded"
-  ></iframe>
+  <div ref="ide-layout">
+    <ephemeral-ide
+      :accepted-languages="acceptedLanguages"
+      :initial-language="initialLanguage"
+      :problem="problem"
+      :can-submit="canSubmit"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop, Ref, Watch } from 'vue-property-decorator';
+import { Component, Vue, Prop, Ref } from 'vue-property-decorator';
 import { types } from '../../api_types';
+import Ephemeral from '../../graderv2/Ephemeral.vue';
 
-interface CSSStyleDeclaration {
-  zoom?: string | number;
-}
-
-@Component
+@Component({
+  components: {
+    'ephemeral-ide': Ephemeral,
+  },
+})
 export default class EphemeralGrader extends Vue {
-  @Ref() grader!: HTMLIFrameElement;
+  @Ref('ide-layout') readonly ideLayout!: HTMLDivElement;
   @Prop() problem!: types.ProblemInfo;
   @Prop({ default: false }) canSubmit!: boolean;
   @Prop({ default: true }) canRun!: boolean;
   @Prop({ default: () => [] }) acceptedLanguages!: string[];
-  @Prop({ default: 'cpp17-gcc' }) preferredLanguage!: string;
+  @Prop({ default: 'cpp17gcc' }) preferredLanguage!: string;
 
   loaded = false;
 
-  // initial language is used. initial source could be used in the future
   get initialLanguage() {
     if (!this.acceptedLanguages.includes(this.preferredLanguage)) {
       return this.acceptedLanguages[0];
     }
     return this.preferredLanguage;
   }
-
-  mounted(): void {
-    (this.$refs.grader as HTMLIFrameElement).onload = () => {
-      this.iframeLoaded();
-    };
-    window.addEventListener('resize', this.handleWindowResize);
-  }
-  unmounted(): void {
-    window.removeEventListener('resize', this.handleWindowResize);
-  }
-  handleWindowResize(): void {
-    const iframe = this.$refs.grader as HTMLIFrameElement;
-
-    (iframe.style as CSSStyleDeclaration).zoom = String(
-      1 / window.devicePixelRatio,
-    );
-  }
-
-  @Watch('problem')
-  onProblemChanged() {
-    if (!this.loaded) {
-      // This will be updated when the component is mounted.
-      return;
-    }
-    this.setSettings();
-  }
-
-  setSettings(): void {
-    ((this.$refs.grader as HTMLIFrameElement)
-      .contentWindow as Window).postMessage(
-      {
-        method: 'setSettings',
-        params: {
-          problem: this.problem,
-          initialLanguage: this.initialLanguage,
-          languages: this.acceptedLanguages,
-          showRunButton: this.canRun,
-          showSubmitButton: this.canSubmit,
-        },
-      },
-      `${window.location.origin}/grader/ephemeral/embedded/`,
-    );
-  }
-
-  iframeLoaded(): void {
-    this.loaded = true;
-    this.setSettings();
-  }
 }
 </script>
-
-<style lang="scss" scoped>
-iframe {
-  width: 100%;
-  min-height: 60em;
-}
-</style>
