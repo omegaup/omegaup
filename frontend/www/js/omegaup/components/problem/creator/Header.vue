5185b1620c2989bad0685f3faba16f31747e9c47
Aritra <aritramajumder8438@gmail.com>
2024-08-28 22:35:50 +0530

    Add feature to download zip file (#7702)
    This PR enables user to download the zip file for creating the problem.
    
    
    
    https://github.com/user-attachments/assets/c7fbcad6-bf58-4a2d-8bd3-7a1c04677de3
    
    Related to: #7700 
    
    # Comments
    - Currently, the `cdp.data` file is not included. It's need to be
    planned how we import the zip file.
    - It directly follows the
    [manual](https://github.com/omegaup/omegaup/wiki/Generaci%C3%B3n-Manual-de-Archivo-.zip-para-Problemas)
    here.
    - It will have 3 folders: `cases`, `soluion`, `statement`, and two files
    `testplan` and `cdp.data` (which contains the store info).
    
    # Checklist:
    
    - [x] The code follows the [coding
    guidelines](https://github.com/omegaup/omegaup/wiki/Coding-guidelines)
    of omegaUp.
    - [x] The tests were executed and all of them passed.
    - [x] If you are creating a feature, the new tests were added.
    - [x] If the change is large (> 200 lines), this PR was split into
    various Pull Requests. It's preferred to create one PR for changes in
    controllers + unit tests in PHPUnit, and then another Pull Request for
    UI + tests in Jest, Cypress or both.
    
    ---------
    
    Signed-off-by: Aritra Majumder <aritramajumder8438@gmail.com>
    Co-authored-by: Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>

--- frontend/www/js/omegaup/components/problem/creator/Header.vue
+++ frontend/www/js/omegaup/components/problem/creator/Header.vue
@@ -15,7 +15,13 @@
           {{ T.problemCreatorLoadProblem }}</span
         >
       </b-button>
-      <b-button class="mr-2" variant="primary" size="sm">
+      <b-button
+        data-download-zip
+        class="mr-2"
+        variant="primary"
+        size="sm"
+        @click="generateProblem()"
+      >
         <BIconDownload class="mr-1" />
         <span class="d-none d-md-inline">
           {{ T.problemCreatorGenerateProblem }}</span
@@ -33,16 +39,75 @@
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
 import T from '../../../lang';
+import { Group, CaseGroupID } from '@/js/omegaup/problem/creator/types';
+
+const casesStore = namespace('casesStore');
+import JSZip from 'jszip';
 
 @Component
 export default class Header extends Vue {
   T = T;
   name: string = T.problemCreatorEmpty;
+  zip: JSZip = new JSZip();
+
+  @casesStore.State('groups') groups!: Group[];
+  @casesStore.Getter('getStringifiedLinesFromCaseGroupID')
+  getStringifiedLinesFromCaseGroupID!: (caseGroupID: CaseGroupID) => string;
 
   @Watch('name')
   onNameChanged(newProblemName: string) {
     this.$store.commit('updateName', newProblemName);
+  }
+
+  getStatement(zip: JSZip) {
+    const folder = zip.folder('statements');
+    const markdownData = this.$store.state.problemMarkdown;
+    folder?.file('es.markdown', markdownData);
+  }
+
+  getSolution(zip: JSZip) {
+    const folder = zip.folder('solutions');
+    const solutionMarkdownData = this.$store.state.problemSolutionMarkdown;
+    folder?.file('es.markdown', solutionMarkdownData);
+  }
+
+  getCasesAndTestPlan(zip: JSZip) {
+    const folder = zip.folder('cases');
+    let testPlanData: string = '';
+
+    this.groups.forEach((_group) => {
+      _group.cases.forEach((_case) => {
+        let fileName = _case.name;
+        if (_group.ungroupedCase === false) {
+          fileName = _group.name + '.' + fileName;
+        }
+        const caseGroupID: CaseGroupID = {
+          groupID: _group.groupID,
+          caseID: _case.caseID,
+        };
+        const input = this.getStringifiedLinesFromCaseGroupID(caseGroupID);
+        folder?.file(fileName + '.in', input);
+        folder?.file(fileName + '.out', _case.output);
+        testPlanData += fileName + ' ' + _case.points + '\n';
+      });
+    });
+
+    zip.file('testplan', testPlanData);
+    zip.file('cdp.data', JSON.stringify(this.$store.state));
+  }
+
+  generateProblem() {
+    this.getStatement(this.zip);
+    this.getSolution(this.zip);
+    this.getCasesAndTestPlan(this.zip);
+
+    const problemName: string = this.$store.state.problemName;
+    this.$emit('download-zip-file', {
+      fileName: problemName.replace(/ /g, '_'),
+      zipContent: this.zip,
+    });
   }
 
   createNewProblem() {
