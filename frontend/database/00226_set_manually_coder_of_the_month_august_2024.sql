af04cf82ebaeb7c265031df8aa810df1f2378b47
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2024-08-28 16:37:27 -0600

    Arreglando la consulta por `REPLACE INTO...`

--- frontend/database/00226_set_manually_coder_of_the_month_august_2024.sql
+++ frontend/database/00226_set_manually_coder_of_the_month_august_2024.sql
@@ -1,10 +1,11 @@
 -- Add a unique constraint to the table in order to avoid duplicates
 ALTER TABLE `Coder_Of_The_Month`
-  ADD UNIQUE KEY `unique_user_time_category` (`user_id`, `time`, `category`, `ranking`);
+  ADD UNIQUE KEY `unique_user_time_category` (`user_id`, `time`, `category`);
 
--- Insert the coder of the month for August 2024
-INSERT INTO `Coder_Of_The_Month`
+-- Replace the coder of the month for August 2024 with the correct one
+REPLACE INTO `Coder_Of_The_Month`
     (
+      `coder_of_the_month_id`,
       `user_id`,
       `time`,
       `ranking`,
@@ -13,15 +14,20 @@ INSERT INTO `Coder_Of_The_Month`
       `score`,
       `problems_solved`
     )
-	VALUES
-		(
+  VALUES
+    (
+      (
+        SELECT `coder_of_the_month_id` FROM (
+          SELECT `coder_of_the_month_id` FROM `Coder_Of_The_Month` WHERE `user_id` = (
+            SELECT `user_id` FROM `Identities` WHERE `username` = 'Mixer6151' AND `time` = '2024-08-01' AND `category` = 'all'
+          )
+        ) AS temp_table
+      ),
       COALESCE((SELECT `user_id` FROM `Identities` WHERE `username` = 'Mixer6151'), 1),
       '2024-08-01',
       '1',
-      COALESCE((SELECT `user_id` FROM `Identities` WHERE `username` = 'heduenas'), 1),
+      COALESCE((SELECT `identity_id` FROM `Identities` WHERE `username` = 'heduenas'), 1),
       'all',
-      420,
-      13
-    )
-	ON DUPLICATE KEY UPDATE
-    `selected_by` = COALESCE((SELECT `user_id` FROM `Identities` WHERE `username` = 'heduenas'), 1);
+      1952,
+      184
+    );
