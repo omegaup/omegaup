e6b2d45f3e50511855326d2565e4c7b8ec62c29a
Aritra Majumder <aritramajumder8438@gmail.com>
2024-09-02 18:20:18 +0530

    Write cypress tests for statement tab
    Signed-off-by: Aritra Majumder <aritramajumder8438@gmail.com>

---
+++ cypress/e2e/problem_creator.cy.ts
@@ -0,0 +1,26 @@
+import { LoginOptions } from "../support/types";
+
+describe('Problem creator Test', () => {
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.visit('/');
+  });
+
+  it('Should write and verify the problem statement statement', () => {
+    const loginOptions: LoginOptions = {
+      username: 'user',
+      password: 'user',
+    };
+    cy.login(loginOptions);
+
+    cy.visit('/problem/creator/');
+    
+    cy.get('[data-problem-creator-statement-tab]').click();
+
+    cy.get('[data-problem-creator-editor-markdown]').type("Hello omegaUp!");
+    cy.get("[data-problem-creator-save-markdown]").click();
+
+    cy.get("[data-problem-creator-previewer-markdown]").should("have.html", "<h1>Previsualización</h1>\n\n<p>Hello omegaUp!</p>")
+  });
+})
