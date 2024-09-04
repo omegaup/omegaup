e5d71d10a1707df1877e5e902489b7cd97efe6d5
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2024-09-04 01:04:31 -0600

    Revirtiendo más cambios en las pruebas

--- frontend/tests/controllers/CoderOfTheMonthTest.php
+++ frontend/tests/controllers/CoderOfTheMonthTest.php
@@ -872,9 +872,12 @@ class CoderOfTheMonthTest extends \OmegaUp\Test\ControllerTestCase {
         // solves same problems than first.
         $runCreationDate = self::setFirstDayOfTheLastMonth();
 
-        $this->createRuns($user1, $runCreationDate, numRuns: 2);
+        $this->createRuns($user1, $runCreationDate, numRuns: 1);
+        $this->createRuns($user1, $runCreationDate, numRuns: 1);
         $this->createRuns($user2, $runCreationDate, numRuns: 1);
-        $this->createRuns($user3, $runCreationDate, numRuns: 3);
+        $this->createRuns($user3, $runCreationDate, numRuns: 1);
+        $this->createRuns($user3, $runCreationDate, numRuns: 1);
+        $this->createRuns($user3, $runCreationDate, numRuns: 1);
 
         \OmegaUp\Test\Utils::runUpdateRanks($runCreationDate);
         $response = \OmegaUp\Controllers\User::apiCoderOfTheMonth(
@@ -907,6 +910,9 @@ class CoderOfTheMonthTest extends \OmegaUp\Test\ControllerTestCase {
         $creatorLogin = self::login($creatorIdentity);
         $group = \OmegaUp\Test\Factories\Groups::createGroup(
             $creatorIdentity,
+            name: null,
+            description: null,
+            alias: null,
             login: $creatorLogin
         );
 
@@ -929,7 +935,10 @@ class CoderOfTheMonthTest extends \OmegaUp\Test\ControllerTestCase {
         );
         $identity->password = $identityPassword;
 
-        $this->createRuns($identity, $runCreationDate, numRuns: 4);
+        $this->createRuns($identity, $runCreationDate, numRuns: 1);
+        $this->createRuns($identity, $runCreationDate, numRuns: 1);
+        $this->createRuns($identity, $runCreationDate, numRuns: 1);
+        $this->createRuns($identity, $runCreationDate, numRuns: 1);
 
         \OmegaUp\Test\Utils::runUpdateRanks($runCreationDate);
         $response = \OmegaUp\Controllers\User::apiCoderOfTheMonth(
