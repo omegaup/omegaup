b075f4f72129259c25bafc131072ed477f3b978a
Ahmed Hani <hanyahmed11811@gmail.com>
2024-09-20 10:00:36 +0300

    Initial payload definition for full IDE (#7830)
    # Description
    
    I defined a new payload that have only two attributes. The first is
    `acceptedLanguages` and the second is `preferredLanguage`. The IDE
    renders with those two properties.
    
    Fixes: #7822
    
    # Comments
    
    The line count is high from the other PR. This PR should be merged first
    https://github.com/omegaup/omegaup/pull/7821
    
    # Checklist:
    
    - [x] The code follows the [coding
    guidelines](https://github.com/omegaup/omegaup/wiki/Coding-guidelines)
    of omegaUp.
    - [x] The tests were executed and all of them passed.
    - [ ] If you are creating a feature, the new tests were added.

--- frontend/www/js/omegaup/graderv2/ide.ts
+++ frontend/www/js/omegaup/graderv2/ide.ts
@@ -2,19 +2,25 @@ import Vue from 'vue';
 import grader_EphemeralIDE from '../components/arena/EphemeralGrader.vue';
 
 import * as Util from './util';
+import { types } from '../api_types';
 import { OmegaUp } from '../omegaup';
 
 OmegaUp.on('ready', () => {
-  // TODO: implement typescript payload parser
   document.body.style.padding = '0';
   const main = document.querySelector('main') as HTMLElement;
   main.style.flex = '1 1 auto';
+
+  const payload = types.payloadParsers.FullIDEPayload();
+  const acceptedLanguages = payload.acceptedLanguages;
+  const preferredLanguage = payload.preferredLanguage || acceptedLanguages[0];
 
   new Vue({
     el: '#main-container',
     render: function (createElement) {
       return createElement(grader_EphemeralIDE, {
         props: {
+          acceptedLanguages,
+          preferredLanguage,
           isEmbedded: false,
           initialTheme: Util.MonacoThemes.VSDark,
         },
