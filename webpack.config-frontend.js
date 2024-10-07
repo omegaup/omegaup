d66fe5331a645e40d9c534bf8c3ad3cf57f52a90
Ahmed Hani <hanyahmed11811@gmail.com>
2024-10-07 05:51:14 +0300

    Remove old code (#7843)
    # Description
    
    Removed code that is no longer necessary after successful migration.
    
    Fixes: #7842
    
    # Comments
    
    Add any comments for the reviewers (e.g., indicating the beginning of
    the revision, something where the reviewer needs to pay special
    attention, etc.). If there are no extra comments this section can be
    deleted.
    
    # Checklist:
    
    - [x] The code follows the [coding
    guidelines](https://github.com/omegaup/omegaup/wiki/Coding-guidelines)
    of omegaUp.
    
    ---------
    
    Co-authored-by: Hugo Dueñas <heduenas@users.noreply.github.com>

--- webpack.config-frontend.js
+++ webpack.config-frontend.js
@@ -69,7 +69,7 @@ module.exports = {
     course_submissions_list:
       './frontend/www/js/omegaup/course/submissions_list.ts',
     course_tabs: './frontend/www/js/omegaup/course/tabs.ts',
-    grader_ide: './frontend/www/js/omegaup/graderv2/ide.ts',
+    grader_ide: './frontend/www/js/omegaup/grader/ide.ts',
     group_edit: './frontend/www/js/omegaup/group/edit.ts',
     group_list: './frontend/www/js/omegaup/group/list.ts',
     group_new: './frontend/www/js/omegaup/group/new.ts',
