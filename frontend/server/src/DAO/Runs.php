430f46fa515dc76940cb3ab88620fdab51e1012d
Nandini-Jaiswal <nandinijaiswal2002@gmail.com>
2025-01-22 00:56:27 +0530

    change back sorting index
    Co-authored-by: Aritra <aritramajumder8438@gmail.com>

--- frontend/server/src/DAO/Runs.php
+++ frontend/server/src/DAO/Runs.php
@@ -360,7 +360,7 @@ class Runs extends \OmegaUp\DAO\Base\Runs {
             $sql .= 'WHERE ' . implode(' AND ', $where);
         }
         $sql .= '
-            ORDER BY s.time DESC
+            ORDER BY s.submission_id DESC
             LIMIT ?, ?;
         ';
         $val[] = $offset * $rowCount;
