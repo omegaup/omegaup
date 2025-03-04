2a5ace035f123cd10efff002e8bc0f0ae5d905b7
Ramez Medhat <iramezdev@gmail.com>
2025-03-04 15:02:59 +0200

    Updating privacy links

--- frontend/www/js/omegaup/components/common/Footer.vue
+++ frontend/www/js/omegaup/components/common/Footer.vue
@@ -145,7 +145,7 @@
             </a>
           </li>
           <li>
-            <a href="https://blog.omegaup.com/privacy-policy/" target="_blank">
+            <a href="https://blog.omegaup.com/policies/privacy-policy/" target="_blank">
               {{ T.frontPageFooterPrivacyPolicy }}
             </a>
           </li>
