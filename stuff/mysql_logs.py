e5cb3cb563d9969f10f9fa09700d42077af05938
ArturoDiZa <s20120174@alumnos.itsur.edu.mx>
2025-03-13 16:14:22 -0600

    las queries ineficientes se buscan en base al tipo de query (ALL)

--- stuff/mysql_logs.py
+++ stuff/mysql_logs.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python3
+'''Looking for inefficient queries in the MySQL log.'''
+#  #from typing import Iterable, Tuple
+from typing import Any, Iterable, Tuple
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector import Error  # type: ignore
+
 
 # Establish connection to MySQL
-def create_connection(host_name, user_name, user_password, db_name):
+def create_connection(
+    host_name: str, user_name: str, user_password: str, db_name: str
+) -> mysql.connector.MySQLConnection | None:
+    '''Connecting to database'''
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password,
+            password=user_password,
             database=db_name,
             port=13306
         )
@@ -17,15 +25,19 @@ def create_connection(host_name, user_name, user_password, db_name):
         print(f"The error '{e}' occurred")
     return connection
 
+
 # Function to retrieve all queries from the general log
-def get_queries_from_general_log(connection):
+def get_queries_from_general_log(
+    connection: mysql.connector.MySQLConnection
+) -> Iterable[Tuple[Any, ...]]:
+    '''Get querys from log'''
     cursor = connection.cursor()
     cursor.execute("""
         USE omegaup
     """)
     cursor.execute("""
-        SELECT CONVERT(argument USING utf8) 
-        FROM mysql.general_log 
+        SELECT CONVERT(argument USING utf8) AS logs
+        FROM mysql.general_log
         WHERE argument IS NOT NULL
         AND (
             argument LIKE "SELECT%" OR
@@ -36,9 +48,14 @@ def get_queries_from_general_log(connection):
     queries = cursor.fetchall()
     return queries
 
-# Function to run EXPLAIN on each query
-def explain_queries(connection, queries):
+
+def explain_queries(
+    connection: mysql.connector.MySQLConnection,
+    queries: Iterable[Tuple[Any, ...]]
+) -> None:
+    '''Run explain command on queries'''
     cursor = connection.cursor()
+    query_count = 0
     for query in queries:
         query_text = query[0]
         try:
@@ -46,46 +63,71 @@ def explain_queries(connection, queries):
             explain_result = cursor.fetchall()
 
             # Get the index of the 'possible_keys' column
-            column_names = [i[0] for i in cursor.description]
+            column_names = [i[0] for i in cursor.description]  # type: ignore
             possible_keys_index = column_names.index('possible_keys')
+            type_row_index = column_names.index('type')
             table_row_index = column_names.index('table')
             extra_row_index = column_names.index('Extra')
 
             # Check if any row has NULL or empty possible_keys
+            no_matching_row = 'no matching row in const table'
+            exclude = ['Languages',
+                       'general_log',
+                       'Roles', 'Groups_',
+                       'Tags',
+                       'urc']
             for row in explain_result:
-                if (str(row[extra_row_index]) != 'no matching row in const table') and\
-                        (row[possible_keys_index] is None or row[possible_keys_index] == '') and\
-                        (str(row[extra_row_index]) != 'Using index') and\
-                        (row[table_row_index] is not None and "<union" not in row[table_row_index]) and\
-                        (row[table_row_index] is not None and "<derived" not in row[table_row_index]) and\
-                        (row[table_row_index] is not None and row[table_row_index] not in ['Languages', 'general_log', 'Roles', 'Groups_', 'Tags']) and\
-                        not (query_text.startswith('DELETE ') and ' WHERE ' not in query_text) and\
-                        not (str("Users u") in query_text and str("verification_id = ") in query_text) and\
-                        not (str("Users_Badges") in query_text and str("badge_alias = ") in query_text): # https://github.com/omegaup/omegaup/issues/7773, # https://github.com/omegaup/omegaup/issues/7774
-                    print(f"Found query with full table scan: {query_text}")
-                    print(query_text.split()[0], row[table_row_index], row[extra_row_index])
-                    #if ("s" == row[table_row_index]):
-                    #print(query_text)
-            #print(f"EXPLAIN for query: {query_text}")
-            #for row in explain_result:
-            #    print(row)
+                if str(row[extra_row_index]) == no_matching_row:
+                    continue
+                if row[type_row_index] != 'ALL':
+                    continue
+                if str(row[extra_row_index]) == 'Using index':
+                    continue
+                if (row[table_row_index] is None
+                    or "<union" in row[table_row_index]
+                    or "<derived" in row[table_row_index]
+                    or row[table_row_index] in exclude):
+                    continue
+                if (query_text.startswith('DELETE ') and
+                    ' WHERE ' not in query_text):
+                    continue
+                if (str("Users u") in query_text and
+                    str("verification_id = ") in query_text):
+                    continue
+                if (str("Users_Badges") in query_text and
+                    str("badge_alias = ") in query_text):
+                    continue
+                query_count += 1
+                print(f"Found query with full table scan: {query_text}")
+                print(query_text.split()[0],
+                      row[table_row_index],
+                      row[extra_row_index],
+                      row[type_row_index],
+                      row[possible_keys_index])
         except Error as e:
             print(f"Failed to explain query: {query_text}")
             print(f"Error: {e}")
+    print(query_count, " inefficient queries found")
+
 
 # Main function to handle the logic
-def main():
+def main() -> None:
+    '''Main function to handle the logic'''
     # Use your credentials
-    connection = create_connection(host_name="mysql",user_name="root",user_password="omegaup",db_name="omegaup",)
-    
+    connection = create_connection(host_name="mysql",
+                                   user_name="root",
+                                   user_password="omegaup",
+                                   db_name="omegaup", )
     if connection:
         queries = get_queries_from_general_log(connection)
         if queries:
             explain_queries(connection, queries)
         else:
             print("No queries found in the general log")
-        
         connection.close()
+
 
 if __name__ == "__main__":
     main()
+
+#
