a7227c32c3d34be238be5c97c54a8f5682f261a1
Himanshu Dangwal <68777129+Himanshu-Dangwal@users.noreply.github.com>
2025-03-18 21:56:24 -0700

    Added dropdown to change font size
    Added a dropdown in the Monaco Editor to change the font-size of the code written by the user.

--- frontend/www/js/omegaup/grader/MonacoEditor.vue
+++ frontend/www/js/omegaup/grader/MonacoEditor.vue
@@ -1,9 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <!-- Font Size Dropdown -->
+    <label for="font-size">Font Size: </label>
+    <select id="font-size" v-model="selectedFontSize" @change="onFontSizeChange">
+      <option v-for="size in fontSizes" :key="size" :value="size">{{ size }}px</option>
+    </select>
+    <!-- Editor Container -->
+    <div ref="editorContainer" style="width: 100%; height: 100%;"></div>
+  </div>
 </template>
 
-<script lang="ts">
 // TODO: replace all instances of any with correct type
+<script lang="ts">
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 import store from './GraderStore';
 import * as Util from './util';
@@ -12,17 +20,15 @@ import * as monaco from 'monaco-editor';
 @Component
 export default class MonacoEditor extends Vue {
   // TODO: place more restrictions on value of keys inside storeMapping
-  @Prop({ required: true }) storeMapping!: {
-    [key: string]: string;
-  };
+  @Prop({ required: true }) storeMapping!: { [key: string]: string };
   @Prop({ default: false }) readOnly!: boolean;
 
   _editor: monaco.editor.IStandaloneCodeEditor | null = null;
   _model: monaco.editor.ITextModel | null = null;
 
-  // default font size and line height
-  readonly baseFontSize: number = 14;
-  readonly baseLineHeight: number = 19;
+  // default font size
+  selectedFontSize: number = 14;
+  fontSizes: number[] = [12, 14, 16, 18, 20, 22, 24, 26];
 
   get theme(): string {
     return store.getters['theme'];
@@ -30,10 +36,6 @@ export default class MonacoEditor extends Vue {
 
   get language(): string {
     return store.getters[this.storeMapping.language];
-  }
-
-  get module(): string {
-    return store.getters[this.storeMapping.module];
   }
 
   get contents(): string {
@@ -44,54 +46,27 @@ export default class MonacoEditor extends Vue {
     store.dispatch(this.storeMapping.contents, value);
   }
 
-  get filename(): string {
-    return `${this.module}.${Util.supportedLanguages[this.language].extension}`;
-  }
-
-  get title(): string {
-    return this.filename;
-  }
-
-  @Watch('language')
-  onLanguageChange(value: string): void {
-    if (this._model) {
-      monaco.editor.setModelLanguage(
-        this._model,
-        Util.supportedLanguages[value].modelMapping,
-      );
-    }
-  }
-
-  @Watch('contents')
-  onContentsChange(value: string): void {
-    if (this._model && this._model.getValue() !== value) {
-      this._model.setValue(value);
-    }
-  }
   @Watch('theme')
   onThemeChange(value: string): void {
     if (this._editor) {
-      this._editor.updateOptions({
-        theme: value,
-      });
+      this._editor.updateOptions({ theme: value });
     }
   }
 
   mounted(): void {
     window.addEventListener('code-and-language-set', this.onCodeAndLanguageSet);
 
-    this._editor = monaco.editor.create(
-      this.$el as HTMLElement,
-      {
-        autoIndent: 'brackets',
-        formatOnPaste: true,
-        formatOnType: true,
-        language: Util.supportedLanguages[this.language].modelMapping,
-        readOnly: this.readOnly,
-        theme: this.theme,
-        value: this.contents,
-      } as monaco.editor.IStandaloneEditorConstructionOptions,
-    );
+    this._editor = monaco.editor.create(this.$refs.editorContainer as HTMLElement, {
+      autoIndent: 'brackets',
+      formatOnPaste: true,
+      formatOnType: true,
+      language: Util.supportedLanguages[this.language].modelMapping,
+      readOnly: this.readOnly,
+      theme: this.theme,
+      value: this.contents,
+      fontSize: this.selectedFontSize,
+    });
+
     this._model = this._editor.getModel();
     if (!this._model) return;
 
@@ -103,24 +78,22 @@ export default class MonacoEditor extends Vue {
     this.onResize();
   }
 
+  onFontSizeChange(): void {
+    if (this._editor) {
+      this._editor.updateOptions({ fontSize: this.selectedFontSize });
+    }
+  }
+
   unmounted(): void {
-    window.removeEventListener(
-      'code-and-language-set',
-      this.onCodeAndLanguageSet,
-    );
+    window.removeEventListener('code-and-language-set', this.onCodeAndLanguageSet);
     window.removeEventListener('resize', this.onResize);
   }
 
   onResize(): void {
+    // scaling does not work as intended
+    // the cursor does not click where it's supposed to
+    // this is an alternative solution to zooming in/out
     if (this._editor) {
-      // scaling does not work as intended
-      // the cursor does not click where it's supposed to
-      // this is an alternative solution to zooming in/out
-
-      this._editor.updateOptions({
-        fontSize: this.baseFontSize * window.devicePixelRatio,
-        lineHeight: this.baseLineHeight * window.devicePixelRatio,
-      });
       this._editor.layout();
     }
   }
@@ -131,6 +104,7 @@ export default class MonacoEditor extends Vue {
   }
 }
 </script>
+
 
 <style scoped>
 div {
