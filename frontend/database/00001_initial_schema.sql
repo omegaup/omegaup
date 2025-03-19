9672a40dc05487eb720da8168b7eeb17509d230d
bikas295 <112216014@ece.iiitp.ac.in>
2025-03-19 23:55:39 +0530

    made orden field mandatory

--- frontend/database/00001_initial_schema.sql
+++ frontend/database/00001_initial_schema.sql
@@ -961,7 +961,9 @@ FOR EACH ROW BEGIN
 END$$
 DELIMITER ;
 
-CREATE INDEX idx_contest_public ON Contests (`public`);
+CREATE INDEX idx_contest_public ON Contests (`public`,
+  CHECK (orden IS NOT NULL)
+);
 CREATE INDEX idx_user_roles_contest ON User_Roles (contest_id);
 CREATE INDEX idx_contest_director_id ON Contests (director_id);
 CREATE INDEX idx_contest_public_director_id ON Contests (`public`, director_id);
