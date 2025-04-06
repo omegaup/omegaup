a674f1582efdac0a9a272b68d0a1886881dd966e
isabelccc <cxl0603@gmail.com>
2025-04-05 20:48:40 -0400

    fix revert and a new entry point with a user-friendly UR added

--- frontend/www/problems/statement_editor.php
+++ frontend/www/problems/statement_editor.php
@@ -3,6 +3,7 @@ namespace OmegaUp;
 require_once(dirname(__DIR__, 2) . '/server/bootstrap.php');
 
 \OmegaUp\UITools::render(
+    /** @return array{templateProperties: array{fullWidth?: bool, hideFooterAndHeader?: bool, payload: array<string, mixed>, scripts?: list<string>, title: \OmegaUp\TranslationString}, entrypoint: string, inContest?: bool, navbarSection?: string} */
     fn (\OmegaUp\Request $r) => [
         'templateProperties' => [
             'title' => new \OmegaUp\TranslationString(
