ccb70a04157d77c722f1bd28d7c05ebc556391e1
Avadhutgiri <avadg252@gmail.com>
2025-04-25 15:32:43 +0530

    Document JSX IntrinsicElements workaround as a dev tip

--- frontend/www/js/omegaup/vue-html.d.ts
+++ frontend/www/js/omegaup/vue-html.d.ts
@@ -1,5 +1,9 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    [elem: string]: any;
-  }
-}
+// This file is intentionally left empty
+// For development purposes, if you need to add JSX IntrinsicElements support,
+// you can add the following declaration:
+// declare namespace JSX {
+//   interface IntrinsicElements {
+//     [elem: string]: any;
+//   }
+// }
+// However, this is not recommended as it could mask real errors and make debugging harder.
