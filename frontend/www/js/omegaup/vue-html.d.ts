a3953d498b06fced006ed34feb20fee0a88a6239
Hugo Dueñas <heduenas@users.noreply.github.com>
2025-04-26 20:33:22 -0700

    Delete frontend/www/js/omegaup/vue-html.d.ts

--- frontend/www/js/omegaup/vue-html.d.ts
+++
@@ -1,9 +0,0 @@
-// This file is intentionally left empty
-// For development purposes, if you need to add JSX IntrinsicElements support,
-// you can add the following declaration:
-// declare namespace JSX {
-//   interface IntrinsicElements {
-//     [elem: string]: any;
-//   }
-// }
-// However, this is not recommended as it could mask real errors and make debugging harder.
