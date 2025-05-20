0ad3bfa97be924d28bca124643390d7812898154
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2025-05-19 23:25:38 -0600

    Eliminando índices ya existentes

--- frontend/database/00234_add_key_in_assignments_table.sql
+++ frontend/database/00234_add_key_in_assignments_table.sql
@@ -1,7 +1,3 @@
--- Add index to the Problemsets table
-ALTER TABLE `Problemsets`
-  ADD KEY `idx_assignment_id_problemset_id_acl_id` (`assignment_id`, `problemset_id`, `acl_id`);
-
 -- Add index to the Assignments table
 ALTER TABLE `Assignments`
-    ADD KEY `idx_finish_time_assignment` (`finish_time`, `assignment_id`);
+    ADD KEY `idx_finish_time` (`finish_time`);
