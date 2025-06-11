6a37de727eb17af51da7175806d137c89f036da1
Juan Pablo <gomez_aguilar_juan_pablo@hotmail.com>
2025-06-11 00:58:44 -0600

    Nit

--- stuff/docker/entrypoint.dev.sh
+++ stuff/docker/entrypoint.dev.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# Build ReKarel webapp si no existe
+# Build ReKarel webapp if not exists
 if [ ! -f /opt/omegaup/frontend/www/rekarel/webapp/js/cindex.js ]; then
   cd /opt/omegaup/frontend/www/rekarel
   npm install
   npx gulp
 fi
 
-# Ejecuta el comando original (supervisord)
+# Execute the original command
 exec "$@"
