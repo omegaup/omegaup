fc25930642359e1c922f54a2a60d3eb4d12bd64d
ArturoDiZa <s20120174@alumnos.itsur.edu.mx>
2025-06-26 16:43:48 -0600

    restaurado update ranks

--- stuff/cron/update_ranks.py
+++ stuff/cron/update_ranks.py
@@ -147,7 +147,7 @@ def update_user_rank(
             `Problems` AS `p`
         ON `p`.`problem_id` = up.`problem_id` AND `p`.visibility > 0
         INNER JOIN
-            `Identities` AS `i` ON `i`.`identity_id` = `u`.`main_identity_id`
+            `Identities` AS `i` ON `i`.`identity_id` = u.`main_identity_id`
         LEFT JOIN
             `Identities_Schools` AS `isc`
         ON
