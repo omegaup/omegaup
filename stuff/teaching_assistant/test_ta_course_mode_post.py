1a5229cc1b70b77a8e234585336bd5319b295f0b
Nandini-Jaiswal <nandinijaiswal2002@gmail.com>
2025-07-12 00:35:22 +0530

    Add test for checking feedback

--- stuff/teaching_assistant/test_ta_course_mode_post.py
+++ stuff/teaching_assistant/test_ta_course_mode_post.py
@@ -14,9 +14,10 @@ from test_ta_course_mode_pre import (
     ASSIGNMENT_ALIAS,
 )
 import requests
-
-def test_verify_feedback():
-    """Test to verify that the feedback was posted."""
+import pytest
+@pytest.fixture
+def get_first_run():
+    """Get the guid of the first run for the course and assignment."""
     global COOKIES, BASE_URL
 
     login_endpoint = get_login_endpoint(TEACHER_USERNAME, TEACHER_PASSWORD)
@@ -35,6 +36,22 @@ def test_verify_feedback():
     runs = requests.get(runs_url, cookies=COOKIES).json()["runs"]
 
     assert len(runs) == 1, "No runs found for the course and assignment."
+    return runs[0]["guid"]
+
+def test_verify_feedback(get_first_run):
+    """Test to verify that the feedback was posted."""
+    global COOKIES, BASE_URL
+
+    run_guid = get_first_run
+    feedback_endpoint = get_runs_submission_feedback_endpoint(run_guid)
+    feedback_url = f"{BASE_URL}/{feedback_endpoint}"
+
+    response = requests.get(feedback_url, cookies=COOKIES)
+    response.raise_for_status()
+
+    feedbacks = response.json()
+    assert len(feedbacks) > 1, "Feedback is empty."
+    
 
     
 
