af9a7bda11e5e138a6d4dee3d1d4e96be90a09d7
Nandini-Jaiswal <nandinijaiswal2002@gmail.com>
2025-07-12 00:42:01 +0530

    Minor fix

--- stuff/teaching_assistant/test_ta_course_mode_post.py
+++ stuff/teaching_assistant/test_ta_course_mode_post.py
@@ -15,8 +15,13 @@ from test_ta_course_mode_pre import (
 )
 import requests
 import pytest
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @pytest.fixture
-def get_first_run():
+def get_runs():
     """Get the guid of the first run for the course and assignment."""
     global COOKIES, BASE_URL
 
@@ -35,14 +40,17 @@ def get_first_run():
     runs_url = f"{BASE_URL}/{runs_endpoint}"
     runs = requests.get(runs_url, cookies=COOKIES).json()["runs"]
 
-    assert len(runs) == 1, "No runs found for the course and assignment."
-    return runs[0]["guid"]
+    yield runs
 
-def test_verify_feedback(get_first_run):
+def test_verify_feedback(get_runs):
     """Test to verify that the feedback was posted."""
     global COOKIES, BASE_URL
 
-    run_guid = get_first_run
+    runs = get_runs
+
+    assert len(runs) == 1, "No runs found for the course and assignment."
+    run_guid = runs[0]["guid"]
+
     feedback_endpoint = get_runs_submission_feedback_endpoint(run_guid)
     feedback_url = f"{BASE_URL}/{feedback_endpoint}"
 
@@ -50,6 +58,7 @@ def test_verify_feedback(get_first_run):
     response.raise_for_status()
 
     feedbacks = response.json()
+    logger.info(f"Feedbacks: {feedbacks}")
     assert len(feedbacks) > 1, "Feedback is empty."
     
 
