30718c9f92efa86ef8e0c035244438a4901d71ce
Pedro Antonio Ayala Ramos <48796502+ElPedrangas03@users.noreply.github.com>
2025-08-16 23:03:47 -0600

    Cambiando el campo orden como mandatorio. (#8379)
    # Description
    
    Cambiando el orden en el metodo getAll de los daos como mandatorio,
    generando nuevamente los daos para que se vea los cambios efectuados en
    el archivo raiz dao.php
    
    Fixes: #8015
    
    # Comments
    
    Son demasiadas lineas de cambios, dado que se efectuo el comando
    `./stuff/update-dao.sh` y se actualizaron todos los daos con los nuevos
    valores.
    
    # Checklist:
    
    - [ ] The code follows the [coding
    guidelines](https://github.com/omegaup/omegaup/blob/main/frontend/www/docs/Coding-guidelines.md)
    of omegaUp.
    - [ ] The tests were executed and all of them passed.
    - [ ] If you are creating a feature, the new tests were added.
    - [ ] If the change is large (> 200 lines), this PR was split into
    various Pull Requests. It's preferred to create one PR for changes in
    controllers + unit tests in PHPUnit, and then another Pull Request for
    UI + tests in Jest, Cypress or both.

--- frontend/server/src/DAO/Base/CarouselItems.php
+++ frontend/server/src/DAO/Base/CarouselItems.php
@@ -195,7 +195,7 @@ abstract class CarouselItems {
      *
      * @param ?int $pagina Página a ver.
      * @param int $filasPorPagina Filas por página.
-     * @param ?string $orden Debe ser una cadena con el nombre de una columna en la base de datos.
+     * @param string $orden Debe ser una cadena con el nombre de una columna en la base de datos.
      * @param string $tipoDeOrden 'ASC' o 'DESC' el default es 'ASC'
      *
      * @return list<\OmegaUp\DAO\VO\CarouselItems> Un arreglo que contiene objetos del tipo
@@ -204,10 +204,21 @@ abstract class CarouselItems {
     final public static function getAll(
         ?int $pagina = null,
         int $filasPorPagina = 100,
-        ?string $orden = null,
+        string $orden = 'carousel_item_id',
         string $tipoDeOrden = 'ASC'
     ): array {
-        $sql = '
+        $sanitizedOrder = \OmegaUp\MySQLConnection::getInstance()->escape(
+            $orden
+        );
+        \OmegaUp\Validators::validateInEnum(
+            $tipoDeOrden,
+            'order_type',
+            [
+                'ASC',
+                'DESC',
+            ]
+        );
+        $sql = "
             SELECT
                 `Carousel_Items`.`carousel_item_id`,
                 `Carousel_Items`.`title`,
@@ -222,15 +233,9 @@ abstract class CarouselItems {
                 `Carousel_Items`.`updated_at`
             FROM
                 `Carousel_Items`
-        ';
-        if (!is_null($orden)) {
-            $sql .= (
-                ' ORDER BY `' .
-                \OmegaUp\MySQLConnection::getInstance()->escape($orden) .
-                '` ' .
-                ($tipoDeOrden == 'DESC' ? 'DESC' : 'ASC')
-            );
-        }
+            ORDER BY
+                `{$sanitizedOrder}` {$tipoDeOrden}
+        ";
         if (!is_null($pagina)) {
             $sql .= (
                 ' LIMIT ' .
--- frontend/server/src/DAO/Base/TeamUsers.php
+++ frontend/server/src/DAO/Base/TeamUsers.php
@@ -137,7 +137,7 @@ abstract class TeamUsers {
      *
      * @param ?int $pagina Página a ver.
      * @param int $filasPorPagina Filas por página.
-     * @param ?string $orden Debe ser una cadena con el nombre de una columna en la base de datos.
+     * @param string $orden Debe ser una cadena con el nombre de una columna en la base de datos.
      * @param string $tipoDeOrden 'ASC' o 'DESC' el default es 'ASC'
      *
      * @return list<\OmegaUp\DAO\VO\TeamUsers> Un arreglo que contiene objetos del tipo
@@ -146,24 +146,29 @@ abstract class TeamUsers {
     final public static function getAll(
         ?int $pagina = null,
         int $filasPorPagina = 100,
-        ?string $orden = null,
+        string $orden = 'team_id',
         string $tipoDeOrden = 'ASC'
     ): array {
-        $sql = '
+        $sanitizedOrder = \OmegaUp\MySQLConnection::getInstance()->escape(
+            $orden
+        );
+        \OmegaUp\Validators::validateInEnum(
+            $tipoDeOrden,
+            'order_type',
+            [
+                'ASC',
+                'DESC',
+            ]
+        );
+        $sql = "
             SELECT
                 `Team_Users`.`team_id`,
                 `Team_Users`.`identity_id`
             FROM
                 `Team_Users`
-        ';
-        if (!is_null($orden)) {
-            $sql .= (
-                ' ORDER BY `' .
-                \OmegaUp\MySQLConnection::getInstance()->escape($orden) .
-                '` ' .
-                ($tipoDeOrden == 'DESC' ? 'DESC' : 'ASC')
-            );
-        }
+            ORDER BY
+                `{$sanitizedOrder}` {$tipoDeOrden}
+        ";
         if (!is_null($pagina)) {
             $sql .= (
                 ' LIMIT ' .
