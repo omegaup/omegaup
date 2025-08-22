900d4e75c8567456ed1b3e3e4e4a780dc9817b5c
Nandini-Jaiswal <nandinijaiswal2002@gmail.com>
2025-08-23 00:55:09 +0530

    Revert back migration and changes in `dao_schema`

--- frontend/database/00241_add_teaching_assistant_enabled_to_courses.sql
+++
@@ -1,5 +0,0 @@
--- Add teaching_assistant_enabled field to Courses table
-
-ALTER TABLE `Courses`
-ADD COLUMN `teaching_assistant_enabled` TINYINT(1) NOT NULL DEFAULT 0
-COMMENT 'Indica si el Asistente de enseñanza de IA está habilitado para este curso';
