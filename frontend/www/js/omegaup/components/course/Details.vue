ef6f16f1b0d3c6dfd1086480c69fed4681e682c6
Nandini-Jaiswal <nandinijaiswal2002@gmail.com>
2025-08-22 14:14:24 +0530

    Modify frontend and UI
    Co-authored-by: Aritra <aritramajumder8438@gmail.com>

--- frontend/www/js/omegaup/components/course/Details.vue
+++ frontend/www/js/omegaup/components/course/Details.vue
@@ -12,6 +12,27 @@
         <font-awesome-icon :icon="['fas', 'edit']" />
       </a>
     </h3>
+    <div v-if="course.is_admin" class="d-flex justify-content-end mb-3">
+      <button
+        data-button-enable-ai-ta
+        :class="[
+          'btn p-1 p-sm-2 mr-2',
+          isTogglingTA ? 'btn-light' : 'btn-primary',
+        ]"
+        type="button"
+        :disabled="isTogglingTA"
+        @click="toggleTeachingAssistant"
+      >
+        {{ teachingAssistantEnabled ? T.wordsDisableAITA : T.wordsEnableAITA }}
+      </button>
+      <button
+        data-button-run-ai-ta
+        class="btn btn-primary p-1 p-sm-2"
+        type="button"
+      >
+        {{ T.wordsRunAITA }}
+      </button>
+    </div>
     <div v-if="isAdminOrTeachingAssistant" class="my-5">
       <div class="my-4 markdown">
         <omegaup-markdown
@@ -137,62 +158,62 @@
                   {{ T.courseContentEmpty }}
                 </td>
               </tr>
-              <tr
-                v-for="assignment in course.assignments"
-                v-else
-                :key="assignment.alias"
-                :data-content-alias="assignment.alias"
-                class="text-center"
-              >
-                <td class="align-middle">
-                  <template v-if="assignment.assignment_type === 'homework'">
-                    <font-awesome-icon icon="file-alt" />
-                    <span class="ml-2">{{ T.wordsHomework }}</span>
-                  </template>
-                  <template v-else-if="assignment.assignment_type === 'lesson'">
-                    <font-awesome-icon icon="chalkboard-teacher" />
-                    <span class="ml-2">{{ T.wordsLesson }}</span>
-                  </template>
-                  <template v-else>
-                    <font-awesome-icon icon="list-alt" />
-                    <span class="ml-2">{{ T.wordsExam }}</span>
-                  </template>
-                </td>
-                <td>
-                  <a
-                    data-course-homework-button
-                    class="align-middle"
-                    :href="`/course/${course.alias}/assignment/${assignment.alias}/`"
-                  >
-                    {{ assignment.name }}
-                  </a>
-                </td>
-                <td v-if="!isAdminOrTeachingAssistant" class="align-middle">
-                  {{ getAssignmentProgress(progress[assignment.alias]) }}
-                </td>
-                <td class="align-middle">
-                  {{ getFormattedTime(assignment.finish_time) }}
-                </td>
-                <td v-if="isAdminOrTeachingAssistant" class="align-middle">
-                  <a
-                    data-course-scoreboard-button
-                    class="mr-2"
-                    :href="`/course/${course.alias}/assignment/${assignment.alias}/scoreboard/${assignment.scoreboard_url}/`"
-                  >
-                    <font-awesome-icon :icon="['fas', 'link']" />{{
-                      T.courseActionScoreboard
-                    }}</a
-                  >
-                  <a
-                    data-course-submisson-button
-                    class="mr-2"
-                    :href="`/course/${course.alias}/assignment/${assignment.alias}/#runs`"
-                  >
-                    <font-awesome-icon :icon="['fas', 'tachometer-alt']" />
-                    {{ T.wordsRuns }}
-                  </a>
-                </td>
-              </tr>
+                              <tr
+                  v-for="assignment in course.assignments"
+                  v-else
+                  :key="assignment.alias"
+                  :data-content-alias="assignment.alias"
+                  class="text-center"
+                >
+                  <td class="align-middle">
+                    <template v-if="assignment.assignment_type === 'homework'">
+                      <font-awesome-icon icon="file-alt" />
+                      <span class="ml-2">{{ T.wordsHomework }}</span>
+                    </template>
+                    <template v-else-if="assignment.assignment_type === 'lesson'">
+                      <font-awesome-icon icon="chalkboard-teacher" />
+                      <span class="ml-2">{{ T.wordsLesson }}</span>
+                    </template>
+                    <template v-else>
+                      <font-awesome-icon icon="list-alt" />
+                      <span class="ml-2">{{ T.wordsExam }}</span>
+                    </template>
+                  </td>
+                  <td>
+                    <a
+                      data-course-homework-button
+                      class="align-middle"
+                      :href="`/course/${course.alias}/assignment/${assignment.alias}/`"
+                    >
+                      {{ assignment.name }}
+                    </a>
+                  </td>
+                  <td v-if="!isAdminOrTeachingAssistant" class="align-middle">
+                    {{ getAssignmentProgress(progress[assignment.alias]) }}
+                  </td>
+                  <td class="align-middle">
+                    {{ getFormattedTime(assignment.finish_time) }}
+                  </td>
+                  <td v-if="isAdminOrTeachingAssistant" class="align-middle">
+                    <a
+                      data-course-scoreboard-button
+                      class="mr-2"
+                      :href="`/course/${course.alias}/assignment/${assignment.alias}/scoreboard/${assignment.scoreboard_url}/`"
+                    >
+                      <font-awesome-icon :icon="['fas', 'link']" />{{
+                        T.courseActionScoreboard
+                      }}</a
+                    >
+                    <a
+                      data-course-submisson-button
+                      class="mr-2"
+                      :href="`/course/${course.alias}/assignment/${assignment.alias}/#runs`"
+                    >
+                      <font-awesome-icon :icon="['fas', 'tachometer-alt']" />
+                      {{ T.wordsRuns }}
+                    </a>
+                  </td>
+                </tr>
             </tbody>
           </table>
         </div>
@@ -371,6 +392,13 @@ export default class CourseDetails extends Vue {
     [Tab.Content]: T.courseDetailsTabContent,
   };
   selectedTab = Tab.Content;
+  teachingAssistantEnabled = false;
+  isTogglingTA = false;
+
+  mounted() {
+    this.teachingAssistantEnabled =
+      this.course.teaching_assistant_enabled || false;
+  }
 
   get overallCompletedPercentage(): number {
     let score = 0;
@@ -419,6 +447,20 @@ export default class CourseDetails extends Vue {
 
   get aliasWithUsername(): string {
     return `${this.course.alias}_${this.currentUsername}`;
+  }
+
+  toggleTeachingAssistant(): void {
+    if (this.isTogglingTA) return;
+    this.isTogglingTA = true;
+    this.$emit('toggle-teaching-assistant', {
+      course_alias: this.course.alias,
+    });
+  }
+
+  updateTeachingAssistantStatus(enabled: boolean): void {
+    this.teachingAssistantEnabled = enabled;
+    this.$set(this.course, 'teaching_assistant_enabled', enabled);
+    this.isTogglingTA = false;
   }
 }
 </script>
--- frontend/www/js/omegaup/course/details.ts
+++ frontend/www/js/omegaup/course/details.ts
@@ -2,14 +2,22 @@ import Vue from 'vue';
 import course_Details from '../components/course/Details.vue';
 import { OmegaUp } from '../omegaup';
 import { types } from '../api_types';
+import * as api from '../api';
+import * as ui from '../ui';
+import T from '../lang';
 
 OmegaUp.on('ready', () => {
   const payload = types.payloadParsers.CourseDetailsPayload();
   const headerPayload = types.payloadParsers.CommonPayload();
+  let detailsComponent: any = null;
+
   new Vue({
     el: '#main-container',
     components: {
       'omegaup-course-details': course_Details,
+    },
+    mounted() {
+      detailsComponent = this.$refs.detailsComponent;
     },
     render: function (createElement) {
       return createElement('omegaup-course-details', {
@@ -18,6 +26,30 @@ OmegaUp.on('ready', () => {
           progress: payload.progress,
           currentUsername: headerPayload.currentUsername,
         },
+        on: {
+          'toggle-teaching-assistant': (request: { course_alias: string }) => {
+            api.Course.toggleTeachingAssistant(request)
+              .then((response) => {
+                if (detailsComponent) {
+                  detailsComponent.updateTeachingAssistantStatus(
+                    response.teaching_assistant_enabled,
+                  );
+                }
+                ui.success(
+                  response.teaching_assistant_enabled
+                    ? T.wordsEnableAITASuccess
+                    : T.wordsDisableAITASuccess,
+                );
+              })
+              .catch(() => {
+                if (detailsComponent) {
+                  detailsComponent.onToggleError();
+                }
+                ui.error(T.unableToUpdateTAEnabledField);
+              });
+          },
+        },
+        ref: 'detailsComponent',
       });
     },
   });
