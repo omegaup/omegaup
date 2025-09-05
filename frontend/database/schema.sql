b798b9374c4b41baccd5ad4189678d93dffca80e
ElPedrangas03 <toniramos2345@gmail.com>
2025-09-05 16:38:44 -0600

    Agregando el indice idx_problems_title para la tabla problems

--- frontend/database/schema.sql
+++ frontend/database/schema.sql
@@ -691,6 +691,7 @@ CREATE TABLE `Problems` (
   KEY `acl_id` (`acl_id`),
   KEY `idx_problems_visibility` (`visibility`),
   KEY `idx_quality_seal` (`quality_seal`),
+  KEY `idx_problems_title` (`title`),
   FULLTEXT KEY `ft_alias_title` (`alias`,`title`),
   CONSTRAINT `fk_pa_acl_id` FOREIGN KEY (`acl_id`) REFERENCES `ACLs` (`acl_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Se crea un registro por cada prob externo.';
