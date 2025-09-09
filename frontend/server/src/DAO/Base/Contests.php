3c5e935eabed8cfb5412751513220f960d3c1968
Pedro Antonio Ayala Ramos <48796502+ElPedrangas03@users.noreply.github.com>
2025-09-08 23:41:38 -0600

    Agregando el metodo countAll() a todos los DAO (#8411)
    # Description
    
    Se agrego el metodo `countAll()` a todos los DAO, esto por si se llega a
    requerir en un futuro. Actualmente, se usara solamente para la parte de
    `CertificatesTest.php`, que es el archivo del fix adjunto.
    
    Fixes: #8269
    
    # Comments
    
    Aun creando el metodo, se requirio mandar a llamar una sola ves a
    `getAll()`, por el uso que este tiene al checar con los valores que
    arroja cada tabla.
    
    ---------
    
    Co-authored-by: Hugo Dueñas <heduenas@users.noreply.github.com>
    Co-authored-by: Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>

--- frontend/server/src/DAO/Base/Contests.php
+++ frontend/server/src/DAO/Base/Contests.php
@@ -223,6 +223,25 @@ abstract class Contests {
     }
 
     /**
+     * Contar todos los registros en `Contests`.
+     *
+     * Este método obtiene el número total de filas de la tabla **sin cargar campos**,
+     * útil para pruebas donde sólo se valida el conteo.
+     *
+     * @return int Número total de registros.
+     */
+    final public static function countAll(): int {
+        $sql = '
+            SELECT
+                COUNT(*)
+            FROM
+                `Contests`;';
+        /** @var int */
+        $count = \OmegaUp\MySQLConnection::getInstance()->GetOne($sql, []);
+        return intval($count);
+    }
+
+    /**
      * Eliminar registros.
      *
      * Este metodo eliminará el registro identificado por la llave primaria en
--- frontend/server/src/DAO/Base/ProblemsForfeited.php
+++ frontend/server/src/DAO/Base/ProblemsForfeited.php
@@ -172,6 +172,25 @@ abstract class ProblemsForfeited {
     }
 
     /**
+     * Contar todos los registros en `Problems_Forfeited`.
+     *
+     * Este método obtiene el número total de filas de la tabla **sin cargar campos**,
+     * útil para pruebas donde sólo se valida el conteo.
+     *
+     * @return int Número total de registros.
+     */
+    final public static function countAll(): int {
+        $sql = '
+            SELECT
+                COUNT(*)
+            FROM
+                `Problems_Forfeited`;';
+        /** @var int */
+        $count = \OmegaUp\MySQLConnection::getInstance()->GetOne($sql, []);
+        return intval($count);
+    }
+
+    /**
      * Eliminar registros.
      *
      * Este metodo eliminará el registro identificado por la llave primaria en
