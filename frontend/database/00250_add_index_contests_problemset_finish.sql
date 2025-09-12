729fec2f811d4d0709c3cdccabc073abe64f5ac7
ElPedrangas03 <toniramos2345@gmail.com>
2025-09-12 13:33:26 -0600

    Cambiando el orden de los campos en el indice y regresando el indice borrado

--- frontend/database/00250_add_index_contests_problemset_finish.sql
+++ frontend/database/00250_add_index_contests_problemset_finish.sql
@@ -1,2 +1,2 @@
 -- Add index to Contests table on problemset_id, finish_time column
-CREATE INDEX idx_contests_problemset_finish ON Contests (problemset_id, finish_time);
+CREATE INDEX idx_contests_problemset_finish ON Contests (finish_time, problemset_id);
