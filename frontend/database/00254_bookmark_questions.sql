f8b63e7ef90f54cd114376bf0e682bf50b53e0e0
k-vijay-05 <kvijay20204567@gmail.com>
2025-12-03 14:17:46 +0530

    Use identity_id instead of user_id for problem bookmarks

--- frontend/database/00254_bookmark_questions.sql
+++ frontend/database/00254_bookmark_questions.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `Problem_Bookmarks` (
-  `user_id` int NOT NULL,
+  `identity_id` int NOT NULL,
   `problem_id` int NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`,`problem_id`),
+  PRIMARY KEY (`identity_id`, `problem_id`),
   KEY `problem_id` (`problem_id`),
   CONSTRAINT `fk_pb_problem_id` FOREIGN KEY (`problem_id`) REFERENCES `Problems` (`problem_id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_pb_user_id` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Problemas que los usuarios marcaron como bookmark';
+  CONSTRAINT `fk_pb_identity_id` FOREIGN KEY (`identity_id`) REFERENCES `Identities` (`identity_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Problemas que las identidades marcaron como bookmark';
