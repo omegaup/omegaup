bf7858fd1d86b78ca0c8e6201f636f6afc620be0
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2026-01-09 12:18:55 -0600

    Update frontend/www/js/omegaup/components/problem/ProblemMarkdown.vue

--- frontend/www/js/omegaup/components/problem/ProblemMarkdown.vue
+++ frontend/www/js/omegaup/components/problem/ProblemMarkdown.vue
@@ -74,7 +74,6 @@ export default class ProblemMarkdown extends Vue {
 
   @Emit('rendered')
   private renderMathJax(): void {
-    // console.log('[ProblemMarkdown.vue] Rendered HTML:', this.html);
     this.root.innerHTML = this.html;
     this.root
       .querySelectorAll(
