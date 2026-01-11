807a1764e142a976403ec3bfe150a0abe3874cae
vivek sahu <viveksahu0110@gmail.com>
2026-01-11 09:17:14 +0000

    Fix CSS spacing in problem statement creator

--- frontend/www/js/omegaup/components/problem/creator/statement/StatementTab.vue
+++ frontend/www/js/omegaup/components/problem/creator/statement/StatementTab.vue
@@ -124,6 +124,7 @@ export default class StatementTab extends Vue {
     overflow-y: auto;
     border: 1px solid var(--markdown-preview-border-color);
     padding: 10px;
+    width: 100%;
   }
 }
 </style>
