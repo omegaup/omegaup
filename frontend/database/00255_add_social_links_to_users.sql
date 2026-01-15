b5b0875885fd75d6561f5ab5d904c4a40ffe4439
anuragdubey137 <anuragdubey13704@gmail.com>
2026-01-15 16:33:03 +0530

    chore: remove unrelated database migration

--- frontend/database/00255_add_social_links_to_users.sql
+++
@@ -1,5 +0,0 @@
--- Add social profile links to Users table
-ALTER TABLE `Users`
-ADD COLUMN `github_url` VARCHAR(255) NULL,
-ADD COLUMN `linkedin_url` VARCHAR(255) NULL,
-ADD COLUMN `website_url` VARCHAR(255) NULL;
