b828bebb8c36afeade80f20c72781162f42e47a3
Prasanna <mishra.prasanna838@gmail.com>
2026-01-18 01:55:31 +0000

    fixed cypress test

--- frontend/www/js/omegaup/api_types.ts
+++ frontend/www/js/omegaup/api_types.ts
@@ -3906,6 +3906,8 @@ export namespace types {
 
   export interface LoginDetailsPayload {
     facebookUrl?: string;
+    githubClientId?: string;
+    githubState?: string;
     hasVisitedSection?: boolean;
     statusError?: string;
     validateRecaptcha: boolean;
