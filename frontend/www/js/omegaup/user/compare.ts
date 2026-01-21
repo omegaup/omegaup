f0e475903c5f2efd1ec6bf792f4488652b726017
Ankit sisodya <118156935+Ankitsinghsisodya@users.noreply.github.com>
2026-01-22 00:31:57 +0530

    Update frontend/www/js/omegaup/user/compare.ts
    Co-authored-by: Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>

--- frontend/www/js/omegaup/user/compare.ts
+++ frontend/www/js/omegaup/user/compare.ts
@@ -66,7 +66,42 @@ OmegaUp.on('ready', () => {
           isLoading: this.isLoading,
         },
         on: {
-          compare: (this as any).onCompare,
+          compare: ({
+            username1,
+            username2,
+          }: {
+            username1?: string;
+            username2?: string;
+          }) => {
+            this.isLoading = true;
+
+            // Update URL
+            const url = new URL(window.location.href);
+            if (username1) {
+              url.searchParams.set('username1', username1);
+            } else {
+              url.searchParams.delete('username1');
+            }
+            if (username2) {
+              url.searchParams.set('username2', username2);
+            } else {
+              url.searchParams.delete('username2');
+            }
+            window.history.pushState({}, '', url.toString());
+
+            api.User.compare({
+              username1: username1 || undefined,
+              username2: username2 || undefined,
+            })
+              .then((response) => {
+                this.user1 = response.user1;
+                this.user2 = response.user2;
+              })
+              .catch(ui.apiError)
+              .finally(() => {
+                this.isLoading = false;
+              });
+          },
         },
       });
     },
