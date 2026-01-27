ca97ad46c2f1c1eb793df56d110c4c066be37ba8
Prashant-thakur77 <prashant101007@gmail.com>
2026-01-27 14:35:18 +0530

    Updted

--- frontend/database/00257_add_social_media_fields_to_users.sql
+++ frontend/database/00257_add_social_media_fields_to_users.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `Users`
-  ADD COLUMN `x_url` varchar(255) DEFAULT NULL COMMENT 'URL el perfil en X (antes Twitter)',
+  ADD COLUMN `x_url` varchar(255) DEFAULT NULL COMMENT 'URL del perfil en X (antes Twitter)',
   ADD COLUMN `linkedin_url` varchar(255) DEFAULT NULL COMMENT 'URL de perfil en LinkedIn',
   ADD COLUMN `github_url` varchar(255) DEFAULT NULL COMMENT 'URL de perfil en GitHub';
