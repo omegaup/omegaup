1ee35306a5b6d69c94cca609536b7447bc3b5aff
waygeance <2023kuec2028@iiitkota.ac.in>
2026-02-02 02:01:07 +0530

    added profile edit feature and test cases for it

--- frontend/www/js/omegaup/components/user/SidebarMainInfo.vue
+++ frontend/www/js/omegaup/components/user/SidebarMainInfo.vue
@@ -2,7 +2,34 @@
   <div class="card">
     <div class="card-header">
       <div class="text-center rounded-circle bottom-margin mt-3">
-        <img class="rounded-circle" :src="profile.gravatar_92" />
+        <div v-if="profile.is_own_profile" class="profile-picture-container">
+          <img
+            class="rounded-circle profile-picture"
+            :src="profile.gravatar_92"
+            @click="redirectToGravatar"
+          />
+          <div
+            class="profile-edit-overlay"
+            :title="T.userEditProfileImage"
+            @click="redirectToGravatar"
+          >
+            <div class="edit-icon">
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
+                  fill="white"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <img v-else class="rounded-circle" :src="profile.gravatar_92" />
       </div>
       <div class="mb-3 text-center mt-2">
         <omegaup-countryflag
@@ -199,5 +226,62 @@ export default class UserSidebarMainInfo extends Vue {
       this.$emit('update:selectedTab', validTab);
     }
   }
+
+  redirectToGravatar(): void {
+    window.open('https://www.gravatar.com', '_blank');
+  }
 }
 </script>
+
+<style scoped>
+.profile-picture-container {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+}
+
+.profile-picture {
+  width: 92px;
+  height: 92px;
+  object-fit: cover;
+  transition: opacity 0.2s ease-in-out;
+}
+
+.profile-edit-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 92px;
+  height: 92px;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  transition: opacity 0.2s ease-in-out;
+}
+
+.profile-picture-container:hover .profile-edit-overlay {
+  opacity: 1;
+}
+
+.profile-picture-container:hover .profile-picture {
+  opacity: 0.8;
+}
+
+.edit-icon {
+  color: white;
+  font-size: 16px;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.edit-icon svg {
+  width: 20px;
+  height: 20px;
+  filter: drop-shadow(0 1px 2px rgba(0, 0, 0, 0.5));
+}
+</style>
