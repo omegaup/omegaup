913fedd7816cac671234f2eb8420dc723209a4c2
Prasanna <mishra.prasanna838@gmail.com>
2026-02-05 08:02:45 +0000

    fixed minor psalm error

--- frontend/server/src/Email.php
+++ frontend/server/src/Email.php
@@ -77,8 +77,8 @@ class Email {
         $mail->SMTPAuth = true;
         $mail->Password = OMEGAUP_EMAIL_SMTP_PASSWORD;
         $mail->From = OMEGAUP_EMAIL_SMTP_FROM;
-        $mail->Port = OMEGAUP_EMAIL_SMTP_PORT;
-        $mail->SMTPSecure = false;
+        $mail->Port = intval(OMEGAUP_EMAIL_SMTP_PORT);
+        $mail->SMTPSecure = '';
         $mail->Username = OMEGAUP_EMAIL_SMTP_USER;
         $mail->Timeout = 30;
         $mail->SMTPKeepAlive = true;
