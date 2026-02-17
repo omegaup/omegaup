8893c817e977f89c484543fd221f404b01912275
antedotee <soniyadav2051982@gmail.com>
2026-02-17 16:10:18 +0530

    fixed the numbering

--- frontend/database/00262_add_index_problems_visibility_submissions.sql
+++ frontend/database/00262_add_index_problems_visibility_submissions.sql
@@ -1,4 +1,4 @@
 -- Add composite index to optimize Problems::byIdentityType query
 -- Supports ORDER BY submissions DESC with visibility filter, reducing filesort and full scan
--- Resolves slow query in DAO/Problems.php byIdentityType (EXPLAIN: Using temporary; Using filesort; type: ALL)
+-- Resolves slow query in DAO/Problems.php byIdentityType
 CREATE INDEX idx_problems_visibility_submissions ON Problems (visibility, submissions DESC);
