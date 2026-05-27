6846873786fb33c5f294aaa5e8a6c3950da58c55
vivek sahu <viveksahu0110@gmail.com>
2026-05-27 04:53:05 +0000

    Apply linter autofix for OverlayPopup.vue and related files

--- frontend/server/src/CdpBuilder.php
+++ frontend/server/src/CdpBuilder.php
@@ -373,8 +373,18 @@ class CdpBuilder {
         $imageCounter = 0;
         $pattern = '/!\[([^\]]+)\]\(([^)]+)\)/';
 
+        /**
+         * Help static analyzers understand variables captured by the closure.
+         *
+         * @var \ZipArchive $zip
+         * @var array<string,array{id:int,data:string}> $processedImages
+         * @var int $imageCounter
+         */
+
         $newMarkdown = preg_replace_callback(
             $pattern,
+            // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
+            /** @psalm-suppress UndefinedVariable */
             function ($matches) use (&$zip, &$processedImages, &$imageCounter) {
                 $description = $matches[1];
                 $filePathInZip = $matches[2];
