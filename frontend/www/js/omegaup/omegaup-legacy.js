9725fa7d0a435ead6578fd256d4cedbbef5a4182
Jay Prakash <tresurejay6@gmail.com>
2026-06-21 14:28:53 +0530

    lint issue resolve

--- frontend/www/js/omegaup/omegaup-legacy.js
+++ frontend/www/js/omegaup/omegaup-legacy.js
@@ -20,13 +20,6 @@ if (
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .catch((error) => {
-        console.error(
-          'Service Worker registration failed:',
-          error,
-        );
-      });
+    navigator.serviceWorker.register('/sw.js');
   });
 }
