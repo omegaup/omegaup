7eda2f612a9160d16f52520c708e6856a125327d
Prasanna <mishra.prasanna838@gmail.com>
2026-06-24 07:29:48 +0000

    wrap view unavailable in card and center

--- frontend/www/js/omegaup/components/common/ViewUnavailable.vue
+++ frontend/www/js/omegaup/components/common/ViewUnavailable.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="view-unavailable text-center text-muted" role="alert">
-    <font-awesome-icon :icon="icon" class="view-unavailable-icon mb-3" />
-    <p class="h5 mb-2">{{ title }}</p>
-    <p v-if="description" class="mb-0">{{ description }}</p>
-    <slot></slot>
+  <div
+    class="view-unavailable d-flex align-items-center justify-content-center"
+  >
+    <div class="card text-center text-muted" role="alert">
+      <div class="card-body">
+        <font-awesome-icon :icon="icon" class="view-unavailable-icon mb-3" />
+        <p class="h5 mb-2">{{ title }}</p>
+        <p v-if="description" class="mb-0">{{ description }}</p>
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,7 +36,12 @@ export default class ViewUnavailable extends Vue {
 
 <style lang="scss" scoped>
 .view-unavailable {
+  min-height: 70vh;
   padding: 2rem 1rem;
+
+  .card {
+    max-width: 32rem;
+  }
 
   .view-unavailable-icon {
     font-size: 2.5rem;
