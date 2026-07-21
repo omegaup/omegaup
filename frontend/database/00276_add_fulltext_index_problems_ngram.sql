840d72e818652c152bc349e5f5db771ef81eed3b
Yishar Piero <204321@unsaac.edu.pe>
2026-07-20 22:24:06 -0500

    Agregar migraciones ngram para Problems

--- frontend/database/00276_add_fulltext_index_problems_ngram.sql
+++ frontend/database/00276_add_fulltext_index_problems_ngram.sql
@@ -1,4 +1,3 @@
--- Replace old fulltext index with ngram fulltext index on Problems(alias, title)
+-- Add ngram FULLTEXT index to Problems(alias, title)
 ALTER TABLE `Problems`
-    DROP INDEX `ft_alias_title`,
     ADD FULLTEXT `ft_alias_title_ngram` (`alias`, `title`) WITH PARSER ngram;
