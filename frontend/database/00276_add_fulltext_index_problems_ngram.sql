4da8a326206592fe79e3e2bfbf0857e289b22dc7
Yishar Piero <204321@unsaac.edu.pe>
2026-07-31 00:36:53 -0500

    Unificar reemplazo de índice fulltext ngram en Problems

--- frontend/database/00276_add_fulltext_index_problems_ngram.sql
+++ frontend/database/00276_add_fulltext_index_problems_ngram.sql
@@ -1,3 +1,4 @@
--- Add ngram FULLTEXT index to Problems(alias, title)
+-- Replace old fulltext index on Problems(alias, title) with the ngram index
 ALTER TABLE `Problems`
+    DROP INDEX `ft_alias_title`,
     ADD FULLTEXT `ft_alias_title_ngram` (`alias`, `title`) WITH PARSER ngram;
--- frontend/database/00277_drop_old_fulltext_index_problems.sql
+++
@@ -1,3 +0,0 @@
--- Drop old fulltext index on Problems(alias, title) after adding the ngram index
-ALTER TABLE `Problems`
-    DROP INDEX `ft_alias_title`;
