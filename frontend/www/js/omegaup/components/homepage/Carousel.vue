2b9a350fbc3e6c3ddca61e574bd3cf0e14313b4e
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2026-08-29 12:00:55 +0500

    fix prevent homepage carousel arrows from overlapping slide text

--- frontend/www/js/omegaup/components/homepage/Carousel.vue
+++ frontend/www/js/omegaup/components/homepage/Carousel.vue
@@ -26,21 +26,11 @@
         ></omegaup-homepage-slide>
       </div>
     </div>
-    <a
-      class="carousel-control-prev"
-      href="#carousel-display"
-      role="button"
-      data-slide="prev"
-    >
+    <a class="carousel-control-prev" href="#carousel-display" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">{{ T.wordsPrevious }}</span>
     </a>
-    <a
-      class="carousel-control-next"
-      href="#carousel-display"
-      role="button"
-      data-slide="next"
-    >
+    <a class="carousel-control-next" href="#carousel-display" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">{{ T.wordsNext }}</span>
     </a>
@@ -60,7 +50,6 @@ import carouselConfig from '../../carousel.config';
 })
 export default class Carousel extends Vue {
   T = T;
-  // Reverse the entries so that newer ones appear first.
   slides = [...carouselConfig].reverse();
 }
 </script>
@@ -71,5 +60,30 @@ export default class Carousel extends Vue {
 .carousel {
   background: var(--homepage-carousel-background-color) !important;
   color: var(--homepage-carousel-font-color);
+}
+
+.carousel-control-prev,
+.carousel-control-next {
+  top: auto;
+  bottom: 20px;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 0.25);
+  opacity: 0.9;
+}
+
+.carousel-control-prev {
+  left: 10px;
+}
+
+.carousel-control-next {
+  right: 10px;
+}
+
+.carousel-control-prev-icon,
+.carousel-control-next-icon {
+  width: 20px;
+  height: 20px;
 }
 </style>
