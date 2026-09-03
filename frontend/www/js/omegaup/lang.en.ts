2635827a34655009f3aaee14e55d8acf6722221e
Juan Pablo Gómez Aguilar <gomez_aguilar_juan_pablo@hotmail.com>
2026-09-03 14:23:57 -0600

    Agregando soporte para lenguaje ReKarel (#10166)
    # Description
    
    Se agrega ReKarel (`rk`) como leguaje soportado en omegaUp.
    
    También se actualizan filtros relacionados con Karel, insignias, filtros
    de envíos, documentación y pruebas para que ReKarel sea tratado como
    parte de la familia Karel sin habilitarlo por defecto en problemas
    regulares.
    
    [Screencast From 2026-09-01
    00-59-48.webm](https://github.com/user-attachments/assets/45870104-ee7a-429b-9163-88238d948648)
    
    
    Fixes: #10098 
    
    # Comments
    
    Este cambio debe desplegarse junto con los cambios correspondientes en
    el runner y la infraestructura.
    
    Por lo pronto, vamos a aprovechar estos días para validar el cambio en
    sandbox antes del próximo despliegue a producción. Si detectamos algún
    problema con ReKarel, podemos revertir el cambio de main antes de que
    llegue a producción.
    
    Por el momento se está agregando como un lenguaje distinto a Karel
    legacy, pero de ser necesario, este puede sustituirlo por completo.
    
    # Checklist:
    
    - [x] The code follows the [coding
    guidelines](https://github.com/omegaup/omegaup/blob/main/frontend/www/docs/Coding-guidelines.md)
    of omegaUp.
    - [x] The tests were executed and all of them passed.
    - [x] If you are creating a feature, the new tests were added.
    - [x] If the change is large (> 200 lines), this PR was split into
    various Pull Requests. It's preferred to create one PR for changes in
    controllers + unit tests in PHPUnit, and then another Pull Request for
    UI + tests in Jest, Cypress or both.

--- frontend/www/js/omegaup/lang.en.ts
+++ frontend/www/js/omegaup/lang.en.ts
@@ -1220,6 +1220,7 @@ const translations: { [key: string]: string; } = {
   omegaupTitleUsersRank: "User ranking",
   onlyDemotionsSupported: "Resolution only supported for demotions for now.",
   openInKarelJs: "You can use karel.js to solve this problem.",
+  openInReKarel: "You can use ReKarel 2.0.0 to solve this problem.",
   openProblemCreator: "Open problem creator",
   originalContestHasNotEnded: "The original contest has not yet ended",
   outputOnly: "Output only",
