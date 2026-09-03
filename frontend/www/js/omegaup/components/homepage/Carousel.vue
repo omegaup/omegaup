32f18713b79b52471d4e2daece520cf3cdab716a
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2026-09-03 17:22:45 +0500

    fix: resolve lint issues and formatting in carousel

--- frontend/www/js/omegaup/components/homepage/Carousel.vue
+++ frontend/www/js/omegaup/components/homepage/Carousel.vue
@@ -26,11 +26,21 @@
         ></omegaup-homepage-slide>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carousel-display" role="button" data-slide="prev">
+    <a
+      class="carousel-control-prev"
+      href="#carousel-display"
+      role="button"
+      data-slide="prev"
+    >
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">{{ T.wordsPrevious }}</span>
     </a>
-    <a class="carousel-control-next" href="#carousel-display" role="button" data-slide="next">
+    <a
+      class="carousel-control-next"
+      href="#carousel-display"
+      role="button"
+      data-slide="next"
+    >
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">{{ T.wordsNext }}</span>
     </a>
