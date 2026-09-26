344d614809cd41bbad71067194c0962948f6a8c2
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2026-09-26 12:05:44 +0500

    fix(courses): fix trailing whitespace in Course.php

--- frontend/server/src/Controllers/Course.php
+++ frontend/server/src/Controllers/Course.php
@@ -441,7 +441,7 @@ class Course extends \OmegaUp\Controllers\Controller {
         \OmegaUp\Validators::validateOptionalStringNonEmpty(
             $r['objective'],
             'objective',
-            required: $isRequired 
+            required: $isRequired
         );
 
         $r->ensureOptionalInt('start_time', null, null, !$isUpdate);
